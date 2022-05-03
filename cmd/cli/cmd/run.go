2313937d4dd6f02b787f8b61bd5adba405cb8ae5
jaychia <jaychia94@gmail.com>
2022-05-03 13:07:44 -0700

    Set up insecure grpc connection between CLI and web service

--- cmd/cli/cmd/run.go
+++ cmd/cli/cmd/run.go
@@ -2,9 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
+	flatbuffers "github.com/google/flatbuffers/go"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/Eventual-Inc/Daft/codegen/go/DaftWeb"
 )
 
 func init() {
@@ -17,6 +23,34 @@ var runCmd = &cobra.Command{
 	Long:  `Runs a Data Function a single time, useful for debugging and for adhoc runs`,
 	Run: func(cmd *cobra.Command, args []string) {
 		authToken := viper.GetString(configKeyBearerAuthToken)
-		fmt.Println(authToken)
+		fmt.Println("Mock auth token:", authToken)
+		addr := viper.GetString(configKeyDaftWebURL)
+		fmt.Println("Using address:", addr)
+		conn, err := grpc.Dial(
+			addr,
+			// We can use this to hardcode tokens for authentication using Interceptors:
+			// https://github.com/grpc/grpc-go/issues/106#issuecomment-246978683
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+			grpc.WithDefaultCallOptions(grpc.ForceCodec(flatbuffers.FlatbuffersCodec{})),
+		)
+		if err != nil {
+			log.Fatalf("Failed to connect: %v", err)
+		}
+		defer conn.Close()
+
+		client := DaftWeb.NewWebClient(conn)
+
+		builder := flatbuffers.NewBuilder(0)
+		functionId := builder.CreateString("foobar")
+		DaftWeb.InvocationRequestStart(builder)
+		DaftWeb.InvocationRequestAddFunctionId(builder, functionId)
+		req := DaftWeb.InvocationRequestEnd(builder)
+		builder.Finish(req)
+
+		response, err := client.Invoke(cmd.Context(), builder)
+		if err != nil {
+			log.Fatalf("Error when invoking: %v", err)
+		}
+		fmt.Println("Function ID:", string(response.FunctionId()))
 	},
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"net"
+
+	"github.com/Eventual-Inc/Daft/codegen/go/DaftWeb"
+	flatbuffers "github.com/google/flatbuffers/go"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/encoding"
+)
+
+const addr = "0.0.0.0:8080"
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		logrus.Fatalf("Failed to listen: %v", err)
+	}
+
+	ser := grpc.NewServer()
+	encoding.RegisterCodec(flatbuffers.FlatbuffersCodec{})
+	DaftWeb.RegisterWebServer(ser, &webServer{})
+
+	if err := ser.Serve(lis); err != nil {
+		logrus.Fatalf("Failed to serve: %v", err)
+	}
+}
