6de706c59047e6797804b5487f15918078a2d708
jaychia <jaychia94@gmail.com>
2022-05-12 23:35:37 -0700

    use DatasetPipeline for Datarepo instead to enable async execution

--- daft/datarepo/datarepo.py
+++ daft/datarepo/datarepo.py
@@ -1,11 +1,16 @@
+import dataclasses
+
 from typing import Dict, List, Generic, TypeVar, Optional
 
 import ray
+import ray.data.dataset_pipeline
+from ray.data.impl.arrow_block import ArrowRow
 import pyarrow as pa
 
 from daft.datarepo import metadata_service
 
-
+# TODO(jaychia): We should derive this in a smarter way, perhaps N_CPUs * 2 or something similar.
+BLOCKS_PER_WINDOW = 8
 DatarepoInfo = Dict[str, str]
 
 
@@ -39,7 +44,8 @@ class Datarepo(Generic[Item]):
     def __init__(
         self,
         datarepo_id: str,
-        ray_dataset: ray.data.Dataset,
+        ray_dataset_pipeline: ray.data.dataset_pipeline.DatasetPipeline,
+        num_rows: Optional[int] = None,
     ):
         """Creates a new Datarepo
 
@@ -48,7 +54,8 @@ class Datarepo(Generic[Item]):
             ray_dataset (ray.data.Dataset): Dataset that backs this Datarepo
         """
         self._id = datarepo_id
-        self._ray_dataset = ray_dataset
+        self._ray_dataset_pipeline = ray_dataset_pipeline
+        self._num_rows = num_rows
 
     def map(self, func: MapFunc[Item, OutputItem]) -> "Datarepo[OutputItem]":
         """Runs a function on each item in the Datarepo, returning a new Datarepo
@@ -59,7 +66,11 @@ class Datarepo(Generic[Item]):
         Returns:
             Datarepo[OutputItem]: Datarepo of outputs
         """
-        return Datarepo(self._id + "[mapped]", self._ray_dataset.map(func))
+        return Datarepo(
+            datarepo_id=f"{self._id}:{func.__name__}",
+            ray_dataset_pipeline=self._ray_dataset_pipeline.map(func),
+            num_rows=self._num_rows,
+        )
 
     def save(self, datarepo_id: str) -> None:
         """Save a datarepo to persistent storage
@@ -68,7 +79,7 @@ class Datarepo(Generic[Item]):
             datarepo_id (str): ID to save datarepo as
         """
         path = metadata_service.get_metadata_service().get_path(datarepo_id)
-        return self._ray_dataset.write_parquet(path)
+        return self._ray_dataset_pipeline.write_parquet(path)
 
     def info(self) -> DatarepoInfo:
         """Retrieves information about the Datarepo. This method never triggers any
@@ -80,39 +91,67 @@ class Datarepo(Generic[Item]):
         """
         return {
             "id": self._id,
-            "num_rows": self._ray_dataset.count(),
+            "num_rows": self._num_rows,
         }
 
     def sample(self, n: int = 5) -> "Datarepo[Item]":
-        """Samples a certain number of Items from the Datarepo
+        """Computes and samples `n` Items from the Datarepo
 
         Args:
             n (int, optional): number of items to sample. Defaults to 5.
 
         Returns:
-            Datarepo[Item]: _description_
+            Datarepo[Item]: new Datarepo with sampled size
         """
-        # TODO(jaychia): This likely triggers a computation, we might need to find
-        # a way to make this lazy, perhaps by building out our own queue of operations?
-        head, _ = self._ray_dataset.split_at_indices([n])
-        return Datarepo(self._id + "[sampled]", head)
+        # TODO(jaychia): This triggers computation of datasets, which is not ideal
+        count = 0
+        datasets = []
+        for ds in self._ray_dataset_pipeline.iter_datasets():
+            datasets.append(ds)
+            count += ds.count()
+            if count > n:
+                minimum_dataset = datasets[0] if len(datasets) == 1 else datasets[0].union(datasets[1:])
+                sampled_dataset, _ = minimum_dataset.split_at_indices([n])
+                return Datarepo(
+                    datarepo_id=f"{self._id}:sampled({n})",
+                    ray_dataset_pipeline=sampled_dataset.window(blocks_per_window=BLOCKS_PER_WINDOW),
+                    num_rows=n,
+                )
+        raise RuntimeError(f"Attempting to sample {n} items but the Datarepo only has {count} items")
 
-    def preview(self) -> None:
-        """Previews the data in a Datarepo, triggering the computation of that data
+    def preview(self, n: int = 1) -> None:
+        """Previews the data in a Datarepo.
 
-        TODO(jaychia): This needs further refinement to display according to our schema
+        TODO(jaychia): This triggers the computation of the first window of the pipeline,
+        which will cause errors on subsequent usage of the pipeline. This behavior needs
+        to be fixed! Ideally we should cache the results of the first window's execution and
+        just continue executing the rest.
         """
         from IPython.display import display
-        items = ray.get(self._ray_dataset.get_internal_block_refs()[0])
+        import PIL.Image
+        # .iter_rows() of the Ray DatasetPipeline, processes windows lazily as they are requested.
+        # If we request a low number of rows for the preview, then we will process a minimal number
+        # of windows - ideally just one!
+        for i, item in enumerate(self._ray_dataset_pipeline.iter_rows()):
+            if i >= n:
+                break
+            if isinstance(item, ArrowRow):
+                for col_name in item:
+                    print(f"{col_name}: {item[col_name]}")
+            elif dataclasses.is_dataclass(item):
+                # TODO(jaychia): This needs further refinement for rich display according
+                # to our schema when @sammy is ready with the schema library, by checking
+                # if the item is an instance of a Daft Dataclass.
+                for field in item.__dataclass_fields__:
+                    val = getattr(item, field)
+                    if isinstance(val, PIL.Image.Image):
+                        print(f"{field}:")
+                        display(val)
+                    else:
+                        print(f"{field}: {val}")
 
-        # HACK(jaychia): In the absence of schemas, we just detect the types dynamically
-        if isinstance(items, pa.Table):
-            display(items.to_pandas())
-        elif isinstance(items, list):
-            for item in items:
-                print(item)
-        else:
-            print(f"Visualization not supported: {type(items)}")
+    def __repr__(self) -> str:
+        return str(self.info())
 
     ###
     # Static methods: Managing Datarepos
@@ -138,4 +177,14 @@ class Datarepo(Generic[Item]):
             Datarepo: retrieved Datarepo
         """
         path = metadata_service.get_metadata_service().get_path(datarepo_id)
-        return cls(datarepo_id, ray.data.read_parquet(path))
+        ds = ray.data.read_parquet(path)
+        # NOTE(jaychia): ds.count() is supposedly O(1) for parquet formats:
+        # https://github.com/ray-project/ray/blob/master/python/ray/data/dataset.py#L1640
+        # But we should benchmark and verify this.
+        ds_len = ds.count()
+        pipeline = ds.window(blocks_per_window=BLOCKS_PER_WINDOW)
+        return cls(
+            datarepo_id,
+            pipeline,
+            num_rows=ds_len,
+        )
