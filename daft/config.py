da2b8d0756e9fbcea14c85e6fc2e6b73503b26ba
jaychia <jaychia94@gmail.com>
2022-05-15 16:47:41 -0700

    initialize daft config file

---
+++ daft/config.py
@@ -0,0 +1,5 @@
+from typing import Optional
+
+class DaftSettings:
+    RAY_ADDRESS: str = "ray://127.0.0.1:10001"
+    NUM_CPUS: Optional[int] = None
--- daft/context.py
+++ daft/context.py
@@ -2,9 +2,18 @@ import ray
 
 from typing import Optional
 
+from daft.config import DaftSettings
+
 class DaftContext:
-    def __init__(self, num_cpus: int = 8):
-        self._ray_context = ray.init(num_cpus=num_cpus)
+    def __init__(
+        self,
+        num_cpus: int = DaftSettings.NUM_CPUS,
+        ray_address: Optional[str] = DaftSettings.RAY_ADDRESS,
+    ):
+        self._ray_context = ray.init(
+            num_cpus=num_cpus,
+            address=ray_address,
+        )
 
 _DEFAULT_CONTEXT: Optional[DaftContext] = None
 
