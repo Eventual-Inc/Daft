ba78a9bbedeb04e701d2c5134b8bb854dbb581c8
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-05-24 21:01:25 -0700

    add filesystem utils

--- daft/filesystem.py
+++ daft/filesystem.py
@@ -1,5 +1,3 @@
-from enum import Enum
-from typing import List
 from fsspec import AbstractFileSystem, get_filesystem_class
 from fsspec.implementations.dirfs import DirFileSystem
 
@@ -8,4 +6,3 @@ def get_filesystem(protocol: str, **kwargs) -> AbstractFileSystem:
     klass = get_filesystem_class(protocol)
     fs = klass(**kwargs)
     return fs
-
