b5293c101c921259d5ed1b37a494e075bb368779
Sammy Sidhu <sammy@eventualcomputing.com>
2022-06-21 17:59:52 -0700

    add table creation

--- icebridge/python/icebridge/client.py
+++ icebridge/python/icebridge/client.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Optional
+
 import os
 import sys
 
@@ -18,13 +20,34 @@ class IceBridgeClient:
 
 
 
+
+
 class IcebergCatalog:
     def __init__(self, client: IceBridgeClient, java_catalog) -> None:
         self.client = client
         self.catalog = java_catalog
     
+    def create_table(
+        self,
+        name:str,
+        schema: IcebergSchema,
+        partition_spec: Optional[IcebergPartitionSpec]=None,
+        namespace: Optional[str] = None
+    ) -> IcebergTable:
+        jvm = self.client.jvm()
+        gateway = self.client.gateway
+        name_vargs = gateway.new_array(jvm.java.lang.String, 1)
+        name_vargs[0] = name
+        if namespace is None:
+            identifier = jvm.org.apache.iceberg.catalog.TableIdentifier.of(name_vargs)
+        else:
+            
+            identifier = jvm.org.apache.iceberg.catalog.TableIdentifier.of(namespace, name)
+        table = self.catalog.createTable(identifier, schema.schema)
+        return IcebergTable(self.client, table)
+
     @classmethod
-    def hadoop_catalog(cls, client: IceBridgeClient, hdfs_path: str) -> IcebergCatalog:
+    def from_hadoop_catalog(cls, client: IceBridgeClient, hdfs_path: str) -> IcebergCatalog:
         jvm = client.jvm()
         hadoop_conf = jvm.org.apache.hadoop.conf.Configuration
         hadoop_catalog = jvm.org.apache.iceberg.hadoop.HadoopCatalog
@@ -32,7 +55,11 @@ class IcebergCatalog:
 
         return cls(client, hadoop_catalog_instance)
 
-
+class IcebergTable:
+    def __init__(self, client: IceBridgeClient, java_table) -> None:
+        self.client = client
+        self.table = java_table
+    
 
 class IcebergSchema:
     arrow_to_iceberg = {
@@ -47,8 +74,11 @@ class IcebergSchema:
         self.client = client
         self.schema = java_schema
     
+    def partition_spec_builder(self) -> IcebergPartitionSpec.Builder:
+        return IcebergPartitionSpec.Builder.from_iceberg_schema(self)
+
     @classmethod
-    def from_arrow_schema(cls, client, arrow_schema) -> IcebergSchema:
+    def from_arrow_schema(cls, client: IceBridgeClient, arrow_schema) -> IcebergSchema:
         jvm = client.jvm()
         iceberg_types = jvm.org.apache.iceberg.types.Types
         names = arrow_schema.names
@@ -79,3 +109,38 @@ class IcebergSchema:
         java_list = ListConverter().convert(iceberg_fields, client.gateway._gateway_client)
         iceberg_schema = jvm.org.apache.iceberg.Schema(java_list)
         return cls(client, iceberg_schema)
+
+
+class IcebergPartitionSpec:
+    def __init__(self, client: IceBridgeClient, java_partition_spec):
+        self.client = client
+        self.partition_spec = java_partition_spec
+    
+
+    class Builder:
+        def __init__(self, client: IceBridgeClient, java_partition_spec_builder) -> None:
+            self.client = client
+            self.partition_spec_builder = java_partition_spec_builder
+
+        def build(self) -> IcebergPartitionSpec:
+            return IcebergPartitionSpec(self.client, self.partition_spec_builder.build())
+
+        def bucket(self, field_name: str, num_buckets: int) -> IcebergPartitionSpec.Builder:
+            return IcebergPartitionSpec.Builder(
+                self.client,
+                self.partition_spec_builder.bucket(field_name, num_buckets)
+            )
+
+        def time(self, field_name: str, time_unit: str) -> IcebergPartitionSpec.Builder:
+            assert time_unit in {'hour', 'day', 'month', 'year'}
+            method = getattr(self.partition_spec_builder, time_unit)
+            return IcebergPartitionSpec.Builder(
+                self.client,
+                method(field_name)
+            )        
+
+        @classmethod
+        def from_iceberg_schema(cls, schema: IcebergSchema) -> IcebergPartitionSpec.Builder:
+            jvm = schema.client.jvm()
+            java_parition_spec_builder = jvm.org.apache.iceberg.PartitionSpec.builderFor(schema.schema)
+            return cls(schema.client, java_parition_spec_builder)
--- tests/test_icebridge.py
+++ tests/test_icebridge.py
@@ -10,8 +10,9 @@ from icebridge.client import IceBridgeClient, IcebergCatalog, IcebergSchema
 def test_hadoop_catalog() -> None:
     client = IceBridgeClient()
     with tempfile.TemporaryDirectory() as tmpdirname:
-        hadoop_catalog = IcebergCatalog.hadoop_catalog(client, f"file://{tmpdirname}")
-        assert isinstance(hadoop_catalog.catalog, py4j.java_gateway.JavaObject)
+        hadoop_catalog = IcebergCatalog.from_hadoop_catalog(client, f"file://{tmpdirname}")
+
+
 
 def test_iceberg_schema_from_arrow() -> None:
     client = IceBridgeClient()
@@ -20,4 +21,12 @@ def test_iceberg_schema_from_arrow() -> None:
         ('some_string', pa.string())
     ])
     iceberg_schema = IcebergSchema.from_arrow_schema(client, pa_schema)
+    builder = iceberg_schema.partition_spec_builder()
+    part_spec = builder.bucket('some_int', 10).build()
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        hadoop_catalog = IcebergCatalog.from_hadoop_catalog(client, f"file://{tmpdirname}")
+        table = hadoop_catalog.create_table("test1", iceberg_schema)
+        assert isinstance(hadoop_catalog.catalog, py4j.java_gateway.JavaObject)
+
 
