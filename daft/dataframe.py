8dec80dd9762f2d228fc0a7ac961d1c3108c0282
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-07-12 00:25:44 -0700

    WIP dataframe: * Seperate out Column concept from dataframe * Column nows can take in operations * Disabled dataframe tests for now * Repurpose Jay's function code for UDFs

--- daft/dataframe.py
+++ daft/dataframe.py
@@ -6,97 +6,78 @@ from typing import Any, Callable, Dict, List, Union
 
 from tabulate import tabulate
 
-@dataclass
-class Column:
-    name: str
+from daft.column import Column, ColumnType, ColumnArgType, ColumnExpression
+from daft.operations import Operation
 
 
-@dataclass
-class QueryExpression:
-    ...
+class RowView:
+    __slots__ = ["_index", "_df"]
+    def __init__(self, df: DataFrame, index: int) -> None:
+        self._index = index
+        self._df = df
+        if index >= df.count():
+            raise ValueError("index out of bounds")
 
-ColumnArgType = Union[Column, str]
+    def __repr__(self) -> str:
+        df = self.__getattribute__("_df")
+        index = self.__getattribute__("_index")
+        return f"RowView: index: {index}\n" + \
+        tabulate([
+            df.columns,
+            [df._data[col][index] for col in df.columns]
+        ])
+
+    def __dir__(self):
+        return self.__getattribute__("_df").columns
+
+    def __getattr__(self, col: str):
+        df = self.__getattribute__("_df")
+        index = self.__getattribute__("_index")
+        if col not in df.columns:
+            raise ValueError(f"column {col} not in {df.columns}")
+        return df._data[col][index]
+
 
 class DataFrame:
-
-    class RowView:
-        __slots__ = ["_index", "_df"]
-        def __init__(self, df: DataFrame, index: int) -> None:
-            self._index = index
-            self._df = df
-            if index >= df.count():
-                raise ValueError("index out of bounds")
-
-        def __repr__(self) -> str:
-            df = self.__getattribute__("_df")
-            index = self.__getattribute__("_index")
-            return f"RowView: index: {index}\n" + \
-            tabulate([
-                df.columns,
-                [df._data[col][index] for col in df.columns]
-            ])
-
-        def __dir__(self):
-            return self.__getattribute__("_df").columns
-
-        def __getattr__(self, col: str):
-            df = self.__getattribute__("_df")
-            index = self.__getattribute__("_index")
-            if col not in df.columns:
-                raise ValueError(f"column {col} not in {df.columns}")
-            return df._data[col][index]
-
-
-    def __init__(self, column_to_list: Dict[str, List]) -> None:
-        if len(column_to_list.keys()) == 0:
-            raise ValueError("no columns passed in")
-        self._data = column_to_list
-        self._columns = list(column_to_list.keys())
-        self._count = max(map(len, column_to_list.values()))
-        min_count = min(map(len, column_to_list.values()))
-        if self._count != min_count:
-            raise ValueError('columns are uneven')
+    def __init__(self, cols: List[ColumnArgType]) -> None:
+        self._cols = [Column.from_arg(c) for c in self._cols]
+        self._column_names = [c.name for c in self._cols]
     
     @property
     def columns(self) -> List[str]:
-        return self._columns
+        return self._column_names
 
-    @classmethod
-    def from_items(cls, data: List[Dict[str, Any]]) -> DataFrame:
-        transposed = defaultdict(list)
-        for row in data:
-            for k, v in row.items():
-                transposed[k].append(v)
-            leftover_keys = transposed.keys() - row.keys()
-            for k in leftover_keys:
-                transposed[k].append(None)
-        return DataFrame(transposed)
-
-    def __getitem__(self, index: int) -> RowView:
-        return DataFrame.RowView(self, index)
-
-
-    def __norm_columns(self, *columns: ColumnArgType) -> List[Column]:
-        norm_columns = [Column(name=c) if isinstance(c, str) else c for c in columns]
-        return norm_columns
-
+    # def __getitem__(self, index: int) -> RowView:
+    #     return DataFrame.RowView(self, index)
 
     def select(self, *columns: ColumnArgType) -> DataFrame:
-        norm_columns = self.__norm_columns(*columns)
-        column_names = [c.name for c in norm_columns]
-        if not all(c in self._columns for c in column_names):
-            raise ValueError(f"Column not in DataFrame {set(column_names) - set(self._columns)}")
-        return DataFrame({k: self._data[k].copy() for k in column_names})
+        selected_cols = []
+        for c in columns:
+            if isinstance(c, Column):
+                selected_cols.append(c)
+            elif isinstance(c, str):
+                assert c in self.columns, f'{c} not found in dataframes columns: {self.columns}'
+                index = self.columns.index(c)
+                selected_cols.append(self._cols[index])
+        return DataFrame(selected_cols)
 
-
-    def count(self) -> int:
-        return self._count
-
-    def with_column(self, name: str, new_col: ColumnArgType) -> Column:
-        ...
+    def with_column(self, name: str, col: Column) -> Column:
+        assert isinstance(col, Column), 'col must be Column type'
+        assert name not in self.columns, f"duplicate column name {name}"
+        return DataFrame(self._cols + [col.alias(name)])
     
-    def where(self, expr: QueryExpression) -> DataFrame:
-        ...
+    def where(self, expr: ColumnExpression) -> DataFrame:
+        new_columns = [Column(
+            name=c.name,
+            column_type=ColumnType.RESULT,
+            operation=Operation("where_pl", [c]),
+        ) for c in self._cols]
+        return DataFrame(new_columns)
     
-    def sort(self, *columns: ColumnArgType) -> DataFrame:
-        ...
+    def limit(self, num: int) -> DataFrame:
+        new_columns = [Column(
+            name=c.name,
+            column_type=ColumnType.RESULT,
+            operation=Operation("limit_pl", [c]),
+        ) for c in self._cols]
+        return DataFrame(new_columns)
