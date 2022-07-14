790d307eb7551921305f5489f9b0842ea15d845b
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-07-14 00:33:33 -0700

    * Dataframe API work; Add basic operations classes such as: 	- Limit 	- Where 	- Alias 	- UDF * Introduce concept of Stages to define relationship between rows

--- daft/column.py
+++ daft/column.py
@@ -18,11 +18,6 @@ class Column:
     column_type: Optional[ColumnType] = None
     operation: Optional[Operation] = None
 
-    def alias(self, name: str) -> Column:
-        from daft.operations import Operation
-
-        return Column(name=name, column_type=ColumnType.RESULT, operation=Operation("alias_pl", [self]))
-
     @classmethod
     def from_arg(cls, c: "ColumnArgType") -> Column:
         if isinstance(c, Column):
