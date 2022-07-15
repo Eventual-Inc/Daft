8e132ab19de96672c222f32b4a57fd6003c164b5
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-07-15 01:41:36 -0700

    * Remove pdb breakpoint in test

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -11,7 +11,4 @@ def test_col_expr_add() -> None:
     assert isinstance(id, ColumnExpression)
     new_id = id + 1
     print(new_id > 10)
-    import ipdb
-
-    ipdb.set_trace()
     assert new_id.is_operation()
