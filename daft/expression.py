61a3da996ca478705e35acf5226773d712070869
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-07-15 17:37:51 -0700

    * Add NAryOpExpressions + udf decorator 	-enables UDF 	-checks for expressions and if it has one returns a new expr

--- daft/expression.py
+++ daft/expression.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+import functools
 import operator
 from functools import partialmethod
-from typing import Any, Callable
+from typing import Any, Callable, Dict, Optional, Tuple
 
 
 def col(name: str) -> ColumnExpression:
@@ -10,18 +11,21 @@ def col(name: str) -> ColumnExpression:
 
 
 class Expression:
-    def __to_expression(self, input: Any) -> Expression:
+    def _to_expression(self, input: Any) -> Expression:
         if not isinstance(input, Expression):
             return LiteralExpression(input)
         return input
 
-    def _binary_op(self, other: Any, func: Callable) -> Expression:
-        other_expr = self.__to_expression(other)
-        return BinaryOpExpression(self, other_expr, func)
+    def _unary_op(self, func: Callable, symbol: Optional[str] = None) -> Expression:
+        return UnaryOpExpression(self, func, symbol=symbol)
 
-    def _reverse_binary_op(self, other: Any, func: Callable) -> Expression:
-        other_expr = self.__to_expression(other)
-        return other_expr._binary_op(self, func)
+    def _binary_op(self, other: Any, func: Callable, symbol: Optional[str] = None) -> Expression:
+        other_expr = self._to_expression(other)
+        return BinaryOpExpression(self, other_expr, func, symbol=symbol)
+
+    def _reverse_binary_op(self, other: Any, func: Callable, symbol: Optional[str] = None) -> Expression:
+        other_expr = self._to_expression(other)
+        return other_expr._binary_op(self, func, symbol=symbol)
 
     def is_literal(self) -> bool:
         return False
@@ -29,36 +33,50 @@ class Expression:
     def is_operation(self) -> bool:
         return False
 
+    # UnaryOps
+
     # Arithmetic
-    __add__ = partialmethod(_binary_op, func=operator.add)
-    __sub__ = partialmethod(_binary_op, func=operator.sub)
-    __mul__ = partialmethod(_binary_op, func=operator.mul)
-    __floordiv__ = partialmethod(_binary_op, func=operator.floordiv)
-    __truediv__ = partialmethod(_binary_op, func=operator.truediv)
-    __pow__ = partialmethod(_binary_op, func=operator.pow)
+    __neg__ = partialmethod(_unary_op, func=operator.neg, symbol="-")
+    __pos__ = partialmethod(_unary_op, func=operator.pos, symbol="+")
+    __abs__ = partialmethod(_unary_op, func=operator.abs)
+
+    __invert__ = partialmethod(_unary_op, func=operator.not_, symbol="~")
+
+    # func
+    map = _unary_op
+
+    # BinaryOps
+
+    # Arithmetic
+    __add__ = partialmethod(_binary_op, func=operator.add, symbol="+")
+    __sub__ = partialmethod(_binary_op, func=operator.sub, symbol="-")
+    __mul__ = partialmethod(_binary_op, func=operator.mul, symbol="*")
+    __floordiv__ = partialmethod(_binary_op, func=operator.floordiv, symbol="//")
+    __truediv__ = partialmethod(_binary_op, func=operator.truediv, symbol="/")
+    __pow__ = partialmethod(_binary_op, func=operator.pow, symbol="**")
 
     # Reverse Arithmetic
-    __radd__ = partialmethod(_reverse_binary_op, func=operator.add)
-    __rsub__ = partialmethod(_reverse_binary_op, func=operator.sub)
-    __rmul__ = partialmethod(_reverse_binary_op, func=operator.mul)
-    __rfloordiv__ = partialmethod(_reverse_binary_op, func=operator.floordiv)
-    __rtruediv__ = partialmethod(_reverse_binary_op, func=operator.truediv)
-    __rpow__ = partialmethod(_reverse_binary_op, func=operator.pow)
+    __radd__ = partialmethod(_reverse_binary_op, func=operator.add, symbol="+")
+    __rsub__ = partialmethod(_reverse_binary_op, func=operator.sub, symbol="-")
+    __rmul__ = partialmethod(_reverse_binary_op, func=operator.mul, symbol="*")
+    __rfloordiv__ = partialmethod(_reverse_binary_op, func=operator.floordiv, symbol="//")
+    __rtruediv__ = partialmethod(_reverse_binary_op, func=operator.truediv, symbol="/")
+    __rpow__ = partialmethod(_reverse_binary_op, func=operator.pow, symbol="**")
 
     # Logical
-    __and__ = partialmethod(_binary_op, func=operator.and_)
-    __or__ = partialmethod(_binary_op, func=operator.or_)
+    __and__ = partialmethod(_binary_op, func=operator.and_, symbol="&")
+    __or__ = partialmethod(_binary_op, func=operator.or_, symbol="|")
 
-    __lt__ = partialmethod(_binary_op, func=operator.lt)
-    __le__ = partialmethod(_binary_op, func=operator.le)
-    __eq__ = partialmethod(_binary_op, func=operator.eq)
-    __ne__ = partialmethod(_binary_op, func=operator.ne)
-    __gt__ = partialmethod(_binary_op, func=operator.gt)
-    __ge__ = partialmethod(_binary_op, func=operator.ge)
+    __lt__ = partialmethod(_binary_op, func=operator.lt, symbol="<")
+    __le__ = partialmethod(_binary_op, func=operator.le, symbol="<=")
+    __eq__ = partialmethod(_binary_op, func=operator.eq, symbol="=")
+    __ne__ = partialmethod(_binary_op, func=operator.ne, symbol="!=")
+    __gt__ = partialmethod(_binary_op, func=operator.gt, symbol=">")
+    __ge__ = partialmethod(_binary_op, func=operator.ge, symbol=">=")
 
     # Reverse Logical
-    __rand__ = partialmethod(_reverse_binary_op, func=operator.and_)
-    __ror__ = partialmethod(_reverse_binary_op, func=operator.or_)
+    __rand__ = partialmethod(_reverse_binary_op, func=operator.and_, symbol="&")
+    __ror__ = partialmethod(_reverse_binary_op, func=operator.or_, symbol="|")
 
 
 class LiteralExpression(Expression):
@@ -72,40 +90,77 @@ class LiteralExpression(Expression):
         return True
 
 
-class BinaryOpExpression(Expression):
-    pretty_print_symbols = {
-        "add": "+",
-        "sub": "-",
-        "mul": "*",
-        "floordiv": "//",
-        "truediv": "/",
-        "pow": "**",
-        "and_": "&",
-        "or_": "|",
-        "lt": "<",
-        "le": "<=",
-        "eq": "=",
-        "ne": "!=",
-        "gt": ">",
-        "ge": ">=",
-    }
-
-    def __init__(self, left: Expression, right: Expression, op: Callable) -> None:
-        self._left = left
-        self._right = right
+class UnaryOpExpression(Expression):
+    def __init__(self, operand: Expression, op: Callable, symbol: Optional[str] = None) -> None:
+        if not isinstance(operand, Expression):
+            raise ValueError(f"expected {operand} to be of type Expression, is {type(operand)}")
+        self._operand = operand
         self._op = op
+        self._symbol = symbol
 
     def __repr__(self) -> str:
         op_name = self._op.__name__
-        symbol = BinaryOpExpression.pretty_print_symbols.get(op_name, op_name)
+        if self._symbol is None:
+            return f"[{op_name}({self._operand})]"
+        else:
+            return f"{self._symbol}({self._operand})"
+
+
+class BinaryOpExpression(Expression):
+    def __init__(self, left: Expression, right: Expression, op: Callable, symbol: Optional[str] = None) -> None:
+        self._left = left
+        self._right = right
+        self._op = op
+        self._symbol = symbol
+
+    def __repr__(self) -> str:
+        op_name = self._op.__name__
+        if self._symbol is None:
+            symbol = op_name
+        else:
+            symbol = self._symbol
         return f"[{self._left} {symbol} {self._right}]"
 
     def is_operation(self) -> bool:
         return True
 
 
+class NAryOpExpression(Expression):
+    def __init__(self, func: Callable, func_args: Tuple, func_kwargs: Optional[Dict[str, Any]] = None) -> None:
+        self._args = (self._to_expression(arg) for arg in func_args)
+        if func_kwargs is None:
+            func_kwargs = dict()
+        self._kwargs = {k: self._to_expression(v) for k, v in func_kwargs.items()}
+        self._func = func
+
+    def is_operation(self) -> bool:
+        return True
+
+    def __repr__(self) -> str:
+        func_name = self._func.__name__
+        args = ", ".join(repr(a) for a in self._args)
+        if len(self._kwargs) == 0:
+            return f"Expr:{func_name}({args})"
+
+        kwargs = ", ".join(f"{k}={repr(v)}" for k, v in self._kwargs.items())
+        return f"Expr:{func_name}({args}, {kwargs})"
+
+
+def udf(f: Callable) -> Callable:
+    @functools.wraps(f)
+    def new_func(*args, **kwargs) -> NAryOpExpression:
+        if any(isinstance(a, Expression) for a in args) or any(isinstance(a, Expression) for a in kwargs.values()):
+            return NAryOpExpression(f, args, kwargs)
+        else:
+            return f(*args, **kwargs)
+
+    return new_func
+
+
 class ColumnExpression(Expression):
     def __init__(self, name: str) -> None:
+        if not isinstance(name, str):
+            raise TypeError(f"Exprected name to be type str, is {type(name)}")
         self._name = name
 
     def __repr__(self) -> str:
