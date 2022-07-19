f029729354e0b101b6e8f64237331b5caaeb2627
Jay Chia <jaychia94@gmail.com>
2022-07-18 22:42:16 -0700

    fix prod deploy

--- eventual-hub/frontend/src/config.ts
+++ eventual-hub/frontend/src/config.ts
@@ -1,7 +1,7 @@
 const baseApiUrl = process.env.REACT_APP_BUILD_ENVIRONMENT == "development" ? "https://dev-api.eventualcomputing.com" : process.env.REACT_APP_BUILD_ENVIRONMENT == "production" ? "https://api.eventualcomputing.com" : "https://localhost:8443"
 const baseUrl = process.env.REACT_APP_BUILD_ENVIRONMENT == "development" ? "https://dev-app.eventualcomputing.com" : process.env.REACT_APP_BUILD_ENVIRONMENT == "production" ? "https://app.eventualcomputing.com" : "https://localhost:3000"
 const auth0Domain = process.env.REACT_APP_BUILD_ENVIRONMENT == "production" ? "eventual-prod.us.auth0.com" : "eventual-dev.us.auth0.com"
-const auth0ClientId = process.env.REACT_APP_BUILD_ENVIRONMENT == "production" ? "eventual-prod.us.auth0.com" : "zByGOxzmeNJhfToH8ENnibcRbKzglexp"
+const auth0ClientId = process.env.REACT_APP_BUILD_ENVIRONMENT == "production" ? "iwPBkx4iP1PJ4hML2C4eD2cUFYjT5vUH" : "zByGOxzmeNJhfToH8ENnibcRbKzglexp"
 
 export default function getConfig() {
   return {
