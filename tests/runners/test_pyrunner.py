a27850bd61a91764474ba07027bef2ad526e6a58
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-07-29 18:38:42 -0700

    implements block operations for expression eval

--- tests/runners/test_pyrunner.py
+++ tests/runners/test_pyrunner.py
@@ -1,10 +1,14 @@
 from typing import Dict, List
 
 from daft.dataframe import DataFrame
+from daft.expressions import col
 from daft.runners.pyrunner import PyRunner
 
 
 def test_pyrunner(valid_data: List[Dict[str, float]]):
     df = DataFrame.from_csv("/Users/sammy/iris.csv")
-    df = df.select("sepal.length", "sepal.width").select("sepal.width")
+    # df = df.select("sepal.length", "sepal.width").select((col("sepal.width") * col("sepal.length")).alias('area'))
+    # df = df.with_column('area+1', col('area') == 20)
+    df = df.select(col("sepal.length") == 10)
+    print(df.plan())
     PyRunner(df.plan()).run()
