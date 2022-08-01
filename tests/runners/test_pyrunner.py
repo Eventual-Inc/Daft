0e45a28547e9e0ae128d5063ce1c2a745fe344d1
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-01 15:24:42 -0700

    add iris csv for testing

--- tests/runners/test_pyrunner.py
+++ tests/runners/test_pyrunner.py
@@ -6,7 +6,7 @@ from daft.runners.pyrunner import PyRunner
 
 
 def test_pyrunner(valid_data: List[Dict[str, float]]):
-    df = DataFrame.from_csv("/Users/sammy/iris.csv")
+    df = DataFrame.from_csv("tests/assets/iris.csv")
     # df = df.select("sepal.length", "sepal.width").select((col("sepal.width") * col("sepal.length")).alias('area'))
     # df = df.with_column('area+1', col('area') == 20)
     df = df.select(col("sepal.length") == 10)
