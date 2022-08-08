403fd558ff1af06323fc8699820687ba7bccf03c
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-07 17:05:35 -0700

    remove cpp file

--- build.py
+++ build.py
@@ -13,25 +13,6 @@ SOURCE_DIR = Path("daft")
 BUILD_DIR = Path(".cython_build")
 
 
-# def build() -> None:
-#     # Collect and cythonize all files
-#     extension_modules = cythonize_helper(get_extension_modules())
-
-#     # Use Setuptools to collect files
-#     distribution = Distribution({
-#         "ext_modules": extension_modules,
-#         "cmdclass": {
-#             "build_ext": cython_build_ext,
-#         },
-#     })
-
-#     # Grab the build_ext command and copy all files back to source dir. This is
-#     # done so that Poetry grabs the files during the next step in its build.
-#     distribution.run_command("build_ext")
-#     build_ext_cmd = distribution.get_command_obj("build_ext")
-#     build_ext_cmd.copy_extensions_to_source()
-
-
 def get_extension_modules() -> List[Extension]:
     """Collect all .py files and turn them into Distutils/Setuptools
     Extensions"""
