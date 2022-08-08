09dbb850dcc4de8a1790a628a8e55ba8caaf1dba
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-07 18:15:29 -0700

    clean up cython code

--- daft/internal/hashing.pyx
+++ daft/internal/hashing.pyx
@@ -1,11 +1,12 @@
 # distutils: language=c++
 # distutils: sources = daft/internal/xxhash.cc
 
-from pyarrow.lib cimport *
+from libc cimport stdint
+from pyarrow.lib cimport CArray, pyarrow_unwrap_array, shared_ptr
 
 
 cdef extern from "xxhash.h":
-    uint64_t XXH3_64bits(const void* input, size_t length);
+    stdint.uint64_t XXH3_64bits(const void* input, size_t length);
 
 
 def get_array_length(obj):
@@ -14,5 +15,5 @@ def get_array_length(obj):
     cdef shared_ptr[CArray] arr = pyarrow_unwrap_array(obj)
     if arr.get() == NULL:
         raise TypeError("not an array")
-    return XXH3_64bits(arr.get(), 8)
-    # return arr.get().length()
+    # return XXH3_64bits(arr.get(), 8)
+    return arr.get().length()
