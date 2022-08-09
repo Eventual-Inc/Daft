b45b97d1d8aa204ff4d4fa428415fd301f1a09cf
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-08 18:51:00 -0700

    add checks for nulls

--- tests/internal/test_hashing.py
+++ tests/internal/test_hashing.py
@@ -10,7 +10,14 @@ int_types = [pa.int8(), pa.uint8(), pa.int16(), pa.uint16(), pa.int32(), pa.uint
 def test_hash_chunked_int_array(dtype):
     arr = pa.chunked_array([[None, 1, 2, 3, 4, None]], type=dtype)
     hash_all = hash_chunked_array(arr)
-    assert hash_all[1:] == hash_chunked_array(arr[1:])
+    assert hash_all[0] == pa.scalar(None, type=pa.uint64())
+    assert hash_all[-1] == pa.scalar(None, type=pa.uint64())
+
+    shifted = hash_chunked_array(arr[1:])
+
+    assert hash_all[1:] == shifted
+    assert shifted[0] != pa.scalar(None, type=pa.uint64())
+    assert shifted[-1] == pa.scalar(None, type=pa.uint64())
 
 
 def test_hash_chunked_string_array():
