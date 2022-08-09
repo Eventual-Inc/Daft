b8ed08c409399bb14e15f49ca21661ccbe8b67b0
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-08 17:54:20 -0700

    hasing int array

--- daft/internal/hashing.pyx
+++ daft/internal/hashing.pyx
@@ -1,13 +1,174 @@
 # distutils: language=c++
+# distutils: sources = daft/internal/xxhash.cc
 
-from pyarrow.lib cimport CArray, pyarrow_unwrap_array, shared_ptr
+from libc cimport math, stdint
+from libc.string cimport memset
+from pyarrow.lib cimport (
+    AllocateBuffer,
+    CArray,
+    CArrayData,
+    CBuffer,
+    CChunkedArray,
+    CDataType,
+    CResult,
+    GetPrimitiveType,
+    GetResultValue,
+    MakeArray,
+    Type,
+    make_shared,
+    pyarrow_unwrap_array,
+    pyarrow_unwrap_chunked_array,
+    pyarrow_wrap_chunked_array,
+    shared_ptr,
+    to_shared,
+    unique_ptr,
+    vector,
+)
+
+import cython
 
 
-def get_array_length(obj):
-    # Just an example function accessing both the pyarrow Cython API
-    # and the Arrow C++ API
-    cdef shared_ptr[CArray] arr = pyarrow_unwrap_array(obj)
-    if arr.get() == NULL:
-        raise TypeError("not an array")
-    # return XXH3_64bits(arr.get(), 8)
-    return arr.get().length()
+cdef extern from "xxhash.h":
+    stdint.uint64_t XXH3_64bits(const void* input, size_t length);
+
+
+
+cdef shared_ptr[CBuffer] _shift_valid_bits(
+    const shared_ptr[CBuffer] valid_bits,
+    const stdint.int64_t num_values,
+    const stdint.int64_t offset):
+
+    cdef const stdint.uint8_t* valid_bits_data_ptr = valid_bits.get().data()
+    cdef stdint.int64_t result_valid_bits_buffer_size = <stdint.uint64_t> math.ceil(num_values / 8.)
+    cdef shared_ptr[CBuffer] result_valid_bits_buffer = to_shared(GetResultValue(AllocateBuffer(result_valid_bits_buffer_size, NULL)))
+    cdef stdint.uint8_t* result_valid_bits_data_ptr = <stdint.uint8_t*> result_valid_bits_buffer.get().mutable_data()
+    cdef stdint.int64_t byte_offset = offset // 8
+    cdef stdint.uint8_t bit_offset = offset % 8
+    cdef stdint.int64_t i, idx
+    for i in range(result_valid_bits_buffer_size):
+        result_valid_bits_data_ptr[i] = valid_bits_data_ptr[i + byte_offset] >> bit_offset
+    return result_valid_bits_buffer
+
+
+cdef shared_ptr[CArray] _hash_integer_array(shared_ptr[CArray] arr):
+    type_id = arr.get().type_id()
+
+    if not ((Type._Type_UINT8 <= type_id) and (type_id <= Type._Type_INT64)):
+        raise TypeError('expected integer array')
+
+    cdef shared_ptr[CArrayData] array_data = arr.get().data()
+    cdef stdint.int64_t length = array_data.get().length
+    cdef stdint.int64_t offset = array_data.get().offset
+    cdef stdint.int64_t null_count = array_data.get().null_count
+    cdef vector[shared_ptr[CBuffer]] buffers = array_data.get().buffers
+
+    cdef shared_ptr[CBuffer] valid_bits = buffers.at(0)
+    cdef shared_ptr[CBuffer] data = buffers.at(1)
+    cdef const stdint.uint8_t* data_ptr = data.get().data()
+
+    cdef stdint.int64_t byte_width =  data.get().size() // length
+
+    cdef stdint.uint64_t result_buffer_size = length * cython.sizeof(stdint.uint64_t)
+    cdef shared_ptr[CBuffer] result_buffer = to_shared(GetResultValue(AllocateBuffer(result_buffer_size, NULL)))
+    cdef stdint.uint64_t* buffer_data_ptr = <stdint.uint64_t*> result_buffer.get().mutable_data()
+
+    cdef stdint.int64_t i, idx
+    cdef stdint.uint64_t h_value = 0
+
+    for i in range(length):
+        idx = (i + offset)
+        h_value = XXH3_64bits(data_ptr + idx*byte_width, byte_width)
+        buffer_data_ptr[i] = h_value
+
+    cdef vector[shared_ptr[CBuffer]] result_buffer_vector
+    if valid_bits.get() != NULL:
+        result_buffer_vector.push_back(_shift_valid_bits(valid_bits, length, offset))
+    else:
+        result_buffer_vector.push_back(valid_bits)
+
+    result_buffer_vector.push_back(result_buffer)
+
+    cdef shared_ptr[CArrayData] result_array_data = CArrayData.Make(
+        GetPrimitiveType(Type._Type_UINT64),
+        length,
+        result_buffer_vector,
+        null_count,
+        0,
+    )
+    return MakeArray(result_array_data)
+
+cdef shared_ptr[CArray] _hash_string_array(shared_ptr[CArray] arr):
+    "https://arrow.apache.org/docs/format/Columnar.html#buffer-listing-for-each-layout"
+    type_id = arr.get().type_id()
+
+    if not (type_id == Type._Type_STRING):
+        raise TypeError('expected string array')
+
+    cdef shared_ptr[CArrayData] array_data = arr.get().data()
+    cdef stdint.int64_t length = array_data.get().length
+    cdef stdint.int64_t offset = array_data.get().offset
+    cdef stdint.int64_t null_count = array_data.get().null_count
+
+    cdef vector[shared_ptr[CBuffer]] buffers = array_data.get().buffers
+
+    cdef shared_ptr[CBuffer] valid_bits = buffers.at(0)
+    cdef shared_ptr[CBuffer] offsets = buffers.at(1)
+    cdef shared_ptr[CBuffer] data = buffers.at(2)
+
+    cdef const stdint.uint8_t* data_ptr = data.get().data() + offset
+    cdef const stdint.uint8_t* offsets_ptr = offsets.get().data() + offset
+
+    cdef stdint.int64_t i = 0
+    cdef stdint.uint64_t h_value = 0
+
+    cdef stdint.uint64_t result_buffer_size = length * cython.sizeof(stdint.uint64_t)
+    cdef shared_ptr[CBuffer] result_buffer = to_shared(GetResultValue(AllocateBuffer(result_buffer_size, NULL)))
+    cdef stdint.uint64_t* buffer_data_ptr = <stdint.uint64_t*> result_buffer.get().mutable_data()
+
+    cdef stdint.uint64_t word_size
+    for i in range(length):
+        word_size = offsets_ptr[i+1] - offsets_ptr[i]
+        #h_value = XXH3_64bits(data_ptr + offsets_ptr[i], word_size)
+        #buffer_data_ptr[i] = h_value
+
+    cdef vector[shared_ptr[CBuffer]] result_buffer_vector
+    result_buffer_vector.push_back(valid_bits)
+    result_buffer_vector.push_back(result_buffer)
+
+    cdef shared_ptr[CArrayData] result_array_data = CArrayData.Make(
+        GetPrimitiveType(Type._Type_UINT64),
+        length,
+        result_buffer_vector,
+        null_count,
+        0,
+    )
+    return MakeArray(result_array_data)
+
+
+
+
+def hash_chunked_array(obj):
+    cdef shared_ptr[CChunkedArray] carr = pyarrow_unwrap_chunked_array(obj)
+    if carr.get() == NULL:
+        raise TypeError("not a chunked array")
+    num_chunks: cython.int = carr.get().num_chunks()
+    cdef shared_ptr[CDataType] ctype = carr.get().type()
+    cdef Type type_id = ctype.get().id()
+    is_integer: cython.bool = False
+    is_string: cython.bool = False
+
+    if (Type._Type_UINT8 <= type_id) and (type_id <= Type._Type_INT64):
+        is_integer = True
+    elif type_id == Type._Type_STRING:
+        is_string = True
+    else:
+        raise TypeError('excepted integer or string got neither')
+
+    cdef shared_ptr[CArray] arr
+    cdef vector[shared_ptr[CArray]] hash_results
+    for i in range(num_chunks):
+        arr = carr.get().chunk(i)
+        hash_results.push_back(_hash_integer_array(arr))
+
+    cdef shared_ptr[CChunkedArray] result = make_shared[CChunkedArray](hash_results, GetPrimitiveType(Type._Type_UINT64))
+    return pyarrow_wrap_chunked_array(result)
--- tests/internal/test_hashing.py
+++ tests/internal/test_hashing.py
@@ -1,6 +1,17 @@
+import pyarrow as pa
+
+from daft.internal.hashing import hash_chunked_array
+
 # @pytest.mark.parametrize("size", [(i + 1) * 1000 + i for i in range(5)])
 # def test_murmur_32_buffer(size: int) -> None:
 #     test_input = np.random.randint(0, 255, size, dtype=np.uint8)
 #     hashing_out = hashing_old.murmur3_hash_32_buffer(test_input)
 #     mmh3_out = mmh3.hash_from_buffer(test_input, signed=False)
 #     assert hashing_out == mmh3_out
+
+
+def test_hash_chunked_int_array():
+    arr = pa.chunked_array([[None, 1, 2, 3, 4] + [None] * 4, [5, 6, 7, 8]], type=pa.uint8())
+    arr = pa.chunked_array([arr.chunk(0)[1:]])
+    result = hash_chunked_array(arr)
+    print(result)
