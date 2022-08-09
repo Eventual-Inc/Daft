1c68a4dde1f8cdbacc8fcf6286ad23366905c581
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-08 18:54:52 -0700

    params for chunks

--- tests/internal/test_hashing.py
+++ tests/internal/test_hashing.py
@@ -6,9 +6,10 @@ from daft.internal.hashing import hash_chunked_array
 int_types = [pa.int8(), pa.uint8(), pa.int16(), pa.uint16(), pa.int32(), pa.uint32(), pa.int64(), pa.uint64()]
 
 
+@pytest.mark.parametrize("num_chunks", range(1, 4))
 @pytest.mark.parametrize("dtype", int_types, ids=[repr(it) for it in int_types])
-def test_hash_chunked_int_array(dtype):
-    arr = pa.chunked_array([[None, 1, 2, 3, 4, None]], type=dtype)
+def test_hash_chunked_int_array(num_chunks, dtype):
+    arr = pa.chunked_array([[None, 1, 2, 3, 4, None] for _ in range(num_chunks)], type=dtype)
     hash_all = hash_chunked_array(arr)
     assert hash_all[0] == pa.scalar(None, type=pa.uint64())
     assert hash_all[-1] == pa.scalar(None, type=pa.uint64())
@@ -20,7 +21,12 @@ def test_hash_chunked_int_array(dtype):
     assert shifted[-1] == pa.scalar(None, type=pa.uint64())
 
 
-def test_hash_chunked_string_array():
-    arr = pa.chunked_array([[None, "", "a", "ab", "abc", "abcd", "", None]], type=pa.string())
+@pytest.mark.parametrize("num_chunks", range(1, 4))
+def test_hash_chunked_string_array(
+    num_chunks,
+):
+    arr = pa.chunked_array(
+        [[None, "", "a", "ab", "abc", "abcd", "", None] for _ in range(num_chunks)], type=pa.string()
+    )
     hash_all = hash_chunked_array(arr)
     assert hash_all[1:] == hash_chunked_array(arr[1:])
