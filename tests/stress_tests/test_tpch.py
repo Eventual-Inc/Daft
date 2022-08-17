38fdd89e09df14e8320f36c95bc3924a13e8f96f
Jay Chia <jay@eventualcomputing.com>
2022-08-17 14:29:10 -0700

    Add q2 for tpch tests

--- tests/stress_tests/test_tpch.py
+++ tests/stress_tests/test_tpch.py
@@ -8,7 +8,7 @@ import pyarrow as pa
 import pytest
 
 from daft.dataframe import DataFrame
-from daft.expressions import col
+from daft.expressions import col, udf
 
 SCHEMA = {
     "part": [
@@ -99,16 +99,22 @@ def gen_tpch():
 
 
 @pytest.fixture(scope="function")
-def lineitem(gen_tpch):
-    return DataFrame.from_csv(
-        "data/tpch/lineitem.tbl", has_headers=False, column_names=SCHEMA["lineitem"] + [""], delimiter="|"
-    )
+def tables(gen_tpch):
+    return {
+        tbl_name: DataFrame.from_csv(
+            f"data/tpch/{tbl_name}.tbl", has_headers=False, column_names=SCHEMA[tbl_name] + [""], delimiter="|"
+        )
+        for tbl_name in SCHEMA
+    }
 
 
 @pytest.mark.parametrize("num_partitions", [None, 3])
-def test_tpch_q1(lineitem, tmp_path, num_partitions):
+def test_tpch_q1(tables, tmp_path, num_partitions):
     if num_partitions is not None:
-        lineitem = lineitem.repartition(num_partitions)
+        for key in tables:
+            tables[key] = tables[key].repartition(num_partitions)
+
+    lineitem = tables["lineitem"]
     discounted_price = col("L_EXTENDEDPRICE") * (1 - col("L_DISCOUNT"))
     taxed_discounted_price = discounted_price * (1 + col("L_TAX"))
     daft_df = (
@@ -147,6 +153,68 @@ def test_tpch_q1(lineitem, tmp_path, num_partitions):
     daft_pd_df.to_csv(csv_out, sep="|", line_terminator="|\n", index=False)
 
     assert run_tpch_checker(1, csv_out)
+
+
+@pytest.mark.parametrize("num_partitions", [None, 3])
+def test_tpch_q2(tables, tmp_path, num_partitions):
+    if num_partitions is not None:
+        for key in tables:
+            tables[key] = tables[key].repartition(num_partitions)
+
+    @udf(return_type=bool)
+    def ends_with(column, suffix):
+        return column.str.endswith(suffix)
+
+    region = tables["region"]
+    nation = tables["nation"]
+    supplier = tables["supplier"]
+    partsupp = tables["partsupp"]
+    part = tables["part"]
+
+    europe = (
+        region.where(col("R_NAME") == "EUROPE")
+        .join(nation, left_on=col("R_REGIONKEY"), right_on=col("N_REGIONKEY"))
+        .join(supplier, left_on=col("N_NATIONKEY"), right_on=col("S_NATIONKEY"))
+        .join(partsupp, left_on=col("S_SUPPKEY"), right_on=col("PS_SUPPKEY"))
+    )
+
+    brass = part.where((col("P_SIZE") == 15) and (ends_with(col("p_type"), "BRASS"))).join(
+        europe,
+        left_on=col("P_PARTKEY"),
+        right_on=col("PS_PARTKEY"),
+    )
+
+    min_cost = brass.groupby(col("PS_PARTKEY")).agg(
+        [
+            (col("PS_SUPPLYCOST").alias("min"), "min"),
+        ]
+    )
+
+    daft_df = (
+        brass.join(min_cost, on=col("PS_PARTKEY"))
+        .where(col("PS_SUPPLYCOST") == col("min"))
+        .select(
+            col("S_ACCTBAL"),
+            col("S_NAME"),
+            col("N_NAME"),
+            col("P_PARTKEY"),
+            col("P_MFGR"),
+            col("S_ADDRESS"),
+            col("S_PHONE"),
+            col("S_COMMENT"),
+        )
+    )
+
+    # Multicol sorts not implemented yet
+    daft_pd_df = daft_df.to_pandas()
+    daft_pd_df = daft_pd_df.sort_values(
+        by=["S_ACCTBAL", "N_NAME", "S_NAME", "P_PARTKEY"], ascending=[False, True, True, True]
+    )
+    daft_pd_df = daft_pd_df.head(100)
+    csv_out = f"{tmp_path}/q2.out"
+    daft_pd_df.to_csv(csv_out, sep="|", line_terminator="|\n", index=False)
+
+    assert run_tpch_checker(2, csv_out)
 
 
 def run_tpch_checker(q_num: int, result_file: str) -> bool:
