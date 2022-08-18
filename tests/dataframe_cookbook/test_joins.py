fad5b645202bb7c73877d8785f7d8acaedad0683
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-18 01:12:15 -0700

    fix test for missing values right

--- tests/dataframe_cookbook/test_joins.py
+++ tests/dataframe_cookbook/test_joins.py
@@ -1,3 +1,5 @@
+import pytest
+
 from daft.expressions import col
 from tests.conftest import assert_df_equals
 from tests.dataframe_cookbook.conftest import (
@@ -6,7 +8,7 @@ from tests.dataframe_cookbook.conftest import (
 )
 
 
-# TODO parameterize other join types [left, right, outer]
+@pytest.mark.tdd
 @parametrize_service_requests_csv_repartition
 @parametrize_service_requests_csv_daft_df
 def test_simple_join(daft_df, service_requests_csv_pd_df, repartition_nparts):
@@ -14,24 +16,24 @@ def test_simple_join(daft_df, service_requests_csv_pd_df, repartition_nparts):
     daft_df_left = daft_df.select(col("Unique Key"), col("Borough"))
     daft_df_right = daft_df.select(col("Unique Key"), col("Created Date")).limit(25)
     daft_df = daft_df_left.join(daft_df_right, col("Unique Key"))
+
     service_requests_csv_pd_df_left = service_requests_csv_pd_df[["Unique Key", "Borough"]]
     service_requests_csv_pd_df_right = service_requests_csv_pd_df[["Unique Key", "Created Date"]]
     service_requests_csv_pd_df = (
         service_requests_csv_pd_df_left.set_index("Unique Key")
-        .join(service_requests_csv_pd_df_right.set_index("Unique Key"), how="inner")
+        .join(service_requests_csv_pd_df_right.set_index("Unique Key"), how="left")
         .reset_index()
     )
     daft_pd_df = daft_df.to_pandas()
     assert_df_equals(daft_pd_df, service_requests_csv_pd_df)
 
 
-# TODO parameterize other join types [left, right, outer]
 @parametrize_service_requests_csv_repartition
 @parametrize_service_requests_csv_daft_df
 def test_simple_join_missing_rvalues(daft_df, service_requests_csv_pd_df, repartition_nparts):
-    daft_df = daft_df.repartition(repartition_nparts)
+    daft_df_right = daft_df.sort("Unique Key").limit(25).repartition(repartition_nparts)
     daft_df_left = daft_df.select(col("Unique Key"), col("Borough"))
-    daft_df_right = daft_df.select(col("Unique Key"), col("Created Date")).sort(col("Unique Key")).limit(25)
+    daft_df_right = daft_df_right.select(col("Unique Key"), col("Created Date")).sort(col("Unique Key"))
     daft_df = daft_df_left.join(daft_df_right, col("Unique Key"))
 
     service_requests_csv_pd_df_left = service_requests_csv_pd_df[["Unique Key", "Borough"]]
@@ -47,7 +49,7 @@ def test_simple_join_missing_rvalues(daft_df, service_requests_csv_pd_df, repart
     assert_df_equals(daft_pd_df, service_requests_csv_pd_df)
 
 
-# TODO parameterize other join types [left, right, outer]
+@pytest.mark.tdd
 @parametrize_service_requests_csv_repartition
 @parametrize_service_requests_csv_daft_df
 def test_simple_join_missing_lvalues(daft_df, service_requests_csv_pd_df, repartition_nparts):
@@ -62,7 +64,7 @@ def test_simple_join_missing_lvalues(daft_df, service_requests_csv_pd_df, repart
     service_requests_csv_pd_df_right = service_requests_csv_pd_df[["Unique Key", "Created Date"]]
     service_requests_csv_pd_df = (
         service_requests_csv_pd_df_left.set_index("Unique Key")
-        .join(service_requests_csv_pd_df_right.set_index("Unique Key"), how="inner")
+        .join(service_requests_csv_pd_df_right.set_index("Unique Key"), how="left")
         .reset_index()
     )
     daft_pd_df = daft_df.to_pandas()
