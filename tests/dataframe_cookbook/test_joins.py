4de4e102cfa125aa91c3b8f634f7bb0915148642
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-18 01:14:29 -0700

    fix test joins

--- tests/dataframe_cookbook/test_joins.py
+++ tests/dataframe_cookbook/test_joins.py
@@ -1,5 +1,3 @@
-import pytest
-
 from daft.expressions import col
 from tests.conftest import assert_df_equals
 from tests.dataframe_cookbook.conftest import (
@@ -8,13 +6,12 @@ from tests.dataframe_cookbook.conftest import (
 )
 
 
-@pytest.mark.tdd
 @parametrize_service_requests_csv_repartition
 @parametrize_service_requests_csv_daft_df
 def test_simple_join(daft_df, service_requests_csv_pd_df, repartition_nparts):
     daft_df = daft_df.repartition(repartition_nparts)
     daft_df_left = daft_df.select(col("Unique Key"), col("Borough"))
-    daft_df_right = daft_df.select(col("Unique Key"), col("Created Date")).limit(25)
+    daft_df_right = daft_df.select(col("Unique Key"), col("Created Date"))
     daft_df = daft_df_left.join(daft_df_right, col("Unique Key"))
 
     service_requests_csv_pd_df_left = service_requests_csv_pd_df[["Unique Key", "Borough"]]
@@ -32,7 +29,8 @@ def test_simple_join(daft_df, service_requests_csv_pd_df, repartition_nparts):
 @parametrize_service_requests_csv_daft_df
 def test_simple_join_missing_rvalues(daft_df, service_requests_csv_pd_df, repartition_nparts):
     daft_df_right = daft_df.sort("Unique Key").limit(25).repartition(repartition_nparts)
-    daft_df_left = daft_df.select(col("Unique Key"), col("Borough"))
+    daft_df_left = daft_df.repartition(repartition_nparts)
+    daft_df_left = daft_df_left.select(col("Unique Key"), col("Borough"))
     daft_df_right = daft_df_right.select(col("Unique Key"), col("Created Date")).sort(col("Unique Key"))
     daft_df = daft_df_left.join(daft_df_right, col("Unique Key"))
 
@@ -49,13 +47,13 @@ def test_simple_join_missing_rvalues(daft_df, service_requests_csv_pd_df, repart
     assert_df_equals(daft_pd_df, service_requests_csv_pd_df)
 
 
-@pytest.mark.tdd
 @parametrize_service_requests_csv_repartition
 @parametrize_service_requests_csv_daft_df
 def test_simple_join_missing_lvalues(daft_df, service_requests_csv_pd_df, repartition_nparts):
-    daft_df = daft_df.repartition(repartition_nparts)
-    daft_df_left = daft_df.select(col("Unique Key"), col("Borough")).sort(col("Unique Key")).limit(25)
-    daft_df_right = daft_df.select(col("Unique Key"), col("Created Date"))
+    daft_df_right = daft_df.repartition(repartition_nparts)
+    daft_df_left = daft_df.sort(col("Unique Key")).limit(25).repartition(repartition_nparts)
+    daft_df_left = daft_df_left.select(col("Unique Key"), col("Borough"))
+    daft_df_right = daft_df_right.select(col("Unique Key"), col("Created Date"))
     daft_df = daft_df_left.join(daft_df_right, col("Unique Key"))
 
     service_requests_csv_pd_df_left = (
