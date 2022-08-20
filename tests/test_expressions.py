675ff5ae75f078dbf85cdb5a7f7b216b5a85c5c4
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-08-19 19:18:23 -0700

    fix test expressions

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -16,11 +16,11 @@ def test_col_expr_add() -> None:
 def test_name() -> None:
     expr = col("a") + col("b")
     assert expr.name() == "a"
-    assert expr.required_columns() == [col("a"), col("b")]
+    assert all(l.is_eq(r) for l, r in zip(expr.required_columns(), [col("a"), col("b")]))
 
     new_expr = col("c") + expr
     new_expr.name() == "c"
-    assert new_expr.required_columns() == [col("c"), col("a"), col("b")]
+    assert all(l.is_eq(r) for l, r in zip(new_expr.required_columns(), [col("c"), col("a"), col("b")]))
 
 
 def test_alias() -> None:
@@ -29,12 +29,11 @@ def test_alias() -> None:
 
     alias_expr = expr.alias("ab")
     assert alias_expr.name() == "ab"
+    assert all(l.is_eq(r) for l, r in zip(alias_expr.required_columns(), [col("a"), col("b")]))
+    assert (alias_expr + col("c")).name() == "ab"
+    assert (col("c") + alias_expr).name() == "c"
 
-    assert alias_expr.required_columns() == [col("a"), col("b")]
-    assert (alias_expr + col("c")) == "ab"
-    assert (col("c") + alias_expr) == "c"
-
-    assert (col("c") + alias_expr).required_columns() == [col("c"), col("a"), col("b")]
+    assert all(l.is_eq(r) for l, r in zip((col("c") + alias_expr).required_columns(), [col("c"), col("a"), col("b")]))
 
 
 def test_column_expr_eq() -> None:
