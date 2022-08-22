022c85a2556f627b372944f2499ea08174578b67
Jay Chia <jay@eventualcomputing.com>
2022-08-21 19:46:45 -0700

    Run make command using github actions

--- scripts/tpch-gen.sh
+++ scripts/tpch-gen.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 TPCH_SQLITE_DIR="data/tpch-sqlite"
-SCALE_FACTOR
 
-if [ ! -d "$TPCH_SQLITE_DIR" ]; then
-    git clone --recursive git@github.com:lovasoa/TPCH-sqlite.git data/tpch-sqlite
-fi
+git clone --recursive git@github.com:lovasoa/TPCH-sqlite.git data/tpch-sqlite
 cd $TPCH_SQLITE_DIR
-SCALE_FACTOR=$${1:-1} make
+SCALE_FACTOR=${1:-1} make
