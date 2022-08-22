a3f99d3e3c2891f4a39b790a21407f6e00b3f90f
Jay Chia <jay@eventualcomputing.com>
2022-08-21 19:39:30 -0700

    use github actions to clone repo

--- scripts/tpch-gen.sh
+++ scripts/tpch-gen.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-git clone --recursive git@github.com:lovasoa/TPCH-sqlite.git data/tpch-sqlite
-cd data/tpch-sqlite
-SCALE_FACTOR=$1 make
+TPCH_SQLITE_DIR="data/tpch-sqlite"
+SCALE_FACTOR
+
+if [ ! -d "$TPCH_SQLITE_DIR" ]; then
+    git clone --recursive git@github.com:lovasoa/TPCH-sqlite.git data/tpch-sqlite
+fi
+cd $TPCH_SQLITE_DIR
+SCALE_FACTOR=$${1:-1} make
