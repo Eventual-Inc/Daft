f765b1e2068fe0d107d88bfe10e3cbde47793f8c
Jay Chia <jaychia94@gmail.com>
2022-08-22 23:19:49 -0700

    Jay/consecutive with column calls (#116)
    Co-authored-by: Jay Chia <jay@eventualcomputing.com>
    Co-authored-by: Sammy Sidhu <sammy.sidhu@gmail.com>

--- tests/dataframe_cookbook/test_computations.py
+++ tests/dataframe_cookbook/test_computations.py
@@ -18,6 +18,18 @@ def test_add_one_to_column(daft_df, service_requests_csv_pd_df, repartition_npar
 
 @parametrize_service_requests_csv_repartition
 @parametrize_service_requests_csv_daft_df
+def test_add_one_twice_to_column(daft_df, service_requests_csv_pd_df, repartition_nparts):
+    """Creating a new column that is derived from (1 + other_column) and retrieving the top N results"""
+    daft_df = daft_df.repartition(repartition_nparts).with_column("unique_key_mod", col("Unique Key") + 1)
+    daft_df = daft_df.with_column("unique_key_mod_second", col("unique_key_mod") + 1)
+    service_requests_csv_pd_df["unique_key_mod"] = service_requests_csv_pd_df["Unique Key"] + 1
+    service_requests_csv_pd_df["unique_key_mod_second"] = service_requests_csv_pd_df["unique_key_mod"] + 1
+    daft_pd_df = daft_df.to_pandas()
+    assert_df_equals(daft_pd_df, service_requests_csv_pd_df)
+
+
+@parametrize_service_requests_csv_repartition
+@parametrize_service_requests_csv_daft_df
 def test_difference_cols(daft_df, service_requests_csv_pd_df, repartition_nparts):
     """Creating a new column that is derived from 2 other columns and retrieving the top N results"""
     daft_df = daft_df.repartition(repartition_nparts).with_column(
