da17bf5e27f1420585f841c3e129911ccd81f032
Sammy Sidhu <samster25@users.noreply.github.com>
2022-08-24 16:25:02 -0700

    allow for schema for to_pandas (#119)
    Bugfix for duplicate column ids for single schema

--- tests/dataframe/test_projection.py
+++ tests/dataframe/test_projection.py
@@ -30,6 +30,24 @@ def test_select_dataframe(valid_data: List[Dict[str, float]]) -> None:
     assert df.column_names() == ["sepal_length", "sepal_width"]
 
 
+def test_multiple_select_same_col(valid_data: List[Dict[str, float]]):
+    df = DataFrame.from_pylist(valid_data)
+    df = df.select(col("sepal_length"), col("sepal_length").alias("sepal_length_2"))
+    pdf = df.to_pandas()
+    assert len(pdf.columns) == 2
+    assert pdf.columns.to_list() == ["sepal_length", "sepal_length_2"]
+
+
+def test_stacked_with_columns(valid_data: List[Dict[str, float]]):
+    df = DataFrame.from_pylist(valid_data)
+    df = df.select(col("sepal_length"))
+    df = df.with_column("sepal_length_2", col("sepal_length"))
+    df = df.with_column("sepal_length_3", col("sepal_length_2"))
+    pdf = df.to_pandas()
+    assert len(pdf.columns) == 3
+    assert pdf.columns.to_list() == ["sepal_length", "sepal_length_2", "sepal_length_3"]
+
+
 def test_select_dataframe_missing_col(valid_data: List[Dict[str, float]]) -> None:
     df = DataFrame.from_pylist(valid_data)
     with pytest.raises(ValueError):
