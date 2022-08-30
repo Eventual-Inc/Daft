47eecf4eed7137dd1a4035bbc96112f41428565b
Jay Chia <jaychia94@gmail.com>
2022-08-29 23:10:39 -0700

    Refactor make_block for more granular handling of corner cases (#129)
    Co-authored-by: Jay Chia <jay@eventualcomputing.com>

--- tests/dataframe_cookbook/test_python_list_cols.py
+++ tests/dataframe_cookbook/test_python_list_cols.py
@@ -11,6 +11,65 @@ from daft.execution.operators import ExpressionType
 from daft.expressions import col, lit, udf
 from tests.conftest import assert_df_equals
 
+###
+# Using Python dicts as an object
+###
+
+
+@pytest.mark.parametrize("repartition_nparts", [1, 5, 6, 10, 11])
+def test_python_dict(repartition_nparts):
+    data = {"id": [i for i in range(10)], "dicts": [{"foo": i} for i in range(10)]}
+    daft_df = DataFrame.from_pydict(data).repartition(repartition_nparts)
+    assert daft_df.schema()["id"].daft_type == ExpressionType.from_py_type(int)
+    assert daft_df.schema()["dicts"].daft_type == ExpressionType.from_py_type(dict)
+    pd_df = pd.DataFrame.from_dict(data)
+    daft_pd_df = daft_df.to_pandas()
+    assert_df_equals(daft_pd_df, pd_df, sort_key="id")
+
+
+@udf(return_type=dict)
+def rename_key_list(dicts):
+    new_dicts = []
+    for d in dicts:
+        new_dicts.append({"bar": d["foo"]})
+    return new_dicts
+
+
+@udf(return_type=dict)
+def rename_key_np(dicts):
+    new_dicts = []
+    for d in dicts:
+        new_dicts.append({"bar": d["foo"]})
+    return np.array(new_dicts)
+
+
+@udf(return_type=dict)
+def rename_key_pd(dicts):
+    new_dicts = []
+    for d in dicts:
+        new_dicts.append({"bar": d["foo"]})
+    return pd.Series(new_dicts)
+
+
+@pytest.mark.parametrize("repartition_nparts", [1, 5, 6, 10, 11])
+@pytest.mark.parametrize("rename_key", [rename_key_list, rename_key_np, rename_key_pd])
+def test_python_dict_udf(repartition_nparts, rename_key):
+    data = {"id": [i for i in range(10)], "dicts": [{"foo": i} for i in range(10)]}
+    daft_df = (
+        DataFrame.from_pydict(data)
+        .repartition(repartition_nparts)
+        .with_column("dicts_renamed", rename_key(col("dicts")))
+    )
+    pd_df = pd.DataFrame.from_dict(data)
+    pd_df["dicts_renamed"] = pd.Series([{"bar": d["foo"]} for d in pd_df["dicts"]])
+    daft_pd_df = daft_df.to_pandas()
+    assert_df_equals(daft_pd_df, pd_df, sort_key="id")
+
+
+###
+# Using np.ndarray as an object
+###
+
 
 @pytest.mark.parametrize("repartition_nparts", [1, 5, 6, 10, 11])
 def test_load_pydict_with_obj(repartition_nparts):
