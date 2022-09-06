19f31e94f79244a4b2b9593e745fa550000a3d23
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-09-06 16:11:44 -0700

    fix wheel name

--- tools/wheels/fix-and-copy-wheel.py
+++ tools/wheels/fix-and-copy-wheel.py
@@ -9,9 +9,9 @@ dest_dir = Path(sys.argv[2])
 wheel_target = dest_dir.joinpath(wheel.name)
 
 platform = sysconfig.get_platform()
+wheel_name = wheel.name
 
-if platform.startswith("macosx") and platform.endswith("arm64") and wheel.endswith("x86_64.whl"):
-    wheel_name = wheel.name
+if platform.startswith("macosx") and platform.endswith("arm64") and wheel_name.endswith("x86_64.whl"):
     prefix = wheel_name.rsplit("-", 1)[0]
     new_wheel_name = f"{prefix}-{platform}.whl"
     wheel_target = dest_dir.joinpath(new_wheel_name)
