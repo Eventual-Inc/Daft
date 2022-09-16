db79ca2729ae61f86e2e50826572e4d850b42fa4
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-09-15 22:25:21 -0700

    change default for ray workers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from typing import List, Union
 
 import pandas as pd
@@ -11,7 +12,7 @@ from daft.config import DaftSettings
 @pytest.fixture(scope="session", autouse=True)
 def ray_cluster():
     if DaftSettings.DAFT_RUNNER.upper() == "RAY":
-        ray.init(num_cpus=4, include_dashboard=False)
+        ray.init(num_cpus=os.cpu_count() // 2, include_dashboard=False)
         yield
         ray.shutdown()
     else:
