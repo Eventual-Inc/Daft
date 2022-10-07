e8ee2b70be60949812ef9a68a4959af2768c5298
Sammy Sidhu <samster25@users.noreply.github.com>
2022-10-07 16:02:16 -0700

    Test Wheels after building to verify they work (#260)
    * We had an issue where we pushed a broken wheel to PYPI. This PR
    ensures that we now test it before it gets uploaded

---
+++ tests/assets/assets.py
@@ -0,0 +1,18 @@
+import os
+
+
+def get_asset_dir() -> str:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return dir_path
+
+
+ASSET_FOLDER = get_asset_dir()
+
+IRIS_CSV = f"{ASSET_FOLDER}/iris.csv"
+SERVICE_REQUESTS_CSV = f"{ASSET_FOLDER}/311-service-requests.50.csv"
+SERVICE_REQUESTS_CSV_FOLDER = f"{ASSET_FOLDER}/311-service-requests.50"
+
+SERVICE_REQUESTS_PARQUET = f"{ASSET_FOLDER}/311-service-requests.50.parquet"
+SERVICE_REQUESTS_PARQUET_FOLDER = f"{ASSET_FOLDER}/311-service-requests.50.parquet_folder"
+TPCH_QUERIES = f"{ASSET_FOLDER}/tpch-sqlite-queries"
+TPCH_DBGEN_DIR = f"{ASSET_FOLDER}/../../data/tpch-dbgen"
