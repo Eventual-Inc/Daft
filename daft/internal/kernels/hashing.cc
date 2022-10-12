e2181e27ccbadbe6ed17d33858a6cd87f8ad2f84
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-10-11 20:15:36 -0700

    fix compile wranigns for unsigned comparision

--- daft/internal/kernels/hashing.cc
+++ daft/internal/kernels/hashing.cc
@@ -18,11 +18,11 @@ namespace bit_util = arrow::BitUtil;
 namespace bit_util = arrow::bit_util;
 #endif
 
-template <size_t WordSize>
+template <int64_t WordSize>
 struct HashingPrimativeArray {
   static void Exec(const arrow::ArrayData *arr, const arrow::ArrayData *seed, arrow::ArrayData *result) {
-    const size_t arr_len = arr->length;
-    const size_t bit_offset = arr->offset;
+    const int64_t arr_len = arr->length;
+    const int64_t bit_offset = arr->offset;
     const uint8_t *data_ptr = arr->GetValues<uint8_t>(1, bit_offset * WordSize);
     ARROW_CHECK(data_ptr != NULL);
 
@@ -34,7 +34,7 @@ struct HashingPrimativeArray {
     ARROW_CHECK(result->type->id() == arrow::Type::UINT64);
 
     ARROW_CHECK(result->GetMutableValues<uint8_t>(0) == NULL) << "bitmask should be NULL";
-    const size_t result_len = result->length;
+    const int64_t result_len = result->length;
     ARROW_CHECK(arr_len == result_len);
 
     const bool has_seed = seed != NULL;
@@ -45,8 +45,8 @@ struct HashingPrimativeArray {
       seed_ptr = seed->GetValues<uint64_t>(1);
     }
 
-    for (size_t idx = 0; idx < arr_len; ++idx) {
-      size_t length = WordSize;
+    for (int64_t idx = 0; idx < arr_len; ++idx) {
+      int64_t length = WordSize;
 
       if (has_nulls) {
         const bool is_valid = bit_util::GetBit(bitmask_ptr, idx + bit_offset);
@@ -81,10 +81,10 @@ struct HashingBinaryArray {
     ARROW_CHECK(result_ptr != NULL);
     ARROW_CHECK(result->GetMutableValues<uint8_t>(0) == NULL) << "bitmask should be NULL";
 
-    const size_t arr_len = arr->length;
-    const size_t bit_offset = arr->offset;
+    const int64_t arr_len = arr->length;
+    const int64_t bit_offset = arr->offset;
 
-    const size_t result_len = result->length;
+    const int64_t result_len = result->length;
     ARROW_CHECK(arr_len == result_len);
 
     const bool has_seed = seed != NULL;
@@ -95,9 +95,9 @@ struct HashingBinaryArray {
       seed_ptr = seed->GetValues<uint64_t>(1);
     }
 
-    for (size_t idx = 0; idx < arr_len; ++idx) {
-      const size_t curr_offset = arr_index_ptr[idx];
-      size_t curr_size = arr_index_ptr[idx + 1] - curr_offset;
+    for (int64_t idx = 0; idx < arr_len; ++idx) {
+      const int64_t curr_offset = arr_index_ptr[idx];
+      int64_t curr_size = arr_index_ptr[idx + 1] - curr_offset;
 
       if (has_nulls && !bit_util::GetBit(bitmask_ptr, idx + bit_offset)) {
         curr_size = 0;
@@ -152,9 +152,9 @@ namespace kernels {
 
 std::shared_ptr<arrow::ChunkedArray> xxhash_chunked_array(const arrow::ChunkedArray *arr, const arrow::ChunkedArray *seed) {
   ARROW_CHECK_NE(arr, NULL);
-  size_t num_chunks = arr->num_chunks();
+  const int64_t num_chunks = arr->num_chunks();
 
-  const size_t size = arr->length();
+  const int64_t size = arr->length();
   std::vector<std::shared_ptr<arrow::Buffer>> result_buffers{2};
   result_buffers[1] = arrow::AllocateBuffer(sizeof(arrow::UInt64Type::c_type) * size).ValueOrDie();
   std::shared_ptr<arrow::ArrayData> result = arrow::ArrayData::Make(std::make_shared<arrow::UInt64Type>(), size, result_buffers, 0);
@@ -163,14 +163,13 @@ std::shared_ptr<arrow::ChunkedArray> xxhash_chunked_array(const arrow::ChunkedAr
     ARROW_CHECK(seed->num_chunks() == 1);
     ARROW_CHECK(seed->null_count() == 0);
     ARROW_CHECK(seed->length() == size);
-    ARROW_CHECK(seed->type()->id() == arrow::UInt64Type);
   }
 
   std::vector<std::shared_ptr<arrow::Array>> result_arrays;
-  size_t offset_so_far = 0;
-  for (size_t i = 0; i < num_chunks; ++i) {
+  int64_t offset_so_far = 0;
+  for (int64_t i = 0; i < num_chunks; ++i) {
     std::shared_ptr<arrow::ArrayData> arr_subslice = arr->chunk(i)->data();
-    size_t arr_len = arr_subslice->length;
+    int64_t arr_len = arr_subslice->length;
     std::shared_ptr<arrow::ArrayData> result_subslice = result->Slice(offset_so_far, arr_len);
     arrow::ArrayData *seed_subslice = seed != NULL ? seed->chunk(0)->Slice(offset_so_far, arr_len)->data().get() : NULL;
 
