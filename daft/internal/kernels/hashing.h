78587ccb5ddb8c93b501bdd671b1a85a5cee4e09
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-10-11 20:19:52 -0700

    remove dead comment

--- daft/internal/kernels/hashing.h
+++ daft/internal/kernels/hashing.h
@@ -13,8 +13,5 @@ namespace kernels {
 
 std::shared_ptr<arrow::ChunkedArray> xxhash_chunked_array(const arrow::ChunkedArray *arr, const arrow::ChunkedArray *seed);
 
-// std::shared_ptr<arrow::ChunkedArray> search_sorted_table(const arrow::Table *data, const arrow::Table *keys,
-//                                                          const std::vector<bool> &input_reversed);
-
 }  // namespace kernels
 }  // namespace daft
