8d093f6e35ab26c0fa1bae474404721258ec65aa
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-10-12 17:15:42 -0700

    working multi column sorts

--- tests/null_tests/test_sorts.py
+++ tests/null_tests/test_sorts.py
@@ -6,7 +6,7 @@ from tests.conftest import assert_arrow_equals
 
 
 @pytest.mark.parametrize("repartition_nparts", [1, 2, 4])
-def test_sort_with_nulls(repartition_nparts):
+def test_int_sort_with_nulls(repartition_nparts):
     daft_df = DataFrame.from_pydict(
         {
             "id": [2, None, 1],
@@ -21,6 +21,24 @@ def test_sort_with_nulls(repartition_nparts):
     }
     daft_df.collect()
 
+    assert_arrow_equals(daft_df._result.to_pydict(), expected_arrow_table, assert_ordering=True)
+
+
+@pytest.mark.parametrize("repartition_nparts", [1, 2, 4])
+def test_str_sort_with_nulls(repartition_nparts):
+    daft_df = DataFrame.from_pydict(
+        {
+            "id": [1, None, 2],
+            "values": ["c1", None, "a1"],
+        }
+    ).repartition(repartition_nparts)
+    daft_df = daft_df.sort(daft_df["values"])
+
+    expected_arrow_table = {
+        "id": pa.array([2, 1, None]),
+        "values": pa.array(["a1", "c1", None]),
+    }
+    daft_df.collect()
     assert_arrow_equals(daft_df._result.to_pydict(), expected_arrow_table, assert_ordering=True)
 
 
@@ -41,7 +59,6 @@ def test_sort_with_nulls_multikey(repartition_nparts):
         "values": pa.array(["e1", "c1", "a1", "d1", "b1"]),
     }
     daft_df.collect()
-
     assert_arrow_equals(daft_df._result.to_pydict(), expected_arrow_table, assert_ordering=True)
 
 
@@ -50,6 +67,7 @@ def test_sort_with_all_nulls(repartition_nparts):
     daft_df = DataFrame.from_pydict(
         {
             "id": pa.array([None, None, None], type=pa.int64()),
+            "values": ["c1", None, "a1"],
         }
     ).repartition(repartition_nparts)
     daft_df = daft_df.sort(daft_df["id"])
