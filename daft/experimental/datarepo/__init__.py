73e076cf2dd44be4609d95a23fd66679ffe114a8
Sammy Sidhu <sammy.sidhu@gmail.com>
2022-10-21 16:15:53 -0700

    remove schema + data repo

--- daft/experimental/datarepo/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from .client import DatarepoClient, get_client
-
-__all__ = [
-    "get_client",
-    "DatarepoClient",
-]
--- tests/experimental/datarepo/utils.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from icebridge.client import IcebergCatalog, IceBridgeClient
-
-
-def create_test_catalog(path: str) -> IcebergCatalog:
-    client = IceBridgeClient()
-    hadoop_catalog = IcebergCatalog.from_hadoop_catalog(client, f"file://{path}")
-    return hadoop_catalog
