bfd3aea3cabae506dc45040fe404e7504a4f8ab7
Jay Chia <jaychia94@gmail.com>
2022-11-04 12:38:35 -0700

    Import `lit` from daft top-level module

--- daft/__init__.py
+++ daft/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 from daft.dataframe import DataFrame
-from daft.expressions import col
+from daft.expressions import col, lit
 from daft.logging import setup_logger
 from daft.udf import polars_udf, udf
 
-__all__ = ["DataFrame", "col", "udf", "polars_udf"]
+__all__ = ["DataFrame", "col", "lit", "udf", "polars_udf"]
 
 setup_logger()
 
