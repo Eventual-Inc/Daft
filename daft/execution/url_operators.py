898c929153c795604a62bcf0ba9e64abdc8d6ca9
Jay Chia <jaychia94@gmail.com>
2022-12-01 20:31:31 -0800

    Add fix for duplicate URL downloads only filling out bytes for one row

--- daft/execution/url_operators.py
+++ daft/execution/url_operators.py
@@ -20,7 +20,7 @@ def download(url_block: ArrowDataBlock) -> list[bytes | None]:
 
     results: list[bytes | None] = [None for _ in range(len(url_block))]
 
-    path_to_result_idx = {}
+    path_to_result_idx: dict[str, list[int]] = {}
     to_download: dict[str, list[str]] = {}
     for i, path in enumerate(url_block.iter_py()):
         if path is None:
@@ -38,16 +38,21 @@ def download(url_block: ArrowDataBlock) -> list[bytes | None]:
         if protocol not in to_download:
             to_download[protocol] = []
         to_download[protocol].append(path)
-        path_to_result_idx[path] = i
+
+        if path not in path_to_result_idx:
+            path_to_result_idx[path] = []
+        path_to_result_idx[path].append(i)
 
     for protocol in to_download:
         fs = filesystem.get_filesystem(protocol)
         data = fs.cat(to_download[protocol], on_error="return")
         for path in data:
             if isinstance(data[path], bytes):
-                results[path_to_result_idx[path]] = data[path]
+                for i in path_to_result_idx[path]:
+                    results[i] = data[path]
             else:
                 logger.error(f"Encountered error during download from URL {path}: {str(data[path])}")
-                results[path_to_result_idx[path]] = None
+                for i in path_to_result_idx[path]:
+                    results[i] = None
 
     return results
