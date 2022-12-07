8795066e7a2678045b3effef77368eaa6c03856e
Sammy Sidhu <samster25@users.noreply.github.com>
2022-12-07 00:37:00 -0800

    all tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,15 @@ mod ffi;
 mod hashing;
 mod search_sorted;
 
+use std::iter::zip;
+
 use arrow2::array::{Array, PrimitiveArray};
 use arrow2::datatypes::DataType;
 
 use arrow2::compute::arithmetics::basic::add_scalar;
+use pyo3::ffi::PyListObject;
 use pyo3::prelude::*;
+use pyo3::types::PyList;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -66,6 +70,42 @@ fn search_sorted_pyarrow_array(
     ffi::to_py_array(Box::new(result_idx.unwrap()), py, pyarrow)
 }
 
+#[pyfunction]
+fn search_sorted_multiple_pyarrow_array(
+    sorted_arrays: &PyList,
+    key_arrays: &PyList,
+    input_reversed: Vec<bool>,
+    py: Python,
+    pyarrow: &PyModule,
+) -> PyResult<PyObject> {
+    assert_eq!(sorted_arrays.len(), key_arrays.len());
+    assert_eq!(sorted_arrays.len(), input_reversed.len());
+    let mut rsorted_arrays: Vec<Box<dyn Array>> = Vec::with_capacity(sorted_arrays.len());
+    let mut rkeys_arrays: Vec<Box<dyn Array>> = Vec::with_capacity(key_arrays.len());
+
+    for (sorted_arr, key_arr) in zip(sorted_arrays.iter(), key_arrays.iter()) {
+        rsorted_arrays.push(ffi::array_to_rust(sorted_arr)?);
+        rkeys_arrays.push(ffi::array_to_rust(key_arr)?);
+    }
+
+    let rsorted_arrays_refs = rsorted_arrays
+        .iter()
+        .map(Box::as_ref)
+        .collect::<Vec<&dyn Array>>();
+    let key_arrays_refs = rkeys_arrays
+        .iter()
+        .map(Box::as_ref)
+        .collect::<Vec<&dyn Array>>();
+
+    let result_idx = search_sorted::search_sorted_multi_array(
+        &rsorted_arrays_refs,
+        &key_arrays_refs,
+        &input_reversed,
+    );
+
+    ffi::to_py_array(Box::new(result_idx.unwrap()), py, pyarrow)
+}
+
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
@@ -74,6 +114,7 @@ fn daft_core(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add_1, m)?)?;
     m.add_function(wrap_pyfunction!(hash_pyarrow_array, m)?)?;
     m.add_function(wrap_pyfunction!(search_sorted_pyarrow_array, m)?)?;
+    m.add_function(wrap_pyfunction!(search_sorted_multiple_pyarrow_array, m)?)?;
 
     Ok(())
 }
