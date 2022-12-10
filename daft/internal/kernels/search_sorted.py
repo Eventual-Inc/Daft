1481bb56e793c7171a24d896e1a86217d89a0671
Sammy Sidhu <samster25@users.noreply.github.com>
2022-12-09 16:58:54 -0800

    fix search sorted bugs

--- daft/internal/kernels/search_sorted.py
+++ daft/internal/kernels/search_sorted.py
@@ -18,6 +18,7 @@ def search_sorted(data, keys, input_reversed=None):
         else:
             data = data.chunk(0)
         result_chunks = [search_sorted_pyarrow_array(data, key_chunk, input_reversed, pa) for key_chunk in keys.chunks]
+
         return pa.chunked_array(result_chunks, type=pa.uint64())
     elif isinstance(data, pa.Table):
         assert isinstance(keys, pa.Table), "expected keys to be a table since data is one"
@@ -39,13 +40,12 @@ def search_sorted(data, keys, input_reversed=None):
             table_input_reversed = [False for _ in range(num_columns)]
 
         if num_columns == 1:
-            return search_sorted(data.columns[0], keys.columns[0])
-
-        data = data.combine_chunks()
-        keys = keys.combine_chunks()
-
+            return search_sorted(data.columns[0], keys.columns[0], table_input_reversed[0])
         result = search_sorted_multiple_pyarrow_array(
-            [c.chunk(0) for c in data.columns], [c.chunk(0) for c in keys.columns], table_input_reversed, pa
+            [c.combine_chunks() for c in data.columns],
+            [c.combine_chunks() for c in keys.columns],
+            table_input_reversed,
+            pa,
         )
         return pa.chunked_array([result], type=pa.uint64())
     else:
