fd012b9a59050faad14f450ad0ea9787eb0ed5ef
Xiayue Charles Lin <x@charlesl.in>
2022-12-12 16:25:47 -0800

    Fleshed out PartitionInstructions. Mega WIP

--- daft/runners/partitioning.py
+++ daft/runners/partitioning.py
@@ -5,7 +5,7 @@ import weakref
 from abc import abstractmethod
 from dataclasses import dataclass
 from functools import partial
-from typing import Any, Callable, Generic, Sequence, TypeVar
+from typing import Any, Callable, Generic, Iterable, Sequence, Tuple, TypeVar
 from uuid import uuid4
 
 import numpy as np
@@ -482,6 +482,13 @@ class PartitionSet(Generic[PartitionT]):
     def to_pandas(self, schema: ExpressionList | None = None) -> pd.DataFrame:
         merged_partition = self._get_merged_vpartition()
         return merged_partition.to_pandas(schema=schema)
+
+    def items(self) -> Iterable[Tuple[PartID, PartitionT]]:
+        """
+        Returns all (partition id, partition) in this PartitionSet,
+        ordered by partition ID.
+        """
+        raise NotImplementedError()
 
     @abstractmethod
     def get_partition(self, idx: PartID) -> PartitionT:
--- daft/runners/pyrunner.py
+++ daft/runners/pyrunner.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import multiprocessing
 from dataclasses import dataclass
-from typing import Callable, ClassVar
+from typing import Callable, ClassVar, Generator, Tuple
 
 import psutil
 
@@ -47,11 +47,14 @@ from daft.runners.shuffle_ops import (
 class LocalPartitionSet(PartitionSet[vPartition]):
     _partitions: dict[PartID, vPartition]
 
+    def items(self) -> list[Tuple[PartID, vPartition]]:
+        return sorted(self._partitions.items())
+
     def _get_merged_vpartition(self) -> vPartition:
-        partition_ids = sorted(list(self._partitions.keys()))
-        assert partition_ids[0] == 0
-        assert partition_ids[-1] + 1 == len(partition_ids)
-        return vPartition.merge_partitions([self._partitions[pid] for pid in partition_ids], verify_partition_id=False)
+        ids_and_partitions = self.items()
+        assert ids_and_partitions[0][0] == 0
+        assert ids_and_partitions[-1][0] + 1 == len(partition_ids)
+        return vPartition.merge_partitions([part for id, part in ids_and_partitions], verify_partition_id=False)
 
     def get_partition(self, idx: PartID) -> vPartition:
         return self._partitions[idx]
