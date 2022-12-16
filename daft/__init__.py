1342c157b652fc89ea9b9772adf2a3bed6bba935
Sammy Sidhu <samster25@users.noreply.github.com>
2022-12-16 14:28:37 -0800

    add daft version from rust built

--- daft/__init__.py
+++ daft/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from daft.daft import version as _version
 from daft.dataframe import DataFrame
 from daft.expressions import col, lit
 from daft.logging import setup_logger
@@ -10,6 +11,13 @@ from daft.udf import polars_udf, udf
 __all__ = ["DataFrame", "col", "lit", "udf", "polars_udf"]
 
 setup_logger()
+
+
+def get_version() -> str:
+    return _version()
+
+
+__version__ = get_version()
 
 if TYPE_CHECKING:
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,15 @@ mod kernels;
 
 use pyo3::prelude::*;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+#[pyfunction]
+fn version() -> &'static str {
+    VERSION
+}
+
 #[pymodule]
 fn daft(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     kernels::register_kernels(_py, m)?;
+    m.add_wrapped(wrap_pyfunction!(version))?;
     Ok(())
 }
