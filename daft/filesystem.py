e33973364fee52b6f5d580d29cca8fd954586025
Jay Chia <jaychia94@gmail.com>
2022-12-30 11:56:35 +0800

    Remove caching from filesystems and benchmarking (#411)
    1. Removes all filesystem caching and associated utilities
    2. Removes cache warmup step from benchmarking

--- daft/filesystem.py
+++ daft/filesystem.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
 
 from fsspec import AbstractFileSystem, get_filesystem_class
-from fsspec.implementations.cached import SimpleCacheFileSystem
-
-from daft.context import get_context
 
 
 def get_filesystem(protocol: str, **kwargs) -> AbstractFileSystem:
@@ -22,12 +19,6 @@ def get_protocol_from_path(path: str, **kwargs) -> str:
 def get_filesystem_from_path(path: str, **kwargs) -> AbstractFileSystem:
     protocol = get_protocol_from_path(path)
     fs = get_filesystem(protocol, **kwargs)
-
-    # If a cache location is set and the protocol is not a local file, use as a simple disk-based cache
-    cache_location = get_context().cache_location
-    if protocol != "file" and cache_location is not None:
-        fs = SimpleCacheFileSystem(fs=fs, cache_storage=str(cache_location / "simple-cache-file-system"))
-
     return fs
 
 
