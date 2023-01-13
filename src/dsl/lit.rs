02655f0cf575bb14d8324b8d5fb1c431bf49031c
Sammy Sidhu <samster25@users.noreply.github.com>
2023-01-12 18:39:20 -0800

    fmt

--- src/dsl/lit.rs
+++ src/dsl/lit.rs
@@ -1,8 +1,6 @@
 use crate::datatypes::DataType;
 use crate::dsl::expr::Expr;
 
-
-
 /// Stores a literal value for queries and computations.
 /// We only need to support the limited types below since those are the types that we would get from python.
 #[derive(Debug)]
