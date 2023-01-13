a23e442d7d901540fdc3f037565c345979ea81bc
Jay Chia <jaychia94@gmail.com>
2023-01-12 18:11:50 -0800

    Cleanup tests, found bugs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import argparse
 from itertools import chain
 
 import pandas as pd
@@ -16,43 +15,22 @@ from daft.types import ExpressionType
 def pytest_addoption(parser):
     parser.addoption("--run_conda", action="store_true", default=False, help="run tests that require conda")
     parser.addoption("--run_docker", action="store_true", default=False, help="run tests that require docker")
-    parser.addoption(
-        "--run_tdd", action="store_true", default=False, help="run tests that are marked for Test Driven Development"
-    )
-    parser.addoption(
-        "--run_tdd_all",
-        action="store_true",
-        default=False,
-        help="run tests that are marked for Test Driven Development (including low priority)",
-    )
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "conda: mark test as requiring conda to run")
     config.addinivalue_line("markers", "docker: mark test as requiring docker to run")
-    config.addinivalue_line("markers", "tdd: mark test as for TDD in active development")
-    config.addinivalue_line("markers", "tdd_all: mark test as for TDD but not in active development")
-    config.addinivalue_line("markers", "tpch: mark as a tpch test")
 
 
 def pytest_collection_modifyitems(config, items):
     marks = {
         "conda": pytest.mark.skip(reason="need --run_conda option to run"),
         "docker": pytest.mark.skip(reason="need --run_docker option to run"),
-        "tdd": pytest.mark.skip(reason="need --run_tdd option to run"),
-        "tdd_all": pytest.mark.skip(reason="need --run_tdd_all option to run"),
     }
     for item in items:
         for keyword in marks:
             if keyword in item.keywords and not config.getoption(f"--run_{keyword}"):
                 item.add_marker(marks[keyword])
-
-
-def run_tdd():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--run_tdd", action="store_true")
-    args, _ = parser.parse_known_args()
-    return args.run_tdd
 
 
 def assert_df_column_type(
