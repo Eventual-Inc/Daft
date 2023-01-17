Eventual-Inc/Daft

commit f5a637d996ff3c16c0f0bfa6e772cdee32289a4f

Sorts on non-column expression (#299)
* Sorts are broken when run on a column that is not just a
ColumnExpression

daft/runners/partitioning.py
@@ -207,6 +207,11 @@ class vPartition:
 
     def eval_expression(self, expr: Expression) -> PyListTile:
         expr_col_id = expr.get_id()
+
+        # Avoid recomputing expressions that have been computed before
+        if expr_col_id in self.columns:
+            return self.columns[expr_col_id]
+
         expr_name = expr.name()
 
         assert expr_col_id is not None

tests/dataframe_cookbook/test_sorting.py
@@ -11,6 +11,20 @@ from tests.dataframe_cookbook.conftest import (
 
 
 @parametrize_service_requests_csv_repartition
+def test_sorted_by_expr(daft_df, service_requests_csv_pd_df, repartition_nparts):
+    """Sort by a column that undergoes an expression"""
+    daft_df = daft_df.repartition(repartition_nparts)
+    daft_sorted_df = daft_df.sort(col("Unique Key") + 1)
+
+    daft_sorted_pd_df = daft_sorted_df.to_pandas()
+    assert_df_equals(
+        daft_sorted_pd_df,
+        service_requests_csv_pd_df.sort_values(by=["Unique Key"]),
+        assert_ordering=True,
+    )
+
+
+@parametrize_service_requests_csv_repartition
 @parametrize_sort_desc("sort_desc")
 @pytest.mark.parametrize(
     "sort_keys",

tests/runners/test_partitioning.py
@@ -149,15 +149,18 @@ def test_vpartition_filter() -> None:
     expr = col("x") < 4
     expr = resolve_expr(expr)
 
+    # Need to make sure there are no conflicts with column ID of `expr`
+    col_ids = [expr.required_columns()[0].get_id()] + list(range(expr.get_id() + 1, expr.get_id() + 4))
+
     tiles = {}
-    col_id = expr.required_columns()[0].get_id()
-    for i in range(col_id, col_id + 4):
+    for i in col_ids:
         block = DataBlock.make_block(np.arange(0, 10, 1))
         tiles[i] = PyListTile(column_id=i, column_name=f"col_{i}", partition_id=0, block=block)
+
     part = vPartition(columns=tiles, partition_id=0)
     part = part.filter(ExpressionList([expr]))
     arrow_table = pa.Table.from_pandas(part.to_pandas())
-    assert arrow_table.column_names == [f"col_{i}" for i in range(col_id, col_id + 4)]
+    assert arrow_table.column_names == [f"col_{i}" for i in col_ids]
 
     for i in range(4):
         assert np.all(arrow_table[i].to_numpy() < 4)

commit cb07b0883257d40edaa2e8ef148d5f4d1f4fe370

Fix alias evaluation (#448)
Closes #447 

Bug introduced by
https://github.com/Eventual-Inc/Daft/pull/299/files#diff-8ce570b3f2f114483c776e449047082aaebf681b7f965f8ec989d12337a421bdR210-R214
which would skip evaluation of aliases because AliasExpressions have the
same ID as its children nodes.

Add a test and fix for this bug

daft/runners/partitioning.py
@@ -209,7 +209,7 @@ class vPartition:
         expr_col_id = expr.get_id()
 
         # Avoid recomputing expressions that have been computed before
-        if expr_col_id in self.columns:
+        if expr_col_id in self.columns and self.columns[expr_col_id].column_name == expr.name():
             return self.columns[expr_col_id]
 
         expr_name = expr.name()

tests/test_viz.py
@@ -1,0 +1,45 @@
+from __future__ import annotations
+
+import re
+
+from daft import DataFrame
+from daft.viz import DataFrameDisplay
+
+ROW_DIVIDER_REGEX = re.compile(r"\+-+\+")
+SHOWING_N_ROWS_REGEX = re.compile(r"\(Showing first (\d+) of (\d+) rows\)")
+
+
+def parse_str_table(table) -> dict[str, tuple[str, list[str]]]:
+    def _split_table_row(row: str) -> list[str]:
+        return [cell.strip() for cell in row.split("|")[1:-1]]
+
+    lines = table.split("\n")
+    assert len(lines) > 4
+    assert ROW_DIVIDER_REGEX.match(lines[0])
+    assert SHOWING_N_ROWS_REGEX.match(lines[-1])
+
+    column_names = _split_table_row(lines[1])
+    column_types = _split_table_row(lines[2])
+
+    data = []
+    for line in lines[4:-1]:
+        if ROW_DIVIDER_REGEX.match(line):
+            continue
+        data.append(_split_table_row(line))
+
+    return {column_names[i]: (column_types[i], [row[i] for row in data]) for i in range(len(column_names))}
+
+
+def test_alias_repr():
+    df = DataFrame.from_pydict({"A": [1, 2, 3]})
+    df = df.select(df["A"].alias("A2"))
+    df.collect()
+    display = DataFrameDisplay(df._preview, df.schema())
+    repr_str = display.__repr__()
+
+    assert parse_str_table(repr_str) == {
+        "A2": (
+            "INTEGER",
+            ["1", "2", "3"],
+        )
+    }