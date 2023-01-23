f870d5aaf19ca6a314bd68672cf8cabfabb77543
Sammy Sidhu <samster25@users.noreply.github.com>
2023-01-22 22:10:21 -0800

    add from methods for series + table

--- src/python/mod.rs
+++ src/python/mod.rs
@@ -1,10 +1,13 @@
 use pyo3::prelude::*;
 mod expr;
+mod series;
 mod table;
 
 pub fn register_modules(_py: Python, parent: &PyModule) -> PyResult<()> {
     parent.add_class::<expr::PyExpr>()?;
     parent.add_class::<table::PyTable>()?;
+    parent.add_class::<series::PySeries>()?;
+
     parent.add_wrapped(wrap_pyfunction!(expr::col))?;
     parent.add_wrapped(wrap_pyfunction!(expr::lit))?;
 
--- src/python/table.rs
+++ src/python/table.rs
@@ -6,3 +6,15 @@ use crate::table;
 pub struct PyTable {
     pub table: table::Table,
 }
+
+impl From<table::Table> for PyTable {
+    fn from(value: table::Table) -> Self {
+        PyTable { table: value }
+    }
+}
+
+impl From<PyTable> for table::Table {
+    fn from(item: PyTable) -> Self {
+        item.table
+    }
+}
