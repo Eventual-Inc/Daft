0b3479c9b74fca67b4c9664b5681e861f6fb0bd0
Sammy Sidhu <samster25@users.noreply.github.com>
2023-02-06 17:30:23 -0800

    add python level bindings for series + repr

--- src/series/ops/mod.rs
+++ src/series/ops/mod.rs
@@ -5,6 +5,7 @@ use super::Series;
 pub mod arithmetic;
 pub mod broadcast;
 pub mod cast;
+pub mod comparision;
 pub mod downcast;
 pub mod take;
 
@@ -55,6 +56,32 @@ macro_rules! with_match_numeric_and_utf_daft_types {(
     use $crate::datatypes::*;
 
     match $key_type {
+        Int8 => __with_ty__! { Int8Type },
+        Int16 => __with_ty__! { Int16Type },
+        Int32 => __with_ty__! { Int32Type },
+        Int64 => __with_ty__! { Int64Type },
+        UInt8 => __with_ty__! { UInt8Type },
+        UInt16 => __with_ty__! { UInt16Type },
+        UInt32 => __with_ty__! { UInt32Type },
+        UInt64 => __with_ty__! { UInt64Type },
+        // Float16 => __with_ty__! { Float16Type },
+        Float32 => __with_ty__! { Float32Type },
+        Float64 => __with_ty__! { Float64Type },
+        Utf8 => __with_ty__! { Utf8Type },
+        _ => panic!("{:?} not implemented", $key_type)
+    }
+})}
+
+#[macro_export]
+macro_rules! with_match_comparable_daft_types {(
+    $key_type:expr, | $_:tt $T:ident | $($body:tt)*
+) => ({
+    macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
+    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::*;
+
+    match $key_type {
+        Boolean => __with_ty__! { BooleanType },
         Int8 => __with_ty__! { Int8Type },
         Int16 => __with_ty__! { Int16Type },
         Int32 => __with_ty__! { Int32Type },
