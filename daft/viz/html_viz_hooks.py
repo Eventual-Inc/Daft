f46297c2d1cd4b489d92b52510ef5442909833e1
Jay Chia <jaychia94@gmail.com>
2023-02-11 20:25:56 -0800

    Refactor code to retrieve visualization logic from a registry (#583)
    * Visualization logic can be registered with Daft at runtime with
    `daft.viz.register_viz_hook`
    * Refactors code for visualizing PIL and Numpy to now be registered with
    `register_viz_hook` at import-time of the module

---
+++ daft/viz/html_viz_hooks.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import base64
+import io
+from typing import TYPE_CHECKING, Callable, TypeVar
+
+if TYPE_CHECKING:
+    import numpy as np
+    import PIL.Image
+
+
+HookClass = TypeVar("HookClass")
+
+_VIZ_HOOKS_REGISTRY = {}
+
+
+def register_viz_hook(klass: type[HookClass], hook: Callable[[HookClass], str]):
+    """Registers a visualization hook that returns the appropriate HTML for
+    visualizing a specific class in HTML"""
+    _VIZ_HOOKS_REGISTRY[klass] = hook
+
+
+def get_viz_hook(val: HookClass) -> Callable[[HookClass], str] | None:
+    for klass in _VIZ_HOOKS_REGISTRY:
+        if isinstance(val, klass):
+            return _VIZ_HOOKS_REGISTRY[klass]
+    return None
+
+
+###
+# Default hooks, registered at import-time
+###
+
+HAS_PILLOW = True
+try:
+    import PIL.Image
+except ImportError:
+    HAS_PILLOW = False
+
+HAS_NUMPY = True
+try:
+    import numpy as np
+except ImportError:
+    HAS_NUMPY = False
+
+if HAS_PILLOW:
+
+    def _viz_pil_image(val: PIL.Image.Image):
+        img = val.copy()
+        img.thumbnail((128, 128))
+        bio = io.BytesIO()
+        img.save(bio, "JPEG")
+        base64_img = base64.b64encode(bio.getvalue())
+        return f'<img style="max-height:128px;width:auto" src="data:image/png;base64, {base64_img.decode("utf-8")}" alt="{str(val)}" />'
+
+    register_viz_hook(PIL.Image.Image, _viz_pil_image)
+
+if HAS_NUMPY:
+
+    def _viz_numpy(val: np.ndarray):
+        return f"&ltnp.ndarray<br>shape={val.shape}<br>dtype={val.dtype}&gt"
+
+    register_viz_hook(np.ndarray, _viz_numpy)
--- daft/viz/repr.py
+++ daft/viz/repr.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-import base64
-import io
+import html
 from typing import Any, Callable, Iterable
 
 from tabulate import tabulate
@@ -9,21 +8,7 @@ from tabulate import tabulate
 from daft.logical.schema import Schema
 from daft.runners.partitioning import vPartition
 from daft.types import ExpressionType
-
-try:
-    import PIL.Image
-
-    HAS_PILLOW = True
-except ImportError:
-    HAS_PILLOW = False
-
-try:
-    import numpy as np
-
-    HAS_NUMPY = True
-except ImportError:
-    HAS_NUMPY = False
-
+from daft.viz.html_viz_hooks import get_viz_hook
 
 DEFAULT_MAX_COL_WIDTH = 20
 DEFAULT_MAX_LINES = 3
@@ -44,16 +29,10 @@ def _stringify_object_default(val: Any, max_col_width: int, max_lines: int):
 
 def _stringify_object_html(val: Any, max_col_width: int, max_lines: int):
     """Stringifies Python objects, with custom handling for specific objects that Daft recognizes as media types"""
-    if HAS_PILLOW and isinstance(val, PIL.Image.Image):
-        img = val.copy()
-        img.thumbnail((128, 128))
-        bio = io.BytesIO()
-        img.save(bio, "JPEG")
-        base64_img = base64.b64encode(bio.getvalue())
-        return f'<img style="max-height:128px;width:auto" src="data:image/png;base64, {base64_img.decode("utf-8")}" alt="{str(val)}" />'
-    elif HAS_NUMPY and isinstance(val, np.ndarray):
-        return f"&ltnp.ndarray<br>shape={val.shape}<br>dtype={val.dtype}&gt"
-    return _truncate(str(val), max_col_width, max_lines)
+    viz_hook = get_viz_hook(val)
+    if viz_hook is not None:
+        return viz_hook(val)
+    return html.escape(_truncate(str(val), max_col_width, max_lines))
 
 
 def _stringify_vpartition(
