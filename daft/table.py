49bea4b4dd4fd2805e204931170323f461a724d2
Sammy Sidhu <samster25@users.noreply.github.com>
2023-02-14 15:34:44 -0800

    python wrapper for table

---
+++ daft/table.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import pyarrow as pa
+
+from daft.daft import PyTable as _PyTable
+
+
+class Table:
+    _table: _PyTable
+
+    def __init__(self) -> None:
+        raise NotImplementedError("We do not support creating a Table via __init__ ")
+
+    @staticmethod
+    def _from_pytable(pyt: _PyTable) -> Table:
+        tab = Table.__new__(Table)
+        tab._table = pyt
+        return tab
+
+    @staticmethod
+    def from_arrow(arrow_table: pa.Table) -> Table:
+        assert isinstance(arrow_table, pa.Table)
+        pyt = _PyTable.from_arrow_record_batches(arrow_table.to_batches())
+        return Table._from_pytable(pyt)
+
+    @staticmethod
+    def from_pydict(data: dict) -> Table:
+        pya_table = pa.Table.from_pydict(data)
+        return Table.from_arrow(pya_table)
+
+    def to_arrow(self) -> pa.Table:
+        return pa.Table.from_batches([self._table.to_arrow_record_batch()])
+
+    def head(self, num: int) -> Table:
+        return Table._from_pytable(self._table.head(num))
+
+    def column_names(self) -> list[str]:
+        return self._table.column_names()
+
+    def __len__(self) -> int:
+        return len(self._table)
+
+    def __repr__(self) -> str:
+        return repr(self._table)
--- tests/table/test_table.py
+++ tests/table/test_table.py
@@ -3,21 +3,21 @@ from __future__ import annotations
 import pyarrow as pa
 import pytest
 
-from daft.daft import PyTable
+from daft.table import Table
 
 
 def test_from_arrow_round_trip() -> None:
     pa_table = pa.Table.from_pydict({"a": [1, 2, 3, 4], "b": [5, 6, 7, 8]})
-    daft_table = PyTable.from_arrow_record_batches(pa_table.to_batches())
+    daft_table = Table.from_arrow(pa_table)
     assert len(daft_table) == 4
     assert daft_table.column_names() == ["a", "b"]
-    read_back = pa.Table.from_batches([daft_table.to_arrow_record_batch()])
+    read_back = daft_table.to_arrow()
     assert pa_table == read_back
 
 
 def test_table_head() -> None:
     pa_table = pa.Table.from_pydict({"a": [1, 2, 3, 4], "b": [5, 6, 7, 8]})
-    daft_table = PyTable.from_arrow_record_batches(pa_table.to_batches())
+    daft_table = Table.from_arrow(pa_table)
     assert len(daft_table) == 4
     assert daft_table.column_names() == ["a", "b"]
 
@@ -25,14 +25,14 @@ def test_table_head() -> None:
     headed = daft_table.head(3)
     assert len(headed) == 3
     assert headed.column_names() == ["a", "b"]
-    pa_headed = pa.Table.from_batches([headed.to_arrow_record_batch()])
+    pa_headed = headed.to_arrow()
     assert pa_table[:3] == pa_headed
 
     # overslice
     headed = daft_table.head(5)
     assert len(headed) == 4
     assert headed.column_names() == ["a", "b"]
-    pa_headed = pa.Table.from_batches([headed.to_arrow_record_batch()])
+    pa_headed = headed.to_arrow()
     assert pa_table == pa_headed
 
     # negative slice
