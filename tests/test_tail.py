5507cba38dbdd0eeb8d594b1b1e7d4a427a4b61b
Xiayue Charles Lin <x@charlesl.in>
2023-02-22 12:03:58 -0800

    Add regular limit (head) to tests as well

--- tests/test_tail.py
+++ tests/test_tail.py
@@ -8,16 +8,21 @@ from daft.expressions import col
 from tests.assets.assets import IRIS_CSV
 
 
+@pytest.mark.parametrize("tail", [True, False])
 @pytest.mark.parametrize("num", [10, 5, 2, 1, 0])
-def test_tail_simple(num):
+def test_tail_simple(tail, num):
     df = DataFrame.from_pydict({"A": [1, 2, 3, 4, 5]})
-    daft_tail = df.limit(num, tail=True).to_pandas()
-    expected = df.to_pandas().tail(num)
+    daft_tail = df.limit(num, tail=tail).to_pandas()
+    if tail:
+        expected = df.to_pandas().tail(num)
+    else:
+        expected = df.to_pandas().head(num)
     assert daft_tail.reset_index(drop=True).equals(expected.reset_index(drop=True)), f"\n{daft_tail} \n!= \n{expected}"
 
 
+@pytest.mark.parametrize("tail", [True, False])
 @pytest.mark.parametrize("num", [100, 10, 1, 0])
-def test_tail_complex_plan(num):
+def test_tail_complex_plan(tail, num):
 
     # Query under test
     df = DataFrame.read_csv(IRIS_CSV)
@@ -26,7 +31,7 @@ def test_tail_complex_plan(num):
     df = df.where(col("area") < 20)
     df = df.where(col("variety") == "Virginica")
     df = df.sort([col("area"), col("sepal.length")])
-    df = df.limit(num, tail=True)
+    df = df.limit(num, tail=tail)
     df = df.collect()
     df = df.to_pandas().reset_index(drop=True)
 
@@ -36,7 +41,10 @@ def test_tail_complex_plan(num):
     pd_df = pd_df[pd_df["area"] < 20]
     pd_df = pd_df[pd_df["variety"] == "Virginica"]
     pd_df = pd_df.sort_values(by=["area", "sepal.length"], ascending=True)
-    pd_df = pd_df.tail(num)
+    if tail:
+        pd_df = pd_df.tail(num)
+    else:
+        pd_df = pd_df.head(num)
     pd_df = pd_df.reset_index(drop=True)
 
     assert df.equals(pd_df), f"\n{df} \n!= \n{pd_df}"
