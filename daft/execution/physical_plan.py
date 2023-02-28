Eventual-Inc/Daft

commit 98f25ed825d1a29c5169742579f432844b4cc295

Add 2x partition size memory to all tasks' ResourceRequests. (#600)
Currently, most partition building tasks do not have any explicit memory
resource request.

This PR adds a memory resource request, sized at 2x the input
partitions.

- Implemented `DataBlock.size_bytes()` and added it to
`vPartition.metadata()`
- In the RayRunner, for performance, build_partitions also runs
`vPartition.metadata()` and returns the results as a separate objectref.

Won't merge until next week.

daft/execution/physical_plan.py
@@ -153,7 +153,10 @@ def join(
             assert next_right.result is not None  # for mypy only; guaranteed by while condition
 
             join_step = ExecutionStepBuilder[PartitionT](
-                inputs=[next_left.result.partition(), next_right.result.partition()]
+                inputs=[next_left.result.partition(), next_right.result.partition()],
+                resource_request=ResourceRequest(
+                    memory_bytes=2 * (next_left.result.metadata().size_bytes + next_right.result.metadata().size_bytes)
+                ),
             ).add_instruction(instruction=execution_step.Join(join))
             yield join_step
 
@@ -235,7 +238,10 @@ def global_limit(
 
             limit = remaining_rows and min(remaining_rows, result.metadata().num_rows)
 
-            global_limit_step = ExecutionStepBuilder[PartitionT](inputs=[result.partition()]).add_instruction(
+            global_limit_step = ExecutionStepBuilder[PartitionT](
+                inputs=[result.partition()],
+                resource_request=ResourceRequest(memory_bytes=2 * result.metadata().size_bytes),
+            ).add_instruction(
                 instruction=execution_step.LocalLimit(limit),
             )
             yield global_limit_step
@@ -254,7 +260,10 @@ def global_limit(
                         result_to_cancel.cancel()
 
                 yield from (
-                    ExecutionStepBuilder[PartitionT](inputs=[result.partition()]).add_instruction(
+                    ExecutionStepBuilder[PartitionT](
+                        inputs=[result.partition()],
+                        resource_request=ResourceRequest(memory_bytes=result.metadata().size_bytes),
+                    ).add_instruction(
                         instruction=execution_step.LocalLimit(0),
                     )
                     for _ in range(remaining_partitions)
@@ -315,7 +324,10 @@ def coalesce(
             if len(ready_to_coalesce) == num_partitions_to_merge:
                 # Coalesce the partition and emit it.
                 merge_step = ExecutionStepBuilder[PartitionT](
-                    inputs=[_.partition() for _ in ready_to_coalesce]
+                    inputs=[_.partition() for _ in ready_to_coalesce],
+                    resource_request=ResourceRequest(
+                        memory_bytes=2 * sum(_.metadata().size_bytes for _ in ready_to_coalesce),
+                    ),
                 ).add_instruction(
                     instruction=execution_step.ReduceMerge(),
                 )
@@ -371,9 +383,13 @@ def reduce(
 
     # Yield all the reduces in order.
     while len(inputs_to_reduce[0]) > 0:
+        batch = [_.popleft() for _ in inputs_to_reduce]
         yield ExecutionStepBuilder[PartitionT](
-            inputs=[result.partition() for result in (_.popleft() for _ in inputs_to_reduce)],
+            inputs=[result.partition() for result in batch],
             instructions=[reduce_instruction],
+            resource_request=ResourceRequest(
+                memory_bytes=2 * sum(result.metadata().size_bytes for result in batch),
+            ),
         )
 
 
@@ -438,15 +454,20 @@ def sort(
 
     # Create a range fanout plan.
     range_fanout_plan = (
-        ExecutionStepBuilder[PartitionT](inputs=[boundaries_partition, source_partition]).add_instruction(
+        ExecutionStepBuilder[PartitionT](
+            inputs=[boundaries_partition, source_result.partition()],
+            resource_request=ResourceRequest(
+                memory_bytes=2 * source_result.metadata().size_bytes,
+            ),
+        ).add_instruction(
             instruction=execution_step.FanoutRange[PartitionT](
                 num_outputs=sort_info.num_partitions(),
                 sort_by=sort_info._sort_by,
                 descending=sort_info._descending,
             ),
         )
-        for source_partition in (
-            source.result.partition() for source in consume_deque(source_materializations) if source.result is not None
+        for source_result in (
+            source.result for source in consume_deque(source_materializations) if source.result is not None
         )
     )
 

daft/runners/ray_runner.py
@@ -182,12 +182,16 @@ def _get_ray_task_options(resource_request: ResourceRequest) -> dict[str, Any]:
     return options
 
 
-def build_partitions(instruction_stack: list[Instruction], *inputs: vPartition) -> vPartition | list[vPartition]:
+def build_partitions(
+    instruction_stack: list[Instruction], *inputs: vPartition
+) -> list[list[PartitionMetadata] | vPartition]:
     partitions = list(inputs)
     for instruction in instruction_stack:
         partitions = instruction.run(partitions)
 
-    return partitions if len(partitions) > 1 else partitions[0]
+    metadatas = [p.metadata() for p in partitions]
+
+    return [metadatas, *partitions]
 
 
 # Give the same function different names to aid in profiling data distribution.
@@ -196,22 +200,28 @@ def build_partitions(instruction_stack: list[Instruction], *inputs: vPartition) 
 @ray.remote
 def single_partition_pipeline(
     instruction_stack: list[Instruction], *inputs: vPartition
-) -> vPartition | list[vPartition]:
+) -> list[list[PartitionMetadata] | vPartition]:
     return build_partitions(instruction_stack, *inputs)
 
 
 @ray.remote
-def fanout_pipeline(instruction_stack: list[Instruction], *inputs: vPartition) -> vPartition | list[vPartition]:
+def fanout_pipeline(
+    instruction_stack: list[Instruction], *inputs: vPartition
+) -> list[list[PartitionMetadata] | vPartition]:
     return build_partitions(instruction_stack, *inputs)
 
 
 @ray.remote(scheduling_strategy="SPREAD")
-def reduce_pipeline(instruction_stack: list[Instruction], *inputs: vPartition) -> vPartition | list[vPartition]:
+def reduce_pipeline(
+    instruction_stack: list[Instruction], *inputs: vPartition
+) -> list[list[PartitionMetadata] | vPartition]:
     return build_partitions(instruction_stack, *inputs)
 
 
 @ray.remote(scheduling_strategy="SPREAD")
-def reduce_and_fanout(instruction_stack: list[Instruction], *inputs: vPartition) -> vPartition | list[vPartition]:
+def reduce_and_fanout(
+    instruction_stack: list[Instruction], *inputs: vPartition
+) -> list[list[PartitionMetadata] | vPartition]:
     return build_partitions(instruction_stack, *inputs)
 
 
@@ -346,7 +356,7 @@ class SchedulerActor:
 def _build_partitions(task: ExecutionStep[ray.ObjectRef]) -> list[ray.ObjectRef]:
     """Run a ExecutionStep and return the resulting list of partitions."""
     ray_options: dict[str, Any] = {
-        "num_returns": task.num_results,
+        "num_returns": task.num_results + 1,
     }
 
     if task.resource_request is not None:
@@ -360,16 +370,13 @@ def _build_partitions(task: ExecutionStep[ray.ObjectRef]) -> list[ray.ObjectRef]
         )
 
     build_remote = build_remote.options(**ray_options)
-    partitions = build_remote.remote(task.instructions, *task.inputs)
-    # Handle ray bug that ignores list interpretation when num_returns=1
-    if task.num_results == 1:
-        partitions = [partitions]
+    [metadatas_ref, *partitions] = build_remote.remote(task.instructions, *task.inputs)
 
     if isinstance(task, MultiOutputExecutionStep):
-        task.results = [RayMaterializedResult(partition) for partition in partitions]
+        task.results = [RayMaterializedResult(partition, metadatas_ref, i) for i, partition in enumerate(partitions)]
     elif isinstance(task, SingleOutputExecutionStep):
         [partition] = partitions
-        task.result = RayMaterializedResult(partition)
+        task.result = RayMaterializedResult(partition, metadatas_ref, 0)
     else:
         raise TypeError(f"Could not type match input {task}")
 
@@ -456,6 +463,8 @@ class RayRunner(Runner):
 @dataclass(frozen=True)
 class RayMaterializedResult(MaterializedResult[ray.ObjectRef]):
     _partition: ray.ObjectRef
+    _metadatas: ray.ObjectRef | None = None
+    _metadata_index: int | None = None
 
     def partition(self) -> ray.ObjectRef:
         return self._partition
@@ -464,7 +473,10 @@ class RayMaterializedResult(MaterializedResult[ray.ObjectRef]):
         return ray.get(self._partition)
 
     def metadata(self) -> PartitionMetadata:
-        return ray.get(get_meta.remote(self._partition))
+        if self._metadatas is not None and self._metadata_index is not None:
+            return ray.get(self._metadatas)[self._metadata_index]
+        else:
+            return ray.get(get_meta.remote(self._partition))
 
     def cancel(self) -> None:
         return ray.cancel(self._partition)

daft/runners/blocks.py
@@ -6,6 +6,7 @@ import functools
 import math
 import operator
 import random
+import statistics
 from abc import abstractmethod
 from functools import partial
 from typing import (
@@ -30,6 +31,7 @@ except ImportError:
     _POLARS_AVAILABLE = False
 
 import numpy as np
+import pickle5
 import pyarrow as pa
 import pyarrow.compute as pac
 from pandas.core.reshape.merge import get_join_indexers
@@ -102,6 +104,11 @@ class DataBlock(Generic[ArrType]):
     @abstractmethod
     def is_scalar(self) -> bool:
         """Returns True if this DataBlock holds a scalar value"""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def size_bytes(self) -> int:
+        """Returns the total size of this DataBlock in bytes."""
         raise NotImplementedError()
 
     @abstractmethod
@@ -413,6 +420,31 @@ class PyListDataBlock(DataBlock[List[T]]):
                 yield self.data
         yield from self.data
 
+    def size_bytes(self) -> int:
+        if self.is_scalar():
+            return len(pickle5.dumps(self.data))
+
+        if len(self.data) == 0:
+            return 0
+
+        # self.data is a non-empty vector.
+
+        # Sample 10% of the items (but at least 10 items) to determine total size.
+        sample_quantity = len(self.data) // 10
+        if sample_quantity < 10:
+            sample_quantity = min(len(self.data), 10)
+
+        samples = random.sample(self.data, sample_quantity)
+        sample_sizes = [len(pickle5.dumps(sample)) for sample in samples]
+
+        if sample_quantity == len(self.data):
+            return sum(sample_sizes)
+
+        mean, stdev = statistics.mean(sample_sizes), statistics.stdev(sample_sizes)
+        one_item_size_estimate = int(mean + stdev)
+
+        return one_item_size_estimate * len(self.data)
+
     def to_numpy(self):
         if self.is_scalar():
             return self.data
@@ -528,6 +560,9 @@ class ArrowDataBlock(DataBlock[ArrowArrType]):
             return ArrowDataBlock, (self._make_empty().data,)
         else:
             return ArrowDataBlock, (self.data,)
+
+    def size_bytes(self) -> int:
+        return self.__sizeof__() + self.data.__sizeof__()
 
     def to_numpy(self):
         if isinstance(self.data, pa.Scalar):

daft/runners/partitioning.py
@@ -99,6 +99,9 @@ class PyListTile:
     def apply(self, func: Callable[[DataBlock], DataBlock]) -> PyListTile:
         return dataclasses.replace(self, block=func(self.block))
 
+    def size_bytes(self) -> int:
+        return self.block.size_bytes()
+
     def split_by_index(
         self,
         num_partitions: int,
@@ -140,6 +143,7 @@ class PyListTile:
 @dataclass(frozen=True)
 class PartitionMetadata:
     num_rows: int
+    size_bytes: int
 
 
 @dataclass(frozen=True)
@@ -165,7 +169,10 @@ class vPartition:
         return len(next(iter(self.columns.values())))
 
     def metadata(self) -> PartitionMetadata:
-        return PartitionMetadata(num_rows=len(self))
+        return PartitionMetadata(
+            num_rows=len(self),
+            size_bytes=sum(tile.size_bytes() for tile in self.columns.values()),
+        )
 
     def get_schema(self) -> Schema:
         """Generates column expressions that represent the vPartition's schema"""

commit 9d0734c0a1f6692a4cceb902d4ae6268fece379b

Fix 100G regression from #600 (#631)
TPCH 100G benchmark regressed after #600 , especially on Q4. Viewing the
task graph, it seems the issue is that tasks are getting run serially.

The issue is that we now request metadata within the physical plan. So
what can happen is:

1. Runner dispatches a task X.
2. Runner asks physical plan for the next task. The next task depends on
the output of task X.
3. Previously, this would get pipelined, and the runner can continue
dispatching tasks. But now, physical plan asks for the metadata of the
previous task, and thus blocks on X completing. This prevents new tasks
from being dispatched while X is completing; in the worst case, if X is
the only task executing, parallelism is completely starved.

The solution in this PR is to do batch dispatch. Batch dispatch is
already implemented, it just had to be turned on in default.

Also, this PR makes the memory requests more conservative (just 1x,
which is the amount the task is guaranteed to use).

daft/execution/physical_plan.py
@@ -78,9 +78,9 @@ def file_read(
                         logplan=scan_info,
                         index=i,
                     ),
-                    # We set the memory request to be 5x filesize for now.
-                    # This might still not be enough depending on the reader, but it's better than 1x.
-                    resource_request=ResourceRequest(num_cpus=0.5, memory_bytes=file_sizes_bytes[i] * 5),
+                    # Set the filesize as the memory request.
+                    # (Note: this is very conservative; file readers empirically use much more peak memory than 1x file size.)
+                    resource_request=ResourceRequest(memory_bytes=file_sizes_bytes[i]),
                 )
                 yield file_read_step
                 output_partition_index += 1
@@ -157,7 +157,7 @@ def join(
             join_step = ExecutionStepBuilder[PartitionT](
                 inputs=[next_left.result.partition(), next_right.result.partition()],
                 resource_request=ResourceRequest(
-                    memory_bytes=2 * (next_left.result.metadata().size_bytes + next_right.result.metadata().size_bytes)
+                    memory_bytes=(next_left.result.metadata().size_bytes + next_right.result.metadata().size_bytes)
                 ),
             ).add_instruction(instruction=execution_step.Join(join))
             yield join_step
@@ -242,7 +242,7 @@ def global_limit(
 
             global_limit_step = ExecutionStepBuilder[PartitionT](
                 inputs=[result.partition()],
-                resource_request=ResourceRequest(memory_bytes=2 * result.metadata().size_bytes),
+                resource_request=ResourceRequest(memory_bytes=result.metadata().size_bytes),
             ).add_instruction(
                 instruction=execution_step.LocalLimit(limit),
             )
@@ -328,7 +328,7 @@ def coalesce(
                 merge_step = ExecutionStepBuilder[PartitionT](
                     inputs=[_.partition() for _ in ready_to_coalesce],
                     resource_request=ResourceRequest(
-                        memory_bytes=2 * sum(_.metadata().size_bytes for _ in ready_to_coalesce),
+                        memory_bytes=sum(_.metadata().size_bytes for _ in ready_to_coalesce),
                     ),
                 ).add_instruction(
                     instruction=execution_step.ReduceMerge(),
@@ -390,7 +390,7 @@ def reduce(
             inputs=[result.partition() for result in batch],
             instructions=[reduce_instruction],
             resource_request=ResourceRequest(
-                memory_bytes=2 * sum(result.metadata().size_bytes for result in batch),
+                memory_bytes=sum(result.metadata().size_bytes for result in batch),
             ),
         )
 
@@ -459,7 +459,7 @@ def sort(
         ExecutionStepBuilder[PartitionT](
             inputs=[boundaries_partition, source_result.partition()],
             resource_request=ResourceRequest(
-                memory_bytes=2 * source_result.metadata().size_bytes,
+                memory_bytes=source_result.metadata().size_bytes,
             ),
         ).add_instruction(
             instruction=execution_step.FanoutRange[PartitionT](

daft/runners/ray_runner.py
@@ -265,7 +265,7 @@ class SchedulerActor:
         # Theoretically, this should be 1.0: we should batch enough tasks for all the cores in a single dispatch;
         # otherwise, we begin dispatching downstream dependencies (that are not immediately executable)
         # before saturating all the cores (and their pipelines).
-        self.batch_dispatch_coeff = batch_dispatch_coeff if batch_dispatch_coeff is not None else 0.0
+        self.batch_dispatch_coeff = batch_dispatch_coeff if batch_dispatch_coeff is not None else 1.0
 
     def remote_run_plan(
         self,