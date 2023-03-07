bd5fb763ac7cb8189a903dd1a03a8fb591e38a95
Jay Chia <jaychia94@gmail.com>
2023-03-06 17:48:25 -0800

    Add `table_io.py` utility module for reading and writing of tables (#663)

---
+++ daft/table_io.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import pathlib
+
+from daft.expressions2 import ExpressionsProjection
+from daft.runners.partitioning import (
+    vPartitionParseCSVOptions,
+    vPartitionReadOptions,
+    vPartitionSchemaInferenceOptions,
+)
+from daft.table import Table
+
+
+def read_json(
+    path: str,
+    read_options: vPartitionReadOptions = vPartitionReadOptions(),
+) -> Table:
+    raise NotImplementedError("[RUST-INT] Implement tableio")
+
+
+def read_parquet(
+    path: str,
+    read_options: vPartitionReadOptions = vPartitionReadOptions(),
+) -> Table:
+    raise NotImplementedError("[RUST-INT] Implement tableio")
+
+
+def read_csv(
+    path: str,
+    csv_options: vPartitionParseCSVOptions = vPartitionParseCSVOptions(),
+    schema_options: vPartitionSchemaInferenceOptions = vPartitionSchemaInferenceOptions(),
+    read_options: vPartitionReadOptions = vPartitionReadOptions(),
+) -> Table:
+    raise NotImplementedError("[RUST-INT] Implement tableio")
+
+
+def write_csv(
+    table: Table,
+    root_path: str | pathlib.Path,
+    partition_cols: ExpressionsProjection | None = None,
+    compression: str | None = None,
+) -> list[str]:
+    raise NotImplementedError("[RUST-INT] Implement tableio")
+
+
+def write_parquet(
+    table: Table,
+    root_path: str | pathlib.Path,
+    partition_cols: ExpressionsProjection | None = None,
+    compression: str | None = None,
+) -> list[str]:
+    raise NotImplementedError("[RUST-INT] Implement tableio")
