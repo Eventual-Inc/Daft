227553b08234971a57a01e3bc835dd4e4a06d241
Xiayue Charles Lin <x@charlesl.in>
2023-03-09 17:02:39 -0800

    Implement count in series - need to move to dataarray though

--- src/series/ops/sum.rs
+++ src/series/ops/sum.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::{
     error::{DaftError, DaftResult},
     series::Series,
@@ -7,6 +9,22 @@ use crate::array::BaseArray;
 use crate::datatypes::*;
 
 impl Series {
+    pub fn count(&self) -> DaftResult<Series> {
+        // let data_type = self.data_type();
+        let data_array = self.array();
+        let arrow_array = data_array.data();
+        let count = match arrow_array.validity() {
+            None => arrow_array.len(),
+            Some(bitmap) => arrow_array.len() - bitmap.unset_bits(),
+        };
+        let arrow_array = arrow2::array::PrimitiveArray::from([Some(count as u64)]);
+        Ok(DataArray::<UInt64Type>::new(
+            Arc::new(data_array.field().clone()),
+            Arc::new(arrow_array),
+        )?
+        .into_series())
+    }
+
     pub fn sum(&self) -> DaftResult<Series> {
         use crate::array::ops::DaftNumericAgg;
         use crate::datatypes::DataType::*;
