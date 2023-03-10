29217db82de24982eac1374a3933b8e5bae2487a
Sammy Sidhu <samster25@users.noreply.github.com>
2023-03-09 19:14:57 -0800

    split by index and split by hash for partition

--- src/table/ops/hash.rs
+++ src/table/ops/hash.rs
@@ -1,16 +1,13 @@
 use crate::{
     datatypes::UInt64Array,
     error::{DaftError, DaftResult},
-    series::Series,
     table::Table,
 };
 
 impl Table {
     pub fn hash_rows(&self) -> DaftResult<UInt64Array> {
         if self.num_columns() == 0 {
-            return Err(DaftError::ValueError(format!(
-                "Attempting to Hash Table with no columns"
-            )));
+            return Err(DaftError::ValueError("Attempting to Hash Table with no columns".to_string()));
         }
         let mut hash_so_far = self.columns.first().unwrap().hash(None)?;
         for c in self.columns.iter().skip(1) {
