cc49ea634789d213429bcb4046d764aef67228b1
Jay Chia <jaychia94@gmail.com>
2023-03-13 17:22:52 -0700

    Simplify PyErr conversion logic (https://github.com/Eventual-Inc/Daft/pull/690\#discussion_r1134731985)

--- src/python/error.rs
+++ src/python/error.rs
@@ -5,8 +5,9 @@ use crate::error::DaftError;
 impl std::convert::From<DaftError> for PyErr {
     fn from(err: DaftError) -> PyErr {
         match err {
-            DaftError::ExprResolveTypeError { .. } => PyTypeError::new_err(err.to_string()),
-            DaftError::TypeError { .. } => PyTypeError::new_err(err.to_string()),
+            DaftError::ExprResolveTypeError { .. } | DaftError::TypeError { .. } => {
+                PyTypeError::new_err(err.to_string())
+            }
             _ => PyValueError::new_err(err.to_string()),
         }
     }
