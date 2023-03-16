17734d9b16fa21e5277d268fae4454cca388a096
Jay Chia <jaychia94@gmail.com>
2023-03-16 16:30:06 -0700

    Return BooleanArray directly in match

--- src/array/ops/utf8.rs
+++ src/array/ops/utf8.rs
@@ -1,6 +1,6 @@
 use crate::array::BaseArray;
 use crate::datatypes::{BooleanArray, Utf8Array};
-use arrow2::array as arrow2_array;
+use arrow2;
 
 use crate::error::{DaftError, DaftResult};
 
@@ -8,47 +8,49 @@ impl Utf8Array {
     pub fn endswith(&self, pattern: &Utf8Array) -> DaftResult<BooleanArray> {
         let data_arrow = self.downcast();
         let pattern_arrow = pattern.downcast();
-        let result_arrow = match (self.len(), pattern.len()) {
+        match (self.len(), pattern.len()) {
             // Broadcast pattern case:
             (data_len, 1) => match pattern_arrow.validity() {
-                Some(validity) if !validity.get_bit(0) => Ok(arrow2_array::BooleanArray::new_null(
-                    arrow2::datatypes::DataType::Boolean,
-                    data_len,
-                )),
+                Some(validity) if !validity.get_bit(0) => {
+                    Ok(BooleanArray::full_null(self.name(), data_len))
+                }
                 _ => {
                     let pattern_val = pattern_arrow.value(0);
-                    Ok(data_arrow
+                    let arrow_result: arrow2::array::BooleanArray = data_arrow
                         .into_iter()
                         .map(|val| Some(val?.ends_with(pattern_val)))
-                        .collect())
+                        .collect();
+                    Ok(BooleanArray::from((self.name(), arrow_result)))
                 }
             },
             // Broadcast data case
             (1, pattern_len) => match data_arrow.validity() {
-                Some(validity) if !validity.get_bit(0) => Ok(arrow2_array::BooleanArray::new_null(
-                    arrow2::datatypes::DataType::Boolean,
-                    pattern_len,
-                )),
+                Some(validity) if !validity.get_bit(0) => {
+                    Ok(BooleanArray::full_null(self.name(), pattern_len))
+                }
                 _ => {
                     let data_val = data_arrow.value(0);
-                    Ok(pattern_arrow
+                    let arrow_result: arrow2::array::BooleanArray = pattern_arrow
                         .into_iter()
                         .map(|pat| Some(data_val.ends_with(pat?)))
-                        .collect())
+                        .collect();
+                    Ok(BooleanArray::from((self.name(), arrow_result)))
                 }
             },
             // Matching len case:
-            (data_len, pattern_len) if data_len == pattern_len => Ok(data_arrow
-                .into_iter()
-                .zip(pattern_arrow.into_iter())
-                .map(|(val, pat)| Some(val?.ends_with(pat?)))
-                .collect()),
+            (data_len, pattern_len) if data_len == pattern_len => {
+                let arrow_result: arrow2::array::BooleanArray = data_arrow
+                    .into_iter()
+                    .zip(pattern_arrow.into_iter())
+                    .map(|(val, pat)| Some(val?.ends_with(pat?)))
+                    .collect();
+                Ok(BooleanArray::from((self.name(), arrow_result)))
+            }
             // Mismatched len case:
             (data_len, pattern_len) => Err(DaftError::ComputeError(format!(
                 "lhs and rhs have different length arrays: {data_len} vs {pattern_len}"
             ))),
-        };
-        Ok((self.name(), result_arrow?).into())
+        }
     }
 }
 
@@ -60,7 +62,7 @@ mod tests {
     fn check_endswith_utf_arrays_broadcast() -> DaftResult<()> {
         let data = Utf8Array::from((
             "data",
-            Box::new(arrow2_array::Utf8Array::<i64>::from(vec![
+            Box::new(arrow2::array::Utf8Array::<i64>::from(vec![
                 "x_foo".into(),
                 "y_foo".into(),
                 "z_bar".into(),
@@ -68,7 +70,7 @@ mod tests {
         ));
         let pattern = Utf8Array::from((
             "pattern",
-            Box::new(arrow2_array::Utf8Array::<i64>::from(vec!["foo".into()])),
+            Box::new(arrow2::array::Utf8Array::<i64>::from(vec!["foo".into()])),
         ));
         let result = &data.endswith(&pattern)?;
         assert_eq!(result.len(), 3);
@@ -82,7 +84,7 @@ mod tests {
     fn check_endswith_utf_arrays() -> DaftResult<()> {
         let data = Utf8Array::from((
             "data",
-            Box::new(arrow2_array::Utf8Array::<i64>::from(vec![
+            Box::new(arrow2::array::Utf8Array::<i64>::from(vec![
                 "x_foo".into(),
                 "y_foo".into(),
                 "z_bar".into(),
@@ -90,7 +92,7 @@ mod tests {
         ));
         let pattern = Utf8Array::from((
             "pattern",
-            Box::new(arrow2_array::Utf8Array::<i64>::from(vec![
+            Box::new(arrow2::array::Utf8Array::<i64>::from(vec![
                 "foo".into(),
                 "wrong".into(),
                 "bar".into(),
