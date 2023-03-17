9297d6129fc8f17c3cc72ba56b50ce3e2f93a58e
Jay Chia <jaychia94@gmail.com>
2023-03-16 17:53:56 -0700

    Issue #707: Fix a join run_partial_metadata logic (#708)
    Closes: #707

--- tests/dataframe/test_joins.py
+++ tests/dataframe/test_joins.py
@@ -24,3 +24,17 @@ def test_multicol_joins(n_partitions: int):
         "C": [True, False, True],
         "right.C": [True, False, True],
     }
+
+
+@pytest.mark.parametrize("n_partitions", [1, 2, 4])
+def test_limit_after_join(n_partitions: int):
+    data = {
+        "A": [1, 2, 3],
+    }
+    df1 = daft.DataFrame.from_pydict(data).repartition(n_partitions, "A")
+    df2 = daft.DataFrame.from_pydict(data).repartition(n_partitions, "A")
+
+    joined = df1.join(df2, on="A").limit(1)
+    joined_data = joined.to_pydict()
+    assert "A" in joined_data
+    assert len(joined_data["A"]) == 1
