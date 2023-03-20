4ce5b5a31ffc1cadeccac193df0b96bcb7d66ff3
Jay Chia <jaychia94@gmail.com>
2023-03-20 10:52:00 -0700

    Fix df.show() always showing at most 10 rows

--- tests/dataframe/test_show.py
+++ tests/dataframe/test_show.py
@@ -3,13 +3,14 @@ from __future__ import annotations
 from daft import DataFrame
 
 
-def test_show_all(valid_data):
+def test_show_default(valid_data):
     df = DataFrame.from_pylist(valid_data)
     df_display = df.show()
 
     assert df_display.schema == df.schema()
     assert len(df_display.preview.preview_partition) == len(valid_data)
     assert df_display.preview.dataframe_num_rows is None
+    assert df_display.num_rows == 8
 
 
 def test_show_some(valid_data):
@@ -19,3 +20,4 @@ def test_show_some(valid_data):
     assert df_display.schema == df.schema()
     assert len(df_display.preview.preview_partition) == 1
     assert df_display.preview.dataframe_num_rows is None
+    assert df_display.num_rows == 1
