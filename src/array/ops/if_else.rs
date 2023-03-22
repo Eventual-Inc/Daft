485aef865c22c032a66f77ac67c2d72426bd9b24
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-03-22 16:05:23 -0700

    use impl<T> DataArray<T> instead of impl BooleanArray

--- src/array/ops/if_else.rs
+++ src/array/ops/if_else.rs
@@ -4,13 +4,12 @@ use arrow2::compute::if_then_else::if_then_else;
 
 use crate::array::BaseArray;
 
-impl BooleanArray {
-    pub fn if_else<T: DaftDataType + 'static>(
-        &self,
-        if_true: &DataArray<T>,
-        if_false: &DataArray<T>,
-    ) -> DaftResult<DataArray<T>> {
-        let result = if_then_else(self.downcast(), if_true.data(), if_false.data())?;
+impl<T> DataArray<T>
+where
+    T: DaftDataType + 'static,
+{
+    pub fn if_else(self, other: DataArray<T>, predicate: BooleanArray) -> DaftResult<DataArray<T>> {
+        let result = if_then_else(predicate.downcast(), self.data(), other.data())?;
         DataArray::try_from((self.name(), result))
     }
 }
