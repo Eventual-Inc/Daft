d1ab8a900668b9d54de2dabc58a79f0822393717
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-03-22 15:22:54 -0700

    hook up to existing impl Not for BooleanArray

--- src/series/ops/not.rs
+++ src/series/ops/not.rs
@@ -1,5 +1,6 @@
 use std::ops::Not;
 
+use crate::array::BaseArray;
 use crate::datatypes::BooleanType;
 use crate::error::DaftResult;
 use crate::series::Series;
@@ -7,9 +8,8 @@ use crate::series::Series;
 impl Not for &Series {
     type Output = DaftResult<Series>;
     fn not(self) -> Self::Output {
-        let _array = self.downcast::<BooleanType>()?;
-        todo!()
-        // Ok(array.not()?.into_series())
+        let array = self.downcast::<BooleanType>()?;
+        Ok((!array)?.into_series())
     }
 }
 
