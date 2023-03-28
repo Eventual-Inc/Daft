27cb303e4b74e8c7ba4614604253b2a783496fe6
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-03-27 23:42:46 -0500

    [Rust] If/Else kernel implementation for rest of physical types (#748)
    * Previously, If/Else only worked for numeric and string types. Our
    tests and schema resolution code had blocks to skip types that were not
    supported
    * This PR implements kernels for the other physical types and enables
    tests
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

--- src/series/ops/take.rs
+++ src/series/ops/take.rs
@@ -1,7 +1,7 @@
 use crate::array::BaseArray;
 use crate::{
     error::DaftResult, series::Series, with_match_comparable_daft_types,
-    with_match_integer_daft_types,
+    with_match_integer_daft_types, with_match_physical_daft_types,
 };
 
 impl Series {
@@ -23,7 +23,7 @@ impl Series {
     pub fn slice(&self, start: usize, end: usize) -> DaftResult<Series> {
         let s = self.as_physical()?;
 
-        let result = with_match_comparable_daft_types!(s.data_type(), |$T| {
+        let result = with_match_physical_daft_types!(s.data_type(), |$T| {
             s.downcast::<$T>()?.slice(start, end)?.into_series()
         });
         if result.data_type() != self.data_type() {
@@ -34,7 +34,7 @@ impl Series {
 
     pub fn take(&self, idx: &Series) -> DaftResult<Series> {
         let s = self.as_physical()?;
-        let result = with_match_comparable_daft_types!(s.data_type(), |$T| {
+        let result = with_match_physical_daft_types!(s.data_type(), |$T| {
             with_match_integer_daft_types!(idx.data_type(), |$S| {
                 s.downcast::<$T>()?.take(idx.downcast::<$S>()?)?.into_series()
             })
@@ -47,7 +47,7 @@ impl Series {
 
     pub fn str_value(&self, idx: usize) -> DaftResult<String> {
         let s = self.as_physical()?;
-        with_match_comparable_daft_types!(s.data_type(), |$T| {
+        with_match_physical_daft_types!(s.data_type(), |$T| {
             s.downcast::<$T>()?.str_value(idx)
         })
     }
