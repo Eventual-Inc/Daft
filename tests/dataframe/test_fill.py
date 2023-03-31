cc859eba1be845b02039788abb0c7eff3a3d5b6e
FelixKleineBoesing <felix.boesing@t-online.de>
2023-03-31 22:57:58 +0200

    update tests

--- tests/dataframe/test_fill.py
+++ tests/dataframe/test_fill.py
@@ -7,25 +7,16 @@ from daft import DataFrame
 
 def test_fill_nan_data_frame(missing_value_data: list[dict[str, Any]]) -> None:
     df = DataFrame.from_pylist(missing_value_data)
-    filled_df = df[["petal_length", "sepal_width"]].fillnan(3.1)
-    pd_df = filled_df.to_pandas()
+    df = df.with_column("sepal_width", df["sepal_width"].fillnan(3.1))
+    df = df.with_column("sepal_width_none", df["sepal_width"].fillnan(None))
+    pd_df = df.to_pandas()
     assert pd_df.loc[1, "sepal_width"] == 3.1
-
-
-def test_fill_nan_expression(missing_value_data: list[dict[str, Any]]) -> None:
-    df = DataFrame.from_pylist(missing_value_data)
-    pd_df = df.with_column("sepal_width", df["sepal_width"].fillnan(3.1)).to_pandas()
-    assert pd_df.loc[1, "sepal_width"] == 3.1
+    assert pd_df.loc[1, "sepal_width_none"] is None
 
 
 def test_fill_null_data_frame(missing_value_data: list[dict[str, Any]]) -> None:
     df = DataFrame.from_pylist(missing_value_data)
-    filled_df = df[["petal_length", "sepal_length"]].fillnull(3.1)
-    pd_df = filled_df.to_pandas()
-    assert pd_df.loc[0, "sepal_length"] == 3.1
-
-
-def test_fill_null_expression(missing_value_data: list[dict[str, Any]]) -> None:
-    df = DataFrame.from_pylist(missing_value_data)
-    pd_df = df.with_column("sepal_length", df["sepal_length"].fillnull(3.1)).to_pandas()
+    df = df.with_column("sepal_length", df["sepal_length"].fillnull(3.1))
+    df = df.with_column("petal_length", df["petal_length"].fillnull(3.1))
+    pd_df = df.to_pandas()
     assert pd_df.loc[0, "sepal_length"] == 3.1
