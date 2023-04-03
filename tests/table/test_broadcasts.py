1bafe9830dcd858b0536f2612fc2e3918bc63b71
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-04-02 17:10:16 -0700

    Add test case for broadcasting all physical types

---
+++ tests/table/test_broadcasts.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import pytest
+
+from daft.expressions import col, lit
+from daft.table import Table
+
+
+@pytest.mark.parametrize("data", [1, "a", True, b"Y", 0.5, None])
+def test_broadcast(data):
+    table = Table.from_pydict({"x": [1, 2, 3]})
+    new_table = table.eval_expression_list([col("x"), lit(data)])
+    assert new_table.to_pydict() == {"x": [1, 2, 3], "literal": [data for _ in range(3)]}
