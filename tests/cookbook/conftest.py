422c0ed2bf11bf81c3933e666226d61745d08e31
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-04-03 21:35:10 -0700

    [RUST] Enable cookbook tests (#763)
    * Migrates our cookbook tests from `legacy_tests/` to `tests/`
    * Simplifies repartitioning logic for test readability
    * Added some fixes to typing for `.count()`
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

---
+++ tests/cookbook/conftest.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import List, Tuple
+
+import pandas as pd
+import pytest
+
+from daft.dataframe import DataFrame
+from daft.expressions import col
+from tests.cookbook.assets import COOKBOOK_DATA_CSV
+
+COLUMNS = ["Unique Key", "Complaint Type", "Borough", "Created Date", "Descriptor"]
+CsvPathAndColumns = Tuple[str, List[str]]
+
+
+@pytest.fixture(scope="function")
+def daft_df():
+    return DataFrame.read_csv(COOKBOOK_DATA_CSV).select(*[col(c) for c in COLUMNS])
+
+
+@pytest.fixture(scope="function")
+def service_requests_csv_pd_df():
+    return pd.read_csv(COOKBOOK_DATA_CSV, keep_default_na=False)[COLUMNS]
+
+
+@pytest.fixture(scope="module", params=[1, 2])
+def repartition_nparts(request):
+    """Adds a `n_repartitions` parameter to test cases which provides the number of
+    partitions that the test case should repartition its dataset into for testing
+    """
+    return request.param
--- tests/cookbook/test_computations.py
+++ tests/cookbook/test_computations.py
@@ -2,12 +2,8 @@ from __future__ import annotations
 
 from daft.expressions import col
 from tests.conftest import assert_df_equals
-from tests.dataframe_cookbook.conftest import (
-    parametrize_service_requests_csv_repartition,
-)
 
 
-@parametrize_service_requests_csv_repartition
 def test_add_one_to_column(daft_df, service_requests_csv_pd_df, repartition_nparts):
     """Creating a new column that is derived from (1 + other_column) and retrieving the top N results"""
     daft_df = daft_df.repartition(repartition_nparts).with_column("unique_key_mod", col("Unique Key") + 1)
@@ -16,7 +12,6 @@ def test_add_one_to_column(daft_df, service_requests_csv_pd_df, repartition_npar
     assert_df_equals(daft_pd_df, service_requests_csv_pd_df)
 
 
-@parametrize_service_requests_csv_repartition
 def test_add_one_to_column_name_override(daft_df, service_requests_csv_pd_df, repartition_nparts):
     """Creating a new column that is derived from (1 + other_column) and retrieving the top N results"""
     daft_df = (
@@ -38,7 +33,6 @@ def test_add_one_to_column_limit(daft_df, service_requests_csv_pd_df):
     assert_df_equals(daft_pd_df, service_requests_csv_pd_df)
 
 
-@parametrize_service_requests_csv_repartition
 def test_add_one_twice_to_column(daft_df, service_requests_csv_pd_df, repartition_nparts):
     """Creating a new column that is derived from (1 + other_column) and retrieving the top N results"""
     daft_df = daft_df.repartition(repartition_nparts).with_column("unique_key_mod", col("Unique Key") + 1)
@@ -49,7 +43,6 @@ def test_add_one_twice_to_column(daft_df, service_requests_csv_pd_df, repartitio
     assert_df_equals(daft_pd_df, service_requests_csv_pd_df)
 
 
-@parametrize_service_requests_csv_repartition
 def test_difference_cols(daft_df, service_requests_csv_pd_df, repartition_nparts):
     """Creating a new column that is derived from 2 other columns and retrieving the top N results"""
     daft_df = daft_df.repartition(repartition_nparts).with_column(
