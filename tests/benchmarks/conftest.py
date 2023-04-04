bbb6a0a1ba8883d85d4398edc74e45ee007dc403
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-04-04 16:39:15 -0700

    Add benchmarks conftest for formatting

---
+++ tests/benchmarks/conftest.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import _pytest
+
+
+# Monkeypatch to use comma delimiters when showing parameter lists.
+# https://github.com/pytest-dev/pytest/blob/31d0b51039fc295dfb14bfc5d2baddebe11bb746/src/_pytest/python.py#L1190
+# Related: https://github.com/pytest-dev/pytest/issues/3617
+def id(self):
+    return ",".join(self._idlist)
+
+
+setattr(_pytest.python.CallSpec2, "id", property(id))
+
+
+def pytest_make_parametrize_id(config, val, argname):
+    return f"{argname}={val}"
