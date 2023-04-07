2957ea59f21aa5c5ebd97b31e06681b4b6324d0a
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-04-07 16:14:38 -0700

    remove all tests from test_partitioning.py

--- tests/benchmarks/test_partitioning.py
+++ tests/benchmarks/test_partitioning.py
@@ -1,23 +1,18 @@
 from __future__ import annotations
 
-import numpy as np
-import pytest
-
-from daft import DataFrame
-
 NUM_SAMPLES = 10_000_000
 NUM_PARTITIONS = 100
 
 
-@pytest.mark.benchmark(group="partitioning")
-@pytest.mark.parametrize("end_partitions", [NUM_PARTITIONS])
-@pytest.mark.parametrize("num_samples", [NUM_SAMPLES])
-def test_split(benchmark, num_samples, end_partitions) -> None:
-    """Test performance of splitting into multiple partitions."""
-    df = DataFrame.from_pydict({"mycol": np.arange(num_samples)}).collect()
+# @pytest.mark.benchmark(group="partitioning")
+# @pytest.mark.parametrize("end_partitions", [NUM_PARTITIONS])
+# @pytest.mark.parametrize("num_samples", [NUM_SAMPLES])
+# def test_split(benchmark, num_samples, end_partitions) -> None:
+#     """Test performance of splitting into multiple partitions."""
+#     df = DataFrame.from_pydict({"mycol": np.arange(num_samples)}).collect()
 
-    # Run the benchmark.
-    def bench() -> DataFrame:
-        return df.into_partitions(end_partitions).collect()
+#     # Run the benchmark.
+#     def bench() -> DataFrame:
+#         return df.into_partitions(end_partitions).collect()
 
-    benchmark(bench)
+#     benchmark(bench)
