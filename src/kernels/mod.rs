0d3a21cc49f3cf8a3637ba156308cbc9620da1d3
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-04-11 15:11:10 -0700

    [RUST] Python feature flag (#795)
    Hides our Python functionality behind a `python` Cargo feature flag
    
    1. All PyO3-related functionality should be localized to `src/python/**`
    2. Deleted Python-related functionality in `src/kernels` which was dead
    code that we don't call anymore
    3. Added compilation feature flags to avoid compiling Python-related
    modules where required
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

--- src/kernels/mod.rs
+++ src/kernels/mod.rs
@@ -1,21 +1,3 @@
 pub mod hashing;
-pub mod utf8;
-
 pub mod search_sorted;
-
-use pyo3::prelude::*;
-
-pub fn register_kernels(py: Python, parent: &PyModule) -> PyResult<()> {
-    let kernels_mod = PyModule::new(py, "kernels")?;
-    kernels_mod.add_function(wrap_pyfunction!(hashing::hash_pyarrow_array, kernels_mod)?)?;
-    kernels_mod.add_function(wrap_pyfunction!(
-        search_sorted::search_sorted_pyarrow_array,
-        kernels_mod
-    )?)?;
-    kernels_mod.add_function(wrap_pyfunction!(
-        search_sorted::search_sorted_multiple_pyarrow_array,
-        kernels_mod
-    )?)?;
-    parent.add_submodule(kernels_mod)?;
-    Ok(())
-}
+pub mod utf8;
