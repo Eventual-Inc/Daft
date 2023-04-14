1ba5c8ab81d52ac2a1270b557eacbf222aea8db8
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-04-13 20:20:24 -0700

    [RUST] UDF Serialization (#804)
    * Enables tests for URL UDFs
    * Adds code for serialization in Rust which delegates to Python pickling
    * Adds a `daft.pickle` module which simply delegates to Python `pickle`
    for now. We can define any custom behaviors we want for pickling UDFs in
    those functions in the future if necessary.
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

--- src/dsl/functions/python/mod.rs
+++ src/dsl/functions/python/mod.rs
@@ -1,59 +1,22 @@
+mod partial_udf;
 mod udf;
 
 use crate::{datatypes::DataType, error::DaftResult};
-use pyo3::{PyObject, Python};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{Deserialize, Serialize};
 
 use crate::dsl::Expr;
 
-// A curried Python function that takes as input a list of Series objects for execution
-#[derive(Debug, Clone)]
-pub struct PartialUDF(PyObject);
-
-impl Serialize for PartialUDF {
-    fn serialize<S>(&self, _serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        Python::with_gil(|_py| {
-            // TODO: Call pickler
-            todo!();
-        })
-    }
-}
-
-impl<'de> Deserialize<'de> for PartialUDF {
-    fn deserialize<D>(_deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        Python::with_gil(|_py| {
-            // TODO: Call depickling
-            todo!();
-        })
-    }
-}
-
-impl<Rhs> PartialEq<Rhs> for PartialUDF {
-    fn eq(&self, _other: &Rhs) -> bool {
-        Python::with_gil(|_py| {
-            // TODO: Call __eq__
-            todo!();
-        })
-    }
-}
-
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct PythonUDF {
-    func: PartialUDF,
+    func: partial_udf::PartialUDF,
     num_expressions: usize,
     return_dtype: DataType,
 }
 
-pub fn udf(func: PyObject, expressions: &[Expr], return_dtype: DataType) -> DaftResult<Expr> {
+pub fn udf(func: pyo3::PyObject, expressions: &[Expr], return_dtype: DataType) -> DaftResult<Expr> {
     Ok(Expr::Function {
         func: super::FunctionExpr::Python(PythonUDF {
-            func: PartialUDF(func),
+            func: partial_udf::PartialUDF(func),
             num_expressions: expressions.len(),
             return_dtype,
         }),
