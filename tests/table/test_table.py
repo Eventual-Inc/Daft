15b4800e7f2ab065026a4e0bda4f33309c76513e
xcharleslin <4212216+xcharleslin@users.noreply.github.com>
2023-04-13 21:49:26 -0700

    Implement concat for PythonArray (#807)
    - Upgrade concat kernels to typematch all types instead of just Arrow
    types.
    - Implement concat separately for VecBackedArray (instead of going
    through arrow2::compute's concat).
    
    ---------
    
    Co-authored-by: Xiayue Charles Lin <charles@eventualcomputing.com>

--- tests/table/test_table.py
+++ tests/table/test_table.py
@@ -1042,13 +1042,15 @@ def test_table_abs_bad_input() -> None:
 
 
 def test_table_concat() -> None:
+    objs1 = [object(), object(), object()]
+    objs2 = [object(), object(), object()]
     tables = [
-        Table.from_pydict({"x": [1, 2, 3], "y": ["a", "b", "c"]}),
-        Table.from_pydict({"x": [4, 5, 6], "y": ["d", "e", "f"]}),
+        Table.from_pydict({"x": [1, 2, 3], "y": ["a", "b", "c"], "z": objs1}),
+        Table.from_pydict({"x": [4, 5, 6], "y": ["d", "e", "f"], "z": objs2}),
     ]
 
     result = Table.concat(tables)
-    assert result.to_pydict() == {"x": [1, 2, 3, 4, 5, 6], "y": ["a", "b", "c", "d", "e", "f"]}
+    assert result.to_pydict() == {"x": [1, 2, 3, 4, 5, 6], "y": ["a", "b", "c", "d", "e", "f"], "z": objs1 + objs2}
 
     tables = [
         Table.from_pydict({"x": [], "y": []}),
@@ -1080,6 +1082,14 @@ def test_table_concat_schema_mismatch() -> None:
     mix_types_table = [
         Table.from_pydict({"x": [1, 2, 3]}),
         Table.from_pydict({"x": [1.0, 2.0, 3.0]}),
+    ]
+
+    with pytest.raises(ValueError, match="Table concat requires all schemas to match"):
+        Table.concat(mix_types_table)
+
+    mix_types_table = [
+        Table.from_pydict({"x": [1, 2, 3]}),
+        Table.from_pydict({"x": [object(), object(), object()]}),
     ]
 
     with pytest.raises(ValueError, match="Table concat requires all schemas to match"):
