e7bc994d030c42bfdc25bba5fab46aa2c968120c
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-04-17 18:03:52 -0700

    [RUST] Implement Expression.apply using UDFs (#813)
    * Implements `Expression.apply` which will cast each element in the
    column to Python and run a per-row UDF
    * This is intended to be a convenience method on top of UDFs
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

---
+++ tests/expressions/test_apply.py
@@ -0,0 +1,54 @@
+# def test_apply_lambda
+# def test_apply_module_func
+# def test_apply_inline_func
+# def test_apply_lambda_pyobj
+
+from __future__ import annotations
+
+import dataclasses
+
+import daft
+from daft import DataType
+
+
+def add_1(x):
+    return x + 1
+
+
+def test_apply_module_func():
+    df = daft.DataFrame.from_pydict({"a": [1, 2, 3]})
+    df = df.with_column("a_plus_1", df["a"].apply(add_1, return_dtype=DataType.int32()))
+    assert df.to_pydict() == {"a": [1, 2, 3], "a_plus_1": [2, 3, 4]}
+
+
+def test_apply_lambda():
+    df = daft.DataFrame.from_pydict({"a": [1, 2, 3]})
+    df = df.with_column("a_plus_1", df["a"].apply(lambda x: x + 1, return_dtype=DataType.int32()))
+    assert df.to_pydict() == {"a": [1, 2, 3], "a_plus_1": [2, 3, 4]}
+
+
+def test_apply_inline_func():
+    def inline_add_1(x):
+        return x + 1
+
+    df = daft.DataFrame.from_pydict({"a": [1, 2, 3]})
+    df = df.with_column("a_plus_1", df["a"].apply(inline_add_1, return_dtype=DataType.int32()))
+    assert df.to_pydict() == {"a": [1, 2, 3], "a_plus_1": [2, 3, 4]}
+
+
+@dataclasses.dataclass
+class MyObj:
+    x: int
+
+
+def test_apply_obj():
+    df = daft.DataFrame.from_pydict({"obj": [MyObj(x=0), MyObj(x=0), MyObj(x=0)]})
+
+    def inline_mutate_obj(obj):
+        obj.x = 1
+        return obj
+
+    df = df.with_column("mut_obj", df["obj"].apply(inline_mutate_obj, return_dtype=DataType.python()))
+    result = df.to_pydict()
+    for mut_obj in result["mut_obj"]:
+        assert mut_obj.x == 1
