105b62fd8bdfeb3dece5ca45132237b81068600b
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-04-18 21:43:43 -0700

    add unit tests

---
+++ tests/table/test_explodes.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import pyarrow as pa
+import pytest
+
+from daft.expressions import col
+from daft.series import Series
+from daft.table import Table
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        Series.from_arrow(pa.array([[1, 2], [3, 4], None, []], type=pa.list_(pa.int64()))),
+        Series.from_arrow(pa.array([[1, 2], [3, 4], None, []], type=pa.large_list(pa.int64()))),
+    ],
+)
+def test_explode(data):
+    table = Table.from_pydict({"nested": data, "sidecar": ["a", "b", "c", "d"]})
+    table = table.explode([col("nested")])
+    assert table.to_pydict() == {"nested": [1, 2, 3, 4, None, None], "sidecar": ["a", "a", "b", "b", "c", "d"]}
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        Series.from_arrow(pa.array([[1, 2], [3, 4], None, []], type=pa.list_(pa.int64()))),
+        Series.from_arrow(pa.array([[1, 2], [3, 4], None, []], type=pa.large_list(pa.int64()))),
+    ],
+)
+def test_explode_multiple_cols(data):
+    table = Table.from_pydict({"nested": data, "nested2": data, "sidecar": ["a", "b", "c", "d"]})
+    # TODO: [RUST-INT][NESTED] Need to fix to allow multiple explodes
+    with pytest.raises(ValueError):
+        table = table.explode([col("nested"), col("nested2")])
+
+
+def test_explode_bad_col_type():
+    table = Table.from_pydict({"a": [1, 2, 3]})
+    with pytest.raises(ValueError, match="explode not implemented for"):
+        table = table.explode([col("a")])
