48ace00bda9005836a6b3ed93baf7be27f38a9e6
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-04-18 17:43:53 -0700

    implement filter

--- src/array/ops/filter.rs
+++ src/array/ops/filter.rs
@@ -65,51 +65,53 @@ impl FixedSizeListArray {
 
 #[cfg(feature = "python")]
 impl crate::datatypes::PythonArray {
-    pub fn filter(&self, _mask: &BooleanArray) -> DaftResult<Self> {
-        todo!();
-        // use crate::array::pseudo_arrow::PseudoArrowArray;
-        // use crate::datatypes::PythonType;
-        // use pyo3::PyObject;
+    pub fn filter(&self, mask: &BooleanArray) -> DaftResult<Self> {
+        use crate::array::pseudo_arrow::PseudoArrowArray;
+        use crate::datatypes::PythonType;
+        use arrow2::array::Array;
+        use pyo3::PyObject;
 
-        // let mask = mask.downcast();
+        let mask = mask.downcast();
 
-        // let new_values = {
-        //     mask.iter()
-        //         .map(|x| x.unwrap_or(false))
-        //         .zip(self.downcast().values().iter())
-        //         .filter_map(|(f, item)| if f { Some(item.clone()) } else { None })
-        //         .collect::<Vec<PyObject>>()
-        // };
+        // Apply the filter mask to the data values, regardless of validity.
+        let new_values = {
+            mask.iter()
+                .map(|x| x.unwrap_or(false))
+                .zip(self.downcast().values().iter())
+                .filter_map(|(f, item)| if f { Some(item.clone()) } else { None })
+                .collect::<Vec<PyObject>>()
+        };
 
-        // let new_validity = {
-        //     let new_validity_dyn_array = self.downcast().validity().map(|old_validity| {
-        //         let old_validity_array = {
-        //             &arrow2::array::BooleanArray::new(
-        //                 arrow2::datatypes::DataType::Boolean,
-        //                 old_validity.clone(),
-        //                 None,
-        //             )
-        //         };
-        //         arrow2::compute::filter::filter(old_validity_array, mask)
-        //     });
+        // Apply the filter mask to the validity bitmap.
+        let new_validity = {
+            self.downcast()
+                .validity()
+                .map(|old_validity| {
+                    let old_validity_array = {
+                        &arrow2::array::BooleanArray::new(
+                            arrow2::datatypes::DataType::Boolean,
+                            old_validity.clone(),
+                            None,
+                        )
+                    };
+                    arrow2::compute::filter::filter(old_validity_array, mask)
+                })
+                .transpose()?
+                .map(|new_validity_dynarray| {
+                    let new_validity_iter = new_validity_dynarray
+                        .as_any()
+                        .downcast_ref::<arrow2::array::BooleanArray>()
+                        .unwrap()
+                        .iter();
+                    arrow2::bitmap::Bitmap::from_iter(
+                        new_validity_iter.map(|valid| valid.unwrap_or(false)),
+                    )
+                })
+        };
 
-        //     //     let new_validity_array = {
-        //     //         ?
-        //     //             .as_any()
-        //     //             .downcast_ref::<arrow2::array::BooleanArray>()
-        //     //             .unwrap()
-        //     //     };
-        //     //     arrow2::bitmap::Bitmap::from_iter(
-        //     //         new_validity_array
-        //     //             .iter()
-        //     //             .map(|valid| valid.unwrap_or(false)),
-        //     //     )
-        //     // })
-        // };
+        let arrow_array: Box<dyn arrow2::array::Array> =
+            Box::new(PseudoArrowArray::new(new_values.into(), new_validity));
 
-        // let arrow_array: Box<dyn arrow2::array::Array> =
-        //     Box::new(PseudoArrowArray::new(new_values.into(), new_validity));
-
-        // DataArray::<PythonType>::new(self.field().clone().into(), arrow_array)
+        DataArray::<PythonType>::new(self.field().clone().into(), arrow_array)
     }
 }
