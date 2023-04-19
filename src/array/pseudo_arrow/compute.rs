74ecd5a218bb4152464b2c8e83172a75c49cf1d9
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-04-18 21:20:37 -0700

    Update concatenate

--- src/array/pseudo_arrow/compute.rs
+++ src/array/pseudo_arrow/compute.rs
@@ -1,12 +1,32 @@
+use std::ops::Not;
+
 use crate::array::pseudo_arrow::PseudoArrowArray;
 
-impl<T: Clone> PseudoArrowArray<T> {
-    pub fn concatenate(_arrays: Vec<&Self>) -> Self {
-        todo!()
-        // let mut concatenated_values: Vec<T> = Vec::new();
-        // for array in arrays {
-        //     concatenated_values.extend_from_slice(array.vec());
-        // }
-        // VecBackedArray::new(concatenated_values)
+use arrow2::array::Array;
+use arrow2::bitmap::Bitmap;
+
+impl<T: Send + Sync + Clone + 'static> PseudoArrowArray<T> {
+    pub fn concatenate(arrays: Vec<&Self>) -> Self {
+        // Concatenate the values and the validity separately.
+
+        let mut concatenated_values: Vec<T> = Vec::new();
+        for array in arrays.iter() {
+            concatenated_values.extend_from_slice(array.values());
+        }
+
+        let bitmaps: Vec<Bitmap> = arrays
+            .iter()
+            .map(|array| {
+                array
+                    .validity()
+                    .cloned()
+                    .unwrap_or_else(|| arrow2::bitmap::Bitmap::new_zeroed(array.len()).not())
+            })
+            .collect();
+
+        let concatenated_validity =
+            Bitmap::from_iter(bitmaps.iter().flat_map(|bitmap| bitmap.iter()));
+
+        PseudoArrowArray::new(concatenated_values.into(), Some(concatenated_validity))
     }
 }
