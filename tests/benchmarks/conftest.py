9ac618690453b12d63e8a3872143903818cd6ef5
Sammy Sidhu <samster25@users.noreply.github.com>
2023-04-20 19:20:24 -0700

    Add string grouping benchmarks (#829)
    * Adds string groupby benchmarks
    * Changes delim in benchmarks to `-` and `:` so we can use `pytest -k`

--- tests/benchmarks/conftest.py
+++ tests/benchmarks/conftest.py
@@ -3,15 +3,18 @@ from __future__ import annotations
 import _pytest
 
 
-# Monkeypatch to use comma delimiters when showing parameter lists.
+# Monkeypatch to use dash delimiters when showing parameter lists.
 # https://github.com/pytest-dev/pytest/blob/31d0b51039fc295dfb14bfc5d2baddebe11bb746/src/_pytest/python.py#L1190
 # Related: https://github.com/pytest-dev/pytest/issues/3617
+# This allows us to perform pytest selection via the `-k` CLI flag
 def id(self):
-    return ",".join(self._idlist)
+    return "-".join(self._idlist)
 
 
 setattr(_pytest.python.CallSpec2, "id", property(id))
 
 
 def pytest_make_parametrize_id(config, val, argname):
-    return f"{argname}={val}"
+    if isinstance(val, int):
+        val = f"{val:_}"
+    return f"{argname}:{val}"
