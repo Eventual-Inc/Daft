81c218296d4afe44a4aca2bd6c597dc1ce0f40c3
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-04-26 21:01:57 -0700

    Failing unit test for empty struct arrays

--- tests/table/test_from_py.py
+++ tests/table/test_from_py.py
@@ -23,6 +23,7 @@ PYTHON_TYPE_ARRAYS = {
     "date": [datetime.date.today(), datetime.date.today()],
     "list": [[1, 2], [3]],
     "struct": [{"a": 1, "b": 2.0}, {"b": 3.0}],
+    "empty_struct": [{}, {}],
     "tensor": list(np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])),
     "null": [None, None],
 }
@@ -37,6 +38,7 @@ INFERRED_TYPES = {
     "date": DataType.date(),
     "list": DataType.list("item", DataType.int64()),
     "struct": DataType.struct({"a": DataType.int64(), "b": DataType.float64()}),
+    "empty_struct": DataType.struct({}),
     "tensor": DataType.python(),
     "null": DataType.null(),
 }
@@ -51,6 +53,7 @@ ROUNDTRIP_TYPES = {
     "date": pa.date32(),
     "list": pa.large_list(pa.int64()),
     "struct": pa.struct({"a": pa.int64(), "b": pa.float64()}),
+    "empty_struct": pa.struct({}),
     "tensor": ArrowTensorType(shape=(2, 2), dtype=pa.int64()),
     "null": pa.null(),
 }
@@ -74,6 +77,7 @@ ARROW_TYPE_ARRAYS = {
     "list": pa.array(PYTHON_TYPE_ARRAYS["list"], pa.list_(pa.int64())),
     "fixed_size_list": pa.array([[1, 2], [3, 4]], pa.list_(pa.int64(), 2)),
     "struct": pa.array(PYTHON_TYPE_ARRAYS["struct"], pa.struct([("a", pa.int64()), ("b", pa.float64())])),
+    "empty_struct": pa.array(PYTHON_TYPE_ARRAYS["empty_struct"], pa.struct([])),
     # TODO(Clark): Uncomment once extension type support has been added.
     # "tensor": ArrowTensorArray.from_numpy(PYTHON_TYPE_ARRAYS["tensor"]),
     "null": pa.array(PYTHON_TYPE_ARRAYS["null"], pa.null()),
@@ -98,6 +102,7 @@ ARROW_ROUNDTRIP_TYPES = {
     "list": pa.large_list(pa.int64()),
     "fixed_size_list": pa.list_(pa.int64(), 2),
     "struct": pa.struct([("a", pa.int64()), ("b", pa.float64())]),
+    "empty_struct": pa.struct([]),
     # TODO(Clark): Uncomment once extension type support has been added.
     # "tensor": ArrowTensorType(shape=(2, 2), dtype=pa.int64()),
     "null": pa.null(),
