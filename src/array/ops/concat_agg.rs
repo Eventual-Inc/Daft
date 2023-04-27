868e989f87ecec7436a13c431fc20d4db7399910
Sammy Sidhu <samster25@users.noreply.github.com>
2023-04-26 17:55:31 -0700

    impl group concat

--- src/array/ops/concat_agg.rs
+++ src/array/ops/concat_agg.rs
@@ -32,7 +32,48 @@ impl DaftConcatAggable for ListArray {
         )?;
         ListArray::new(self.field.clone(), result)
     }
-    fn grouped_concat(&self, _groups: &super::GroupIndices) -> Self::Output {
-        todo!("impl grouped version")
+    fn grouped_concat(&self, groups: &super::GroupIndices) -> Self::Output {
+        let child_array = self.downcast();
+
+        let old_offsets = child_array.offsets();
+        let mut offsets = Vec::with_capacity(groups.len() + 1);
+        offsets.push(0);
+
+        for g in groups {
+            let total_elems: i64 = g
+                .iter()
+                .map(|g_idx| {
+                    let g_idx = *g_idx as usize;
+                    old_offsets.get(g_idx + 1 as usize).unwrap()
+                        - old_offsets.get(g_idx as usize).unwrap()
+                })
+                .sum();
+
+            offsets.push(offsets.last().unwrap() + total_elems as i64);
+        }
+
+        let total_capacity = *offsets.last().unwrap();
+
+        let offsets: OffsetsBuffer<i64> = arrow2::offset::OffsetsBuffer::try_from(offsets)?;
+
+        let mut growable =
+            arrow2::array::growable::make_growable(&[child_array], true, total_capacity as usize);
+        for g in groups {
+            for idx in g {
+                let idx = *idx as usize;
+                let start = *old_offsets.get(idx).unwrap();
+                let len = old_offsets.get(idx + 1).unwrap() - start;
+                growable.extend(0, start as usize, len as usize);
+            }
+        }
+
+        let nested_array = Box::new(arrow2::array::ListArray::<i64>::try_new(
+            self.data_type().to_arrow()?,
+            offsets,
+            growable.as_box(),
+            None,
+        )?);
+
+        ListArray::new(self.field.clone(), nested_array)
     }
 }
