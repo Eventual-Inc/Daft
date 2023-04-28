05c4ba978ea14979f8d6c708f710720c21b399f1
Sammy Sidhu <samster25@users.noreply.github.com>
2023-04-27 18:42:06 -0700

    Revert "multi col explodes for List"
    This reverts commit c3c7625a91d30548b4eb3731898c23511cdfc6e6.

--- src/array/ops/list.rs
+++ src/array/ops/list.rs
@@ -8,56 +8,20 @@ use crate::with_match_arrow_daft_types;
 use arrow2;
 use arrow2::array::Array;
 
-use crate::error::{DaftError, DaftResult};
+use crate::error::DaftResult;
 
 use super::downcast::Downcastable;
 
 impl ListArray {
-    pub fn explode(inputs: &[&Self]) -> DaftResult<(Vec<Series>, UInt64Array)> {
-        assert_ne!(inputs.len(), 0, "Can only run explode on non-zero columns");
-        let list_arrays = inputs.iter().map(|a| a.downcast()).collect::<Vec<_>>();
+    pub fn explode(&self) -> DaftResult<(Series, UInt64Array)> {
+        let list_array = self.downcast();
+        let child_array = list_array.values().as_ref();
+        let offsets = list_array.offsets();
 
-        let first_array = *list_arrays.first().unwrap();
-        let first_len = first_array.len();
-
-        if list_arrays.iter().skip(1).any(|a| a.len() != first_len) {
-            return Err(DaftError::ValueError(format!(
-                "Expected all arrays to be the same length in explode"
-            )));
-        }
-
-        let child_arrays = list_arrays
-            .iter()
-            .map(|a| a.values().as_ref())
-            .collect::<Vec<_>>();
-        let offsets = list_arrays.iter().map(|a| a.offsets()).collect::<Vec<_>>();
-
-        let first_offset_buffer = first_array.offsets();
-
-        if offsets.iter().skip(1).any(|o| (*o).ne(first_offset_buffer)) {
-            return Err(DaftError::ValueError(format!(
-                "Expected all lists in ListArray to be the same length in explode"
-            )));
-        }
-
-        let validity_arrays = list_arrays.iter().map(|a| a.validity()).collect::<Vec<_>>();
-        let first_validity = first_array.validity();
-
-        if validity_arrays
-            .iter()
-            .skip(1)
-            .any(|o| (*o).ne(&first_validity))
-        {
-            return Err(DaftError::ValueError(format!(
-                "Expected validity in ListArray to be the same for all columns in explode"
-            )));
-        }
-
-        let total_capacity: i64 = (0..first_len)
+        let total_capacity: i64 = (0..list_array.len())
             .map(|i| {
-                let is_valid = first_array.is_valid(i);
-                let len: i64 =
-                    first_offset_buffer.get(i + 1).unwrap() - first_offset_buffer.get(i).unwrap();
+                let is_valid = list_array.is_valid(i);
+                let len: i64 = offsets.get(i + 1).unwrap() - offsets.get(i).unwrap();
                 match (is_valid, len) {
                     (false, _) => 1,
                     (true, 0) => 1,
@@ -65,54 +29,43 @@ impl ListArray {
                 }
             })
             .sum();
-
-        let mut growables = child_arrays
-            .iter()
-            .map(|c| arrow2::array::growable::make_growable(&[*c], true, total_capacity as usize))
-            .collect::<Vec<_>>();
+        let mut growable =
+            arrow2::array::growable::make_growable(&[child_array], true, total_capacity as usize);
         let mut indices = Vec::with_capacity(total_capacity as usize);
 
-        for i in 0..first_len {
-            let is_valid = first_array.is_valid(i);
-            let start = first_offset_buffer.get(i).unwrap();
-            let len = first_offset_buffer.get(i + 1).unwrap() - start;
+        for i in 0..list_array.len() {
+            let is_valid = list_array.is_valid(i);
+            let start = offsets.get(i).unwrap();
+            let len = offsets.get(i + 1).unwrap() - start;
             match (is_valid, len) {
                 (false, _) => {
-                    growables.iter_mut().for_each(|g| g.extend_validity(1));
+                    growable.extend_validity(1);
                     indices.push(i as u64);
                 }
                 (true, 0) => {
-                    growables.iter_mut().for_each(|g| g.extend_validity(1));
+                    growable.extend_validity(1);
                     indices.push(i as u64);
                 }
                 (true, l) => {
-                    growables
-                        .iter_mut()
-                        .for_each(|g| g.extend(0, *start as usize, l as usize));
+                    growable.extend(0, *start as usize, l as usize);
                     (0..l).for_each(|_| indices.push(i as u64));
                 }
             }
         }
 
+        let child_data_type = match self.data_type() {
+            DataType::List(field) => &field.dtype,
+            _ => panic!("Expected List type but received {:?}", self.data_type()),
+        };
+
         let idx_to_take = UInt64Array::from(("idx_to_take", indices));
-        let mut series_to_return = Vec::with_capacity(inputs.len());
-
-        for (a, g) in inputs.iter().zip(growables.iter()) {
-            let child_data_type = match a.data_type() {
-                DataType::List(field) => &field.dtype,
-                _ => panic!("Expected List type but received {:?}", a.data_type()),
-            };
-
-            with_match_arrow_daft_types!(child_data_type,|$T| {
-                let new_data_arr = DataArray::<$T>::new(Arc::new(Field {
-                    name: a.field.name.clone(),
-                    dtype: child_data_type.clone(),
-                }), g.as_box())?;
-                series_to_return.push(new_data_arr.into_series());
-            })
-        }
-
-        Ok((series_to_return, idx_to_take))
+        with_match_arrow_daft_types!(child_data_type,|$T| {
+            let new_data_arr = DataArray::<$T>::new(Arc::new(Field {
+                name: self.field.name.clone(),
+                dtype: child_data_type.clone(),
+            }), growable.as_box())?;
+            Ok((new_data_arr.into_series(), idx_to_take))
+        })
     }
 }
 
