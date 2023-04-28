823b273c9bea735a72092a6952acc8fca228c766
Sammy Sidhu <samster25@users.noreply.github.com>
2023-04-28 15:58:38 -0700

    Remove experimental (#875)

--- daft/experimental/serving/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-
-from .backend import get_serving_backend
-from .endpoint import HTTPEndpoint
--- daft/experimental/serving/backends/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .aws_lambda import AWSLambdaEndpointBackend
-from .docker import DockerEndpointBackend
-from .multiprocessing import MultiprocessingEndpointBackend
