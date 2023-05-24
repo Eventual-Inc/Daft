2cf9e407314dc4d50ecd5a53c3b38bbd1b3cdef1
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-05-23 17:48:52 -0700

    fixing rowcount

--- tests/benchmarks/test_file_read.py
+++ tests/benchmarks/test_file_read.py
@@ -61,7 +61,7 @@ def test_csv_read(gen_simple_csvs, benchmark):
 @pytest.mark.parametrize("prune", [True, False])
 def test_s3_parquet_read_1x64mb(benchmark, prune):
     parquet_glob = "s3://daft-public-data/test_fixtures/parquet/*"
-    expected_rows = 3000000
+    expected_rows = 1500000
 
     def bench() -> DataFrame:
         df = daft.read_parquet(parquet_glob)
