3f3ec9d45894c8f697a3e21ed2ffaeb5ec492c5a
Sammy Sidhu <samster25@users.noreply.github.com>
2023-05-30 18:25:18 -0700

    finish collate code

--- src/array/ops/image.rs
+++ src/array/ops/image.rs
@@ -1,13 +1,11 @@
 use std::borrow::Cow;
 use std::vec;
 
-use image::{ImageBuffer, Pixel, RgbImage};
-use lazy_static::__Deref;
+use image::ImageBuffer;
 
-use crate::datatypes::logical::FixedShapeImageArray;
-use crate::datatypes::ImageMode;
+use crate::datatypes::logical::ImageArray;
+use crate::datatypes::{Field, ImageMode};
 use crate::error::DaftResult;
-use crate::{datatypes::logical::ImageArray, error::DaftError};
 use image::{Luma, LumaA, Rgb, Rgba};
 
 use super::as_arrow::AsArrow;
@@ -15,6 +13,7 @@ use num_traits::FromPrimitive;
 
 use std::ops::Deref;
 
+#[allow(clippy::upper_case_acronyms, dead_code)]
 #[derive(Debug)]
 enum DaftImageBuffer<'a> {
     L(ImageBuffer<Luma<u8>, Cow<'a, [u8]>>),
@@ -59,6 +58,16 @@ impl<'a> DaftImageBuffer<'a> {
         with_method_on_image_buffer!(self, width)
     }
 
+    pub fn channels(&self) -> u16 {
+        use DaftImageBuffer::*;
+        match self {
+            L(..) | L16(..) => 1,
+            LA(..) | LA16(..) => 2,
+            RGB(..) | RGB16(..) | RGB32F(..) => 3,
+            RGBA(..) | RGBA16(..) | RGBA32F(..) => 4,
+        }
+    }
+
     pub fn get_as_u8(&'a self) -> &'a [u8] {
         use DaftImageBuffer::*;
         match self {
@@ -85,9 +94,39 @@ impl<'a> DaftImageBuffer<'a> {
             RGBA32F(..) => ImageMode::RGBA32F,
         }
     }
+
+    pub fn resize(&self, w: u32, h: u32) -> Self {
+        use DaftImageBuffer::*;
+        match self {
+            L(imgbuf) => {
+                let result =
+                    image::imageops::resize(imgbuf, w, h, image::imageops::FilterType::Triangle);
+                DaftImageBuffer::L(image_buffer_vec_to_cow(result))
+            }
+            LA(imgbuf) => {
+                let result =
+                    image::imageops::resize(imgbuf, w, h, image::imageops::FilterType::Triangle);
+                DaftImageBuffer::LA(image_buffer_vec_to_cow(result))
+            }
+            RGB(imgbuf) => {
+                let result =
+                    image::imageops::resize(imgbuf, w, h, image::imageops::FilterType::Triangle);
+                DaftImageBuffer::RGB(image_buffer_vec_to_cow(result))
+            }
+            RGBA(imgbuf) => {
+                let result =
+                    image::imageops::resize(imgbuf, w, h, image::imageops::FilterType::Triangle);
+                DaftImageBuffer::RGBA(image_buffer_vec_to_cow(result))
+            }
+            _ => unimplemented!("mode not implemented"),
+        }
+    }
 }
 
-fn collate_daft_image_buffers<'a>(inputs: &[Option<DaftImageBuffer<'a>>]) {
+fn collate_daft_image_buffers(
+    name: &str,
+    inputs: &[Option<DaftImageBuffer<'_>>],
+) -> DaftResult<ImageArray> {
     use DaftImageBuffer::*;
     let is_all_u8 = inputs
         .iter()
@@ -97,6 +136,7 @@ fn collate_daft_image_buffers<'a>(inputs: &[Option<DaftImageBuffer<'a>>]) {
 
     let mut data_ref = Vec::with_capacity(inputs.len());
     let mut heights = Vec::with_capacity(inputs.len());
+    let mut channels = Vec::with_capacity(inputs.len());
     let mut modes = Vec::with_capacity(inputs.len());
     let mut widths = Vec::with_capacity(inputs.len());
     let mut offsets = Vec::with_capacity(inputs.len() + 1);
@@ -107,6 +147,7 @@ fn collate_daft_image_buffers<'a>(inputs: &[Option<DaftImageBuffer<'a>>]) {
         if let Some(ib) = ib {
             heights.push(ib.height());
             widths.push(ib.width());
+            channels.push(ib.channels());
 
             let buffer = ib.get_as_u8();
             data_ref.push(buffer);
@@ -116,6 +157,7 @@ fn collate_daft_image_buffers<'a>(inputs: &[Option<DaftImageBuffer<'a>>]) {
         } else {
             heights.push(0u32);
             widths.push(0u32);
+            channels.push(0u16);
             offsets.push(*offsets.last().unwrap());
             modes.push(0);
             is_valid.push(false);
@@ -123,6 +165,48 @@ fn collate_daft_image_buffers<'a>(inputs: &[Option<DaftImageBuffer<'a>>]) {
     }
 
     let collected_data = data_ref.concat();
+    let offsets = arrow2::offset::OffsetsBuffer::try_from(offsets)?;
+    let data_type =
+        crate::datatypes::DataType::Image(Box::new(crate::datatypes::DataType::UInt8), None);
+
+    let validity = arrow2::bitmap::Bitmap::from(is_valid);
+
+    let list_datatype = arrow2::datatypes::DataType::LargeList(Box::new(
+        arrow2::datatypes::Field::new("data", arrow2::datatypes::DataType::UInt8, true),
+    ));
+    let data_array = Box::new(arrow2::array::ListArray::<i64>::new(
+        list_datatype,
+        offsets,
+        Box::new(arrow2::array::UInt8Array::from_vec(collected_data)),
+        Some(validity.clone()),
+    ));
+
+    let values: Vec<Box<dyn arrow2::array::Array>> = vec![
+        data_array,
+        Box::new(
+            arrow2::array::UInt16Array::from_vec(channels).with_validity(Some(validity.clone())),
+        ),
+        Box::new(
+            arrow2::array::UInt32Array::from_vec(heights).with_validity(Some(validity.clone())),
+        ),
+        Box::new(
+            arrow2::array::UInt32Array::from_vec(widths).with_validity(Some(validity.clone())),
+        ),
+        Box::new(arrow2::array::UInt8Array::from_vec(modes).with_validity(Some(validity.clone()))),
+    ];
+    let physical_type = data_type.to_physical();
+    let struct_array = Box::new(arrow2::array::StructArray::new(
+        physical_type.to_arrow()?,
+        values,
+        Some(validity),
+    ));
+
+    let daft_struct_array =
+        crate::datatypes::StructArray::new(Field::new(name, physical_type).into(), struct_array)?;
+    Ok(ImageArray::new(
+        Field::new(name, data_type),
+        daft_struct_array,
+    ))
 }
 
 fn image_buffer_vec_to_cow<'a, P, T>(input: ImageBuffer<P, Vec<T>>) -> ImageBuffer<P, Cow<'a, [T]>>
@@ -223,54 +307,10 @@ impl ImageArray {
 
     pub fn resize(&self, w: u32, h: u32) -> DaftResult<Self> {
         let result = (0..self.len())
-            .into_iter()
             .map(|i| self.as_image_obj(i))
-            .map(|img| {
-                img.and_then(|img| {
-                    use DaftImageBuffer::*;
-                    match img {
-                        L(imgbuf) => {
-                            let result = image::imageops::resize(
-                                &imgbuf,
-                                w,
-                                h,
-                                image::imageops::FilterType::Triangle,
-                            );
-                            Some(DaftImageBuffer::L(image_buffer_vec_to_cow(result)))
-                        }
-                        LA(imgbuf) => {
-                            let result = image::imageops::resize(
-                                &imgbuf,
-                                w,
-                                h,
-                                image::imageops::FilterType::Triangle,
-                            );
-                            Some(DaftImageBuffer::LA(image_buffer_vec_to_cow(result)))
-                        }
-                        RGB(imgbuf) => {
-                            let result = image::imageops::resize(
-                                &imgbuf,
-                                w,
-                                h,
-                                image::imageops::FilterType::Triangle,
-                            );
-                            Some(DaftImageBuffer::RGB(image_buffer_vec_to_cow(result)))
-                        }
-                        RGBA(imgbuf) => {
-                            let result = image::imageops::resize(
-                                &imgbuf,
-                                w,
-                                h,
-                                image::imageops::FilterType::Triangle,
-                            );
-                            Some(DaftImageBuffer::RGBA(image_buffer_vec_to_cow(result)))
-                        }
-                        _ => unimplemented!("mode not implemented"),
-                    }
-                })
-            })
+            .map(|img| img.map(|img| img.resize(w, h)))
             .collect::<Vec<_>>();
 
-        todo!("return the resized images as a Array")
+        collate_daft_image_buffers(self.name(), result.as_slice())
     }
 }
