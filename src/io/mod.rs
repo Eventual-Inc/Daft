e8b2e0bee041be967811b13c4a73c15b0f99ca15
Sammy Sidhu <samster25@users.noreply.github.com>
2023-06-02 20:38:03 -0700

    prototype of url download

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,7 +1,4 @@
 use futures::StreamExt;
-use reqwest;
-
-use tokio;
 
 use crate::{
     array::ops::as_arrow::AsArrow,
@@ -10,37 +7,50 @@ use crate::{
 };
 
 pub fn url_download<S: ToString>(urls: &[S]) -> DaftResult<Vec<u8>> {
-    let mut results = Vec::with_capacity(urls.len());
-
-    let fetches = futures::stream::iter(urls.iter().map(|url| {
-        let owned_url: String = url.to_string();
-        tokio::spawn(async move {
-            match reqwest::get(owned_url).await {
-                Ok(result) => result.bytes().await,
-                Err(error) => Err(error),
-            }
-        })
-    }))
-    .buffered(32)
-    .map(|f| match f {
-        Ok(Ok(bytes)) => Some(bytes),
-        _ => None,
-    });
-
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?;
 
-    let data = rt.block_on(async move {
-        let mut result = vec![];
-        let _ = fetches.map(|b| match b {
-            Some(bytes) => {
-                result.extend_from_slice(bytes.as_ref());
+    let fetches = futures::stream::iter(urls.iter().enumerate().map(|(i, url)| {
+        let owned_url: String = url.to_string();
+        tokio::spawn(async move {
+            match reqwest::get(owned_url).await {
+                Ok(result) => Ok((i, result.bytes().await?)),
+                Err(error) => Err(error),
             }
-            None => println!("None"),
-        });
-        result
+        })
+    }))
+    .buffer_unordered(32)
+    .map(|f| match f {
+        Ok(Ok((i, bytes))) => Some((i, bytes)),
+        _ => None,
     });
+
+    let mut results = rt.block_on(async move {
+        // let mut result = vec![];
+
+        fetches
+            .filter_map(|b| async move { b })
+            .collect::<Vec<_>>()
+            .await
+
+        // let _sizes = fetches.map(|b| match b {
+        //     Some((i, bytes)) => {
+        //         result.extend_from_slice(bytes.as_ref());
+        //         bytes.len()
+        //     }
+        //     None => 0,
+        // }).collect::<Vec<_>>().await;
+        // result
+    });
+
+    results.sort_by_key(|k| k.0);
+    let data = results
+        .iter()
+        .map(|(_, b)| b.as_ref())
+        .collect::<Vec<_>>()
+        .concat();
+
     println!("data len: {}", data.len());
     Ok(data)
 }
@@ -48,7 +58,7 @@ pub fn url_download<S: ToString>(urls: &[S]) -> DaftResult<Vec<u8>> {
 impl Utf8Array {
     pub fn url_download(&self) -> DaftResult<BinaryArray> {
         let urls = self.as_arrow().values_iter().collect::<Vec<_>>();
-        let data = url_download(urls.as_slice())?;
+        let _data = url_download(urls.as_slice())?;
         Ok(BinaryArray::empty(
             self.name(),
             &crate::datatypes::DataType::Binary,
