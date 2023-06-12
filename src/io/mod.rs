da59e6e4f9008949bb54084b91bb4c4b720b8582
Sammy Sidhu <samster25@users.noreply.github.com>
2023-06-12 16:18:37 -0700

    s3 client  from env

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,5 +1,8 @@
 mod http;
 mod object_io;
+mod s3_like;
+
+use std::sync::Arc;
 
 use futures::{StreamExt, TryStreamExt};
 
@@ -7,14 +10,38 @@ use crate::{
     array::ops::as_arrow::AsArrow,
     datatypes::{BinaryArray, Utf8Array},
     error::{DaftError, DaftResult},
+    io::s3_like::S3LikeSource,
 };
 
-use self::http::HttpSource;
+use self::{http::HttpSource, object_io::GetResult};
 
-impl From<reqwest::Error> for DaftError {
-    fn from(error: reqwest::Error) -> Self {
+impl From<url::ParseError> for DaftError {
+    fn from(error: url::ParseError) -> Self {
         DaftError::IoError(error.into())
     }
+}
+
+#[derive(Clone)]
+struct MegaClient {
+    pub s3_client: S3LikeSource,
+}
+
+async fn single_url_download(input: String) -> anyhow::Result<GetResult> {
+    use crate::io::object_io::ObjectSource;
+    let parsed = url::Url::parse(input.as_str())?;
+    match parsed.scheme() {
+        "https" | "http" => HttpSource {}.get(input).await,
+        "s3" => CLIENT.s3_client.get(input).await,
+        // return a DaftIoError instead
+        v => panic!("protocol {v} not supported for url: {input}"),
+    }
+}
+
+lazy_static! {
+    /// This is an example for using doc comment attributes
+    static ref CLIENT: MegaClient = MegaClient {
+        s3_client: S3LikeSource::new()
+    };
 }
 
 pub fn url_download<S: ToString, I: Iterator<Item = Option<S>>>(
@@ -23,6 +50,7 @@ pub fn url_download<S: ToString, I: Iterator<Item = Option<S>>>(
     max_connections: usize,
     raise_error_on_failure: bool,
 ) -> DaftResult<BinaryArray> {
+    let _ = &CLIENT.s3_client;
     if max_connections == 0 {
         return Err(DaftError::ValueError(
             "max_connections for url_download must be non-zero".into(),
@@ -34,16 +62,15 @@ pub fn url_download<S: ToString, I: Iterator<Item = Option<S>>>(
 
     let fetches = futures::stream::iter(urls.enumerate().map(|(i, url)| {
         let owned_url = url.map(|s| s.to_string());
-        use crate::io::object_io::ObjectSource;
         tokio::spawn(async move {
             if owned_url.is_none() {
                 (i, None)
             } else {
-                let res = HttpSource {}.get(owned_url.unwrap()).await;
+                let res = single_url_download(owned_url.unwrap()).await;
 
                 let res = match res {
                     Ok(res) => res.bytes().await,
-                    Err(err) => Err(err),
+                    Err(err) => Err(err.into()),
                 };
                 (i, Some(res))
             }
