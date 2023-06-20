db4dd61f5ac553f17cfd421e4e5931e183e27414
Sammy Sidhu <samster25@users.noreply.github.com>
2023-06-20 13:56:48 -0700

    use client for reqwest instead for each request

--- src/io/http.rs
+++ src/io/http.rs
@@ -3,7 +3,9 @@ use futures::{StreamExt, TryStreamExt};
 
 use super::object_io::{GetResult, ObjectSource};
 
-pub(crate) struct HttpSource {}
+pub(crate) struct HttpSource {
+    client: reqwest::Client,
+}
 
 impl From<reqwest::Error> for super::Error {
     fn from(error: reqwest::Error) -> Self {
@@ -16,14 +18,16 @@ impl From<reqwest::Error> for super::Error {
 
 impl HttpSource {
     pub async fn new() -> Self {
-        HttpSource {}
+        HttpSource {
+            client: reqwest::ClientBuilder::default().build().unwrap(),
+        }
     }
 }
 
 #[async_trait]
 impl ObjectSource for HttpSource {
     async fn get(&self, uri: &str) -> super::Result<GetResult> {
-        let response = reqwest::get(uri).await?;
+        let response = self.client.get(uri).send().await?;
         let response = response.error_for_status()?;
         let size_bytes = response.content_length().map(|s| s as usize);
         let stream = response.bytes_stream();
