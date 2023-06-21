6d1feb777fec0a80062164f79f3e629342b7406b
Clark Zinzow <clarkzinzow@gmail.com>
2023-06-21 10:09:46 -0700

    [FEAT] [Images] [9/N] Infer `Image` type for PIL images on ingress. (#1067)
    This PR infers the `Image` type for PIL images provided to
    `Series.from_pylist()`, and therefore for `Table.from_pydict()` and
    `DataFrame.from_pydict()`.

--- src/datatypes/image_mode.rs
+++ src/datatypes/image_mode.rs
@@ -59,6 +59,26 @@ impl ImageMode {
 }
 
 impl ImageMode {
+    pub fn from_pil_mode_str(mode: &str) -> DaftResult<Self> {
+        use ImageMode::*;
+
+        match mode {
+            "L" => Ok(L),
+            "LA" => Ok(LA),
+            "RGB" => Ok(RGB),
+            "RGBA" => Ok(RGBA),
+            "1" | "P" | "CMYK" | "YCbCr" | "LAB" | "HSV" | "I" | "F" | "PA" | "RGBX" | "RGBa" | "La" | "I;16" | "I;16L" | "I;16B" | "I;16N" | "BGR;15" | "BGR;16" | "BGR;24" => Err(DaftError::TypeError(format!(
+                "PIL image mode {} is not supported; only the following modes are supported: {:?}",
+                mode,
+                ImageMode::iterator().as_slice()
+            ))),
+            _ => Err(DaftError::TypeError(format!(
+                "Image mode {} is not a valid PIL image mode; see https://pillow.readthedocs.io/en/stable/handbook/concepts.html#modes for valid PIL image modes. Of these, only the following modes are supported by Daft: {:?}",
+                mode,
+                ImageMode::iterator().as_slice()
+            ))),
+        }
+    }
     pub fn try_from_num_channels(num_channels: u16, dtype: &DataType) -> DaftResult<Self> {
         use ImageMode::*;
 
