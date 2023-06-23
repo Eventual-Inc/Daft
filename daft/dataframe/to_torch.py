2aeb22e32f00b7ae9f3faa0bfa357851a8f1b9d0
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-06-23 13:43:46 -0700

    Prefer torchdata DataPipes over torch Datasets

--- daft/dataframe/to_torch.py
+++ daft/dataframe/to_torch.py
@@ -5,13 +5,23 @@ from typing import Any, Iterable, Iterator
 from loguru import logger
 
 try:
-    import torch
+    # When available, subclass from the newer torchdata DataPipes instead of torch Datasets.
+    import torchdata
+
+    MAP_DATASET_CLASS = torchdata.datapipes.map.MapDataPipe
+    ITER_DATASET_CLASS = torchdata.datapipes.iter.IterDataPipe
 except ImportError:
-    logger.error(f"Error when importing Torch. To use PyTorch features, please install torch.")
-    raise
+    try:
+        import torch
+
+        MAP_DATASET_CLASS = torch.utils.data.Dataset
+        ITER_DATASET_CLASS = torch.utils.data.IterableDataset
+    except ImportError:
+        logger.error(f"Error when importing Torch. To use PyTorch features, please install torch.")
+        raise
 
 
-class DaftTorchDataset(torch.utils.data.Dataset):
+class DaftTorchDataset(MAP_DATASET_CLASS):  # type: ignore
     """A wrapper to create a torch map-style Dataset from a Daft pydict of items."""
 
     def __init__(self, data: dict[str, list[Any]], length: int):
@@ -25,7 +35,7 @@ class DaftTorchDataset(torch.utils.data.Dataset):
         return {key: vallist[i] for (key, vallist) in self.data.items()}
 
 
-class DaftTorchIterableDataset(torch.utils.data.IterableDataset):
+class DaftTorchIterableDataset(ITER_DATASET_CLASS):  # type: ignore
     """A thin wrapper to create a torch IterableDataset from an iterable."""
 
     def __init__(self, iterable: Iterable[dict[str, Any]]):
