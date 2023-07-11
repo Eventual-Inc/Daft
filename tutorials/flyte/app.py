ad7b0846b3499d0115e237230aa6561763138c54
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-07-11 08:37:45 -0700

    [DOCS] Add Flyte example (#1150)
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

---
+++ tutorials/flyte/app.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+from flytekit import current_context, task, workflow
+from flytekitplugins.ray import (  # noqa # pylint: disable=unused-import
+    RayJobConfig,
+    WorkerNodeConfig,
+)
+
+import daft
+
+
+###
+# Run Daft on a Ray cluster
+###
+# @task(
+#     task_config=RayJobConfig(
+#         worker_node_config=[
+#             WorkerNodeConfig(
+#                 group_name="ray-group", replicas=1,
+#             )
+#         ],
+#     )
+# )
+#
+###
+# Run Daft on a (big!) Flyte task
+###
+@task()
+def produce_resized_image_dataset(limit: int) -> list[str]:
+
+    # NOTE: Use Ray Runner:
+    #
+    #     1. If Ray connection has been set up already by Flyte, it will use the initialized Ray cluster connection
+    #     2. Otherwise, it will create a local Ray cluster on the Flyte task
+    #
+    daft.context.set_runner_ray()
+
+    written_df = daft_notebook_code(limit)
+
+    # Return the number of rows written
+    return written_df.to_pydict()["file_path"]
+
+
+@workflow()
+def wf(limit: int):
+    produce_resized_image_dataset(limit=limit)
+
+
+def daft_notebook_code(limit: int):
+    # Download data from Parquet file
+    PARQUET_PATH = "https://huggingface.co/datasets/ChristophSchuhmann/improved_aesthetics_6.5plus/resolve/main/data/train-00000-of-00001-6f24a7497df494ae.parquet"
+    parquet_df = daft.read_parquet(PARQUET_PATH)
+    parquet_df = parquet_df.select(parquet_df["URL"], parquet_df["TEXT"], parquet_df["AESTHETIC_SCORE"])
+
+    # Filter for only "pretty" images
+    filtered_df = parquet_df.where(parquet_df["TEXT"].str.contains("darkness"))
+
+    # Download and resize images
+    filtered_df = filtered_df.with_column(
+        "image",
+        filtered_df["URL"].url.download(on_error="null").image.decode(),
+    )
+    filtered_df = filtered_df.with_column(
+        "resized_image",
+        filtered_df["image"].image.resize(32, 32),
+    )
+
+    # Write processed data out to Parquet file
+    written_df = (
+        filtered_df.select("URL", "TEXT", "resized_image")
+        .limit(limit)
+        .write_parquet(
+            # NOTE: In practice, you will be writing to cloud storage such as AWS S3.
+            # This currently writes to a local (ephemeral) file.
+            f"my-s3-bucket/{current_context().execution_id.name}/resized_images.parquet"
+        )
+    )
+    return written_df
