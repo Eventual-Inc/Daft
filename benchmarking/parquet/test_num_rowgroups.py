d5619d89f55c1055f425d4195259d3e1cea5bc7c
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-07-12 02:00:39 -0700

    Add instructions and deps for running memray

--- benchmarking/parquet/test_num_rowgroups.py
+++ benchmarking/parquet/test_num_rowgroups.py
@@ -7,16 +7,18 @@ import pytest
 @pytest.mark.parametrize(
     "path",
     [
-        "s3://daft-public-data/test_fixtures/parquet-dev/daft_tpch_100g_32part_1RG.parquet"
+        "s3://daft-public-data/test_fixtures/parquet-dev/daft_tpch_100g_32part_1RG.parquet",
         "s3://daft-public-data/test_fixtures/parquet-dev/daft_tpch_100g_32part.parquet",
-        "s3://daft-public-data/test_fixtures/parquet-dev/daft_tpch_100g_32part_18kRG.parquet"
-        "s3://daft-public-data/test_fixtures/parquet-dev/daft_tpch_100g_32part_180kRG.parquet",
+        # Disabled: too slow!
+        # "s3://daft-public-data/test_fixtures/parquet-dev/daft_tpch_100g_32part_18kRG.parquet"
+        # "s3://daft-public-data/test_fixtures/parquet-dev/daft_tpch_100g_32part_180kRG.parquet",
     ],
     ids=[
         "1",
         "2k",
-        "18k",
-        "180k",
+        # Disabled: too slow!
+        # "18k",
+        # "180k",
     ],
 )
 def test_read_parquet_num_rowgroups(path, read_fn, benchmark):
