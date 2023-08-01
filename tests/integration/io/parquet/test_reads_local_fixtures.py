11a84d3ba3dc4e2e822312320121754fdac1e925
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-08-01 11:42:38 -0700

    Use port 8001 instead to avoid conflict in CI

--- tests/integration/io/parquet/test_reads_local_fixtures.py
+++ tests/integration/io/parquet/test_reads_local_fixtures.py
@@ -4,7 +4,6 @@ import uuid
 
 import pytest
 
-from daft.io import IOConfig, S3Config
 from daft.table import Table
 
 BUCKETS = ["head-retries-bucket", "get-retries-bucket"]
@@ -13,12 +12,11 @@ BUCKETS = ["head-retries-bucket", "get-retries-bucket"]
 @pytest.mark.integration()
 @pytest.mark.parametrize("status_code", [400, 403, 404])
 @pytest.mark.parametrize("bucket", BUCKETS)
-def test_non_retryable_errors(status_code: int, bucket: str):
-    io_config = IOConfig(s3=S3Config(endpoint_url="http://127.0.0.1:8000"))
+def test_non_retryable_errors(retry_server_s3_config, status_code: int, bucket: str):
     data_path = f"s3://{bucket}/{status_code}/1/{uuid.uuid4()}"
 
     with pytest.raises(ValueError):
-        Table.read_parquet(data_path, io_config=io_config)
+        Table.read_parquet(data_path, io_config=retry_server_s3_config)
 
 
 @pytest.mark.integration()
@@ -41,11 +39,9 @@ def test_non_retryable_errors(status_code: int, bucket: str):
     ],
 )
 @pytest.mark.parametrize("bucket", BUCKETS)
-def test_retryable_errors(status_code: int, bucket: str):
-    io_config = IOConfig(s3=S3Config(endpoint_url="http://127.0.0.1:8000"))
-
+def test_retryable_errors(retry_server_s3_config, status_code: int, bucket: str):
     # By default the SDK retries 3 times, so we should be able to tolerate NUM_ERRORS=2
     NUM_ERRORS = 2
     data_path = f"s3://{bucket}/{status_code}/{NUM_ERRORS}/{uuid.uuid4()}"
 
-    Table.read_parquet(data_path, io_config=io_config)
+    Table.read_parquet(data_path, io_config=retry_server_s3_config)
