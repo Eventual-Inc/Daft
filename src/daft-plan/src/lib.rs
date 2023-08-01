bd4235e2b3b9abb93e6df2483f7494cbbf7e834b
xcharleslin <4212216+xcharleslin@users.noreply.github.com>
2023-08-01 09:49:42 -0700

    [CHORE] Rust Logical plan skeleton (#1192)
    Skeleton for new logical plan in Rust. Includes the following building
    blocks:
    
    - LogicalPlan enum. This is the core logical plan object. It currently
    has just one variant, Source.
    - SourceInfo enum, which contains information about what the data source
    is. Currently has one variant, FilesInfo, to represent file source.
    - LogicalPlanBuilder, which wraps a LogicalPlan to:
      - Serve as the API surface for Python.
      - (later) Do resolution of unresolved names to field IDs.
    - PyO3 bindings for LogicalPlanBuilder.
    
    Next steps (separate PRs): 
    
    - Add Daft flag to switch between old and new logical plans.
    - Have Daft create the new logical plan for file source operations. This
    will involve a new metadata resolution path.
    - A lightweight physical plan translation in Rust.
    - Have Daft be able to dispatch the translated physical plan, by hooking
    into the existing plan dispatching machinery.
    
    ---------
    
    Co-authored-by: Xiayue Charles Lin <charles@eventualcomputing.com>

--- src/daft-plan/src/lib.rs
+++ src/daft-plan/src/lib.rs
@@ -1,1 +1,18 @@
+mod builder;
 mod display;
+mod logical_plan;
+mod ops;
+mod source_info;
+
+pub use builder::LogicalPlanBuilder;
+pub use logical_plan::LogicalPlan;
+
+#[cfg(feature = "python")]
+use pyo3::prelude::*;
+
+#[cfg(feature = "python")]
+pub fn register_modules(_py: Python, parent: &PyModule) -> PyResult<()> {
+    parent.add_class::<LogicalPlanBuilder>()?;
+
+    Ok(())
+}
---
+++ src/daft-plan/src/ops/source.rs
@@ -0,0 +1,32 @@
+use std::sync::Arc;
+
+use daft_core::schema::SchemaRef;
+use daft_dsl::ExprRef;
+
+use crate::source_info::SourceInfo;
+
+#[derive(Clone)]
+pub struct Source {
+    /// The schema of the output of this node (the source data schema).
+    /// May be a subset of the source data schema; executors should push down this projection if possible.
+    pub schema: SchemaRef,
+
+    /// Information about the source data location.
+    pub source_info: Arc<SourceInfo>,
+
+    /// Optional filters to apply to the source data.
+    pub filters: Vec<ExprRef>,
+    /// Optional number of rows to read.
+    pub limit: Option<usize>,
+}
+
+impl Source {
+    pub(crate) fn new(schema: SchemaRef, source_info: Arc<SourceInfo>) -> Self {
+        Self {
+            schema,
+            source_info,
+            filters: vec![], // Will be populated by plan optimizer.
+            limit: None,     // Will be populated by plan optimizer.
+        }
+    }
+}
