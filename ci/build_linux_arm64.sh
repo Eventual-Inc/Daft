f33cb55732aa076e5a1b8cf7d3bad5eea307de17
Sammy Sidhu <samster25@users.noreply.github.com>
2023-08-01 22:42:01 -0700

    -y for rustup

--- ci/build_linux_arm64.sh
+++ ci/build_linux_arm64.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal --default-toolchain nightly
+curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal --default-toolchain nightly -y
 pip3 install "maturin[readelf]"
 ls
