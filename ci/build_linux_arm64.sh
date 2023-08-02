b34e51101dcd8662f30a4956883967f343929eb1
Sammy Sidhu <samster25@users.noreply.github.com>
2023-08-01 22:45:46 -0700

    build wheel now

--- ci/build_linux_arm64.sh
+++ ci/build_linux_arm64.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal --default-toolchain nightly -y
+source "$HOME/.cargo/env"
 pip3 install maturin
-ls
+maturin build --compatibility manylinux2014 --out dist
