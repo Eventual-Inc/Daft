475871886b815020cadfcec2c577a0dd19effd0f
Sammy Sidhu <samster25@users.noreply.github.com>
2023-08-02 17:06:35 -0700

    rename tests

--- tests/integration/io/test_url_download_private_aws_s3.py
+++ tests/integration/io/test_url_download_private_aws_s3.py
@@ -21,7 +21,7 @@ def io_config() -> IOConfig:
 
 
 @pytest.mark.integration()
-def test_url_download_aws_s3_private_bucket_(small_images_s3_paths, io_config):
+def test_url_download_aws_s3_public_bucket_with_creds(small_images_s3_paths, io_config):
     data = {"urls": small_images_s3_paths}
     df = daft.from_pydict(data)
     df = df.with_column("data", df["urls"].url.download(use_native_downloader=True, io_config=io_config))
@@ -33,7 +33,7 @@ def test_url_download_aws_s3_private_bucket_(small_images_s3_paths, io_config):
 
 
 @pytest.mark.integration()
-def test_url_download_aws_s3_private_bucket_parquet(io_config):
+def test_read_parquet_aws_s3_private_bucket_with_creds(io_config):
     filename = "s3://eventual-data-test-bucket/benchmarking/1M-writethrough.parquet/part-00000-ca6132d9-d056-45ad-8d67-e19cf896bc8a-c000.parquet"
     df = daft.read_parquet(filename, io_config=io_config, use_native_downloader=True).collect()
     assert len(df) == 2603
