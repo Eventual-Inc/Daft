9cb663c263a297254c05f5975c1c8ccc7f54232c
Sammy Sidhu <samster25@users.noreply.github.com>
2023-08-03 21:48:55 -0700

    fix assert

--- benchmarking/parquet/test_bulk_reads.py
+++ benchmarking/parquet/test_bulk_reads.py
@@ -18,7 +18,7 @@ def test_read_parquet_num_files_single_column(num_files, bulk_read_fn, benchmark
     # Make sure the data is correct
     for i in range(num_files):
         assert data[i].column_names == ["L_ORDERKEY"]
-        assert len(data[i]) == 18751674
+        assert len(data[i]) == 5515199
 
 
 @pytest.mark.benchmark(group="num_rowgroups_all_columns")
@@ -33,4 +33,4 @@ def test_read_parquet_num_files_all_columns(num_files, bulk_read_fn, benchmark):
     # Make sure the data is correct
     for i in range(num_files):
         assert len(data[i].column_names) == 16
-        assert len(data[i]) == 18751674
+        assert len(data[i]) == 5515199
