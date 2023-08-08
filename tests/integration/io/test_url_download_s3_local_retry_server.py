86d2d6924884d82c67cad56f94cd27b7cf773c72
Sammy Sidhu <samster25@users.noreply.github.com>
2023-08-08 16:50:02 -0700

    remove comment

--- tests/integration/io/test_url_download_s3_local_retry_server.py
+++ tests/integration/io/test_url_download_s3_local_retry_server.py
@@ -6,12 +6,6 @@ import daft
 
 
 @pytest.mark.integration()
-# @pytest.mark.skip(
-#     reason="""[IO-RETRIES] This currently fails: we need better retry policies to have this work consistently.
-#                   Currently, if all the retries for a given URL happens to land in the same 1-second window, the request fails.
-#                   We should be able to get around this with a more generous retry policy, with larger increments between backoffs.
-#                   """
-# )
 def test_url_download_local_retry_server(retry_server_s3_config):
     bucket = "80-per-second-rate-limited-gets-bucket"
     data = {"urls": [f"s3://{bucket}/foo{i}" for i in range(100)]}
