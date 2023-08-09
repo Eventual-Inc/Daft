d25e533781ab6ded7eb5b497cd225f8840161b34
Xiayue Charles Lin <charles@eventualcomputing.com>
2023-08-09 16:08:47 -0700

    Keep in single partition shortcut for now since naming is not decoupled from plan builder yet

--- src/daft-plan/src/planner.rs
+++ src/daft-plan/src/planner.rs
@@ -85,57 +85,89 @@ pub fn plan(logical_plan: &LogicalPlan) -> DaftResult<PhysicalPlan> {
                 unimplemented!("{:?}", group_by);
             }
 
-            // Resolve and assign intermediate names for the aggregations.
-            let schema = logical_plan.schema();
-            let names: Vec<daft_core::datatypes::FieldID> = aggregations
-                .iter()
-                .map(|agg_expr| agg_expr.semantic_id(&schema))
-                .collect();
+            let num_input_partitions = logical_plan.partition_spec().num_partitions;
 
-            let first_stage_aggs: Vec<daft_dsl::AggExpr> = aggregations
-                .iter()
-                .zip(names.iter())
-                .map(|(agg_expr, field_id)| match agg_expr {
-                    Count(e) => Count(e.alias(field_id.id.clone()).into()),
-                    Sum(e) => Sum(e.alias(field_id.id.clone()).into()),
-                    Mean(e) => Mean(e.alias(field_id.id.clone()).into()),
-                    Min(e) => Min(e.alias(field_id.id.clone()).into()),
-                    Max(e) => Max(e.alias(field_id.id.clone()).into()),
-                    List(e) => List(e.alias(field_id.id.clone()).into()),
-                    Concat(e) => Concat(e.alias(field_id.id.clone()).into()),
-                })
-                .collect();
-
-            let second_stage_aggs: Vec<daft_dsl::AggExpr> = aggregations
-                .iter()
-                .zip(names.iter())
-                .map(|(agg_expr, field_id)| match agg_expr {
-                    Count(_) => Count(daft_dsl::Expr::Column(field_id.id.clone().into()).into()),
-                    Sum(_) => Sum(daft_dsl::Expr::Column(field_id.id.clone().into()).into()),
-                    Mean(_) => Mean(daft_dsl::Expr::Column(field_id.id.clone().into()).into()),
-                    Min(_) => Min(daft_dsl::Expr::Column(field_id.id.clone().into()).into()),
-                    Max(_) => Max(daft_dsl::Expr::Column(field_id.id.clone().into()).into()),
-                    List(_) => List(daft_dsl::Expr::Column(field_id.id.clone().into()).into()),
-                    Concat(_) => Concat(daft_dsl::Expr::Column(field_id.id.clone().into()).into()),
-                })
-                .collect();
-
-            let result_plan = {
-                let result_plan = PhysicalPlan::Aggregate(Aggregate::new(
+            let result_plan = match num_input_partitions {
+                1 => PhysicalPlan::Aggregate(Aggregate::new(
                     result_plan.into(),
-                    first_stage_aggs,
+                    aggregations.clone(),
                     vec![],
-                ));
-                let result_plan = PhysicalPlan::Coalesce(Coalesce::new(
-                    result_plan.into(),
-                    logical_plan.partition_spec().num_partitions,
-                    1,
-                ));
-                PhysicalPlan::Aggregate(Aggregate::new(
-                    result_plan.into(),
-                    second_stage_aggs,
-                    vec![],
-                ))
+                )),
+                _ => {
+                    // Resolve and assign intermediate names for the aggregations.
+                    let schema = logical_plan.schema();
+                    let intermediate_names: Vec<daft_core::datatypes::FieldID> = aggregations
+                        .iter()
+                        .map(|agg_expr| agg_expr.semantic_id(&schema))
+                        .collect();
+
+                    let first_stage_aggs: Vec<daft_dsl::AggExpr> = aggregations
+                        .iter()
+                        .zip(intermediate_names.iter())
+                        .map(|(agg_expr, field_id)| match agg_expr {
+                            Count(e) => Count(e.alias(field_id.id.clone()).into()),
+                            Sum(e) => Sum(e.alias(field_id.id.clone()).into()),
+                            Mean(e) => Mean(e.alias(field_id.id.clone()).into()),
+                            Min(e) => Min(e.alias(field_id.id.clone()).into()),
+                            Max(e) => Max(e.alias(field_id.id.clone()).into()),
+                            List(e) => List(e.alias(field_id.id.clone()).into()),
+                            Concat(e) => Concat(e.alias(field_id.id.clone()).into()),
+                        })
+                        .collect();
+
+                    let second_stage_aggs: Vec<daft_dsl::AggExpr> = intermediate_names
+                        .iter()
+                        .zip(schema.fields.keys())
+                        .zip(aggregations.iter())
+                        .map(|((field_id, original_name), agg_expr)| match agg_expr {
+                            Count(_) => Count(
+                                daft_dsl::Expr::Column(field_id.id.clone().into())
+                                    .alias(&**original_name)
+                                    .into(),
+                            ),
+                            Sum(_) => Sum(daft_dsl::Expr::Column(field_id.id.clone().into())
+                                .alias(&**original_name)
+                                .into()),
+                            Mean(_) => Mean(
+                                daft_dsl::Expr::Column(field_id.id.clone().into())
+                                    .alias(&**original_name)
+                                    .into(),
+                            ),
+                            Min(_) => Min(daft_dsl::Expr::Column(field_id.id.clone().into())
+                                .alias(&**original_name)
+                                .into()),
+                            Max(_) => Max(daft_dsl::Expr::Column(field_id.id.clone().into())
+                                .alias(&**original_name)
+                                .into()),
+                            List(_) => List(
+                                daft_dsl::Expr::Column(field_id.id.clone().into())
+                                    .alias(&**original_name)
+                                    .into(),
+                            ),
+                            Concat(_) => Concat(
+                                daft_dsl::Expr::Column(field_id.id.clone().into())
+                                    .alias(&**original_name)
+                                    .into(),
+                            ),
+                        })
+                        .collect();
+
+                    let result_plan = PhysicalPlan::Aggregate(Aggregate::new(
+                        result_plan.into(),
+                        first_stage_aggs,
+                        vec![],
+                    ));
+                    let result_plan = PhysicalPlan::Coalesce(Coalesce::new(
+                        result_plan.into(),
+                        logical_plan.partition_spec().num_partitions,
+                        1,
+                    ));
+                    PhysicalPlan::Aggregate(Aggregate::new(
+                        result_plan.into(),
+                        second_stage_aggs,
+                        vec![],
+                    ))
+                }
             };
 
             Ok(result_plan)
