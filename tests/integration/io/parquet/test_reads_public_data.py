fd531a6d5a1eda4c9ce49a8a8337a8dfc65428d9
Sammy Sidhu <samster25@users.noreply.github.com>
2023-08-09 20:27:03 -0700

    try 3.8

--- tests/integration/io/parquet/test_reads_public_data.py
+++ tests/integration/io/parquet/test_reads_public_data.py
@@ -186,7 +186,6 @@ def read_parquet_with_pyarrow(path) -> pa.Table:
     if get_protocol_from_path(path) == "az":
         kwargs["account_name"] = "dafttestdata"
         kwargs["anon"] = True
-        kwargs["credential"] = "anon"
 
     fs = get_filesystem_from_path(path, **kwargs)
     table = pq.read_table(path, filesystem=fs)
@@ -221,7 +220,7 @@ def test_parquet_read_df(parquet_file, public_storage_io_config):
     if url.startswith("az"):
         import adlfs
 
-        fs = adlfs.AzureBlobFileSystem(account_name="dafttestdata", credential="anon")
+        fs = adlfs.AzureBlobFileSystem(account_name="dafttestdata", anon=True)
     else:
         fs = None
     daft_native_read = daft.read_parquet(url, io_config=public_storage_io_config, use_native_downloader=True, fs=fs)
