ee9df00fa7aee73065fb4aa38fcddd1df5dba7e1
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-08-18 15:54:26 -0700

    impl DaftCountAggable

--- src/daft-core/src/series/from.rs
+++ src/daft-core/src/series/from.rs
@@ -19,6 +19,26 @@ impl TryFrom<(&str, Box<dyn arrow2::array::Array>)> for Series {
         let source_arrow_type = array.data_type();
         let dtype: DataType = source_arrow_type.into();
         let field = Arc::new(Field::new(name, dtype.clone()));
+
+        // TODO(Nested): Refactor this out with nested logical types in StructArray and ListArray
+        // Corner-case nested logical types that have not yet been migrated to new Array formats
+        // to hold only casted physical arrow arrays.
+        let physical_type = dtype.to_physical();
+        if physical_type != dtype {
+            let arrow_physical_type = physical_type.to_arrow()?;
+            let casted_array = arrow2::compute::cast::cast(
+                array.as_ref(),
+                &arrow_physical_type,
+                arrow2::compute::cast::CastOptions {
+                    wrapped: true,
+                    partial: false,
+                },
+            )?;
+            return Ok(
+                with_match_daft_types!(physical_type, |$T| <$T as DaftDataType>::ArrayType::from_arrow(field.as_ref(), casted_array)?.into_series()),
+            );
+        }
+
         with_match_daft_types!(dtype, |$T| {
             Ok(<$T as DaftDataType>::ArrayType::from_arrow(&field, array)?.into_series())
         })
