debe1711c70ea0f3d6d56ec1207b1c0d9c0a10e0
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-08-24 21:50:06 -0700

    [CHORE] Code reduction in growable macros + logical if/else refactor (#1301)
    1. Cleans up our GrowableArray macros -- they actually all look the same
    across the arrow and logical ones and can be unified
    2. Also cleans up the implementation of Logical SeriesLike if/elses --
    we can just delegate directly to the physical arrays
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

--- src/daft-core/src/series/array_impl/logical_array.rs
+++ src/daft-core/src/series/array_impl/logical_array.rs
@@ -67,10 +67,11 @@ macro_rules! impl_series_like_for_logical_array {
             }
 
             fn if_else(&self, other: &Series, predicate: &Series) -> DaftResult<Series> {
-                Ok(self
+                let physical_if_else = self
                     .0
-                    .if_else(other.downcast()?, predicate.downcast()?)?
-                    .into_series())
+                    .physical
+                    .if_else(&other.downcast::<$da>()?.physical, predicate.downcast()?)?;
+                Ok($da::new(self.0.field.clone(), physical_if_else).into_series())
             }
 
             fn is_null(&self) -> DaftResult<Series> {
