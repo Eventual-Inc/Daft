c962079205dd119a291b82a4677064cec9e3a127
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-08-25 18:30:17 -0700

    [CHORE] Cleanup matching file

--- src/daft-core/src/datatypes/matching.rs
+++ src/daft-core/src/datatypes/matching.rs
@@ -3,43 +3,43 @@ macro_rules! with_match_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Null => __with_ty__! { NullType },
-        Boolean => __with_ty__! { BooleanType },
-        Int8 => __with_ty__! { Int8Type },
-        Int16 => __with_ty__! { Int16Type },
-        Int32 => __with_ty__! { Int32Type },
-        Int64 => __with_ty__! { Int64Type },
-        Int128(..) => __with_ty__! { Int128Type },
-        UInt8 => __with_ty__! { UInt8Type },
-        UInt16 => __with_ty__! { UInt16Type },
-        UInt32 => __with_ty__! { UInt32Type },
-        UInt64 => __with_ty__! { UInt64Type },
-        Float32 => __with_ty__! { Float32Type },
-        Float64 => __with_ty__! { Float64Type },
-        Timestamp(_, _) => __with_ty__! { TimestampType },
-        Date => __with_ty__! { DateType },
-        Time(_) => __with_ty__! { TimeType },
-        Duration(_) => __with_ty__! { DurationType },
-        Binary => __with_ty__! { BinaryType },
-        Utf8 => __with_ty__! { Utf8Type },
-        FixedSizeList(_, _) => __with_ty__! { FixedSizeListType },
-        List(_) => __with_ty__! { ListType },
-        Struct(_) => __with_ty__! { StructType },
-        Extension(_, _, _) => __with_ty__! { ExtensionType },
+        DataType::Null => __with_ty__! { NullType },
+        DataType::Boolean => __with_ty__! { BooleanType },
+        DataType::Int8 => __with_ty__! { Int8Type },
+        DataType::Int16 => __with_ty__! { Int16Type },
+        DataType::Int32 => __with_ty__! { Int32Type },
+        DataType::Int64 => __with_ty__! { Int64Type },
+        DataType::Int128(..) => __with_ty__! { Int128Type },
+        DataType::UInt8 => __with_ty__! { UInt8Type },
+        DataType::UInt16 => __with_ty__! { UInt16Type },
+        DataType::UInt32 => __with_ty__! { UInt32Type },
+        DataType::UInt64 => __with_ty__! { UInt64Type },
+        DataType::Float32 => __with_ty__! { Float32Type },
+        DataType::Float64 => __with_ty__! { Float64Type },
+        DataType::Timestamp(_, _) => __with_ty__! { TimestampType },
+        DataType::Date => __with_ty__! { DateType },
+        DataType::Time(_) => __with_ty__! { TimeType },
+        DataType::Duration(_) => __with_ty__! { DurationType },
+        DataType::Binary => __with_ty__! { BinaryType },
+        DataType::Utf8 => __with_ty__! { Utf8Type },
+        DataType::FixedSizeList(_, _) => __with_ty__! { FixedSizeListType },
+        DataType::List(_) => __with_ty__! { ListType },
+        DataType::Struct(_) => __with_ty__! { StructType },
+        DataType::Extension(_, _, _) => __with_ty__! { ExtensionType },
         #[cfg(feature = "python")]
-        Python => __with_ty__! { PythonType },
-        Embedding(..) => __with_ty__! { EmbeddingType },
-        Image(..) => __with_ty__! { ImageType },
-        FixedShapeImage(..) => __with_ty__! { FixedShapeImageType },
-        Tensor(..) => __with_ty__! { TensorType },
-        FixedShapeTensor(..) => __with_ty__! { FixedShapeTensorType },
-        Decimal128(..) => __with_ty__! { Decimal128Type },
-        Float16 => unimplemented!("Array for Float16 DataType not implemented"),
-        Unknown => unimplemented!("Array for Unknown DataType not implemented"),
+        DataType::Python => __with_ty__! { PythonType },
+        DataType::Embedding(..) => __with_ty__! { EmbeddingType },
+        DataType::Image(..) => __with_ty__! { ImageType },
+        DataType::FixedShapeImage(..) => __with_ty__! { FixedShapeImageType },
+        DataType::Tensor(..) => __with_ty__! { TensorType },
+        DataType::FixedShapeTensor(..) => __with_ty__! { FixedShapeTensorType },
+        DataType::Decimal128(..) => __with_ty__! { Decimal128Type },
+        DataType::Float16 => unimplemented!("Array for Float16 DataType not implemented"),
+        DataType::Unknown => unimplemented!("Array for Unknown DataType not implemented"),
 
         // NOTE: We should not implement a default for match here, because this is meant to be
         // an exhaustive match across **all** Daft types.
@@ -52,32 +52,30 @@ macro_rules! with_match_physical_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
-    #[allow(unused_imports)]
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Null => __with_ty__! { NullType },
-        Boolean => __with_ty__! { BooleanType },
-        Int8 => __with_ty__! { Int8Type },
-        Int16 => __with_ty__! { Int16Type },
-        Int32 => __with_ty__! { Int32Type },
-        Int64 => __with_ty__! { Int64Type },
-        UInt8 => __with_ty__! { UInt8Type },
-        UInt16 => __with_ty__! { UInt16Type },
-        UInt32 => __with_ty__! { UInt32Type },
-        UInt64 => __with_ty__! { UInt64Type },
-        // Float16 => __with_ty__! { Float16Type },
-        Float32 => __with_ty__! { Float32Type },
-        Float64 => __with_ty__! { Float64Type },
-        Binary => __with_ty__! { BinaryType },
-        Utf8 => __with_ty__! { Utf8Type },
-        FixedSizeList(_, _) => __with_ty__! { FixedSizeListType },
-        List(_) => __with_ty__! { ListType },
-        Struct(_) => __with_ty__! { StructType },
-        Extension(_, _, _) => __with_ty__! { ExtensionType },
+        DataType::Null => __with_ty__! { NullType },
+        DataType::Boolean => __with_ty__! { BooleanType },
+        DataType::Int8 => __with_ty__! { Int8Type },
+        DataType::Int16 => __with_ty__! { Int16Type },
+        DataType::Int32 => __with_ty__! { Int32Type },
+        DataType::Int64 => __with_ty__! { Int64Type },
+        DataType::UInt8 => __with_ty__! { UInt8Type },
+        DataType::UInt16 => __with_ty__! { UInt16Type },
+        DataType::UInt32 => __with_ty__! { UInt32Type },
+        DataType::UInt64 => __with_ty__! { UInt64Type },
+        DataType::Float32 => __with_ty__! { Float32Type },
+        DataType::Float64 => __with_ty__! { Float64Type },
+        DataType::Binary => __with_ty__! { BinaryType },
+        DataType::Utf8 => __with_ty__! { Utf8Type },
+        DataType::FixedSizeList(_, _) => __with_ty__! { FixedSizeListType },
+        DataType::List(_) => __with_ty__! { ListType },
+        DataType::Struct(_) => __with_ty__! { StructType },
+        DataType::Extension(_, _, _) => __with_ty__! { ExtensionType },
         #[cfg(feature = "python")]
-        Python => __with_ty__! { PythonType },
+        DataType::Python => __with_ty__! { PythonType },
         _ => panic!("{:?} not implemented for with_match_physical_daft_types", $key_type)
     }
 })}
@@ -87,31 +85,28 @@ macro_rules! with_match_arrow_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Null => __with_ty__! { NullType },
-        Boolean => __with_ty__! { BooleanType },
-        Binary => __with_ty__! { BinaryType },
-        Int8 => __with_ty__! { Int8Type },
-        Int16 => __with_ty__! { Int16Type },
-        Int32 => __with_ty__! { Int32Type },
-        Int64 => __with_ty__! { Int64Type },
-        UInt8 => __with_ty__! { UInt8Type },
-        UInt16 => __with_ty__! { UInt16Type },
-        UInt32 => __with_ty__! { UInt32Type },
-        UInt64 => __with_ty__! { UInt64Type },
-        // Float16 => __with_ty__! { Float16Type },
-        Float32 => __with_ty__! { Float32Type },
-        Float64 => __with_ty__! { Float64Type },
-        // Date => __with_ty__! { DateType },
-        // Timestamp(_, _) => __with_ty__! { TimestampType },
-        List(_) => __with_ty__! { ListType },
-        FixedSizeList(..) => __with_ty__! { FixedSizeListType },
-        Struct(_) => __with_ty__! { StructType },
-        Extension(_, _, _) => __with_ty__! { ExtensionType },
-        Utf8 => __with_ty__! { Utf8Type },
+        DataType::Null => __with_ty__! { NullType },
+        DataType::Boolean => __with_ty__! { BooleanType },
+        DataType::Binary => __with_ty__! { BinaryType },
+        DataType::Int8 => __with_ty__! { Int8Type },
+        DataType::Int16 => __with_ty__! { Int16Type },
+        DataType::Int32 => __with_ty__! { Int32Type },
+        DataType::Int64 => __with_ty__! { Int64Type },
+        DataType::UInt8 => __with_ty__! { UInt8Type },
+        DataType::UInt16 => __with_ty__! { UInt16Type },
+        DataType::UInt32 => __with_ty__! { UInt32Type },
+        DataType::UInt64 => __with_ty__! { UInt64Type },
+        DataType::Float32 => __with_ty__! { Float32Type },
+        DataType::Float64 => __with_ty__! { Float64Type },
+        DataType::List(_) => __with_ty__! { ListType },
+        DataType::FixedSizeList(..) => __with_ty__! { FixedSizeListType },
+        DataType::Struct(_) => __with_ty__! { StructType },
+        DataType::Extension(_, _, _) => __with_ty__! { ExtensionType },
+        DataType::Utf8 => __with_ty__! { Utf8Type },
         _ => panic!("{:?} not implemented", $key_type)
     }
 })}
@@ -121,25 +116,23 @@ macro_rules! with_match_comparable_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
-    #[allow(unused_imports)]
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Null => __with_ty__! { NullType },
-        Boolean => __with_ty__! { BooleanType },
-        Int8 => __with_ty__! { Int8Type },
-        Int16 => __with_ty__! { Int16Type },
-        Int32 => __with_ty__! { Int32Type },
-        Int64 => __with_ty__! { Int64Type },
-        UInt8 => __with_ty__! { UInt8Type },
-        UInt16 => __with_ty__! { UInt16Type },
-        UInt32 => __with_ty__! { UInt32Type },
-        UInt64 => __with_ty__! { UInt64Type },
-        // Float16 => __with_ty__! { Float16Type },
-        Float32 => __with_ty__! { Float32Type },
-        Float64 => __with_ty__! { Float64Type },
-        Utf8 => __with_ty__! { Utf8Type },
+        DataType::Null => __with_ty__! { NullType },
+        DataType::Boolean => __with_ty__! { BooleanType },
+        DataType::Int8 => __with_ty__! { Int8Type },
+        DataType::Int16 => __with_ty__! { Int16Type },
+        DataType::Int32 => __with_ty__! { Int32Type },
+        DataType::Int64 => __with_ty__! { Int64Type },
+        DataType::UInt8 => __with_ty__! { UInt8Type },
+        DataType::UInt16 => __with_ty__! { UInt16Type },
+        DataType::UInt32 => __with_ty__! { UInt32Type },
+        DataType::UInt64 => __with_ty__! { UInt64Type },
+        DataType::Float32 => __with_ty__! { Float32Type },
+        DataType::Float64 => __with_ty__! { Float64Type },
+        DataType::Utf8 => __with_ty__! { Utf8Type },
         _ => panic!("{:?} not implemented", $key_type)
     }
 })}
@@ -149,22 +142,21 @@ macro_rules! with_match_numeric_and_utf_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Int8 => __with_ty__! { Int8Type },
-        Int16 => __with_ty__! { Int16Type },
-        Int32 => __with_ty__! { Int32Type },
-        Int64 => __with_ty__! { Int64Type },
-        UInt8 => __with_ty__! { UInt8Type },
-        UInt16 => __with_ty__! { UInt16Type },
-        UInt32 => __with_ty__! { UInt32Type },
-        UInt64 => __with_ty__! { UInt64Type },
-        // Float16 => __with_ty__! { Float16Type },
-        Float32 => __with_ty__! { Float32Type },
-        Float64 => __with_ty__! { Float64Type },
-        Utf8 => __with_ty__! { Utf8Type },
+        DataType::Int8 => __with_ty__! { Int8Type },
+        DataType::Int16 => __with_ty__! { Int16Type },
+        DataType::Int32 => __with_ty__! { Int32Type },
+        DataType::Int64 => __with_ty__! { Int64Type },
+        DataType::UInt8 => __with_ty__! { UInt8Type },
+        DataType::UInt16 => __with_ty__! { UInt16Type },
+        DataType::UInt32 => __with_ty__! { UInt32Type },
+        DataType::UInt64 => __with_ty__! { UInt64Type },
+        DataType::Float32 => __with_ty__! { Float32Type },
+        DataType::Float64 => __with_ty__! { Float64Type },
+        DataType::Utf8 => __with_ty__! { Utf8Type },
         _ => panic!("{:?} not implemented", $key_type)
     }
 })}
@@ -174,21 +166,20 @@ macro_rules! with_match_numeric_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Int8 => __with_ty__! { Int8Type },
-        Int16 => __with_ty__! { Int16Type },
-        Int32 => __with_ty__! { Int32Type },
-        Int64 => __with_ty__! { Int64Type },
-        UInt8 => __with_ty__! { UInt8Type },
-        UInt16 => __with_ty__! { UInt16Type },
-        UInt32 => __with_ty__! { UInt32Type },
-        UInt64 => __with_ty__! { UInt64Type },
-        // Float16 => __with_ty__! { Float16Type },
-        Float32 => __with_ty__! { Float32Type },
-        Float64 => __with_ty__! { Float64Type },
+        DataType::Int8 => __with_ty__! { Int8Type },
+        DataType::Int16 => __with_ty__! { Int16Type },
+        DataType::Int32 => __with_ty__! { Int32Type },
+        DataType::Int64 => __with_ty__! { Int64Type },
+        DataType::UInt8 => __with_ty__! { UInt8Type },
+        DataType::UInt16 => __with_ty__! { UInt16Type },
+        DataType::UInt32 => __with_ty__! { UInt32Type },
+        DataType::UInt64 => __with_ty__! { UInt64Type },
+        DataType::Float32 => __with_ty__! { Float32Type },
+        DataType::Float64 => __with_ty__! { Float64Type },
         _ => panic!("{:?} not implemented", $key_type)
     }
 })}
@@ -198,18 +189,18 @@ macro_rules! with_match_integer_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Int8 => __with_ty__! { Int8Type },
-        Int16 => __with_ty__! { Int16Type },
-        Int32 => __with_ty__! { Int32Type },
-        Int64 => __with_ty__! { Int64Type },
-        UInt8 => __with_ty__! { UInt8Type },
-        UInt16 => __with_ty__! { UInt16Type },
-        UInt32 => __with_ty__! { UInt32Type },
-        UInt64 => __with_ty__! { UInt64Type },
+        DataType::Int8 => __with_ty__! { Int8Type },
+        DataType::Int16 => __with_ty__! { Int16Type },
+        DataType::Int32 => __with_ty__! { Int32Type },
+        DataType::Int64 => __with_ty__! { Int64Type },
+        DataType::UInt8 => __with_ty__! { UInt8Type },
+        DataType::UInt16 => __with_ty__! { UInt16Type },
+        DataType::UInt32 => __with_ty__! { UInt32Type },
+        DataType::UInt64 => __with_ty__! { UInt64Type },
         _ => panic!("Only Integer Types are supported, {:?} not implemented", $key_type)
     }
 })}
@@ -219,14 +210,14 @@ macro_rules! with_match_float_and_null_daft_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Null => __with_ty__! { NullType },
+        DataType::Null => __with_ty__! { NullType },
         // Float16 => __with_ty__! { Float16Type },
-        Float32 => __with_ty__! { Float32Type },
-        Float64 => __with_ty__! { Float64Type },
+        DataType::Float32 => __with_ty__! { Float32Type },
+        DataType::Float64 => __with_ty__! { Float64Type },
         _ => panic!("Only Float Types are supported, {:?} not implemented", $key_type)
     }
 })}
@@ -236,20 +227,19 @@ macro_rules! with_match_daft_logical_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
-    #[allow(unused_imports)]
+    use $crate::datatypes::DataType;
     use $crate::datatypes::*;
 
     match $key_type {
-        Decimal128(..) => __with_ty__! { Decimal128Type },
-        Date => __with_ty__! { DateType },
-        Duration(..) => __with_ty__! { DurationType },
-        Timestamp(..) => __with_ty__! { TimestampType },
-        Embedding(..) => __with_ty__! { EmbeddingType },
-        Image(..) => __with_ty__! { ImageType },
-        FixedShapeImage(..) => __with_ty__! { FixedShapeImageType },
-        Tensor(..) => __with_ty__! { TensorType },
-        FixedShapeTensor(..) => __with_ty__! { FixedShapeTensorType },
+        DataType::Decimal128(..) => __with_ty__! { Decimal128Type },
+        DataType::Date => __with_ty__! { DateType },
+        DataType::Duration(..) => __with_ty__! { DurationType },
+        DataType::Timestamp(..) => __with_ty__! { TimestampType },
+        DataType::Embedding(..) => __with_ty__! { EmbeddingType },
+        DataType::Image(..) => __with_ty__! { ImageType },
+        DataType::FixedShapeImage(..) => __with_ty__! { FixedShapeImageType },
+        DataType::Tensor(..) => __with_ty__! { TensorType },
+        DataType::FixedShapeTensor(..) => __with_ty__! { FixedShapeTensorType },
         _ => panic!("{:?} not implemented for with_match_daft_logical_types", $key_type)
     }
 })}
@@ -259,12 +249,12 @@ macro_rules! with_match_daft_logical_primitive_types {(
     $key_type:expr, | $_:tt $T:ident | $($body:tt)*
 ) => ({
     macro_rules! __with_ty__ {( $_ $T:ident ) => ( $($body)* )}
-    use $crate::datatypes::DataType::*;
+    use $crate::datatypes::DataType;
     match $key_type {
-        Decimal128(..) => __with_ty__! { i128 },
-        Duration(..) => __with_ty__! { i64 },
-        Date => __with_ty__! { i32 },
-        Timestamp(..) => __with_ty__! { i64 },
+        DataType::Decimal128(..) => __with_ty__! { i128 },
+        DataType::Duration(..) => __with_ty__! { i64 },
+        DataType::Date => __with_ty__! { i32 },
+        DataType::Timestamp(..) => __with_ty__! { i64 },
         _ => panic!("no logical -> primitive conversion available for {:?}", $key_type)
     }
 })}
