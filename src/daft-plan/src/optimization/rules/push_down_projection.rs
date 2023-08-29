8445ca6693e89316f2925ce9925680a9e183bf82
xcharleslin <4212216+xcharleslin@users.noreply.github.com>
2023-08-29 15:42:22 -0700

    Update src/daft-plan/src/optimization/rules/push_down_projection.rs
    Co-authored-by: Clark Zinzow <clarkzinzow@gmail.com>

--- src/daft-plan/src/optimization/rules/push_down_projection.rs
+++ src/daft-plan/src/optimization/rules/push_down_projection.rs
@@ -140,10 +140,8 @@ impl PushDownProjection {
                     // Retry optimization now that the upstream node is different.
                     let new_plan = self
                         .try_optimize(new_plan.clone())?
-                        .or(Transformed::Yes(new_plan))
-                        .unwrap()
-                        .clone();
-                    Ok(Transformed::Yes(new_plan))
+                        .or(Transformed::Yes(new_plan));
+                    Ok(new_plan)
                 } else {
                     Ok(Transformed::No(plan))
                 }
