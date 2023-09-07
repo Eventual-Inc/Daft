6f6ca88fe5e8d479fa536574bdf968c6ecdb3587
Jay Chia <17691182+jaychia@users.noreply.github.com>
2023-09-07 16:22:57 -0700

    [CHORE] Add status code to IO integration tests (#1356)
    Some retry logic only triggers on the string status code in the
    `<Code></Code>` section of the XML response from S3, not on the actual
    int status code itself
    
    This PR adds the ability to supply this string status code to our local
    test fixtures so we can test for these cases.
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

--- tests/integration/docker-compose/retry_server/utils/responses.py
+++ tests/integration/docker-compose/retry_server/utils/responses.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 from fastapi import Response
 
 
-def get_response(url: str, status_code: int):
+def get_response(url: str, status_code: int, code_str: str):
     return Response(
         status_code=status_code,
         content=f"""<?xml version="1.0" encoding="UTF-8"?>
 <Error>
-<Code></Code>
-<Message>This is a mock error message</Message>
+<Code>{code_str}</Code>
+<Message>{code_str}</Message>
 <Resource>{url}</Resource>
 <RequestId>4442587FB7D0A2F9</RequestId>
 </Error>""",
