244e0715643f89a3c227bf40eb3498ef2ded93eb
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-09-18 23:41:33 -0700

    Add a test that tests timezone-aware timestamp types

--- tests/series/test_temporal_ops.py
+++ tests/series/test_temporal_ops.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from daft.datatype import DataType
+import pytest
+
+from daft.datatype import DataType, TimeUnit
 from daft.series import Series
 
 
@@ -81,37 +83,42 @@ def test_series_date_day_of_week_operation() -> None:
     assert [0, 1, 2, None, 3, 4, None, 5, 6] == day_of_weeks.to_pylist()
 
 
-def test_series_timestamp_day_operation() -> None:
+@pytest.mark.parametrize("tz", [None, "UTC", "+08:00"])
+def test_series_timestamp_day_operation(tz) -> None:
     from datetime import datetime
 
     def ts_maker(d):
         if d is None:
             return None
-        return datetime(2023, 1, d, 1, 1, 1)
+        return datetime(2023, 1, d, 23, 1, 1)
 
     input = [1, 5, 14, None, 23, None, 28]
 
     input_ts = list(map(ts_maker, input))
-    s = Series.from_pylist(input_ts)
+    s = Series.from_pylist(input_ts).cast(DataType.timestamp(TimeUnit.ms(), timezone=tz))
     days = s.dt.day()
 
     assert days.datatype() == DataType.uint32()
 
-    assert input == days.to_pylist()
+    # If tz is +08:00, then we expect the days to be +1 because our ts_maker makes timestamps with times at 23:00
+    expected = [d and d + 1 for d in input] if tz == "+08:00" else input
+
+    assert expected == days.to_pylist()
 
 
-def test_series_timestamp_month_operation() -> None:
+@pytest.mark.parametrize("tz", [None, "UTC", "+08:00"])
+def test_series_timestamp_month_operation(tz) -> None:
     from datetime import datetime
 
     def ts_maker(m):
         if m is None:
             return None
-        return datetime(2023, m, 1, 1, 1, 1)
+        return datetime(2023, m, 1, 23, 1, 1)
 
     input = list(range(1, 10)) + [None, 11, 12]
 
     input_ts = list(map(ts_maker, input))
-    s = Series.from_pylist(input_ts)
+    s = Series.from_pylist(input_ts).cast(DataType.timestamp(TimeUnit.ms(), timezone=tz))
     months = s.dt.month()
 
     assert months.datatype() == DataType.uint32()
@@ -119,18 +126,19 @@ def test_series_timestamp_month_operation() -> None:
     assert input == months.to_pylist()
 
 
-def test_series_timestamp_year_operation() -> None:
+@pytest.mark.parametrize("tz", [None, "UTC", "+08:00"])
+def test_series_timestamp_year_operation(tz) -> None:
     from datetime import datetime
 
     def ts_maker(y):
         if y is None:
             return None
-        return datetime(y, 1, 1, 1, 1, 1)
+        return datetime(y, 1, 1, 23, 1, 1)
 
     input = [1, 1969, 2023 + 5, 2023 + 4, 2023 + 1, None, 2023 + 2, None]
 
     input_ts = list(map(ts_maker, input))
-    s = Series.from_pylist(input_ts)
+    s = Series.from_pylist(input_ts).cast(DataType.timestamp(TimeUnit.ms(), timezone=tz))
     years = s.dt.year()
 
     assert years.datatype() == DataType.int32()
