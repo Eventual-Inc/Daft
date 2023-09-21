54d2ceac46e63146e23bf5ec9047fb13504519dc
Sammy Sidhu <samster25@users.noreply.github.com>
2023-09-20 17:34:52 -0700

    set argument for num_parallel_tasks

--- daft/daft.pyi
+++ daft/daft.pyi
@@ -388,6 +388,7 @@ def read_parquet_into_pyarrow(
     num_rows: int | None = None,
     row_groups: list[int] | None = None,
     io_config: IOConfig | None = None,
+    num_parallel_tasks: int | None = None,
     multithreaded_io: bool | None = None,
     coerce_int96_timestamp_unit: PyTimeUnit | None = None,
 ): ...
@@ -398,6 +399,7 @@ def read_parquet_into_pyarrow_bulk(
     num_rows: int | None = None,
     row_groups: list[list[int]] | None = None,
     io_config: IOConfig | None = None,
+    num_parallel_tasks: int | None = None,
     multithreaded_io: bool | None = None,
     coerce_int96_timestamp_unit: PyTimeUnit | None = None,
 ): ...
