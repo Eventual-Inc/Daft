c96251d1a944ad58b7a3dedbbbe39c20fd7a8da5
Sammy Sidhu <samster25@users.noreply.github.com>
2023-09-21 21:16:59 -0700

    initial stubs

---
+++ daft/stubs/catalog/iceberg.py
@@ -0,0 +1,106 @@
+# mypy: ignore-errors
+
+from __future__ import annotations
+
+from contextlib import AbstractContextManager
+
+import daft
+from daft.daft import PartitionSpec
+from daft.dataframe import DataFrame
+from daft.logical.schema import Schema
+
+
+class IcebergCatalog:
+    # creator functions
+    @classmethod
+    def from_glue(cls, args) -> IcebergCatalog:
+        ...
+
+    def from_storage(cls, args) -> IcebergCatalog:
+        ...
+
+    # namespace functions
+    def list_namespaces(self) -> DataFrame:
+        ...
+
+    def create_namespace(self, namespace: str) -> bool:
+        ...
+
+    def drop_namespace(self, namespace: str) -> bool:
+        ...
+
+    # table functions
+    def list_tables(self) -> DataFrame:
+        ...
+
+    def create_table(
+        self,
+        identifier: str,
+        schema: Schema,
+        partition_spec: PartitionSpec | None = None,
+        sort_columns: list[str] | None = None,
+    ) -> Table:
+        ...
+
+    def load_table(self, identifier: str) -> Table:
+        ...
+
+
+class Table:
+    def history(self) -> DataFrame:
+        ...
+
+    def schema(self) -> Schema:
+        ...
+
+    def schemas(self) -> dict[int, Schema]:
+        ...
+
+    def partition_spec(self) -> PartitionSpec:
+        ...
+
+    def partition_specs(self) -> dict[int, PartitionSpec]:
+        ...
+
+    def update_schema(self) -> SchemaUpdate:
+        ...
+
+    def update_partition_spec(self) -> PartitionSpecUpdate:
+        ...
+
+
+class SchemaUpdate(AbstractContextManager):
+    ...
+
+
+class PartitionSpecUpdate(AbstractContextManager):
+    ...
+
+
+class DataframeIcebergNamespace:
+    def __init__(self, df: DataFrame) -> None:
+        self.df = df
+
+    @classmethod
+    def read(cls, catalog: IcebergCatalog, table_identifier: str, snapshot_id: int) -> DataFrame:
+        ...
+
+    def append(self, catalog: IcebergCatalog, table_identifier: str) -> None:
+        ...
+
+    def overwrite(self, catalog: IcebergCatalog, table_identifier: str) -> None:
+        ...
+
+
+def example_deleting_rows() -> None:
+    catalog = IcebergCatalog.from_glue("path/to/glue")
+    df: DataFrame = daft.read_iceberg(catalog, "my-table")
+    df = df.where(df["id"] > 10 & df["id"] < 20)
+    df.iceberg.overwrite(catalog, "my-table")
+
+
+def example_updating_rows() -> None:
+    catalog = IcebergCatalog.from_glue("path/to/glue")
+    df: DataFrame = daft.read_iceberg(catalog, "my-table")
+    df = df.with_column("x", (df["x"] < 10).if_else(0, df["x"]))
+    df.iceberg.overwrite(catalog, "my-table")
