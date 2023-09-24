a3294c3ae2970306d8fb410400fe1b1b1106a2d0
Sammy Sidhu <samster25@users.noreply.github.com>
2023-09-24 15:54:09 -0700

    try without comparision

--- src/daft-io/src/lib.rs
+++ src/daft-io/src/lib.rs
@@ -250,6 +250,8 @@ fn parse_url(input: &str) -> Result<(SourceType, Cow<'_, str>)> {
         "s3" => Ok((SourceType::S3, fixed_input)),
         "az" | "abfs" => Ok((SourceType::AzureBlob, fixed_input)),
         "gcs" | "gs" => Ok((SourceType::GCS, fixed_input)),
+        #[cfg(target_env = "msvc")]
+        _ if scheme.len() == 1 => k((SourceType::File, Cow::Owned(format!("file://{input}")))),
         _ => Err(Error::NotImplementedSource { store: scheme }),
     }
 }
