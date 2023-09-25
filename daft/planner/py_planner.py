f45f5231cd6d8b8a362d65cd4b74281fc16f3a32
Clark Zinzow <clarkzinzow@gmail.com>
2023-09-25 15:00:10 -0700

    [BUG] Fix runner check at plan execution time for new query planner (#1435)
    This PR fixes `is_ray_runner` check at plan execution time in the new
    query planner. Previously, we were checking the Daft context at
    execution time, which wouldn't be properly set under the Ray runner,
    since we don't propagate the Daft context to all Ray workers. This PR
    changes the runner check to be based on an explicit flag passed from the
    runner to the physical plan execution API.
    
    Closes #1433

--- daft/planner/py_planner.py
+++ daft/planner/py_planner.py
@@ -9,5 +9,7 @@ class PyPhysicalPlanScheduler(PhysicalPlanScheduler):
     def __init__(self, plan: logical_plan.LogicalPlan):
         self._plan = plan
 
-    def to_partition_tasks(self, psets: dict[str, list[PartitionT]]) -> physical_plan.MaterializedPhysicalPlan:
+    def to_partition_tasks(
+        self, psets: dict[str, list[PartitionT]], is_ray_runner: bool
+    ) -> physical_plan.MaterializedPhysicalPlan:
         return physical_plan.materialize(physical_plan_factory._get_physical_plan(self._plan, psets))
