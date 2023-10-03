70f292265dfd200fa1662e3dcc049e24f4e11754
Sammy Sidhu <samster25@users.noreply.github.com>
2023-10-03 16:35:34 -0700

    add local int tests

--- tests/integration/io/parquet/test_reads_public_data.py
+++ tests/integration/io/parquet/test_reads_public_data.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+import os
+import tempfile
+
 import pandas as pd
 import pyarrow as pa
 import pytest
+import requests
 from pyarrow import parquet as pq
 
 import daft
@@ -188,6 +192,22 @@ def parquet_file(request) -> tuple[str, str]:
     return request.param
 
 
+HTTP_CAN_READ_FILES = [param for param in DAFT_CAN_READ_FILES if param[1].startswith("http")]
+
+
+@pytest.fixture(scope="session", params=HTTP_CAN_READ_FILES, ids=[name for name, _ in HTTP_CAN_READ_FILES])
+def local_parquet_file(request) -> tuple[str, str]:
+    """Returns a tuple of (`name`, `url`) of files that Daft should be able to handle. URLs input are remote but will return local paths."""
+    name, url = request.param
+    with tempfile.TemporaryDirectory() as dir:
+        file = os.path.join(dir, "tempfile")
+        response = requests.get(url)
+        with open(file, "wb") as f:
+            f.write(response.content)
+
+        yield (name, file)
+
+
 def read_parquet_with_pyarrow(path) -> pa.Table:
     kwargs = {}
     if get_protocol_from_path(path) == "s3":
@@ -208,6 +228,19 @@ def read_parquet_with_pyarrow(path) -> pa.Table:
 )
 def test_parquet_read_table(parquet_file, public_storage_io_config, multithreaded_io):
     _, url = parquet_file
+    daft_native_read = Table.read_parquet(url, io_config=public_storage_io_config, multithreaded_io=multithreaded_io)
+    pa_read = Table.from_arrow(read_parquet_with_pyarrow(url))
+    assert daft_native_read.schema() == pa_read.schema()
+    pd.testing.assert_frame_equal(daft_native_read.to_pandas(), pa_read.to_pandas())
+
+
+@pytest.mark.integration()
+@pytest.mark.parametrize(
+    "multithreaded_io",
+    [False, True],
+)
+def test_parquet_read_table_local(local_parquet_file, public_storage_io_config, multithreaded_io):
+    _, url = local_parquet_file
     daft_native_read = Table.read_parquet(url, io_config=public_storage_io_config, multithreaded_io=multithreaded_io)
     pa_read = Table.from_arrow(read_parquet_with_pyarrow(url))
     assert daft_native_read.schema() == pa_read.schema()
