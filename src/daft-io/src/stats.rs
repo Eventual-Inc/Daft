a9fd6e0f94ba1b8e168d3e36eafc8f0b64862724
Sammy Sidhu <samster25@users.noreply.github.com>
2023-10-16 16:47:38 -0700

    add pq loading

--- src/daft-io/src/stats.rs
+++ src/daft-io/src/stats.rs
@@ -19,7 +19,7 @@ impl Drop for IOStatsContext {
         let bytes_read = self.load_bytes_read();
         let num_gets = self.load_get_requests();
         let mean_size = (bytes_read as f64) / (num_gets as f64);
-        log::debug!(
+        log::info!(
             "IOStatsContext: {}, Gets: {}, Heads: {}, Lists: {}, BytesRead: {}, AvgGetSize: {}",
             self.name,
             num_gets,
