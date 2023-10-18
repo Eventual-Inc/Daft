75086c4ad16614640e88679c63be6d1f914e3de2
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-10-19 08:21:38 +0900

    Add size_bytes tests

--- tests/table/test_size_bytes.py
+++ tests/table/test_size_bytes.py
@@ -6,6 +6,22 @@ from daft import DataType, col
 from daft.table import Table
 
 
+def test_micropartitions_size_bytes_empty() -> None:
+    mp = Table.from_pydict({"a": []})
+    assert mp.size_bytes() == 0
+
+
+@pytest.mark.parametrize(
+    "mp",
+    [
+        Table.from_pydict({"a": [1, 3, 2, 4]}),  # 1 table
+        Table.concat([Table.from_pydict({"a": [1]}), Table.from_pydict({"a": [3, 2, 4]})]),  # 2 tables
+    ],
+)
+def test_micropartitions_size_bytes(mp) -> None:
+    assert mp.size_bytes() == (4 * 8)
+
+
 def test_table_size_bytes() -> None:
 
     data = Table.from_pydict({"a": [1, 2, 3, 4, None], "b": [False, True, False, True, None]}).eval_expression_list(
