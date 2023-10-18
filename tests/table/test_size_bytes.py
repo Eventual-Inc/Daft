d8439b5e3009367835b739a104f412a0fe8aa323
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-10-18 15:21:59 +0900

    test_size_bytes.py

--- tests/table/test_size_bytes.py
+++ tests/table/test_size_bytes.py
@@ -3,21 +3,23 @@ from __future__ import annotations
 import pytest
 
 from daft import DataType, col
-from daft.table import Table
+from daft.table import MicroPartition, Table
 
 
-def test_table_size_bytes() -> None:
+@pytest.mark.parametrize("TableCls", [Table, MicroPartition])
+def test_table_size_bytes(TableCls) -> None:
 
-    data = Table.from_pydict({"a": [1, 2, 3, 4, None], "b": [False, True, False, True, None]}).eval_expression_list(
+    data = TableCls.from_pydict({"a": [1, 2, 3, 4, None], "b": [False, True, False, True, None]}).eval_expression_list(
         [col("a").cast(DataType.int64()), col("b")]
     )
     assert data.size_bytes() == (5 * 8 + 1) + (1 + 1)
 
 
 @pytest.mark.parametrize("length", [10, 200])
-def test_table_size_bytes_pyobj(length) -> None:
+@pytest.mark.parametrize("TableCls", [Table, MicroPartition])
+def test_table_size_bytes_pyobj(TableCls, length) -> None:
     import pickle
 
     size_per_obj = len(pickle.dumps(object()))
-    data = Table.from_pydict({"a": [object()] * length})
+    data = TableCls.from_pydict({"a": [object()] * length})
     assert data.size_bytes() == size_per_obj * length
