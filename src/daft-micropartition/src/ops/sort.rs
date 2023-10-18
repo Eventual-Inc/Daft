2801c71aab5d7639c8cb7a62251a6690bf96817d
Sammy Sidhu <samster25@users.noreply.github.com>
2023-10-17 17:17:41 -0700

    sort methods

---
+++ src/daft-micropartition/src/ops/sort.rs
@@ -0,0 +1,42 @@
+use std::sync::Arc;
+
+use common_error::DaftResult;
+use daft_core::Series;
+use daft_dsl::Expr;
+use snafu::ResultExt;
+
+use crate::{
+    column_stats::TruthValue,
+    micropartition::{MicroPartition, TableState},
+    table_metadata::TableMetadata,
+    DaftCoreComputeSnafu,
+};
+
+impl MicroPartition {
+
+    pub fn sort(&self, sort_keys: &[Expr], descending: &[bool]) -> DaftResult<Self> {
+        let tables = self.concat_or_get()?;
+        if let [single] = tables.as_slice() {
+            let sorted = single.sort(sort_keys, descending)?;
+            Ok(Self::new(
+                self.schema.clone(),
+                TableState::Loaded(Arc::new(vec![sorted])),
+                self.metadata.clone(),
+                self.statistics.clone(),
+            ))
+        } else {
+            unreachable!()
+        }
+    }
+
+    pub fn argsort(&self, sort_keys: &[Expr], descending: &[bool]) -> DaftResult<Series> {
+        let tables = self.concat_or_get()?;
+        if let [single] = tables.as_slice() {
+            single.argsort(sort_keys, descending)
+        } else {
+            unreachable!()
+        }
+
+    }
+
+}
