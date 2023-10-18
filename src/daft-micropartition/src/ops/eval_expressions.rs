36e744fe61e3bbe9b2dcb6b742ae2445808a0323
Sammy Sidhu <samster25@users.noreply.github.com>
2023-10-17 18:46:21 -0700

    add eval expressions

--- src/daft-micropartition/src/ops/eval_expressions.rs
+++ src/daft-micropartition/src/ops/eval_expressions.rs
@@ -1,4 +1,7 @@
-use common_error::DaftResult;
+use std::{collections::HashSet, sync::Arc};
+
+use common_error::{DaftError, DaftResult};
+use daft_core::schema::{Schema, SchemaRef};
 use daft_dsl::Expr;
 use snafu::ResultExt;
 
@@ -9,16 +12,45 @@ use crate::{
     DaftCoreComputeSnafu,
 };
 
+fn infer_schema(exprs: &[Expr], schema: &Schema) -> DaftResult<SchemaRef> {
+    let fields = exprs
+        .iter()
+        .map(|e| e.to_field(schema).context(DaftCoreComputeSnafu))
+        .collect::<crate::Result<Vec<_>>>()?;
+
+    let mut seen: HashSet<String> = HashSet::new();
+    for field in fields.iter() {
+        let name = &field.name;
+        if seen.contains(name) {
+            return Err(DaftError::ValueError(format!(
+                "Duplicate name found when evaluating expressions: {name}"
+            )));
+        }
+        seen.insert(name.clone());
+    }
+    Ok(Schema::new(fields)?.into())
+}
+
 impl MicroPartition {
     pub fn eval_expression_list(&self, exprs: &[Expr]) -> DaftResult<Self> {
+        let expected_schema = infer_schema(exprs, &self.schema)?;
         let tables = self.tables_or_read(None)?;
         let evaluated_tables = tables
             .iter()
             .map(|t| t.eval_expression_list(exprs))
             .collect::<DaftResult<Vec<_>>>()?;
+
         let eval_stats = self
             .statistics
-            .and_then(|s| Some(s.eval_expression_list(exprs, self.schema.as_ref())))
+            .as_ref()
+            .and_then(|s| Some(s.eval_expression_list(exprs, expected_schema.as_ref())))
             .transpose()?;
+
+        Ok(MicroPartition::new(
+            expected_schema,
+            TableState::Loaded(Arc::new(evaluated_tables)),
+            TableMetadata { length: self.len() },
+            eval_stats,
+        ))
     }
 }
