042f7f2793015b318811e96ac6e91c9c016ee468
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-10-30 13:12:09 -0700

    Use single-threaded runtime

--- src/daft-scan/src/glob.rs
+++ src/daft-scan/src/glob.rs
@@ -2,7 +2,7 @@ use std::{fmt::Display, sync::Arc};
 
 use common_error::DaftResult;
 use daft_core::schema::SchemaRef;
-use daft_io::{get_io_client, IOClient};
+use daft_io::{get_io_client, get_runtime};
 
 use crate::{DataFileSource, FileType, PartitionField, ScanOperator, ScanOperatorRef, ScanTask};
 #[derive(Debug)]
@@ -12,15 +12,13 @@ pub struct GlobScanOperator {
     columns_to_select: Option<Vec<String>>,
     limit: Option<usize>,
     schema: SchemaRef,
-    runtime: Arc<tokio::runtime::Runtime>,
     io_config: Arc<daft_io::IOConfig>,
 }
 
-fn run_glob(
-    glob_path: &str,
-    io_client: Arc<IOClient>,
-    runtime: Arc<tokio::runtime::Runtime>,
-) -> DaftResult<Vec<String>> {
+fn run_glob(glob_path: &str, io_config: Arc<daft_io::IOConfig>) -> DaftResult<Vec<String>> {
+    // Use single-threaded runtime which should be safe here as it is not shared across async contexts
+    let runtime = get_runtime(false)?;
+    let io_client = get_io_client(false, io_config)?;
     runtime.block_on(async {
         Ok(io_client
             .as_ref()
@@ -37,16 +35,13 @@ impl GlobScanOperator {
         glob_path: &str,
         file_type: FileType,
         io_config: Arc<daft_io::IOConfig>,
-        runtime: Arc<tokio::runtime::Runtime>,
     ) -> DaftResult<Self> {
-        // TODO: Allow for returning errors from this function
-        let io_client = get_io_client(false, io_config)?;
         // TODO: Glob for first file using a limit
-        let paths = run_glob(glob_path, io_client, runtime)?;
+        let paths = run_glob(glob_path, io_config)?;
         let _first_filepath = paths[0].as_str();
         let schema = match file_type {
-            FileType::Csv => todo!(),
             FileType::Parquet => todo!(),
+            FileType::Csv => todo!(),
             FileType::Avro => todo!("Schema inference for Avro not implemented"),
             FileType::Orc => todo!("Schema inference for Orc not implemented"),
         };
@@ -57,7 +52,6 @@ impl GlobScanOperator {
             columns_to_select: None,
             limit: None,
             schema,
-            runtime: runtime.clone(),
             io_config,
         })
     }
@@ -109,8 +103,7 @@ impl ScanOperator for GlobScanOperator {
     fn to_scan_tasks(
         self: Box<Self>,
     ) -> DaftResult<Box<dyn Iterator<Item = DaftResult<crate::ScanTask>>>> {
-        let io_client = get_io_client(false, self.io_config.clone())?;
-        let files = run_glob(self.glob_path.as_str(), io_client, self.runtime.clone())?;
+        let files = run_glob(self.glob_path.as_str(), self.io_config.clone())?;
         let iter = files.into_iter().map(move |f| {
             let source = DataFileSource::AnonymousDataFile {
                 file_type: self.file_type,
