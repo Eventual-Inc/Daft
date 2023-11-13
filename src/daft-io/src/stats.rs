e01d25cd1d7e3a6d2fa9a3fa4ac111d2e280c1b1
Sammy Sidhu <samster25@users.noreply.github.com>
2023-11-13 14:51:57 -0800

    into string

--- src/daft-io/src/stats.rs
+++ src/daft-io/src/stats.rs
@@ -38,9 +38,9 @@ pub(crate) struct IOStatsByteStreamContextHandle {
 }
 
 impl IOStatsContext {
-    pub fn new(name: String) -> IOStatsRef {
+    pub fn new<S: Into<String>>(name: S) -> IOStatsRef {
         Arc::new(IOStatsContext {
-            name,
+            name: name.into(),
             num_get_requests: atomic::AtomicUsize::new(0),
             num_head_requests: atomic::AtomicUsize::new(0),
             num_list_requests: atomic::AtomicUsize::new(0),
