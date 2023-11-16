7246386f3d08352305dd5fd1e4867ec6c0a0fc4d
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-11-16 12:02:12 -0800

    [BUG] Self-concat breaks with the RayRunner

--- tests/dataframe/test_concat.py
+++ tests/dataframe/test_concat.py
@@ -17,3 +17,8 @@ def test_concat_schema_mismatch():
     df2 = daft.from_pydict({"foo": ["4", "5", "6"]})
     with pytest.raises(ValueError):
         df1.concat(df2)
+
+
+def test_self_concat():
+    df = daft.from_pydict({"foo": [1, 2, 3]})
+    assert df.concat(df).to_pydict() == {"foo": [1, 2, 3, 1, 2, 3]}
