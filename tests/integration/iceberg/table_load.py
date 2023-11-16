6aea0a7a8fb4d10f0092a1ebf627cb66c2a846c8
Sammy Sidhu <samster25@users.noreply.github.com>
2023-11-16 12:26:18 -0800

    add github workflow

--- tests/integration/iceberg/table_load.py
+++ tests/integration/iceberg/table_load.py
@@ -5,6 +5,7 @@ from pyiceberg.io.pyarrow import schema_to_pyarrow
 
 import daft
 from daft.logical.schema import Schema
+from tests.conftest import assert_df_equals
 
 
 @pytest.mark.integration()
@@ -17,7 +18,7 @@ def test_daft_iceberg_table_open(local_iceberg_tables):
 
 
 WORKING_SHOW_COLLECT = [
-    "test_all_types",
+    # "test_all_types", # ValueError: DaftError::ArrowError Not yet implemented: Deserializing type Decimal(10, 2) from parquet
     "test_limit",
     "test_null_nan",
     "test_null_nan_rewritten",
@@ -28,15 +29,28 @@ WORKING_SHOW_COLLECT = [
     "test_partitioned_by_months",
     "test_partitioned_by_truncate",
     "test_partitioned_by_years",
-    "test_positional_mor_deletes",
-    "test_positional_mor_double_deletes",
-    "test_table_sanitized_character",
-    "test_table_version",
+    # "test_positional_mor_deletes", # Need Merge on Read
+    # "test_positional_mor_double_deletes", # Need Merge on Read
+    # "test_table_sanitized_character", # Bug in scan().to_arrow().to_arrow()
+    # "test_table_version", # we have bugs when loading no files
     "test_uuid_and_fixed_unpartitioned",
 ]
 
 
 @pytest.mark.integration()
-def test_daft_iceberg_table_show(local_iceberg_tables):
-    df = daft.read_iceberg(local_iceberg_tables)
+@pytest.mark.parametrize("table_name", WORKING_SHOW_COLLECT)
+def test_daft_iceberg_table_show(table_name, local_iceberg_catalog):
+    tab = local_iceberg_catalog.load_table(f"default.{table_name}")
+    df = daft.read_iceberg(tab)
     df.show()
+
+
+@pytest.mark.integration()
+@pytest.mark.parametrize("table_name", WORKING_SHOW_COLLECT)
+def test_daft_iceberg_table_collect_correct(table_name, local_iceberg_catalog):
+    tab = local_iceberg_catalog.load_table(f"default.{table_name}")
+    df = daft.read_iceberg(tab)
+    df.collect()
+    daft_pandas = df.to_pandas()
+    iceberg_pandas = tab.scan().to_arrow().to_pandas()
+    assert_df_equals(daft_pandas, iceberg_pandas, sort_key=[])
