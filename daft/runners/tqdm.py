7ae353844c5de326536ef4f652f9676813b6c5dc
Sammy Sidhu <samster25@users.noreply.github.com>
2023-11-16 16:18:18 -0800

    Update daft/runners/tqdm.py
    Co-authored-by: Jay Chia <17691182+jaychia@users.noreply.github.com>

--- daft/runners/tqdm.py
+++ daft/runners/tqdm.py
@@ -6,5 +6,5 @@ def is_running_from_ipython():
         from IPython import get_ipython
 
         return get_ipython() is not None
-    except Exception:
+    except ImportError:
         return False
