f166634c74f2aa7464687683c3256e2e6ee8bc25
Sammy Sidhu <samster25@users.noreply.github.com>
2023-11-28 12:26:16 -0800

    required column visitor

--- src/daft-dsl/src/optimization.rs
+++ src/daft-dsl/src/optimization.rs
@@ -1,46 +1,29 @@
 use std::collections::HashMap;
 
+use common_error::DaftResult;
+use common_treenode::{TreeNode, TreeNodeVisitor, VisitRecursion};
+
 use super::expr::{AggExpr, Expr};
 
-pub fn get_required_columns(e: &Expr) -> Vec<String> {
-    // Returns all the column names required by this expression
-    match e {
-        Expr::Alias(child, _) => get_required_columns(child),
-        Expr::Agg(agg) => match agg {
-            AggExpr::Count(child, ..)
-            | AggExpr::Sum(child)
-            | AggExpr::Mean(child)
-            | AggExpr::Min(child)
-            | AggExpr::Max(child)
-            | AggExpr::List(child)
-            | AggExpr::Concat(child) => get_required_columns(child),
-        },
-        Expr::BinaryOp { left, right, .. } => {
-            let mut req_cols = get_required_columns(left);
-            req_cols.extend(get_required_columns(right));
-            req_cols
-        }
-        Expr::Cast(child, _) => get_required_columns(child),
-        Expr::Column(name) => vec![name.to_string()],
-        Expr::Function { inputs, .. } => {
-            let child_required_columns: Vec<Vec<String>> =
-                inputs.iter().map(get_required_columns).collect();
-            child_required_columns.concat()
-        }
-        Expr::Not(child) => get_required_columns(child),
-        Expr::IsNull(child) => get_required_columns(child),
-        Expr::Literal(_) => vec![],
-        Expr::IfElse {
-            if_true,
-            if_false,
-            predicate,
-        } => {
-            let mut req_cols = get_required_columns(if_true);
-            req_cols.extend(get_required_columns(if_false));
-            req_cols.extend(get_required_columns(predicate));
-            req_cols
-        }
+struct RequiredColumnVisitor {
+    required: Vec<String>,
+}
+
+impl TreeNodeVisitor for RequiredColumnVisitor {
+    type N = Expr;
+    fn pre_visit(&mut self, node: &Self::N) -> DaftResult<VisitRecursion> {
+        if let Expr::Column(name) = node {
+            self.required.push(name.as_ref().into());
+        };
+        Ok(VisitRecursion::Continue)
     }
+}
+
+pub fn get_required_columns(e: &Expr) -> Vec<String> {
+    let mut visitor = RequiredColumnVisitor { required: vec![] };
+    e.visit(&mut visitor)
+        .expect("Error occurred when visiting for required columns");
+    visitor.required
 }
 
 pub fn requires_computation(e: &Expr) -> bool {
@@ -126,76 +109,6 @@ pub fn replace_columns_with_expressions(expr: &Expr, replace_map: &HashMap<Strin
             if_true: replace_columns_with_expressions(if_true, replace_map).into(),
             if_false: replace_columns_with_expressions(if_false, replace_map).into(),
             predicate: replace_columns_with_expressions(predicate, replace_map).into(),
-        },
-    }
-}
-
-pub fn replace_column_with_expression(expr: &Expr, column_name: &str, new_expr: &Expr) -> Expr {
-    // Constructs a new deep-copied Expr which is `expr` but with all occurrences of Column(column_name) recursively
-    // replaced with `new_expr`
-    match expr {
-        // BASE CASE: found a matching column
-        Expr::Column(name) if name.as_ref().eq(column_name) => new_expr.clone(),
-
-        // BASE CASE: reached non-matching leaf node
-        Expr::Column(..) => expr.clone(),
-        Expr::Literal(_) => expr.clone(),
-
-        // RECURSIVE CASE: recursively replace for matching column
-        Expr::Alias(child, name) => Expr::Alias(
-            replace_column_with_expression(child, column_name, new_expr).into(),
-            (*name).clone(),
-        ),
-        Expr::Agg(agg) => match agg {
-            AggExpr::Count(child, mode) => Expr::Agg(AggExpr::Count(
-                replace_column_with_expression(child, column_name, new_expr).into(),
-                *mode,
-            )),
-            AggExpr::Sum(child) => Expr::Agg(AggExpr::Sum(
-                replace_column_with_expression(child, column_name, new_expr).into(),
-            )),
-            AggExpr::Mean(child) => Expr::Agg(AggExpr::Mean(
-                replace_column_with_expression(child, column_name, new_expr).into(),
-            )),
-            AggExpr::Min(child) => Expr::Agg(AggExpr::Min(
-                replace_column_with_expression(child, column_name, new_expr).into(),
-            )),
-            AggExpr::Max(child) => Expr::Agg(AggExpr::Max(
-                replace_column_with_expression(child, column_name, new_expr).into(),
-            )),
-            AggExpr::List(child) => Expr::Agg(AggExpr::List(
-                replace_column_with_expression(child, column_name, new_expr).into(),
-            )),
-            AggExpr::Concat(child) => Expr::Agg(AggExpr::List(
-                replace_column_with_expression(child, column_name, new_expr).into(),
-            )),
-        },
-        Expr::BinaryOp { left, right, op } => Expr::BinaryOp {
-            op: *op,
-            left: replace_column_with_expression(left, column_name, new_expr).into(),
-            right: replace_column_with_expression(right, column_name, new_expr).into(),
-        },
-        Expr::Cast(child, name) => Expr::Cast(
-            replace_column_with_expression(child, column_name, new_expr).into(),
-            (*name).clone(),
-        ),
-        Expr::Function { inputs, func } => Expr::Function {
-            func: func.clone(),
-            inputs: inputs
-                .iter()
-                .map(|e| replace_column_with_expression(e, column_name, new_expr))
-                .collect(),
-        },
-        Expr::Not(child) => replace_column_with_expression(child, column_name, new_expr),
-        Expr::IsNull(child) => replace_column_with_expression(child, column_name, new_expr),
-        Expr::IfElse {
-            if_true,
-            if_false,
-            predicate,
-        } => Expr::IfElse {
-            if_true: replace_column_with_expression(if_true, column_name, new_expr).into(),
-            if_false: replace_column_with_expression(if_false, column_name, new_expr).into(),
-            predicate: replace_column_with_expression(predicate, column_name, new_expr).into(),
         },
     }
 }
