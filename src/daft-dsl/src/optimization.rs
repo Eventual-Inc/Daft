94e571d0de5bc58ba87ed640ee15620f90cb5b1c
Sammy Sidhu <samster25@users.noreply.github.com>
2023-11-28 13:27:16 -0800

    use transform instead

--- src/daft-dsl/src/optimization.rs
+++ src/daft-dsl/src/optimization.rs
@@ -1,9 +1,7 @@
 use std::collections::HashMap;
 
 use common_error::DaftResult;
-use common_treenode::{
-    RewriteRecursion, TreeNode, TreeNodeRewriter, TreeNodeVisitor, VisitRecursion,
-};
+use common_treenode::{TreeNode, TreeNodeVisitor, VisitRecursion};
 
 use super::expr::Expr;
 
@@ -43,33 +41,14 @@ pub fn requires_computation(e: &Expr) -> bool {
     }
 }
 
-struct ColumnExpressionRewriter<'a> {
-    mapping: &'a HashMap<String, Expr>,
-}
-
-impl<'a> TreeNodeRewriter for ColumnExpressionRewriter<'a> {
-    type N = Expr;
-    fn pre_visit(&mut self, node: &Self::N) -> DaftResult<RewriteRecursion> {
-        if let Expr::Column(name) = node && self.mapping.contains_key(name.as_ref()) {
-            Ok(RewriteRecursion::Continue)
-        } else {
-            Ok(RewriteRecursion::Skip)
-        }
-    }
-    fn mutate(&mut self, node: Self::N) -> DaftResult<Self::N> {
-        if let Expr::Column(ref name) = node && let Some(tgt) = self.mapping.get(name.as_ref()){
-            Ok(tgt.clone())
-        } else {
-            Ok(node)
-        }
-    }
-}
-
 pub fn replace_columns_with_expressions(expr: &Expr, replace_map: &HashMap<String, Expr>) -> Expr {
-    let mut column_rewriter = ColumnExpressionRewriter {
-        mapping: replace_map,
-    };
     expr.clone()
-        .rewrite(&mut column_rewriter)
+        .transform(&|e| {
+            if let Expr::Column(ref name) = e && let Some(tgt) = replace_map.get(name.as_ref()){
+            Ok(common_treenode::Transformed::Yes(tgt.clone()))
+        } else {
+            Ok(common_treenode::Transformed::No(e))
+        }
+        })
         .expect("Error occurred when rewriting column expressions")
 }
