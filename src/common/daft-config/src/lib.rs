c58609705410579ac1e79a725c442ccb0d91ac09
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-12-08 16:26:47 -0800

    Add DaftPlanningConfig

--- src/common/daft-config/src/lib.rs
+++ src/common/daft-config/src/lib.rs
@@ -1,5 +1,25 @@
+use common_io_config::IOConfig;
 use serde::{Deserialize, Serialize};
 
+/// Configurations for Daft to use during the building of a Dataframe's plan.
+///
+/// 1. Creation of a Dataframe including any file listing and schema inference that needs to happen. Note
+///     that this does not include the actual scan, which is taken care of by the DaftExecutionConfig.
+/// 2. Building of logical plan nodes
+#[derive(Clone, Serialize, Deserialize, Default)]
+pub struct DaftPlanningConfig {
+    pub default_io_config: IOConfig,
+}
+
+/// Configurations for Daft to use during the execution of a Dataframe
+///  Note that this should be immutable for a given end-to-end execution of a logical plan.
+///
+/// Execution entails everything that happens when a Dataframe `.collect()`, `.show()` or similar is called:
+/// 1. Logical plan optimization
+/// 2. Logical-to-physical-plan translation
+/// 3. Task generation from physical plan
+/// 4. Task scheduling
+/// 5. Task local execution
 #[derive(Clone, Serialize, Deserialize)]
 pub struct DaftExecutionConfig {
     pub merge_scan_tasks_min_size_bytes: usize,
@@ -29,6 +49,7 @@ use pyo3::prelude::*;
 #[cfg(feature = "python")]
 pub fn register_modules(_py: Python, parent: &PyModule) -> PyResult<()> {
     parent.add_class::<python::PyDaftExecutionConfig>()?;
+    parent.add_class::<python::PyDaftPlanningConfig>()?;
 
     Ok(())
 }
--- src/common/daft-config/src/python.rs
+++ src/common/daft-config/src/python.rs
@@ -3,7 +3,64 @@ use std::sync::Arc;
 use pyo3::{prelude::*, PyTypeInfo};
 use serde::{Deserialize, Serialize};
 
-use crate::DaftExecutionConfig;
+use crate::{DaftExecutionConfig, DaftPlanningConfig};
+use common_io_config::python::IOConfig as PyIOConfig;
+
+#[derive(Clone, Default, Serialize, Deserialize)]
+#[pyclass(module = "daft.daft")]
+pub struct PyDaftPlanningConfig {
+    pub config: Arc<DaftPlanningConfig>,
+}
+
+#[pymethods]
+impl PyDaftPlanningConfig {
+    #[new]
+    pub fn new() -> Self {
+        PyDaftPlanningConfig::default()
+    }
+
+    fn with_config_values(
+        &mut self,
+        default_io_config: Option<PyIOConfig>,
+    ) -> PyResult<PyDaftPlanningConfig> {
+        let mut config = self.config.as_ref().clone();
+
+        if let Some(default_io_config) = default_io_config {
+            config.default_io_config = default_io_config.config;
+        }
+
+        Ok(PyDaftPlanningConfig {
+            config: Arc::new(config),
+        })
+    }
+
+    #[getter(default_io_config)]
+    fn default_io_config(&self) -> PyResult<PyIOConfig> {
+        Ok(PyIOConfig {
+            config: self.config.default_io_config.clone(),
+        })
+    }
+
+    fn __reduce__(&self, py: Python) -> PyResult<(PyObject, (Vec<u8>,))> {
+        let bin_data = bincode::serialize(self.config.as_ref())
+            .expect("DaftPlanningConfig should be serializable to bytes");
+        Ok((
+            Self::type_object(py)
+                .getattr("_from_serialized")?
+                .to_object(py),
+            (bin_data,),
+        ))
+    }
+
+    #[staticmethod]
+    fn _from_serialized(bin_data: Vec<u8>) -> PyResult<PyDaftPlanningConfig> {
+        let daft_planning_config: DaftPlanningConfig = bincode::deserialize(bin_data.as_slice())
+            .expect("DaftExecutionConfig should be deserializable from bytes");
+        Ok(PyDaftPlanningConfig {
+            config: daft_planning_config.into(),
+        })
+    }
+}
 
 #[derive(Clone, Default, Serialize, Deserialize)]
 #[pyclass(module = "daft.daft")]
