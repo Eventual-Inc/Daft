3bc33cadd0f9a932ce8b6484699e41eb3822534d
Jay Chia <17691182+jaychia@users.noreply.github.com>
2024-01-17 11:40:44 -0800

    [DOCS] Fix notebooks to use new public parquet file (#1788)
    1. Use a Parquet file uploaded to S3 instead of the (now missing)
    huggingface link
    2. Skip execution of notebooks that use private data
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

--- tutorials/flyte/app.py
+++ tutorials/flyte/app.py
@@ -48,8 +48,11 @@ def wf(limit: int):
 
 def daft_notebook_code(limit: int):
     # Download data from Parquet file
-    PARQUET_PATH = "https://huggingface.co/datasets/ChristophSchuhmann/improved_aesthetics_6.5plus/resolve/main/data/train-00000-of-00001-6f24a7497df494ae.parquet"
-    parquet_df = daft.read_parquet(PARQUET_PATH)
+    IO_CONFIG = IO_CONFIG = daft.io.IOConfig(
+        s3=daft.io.S3Config(anonymous=True, region_name="us-west-2")
+    )  # Use anonymous-mode for accessing AWS S3
+    PARQUET_PATH = "s3://daft-public-data/tutorials/laion-parquet/train-00000-of-00001-6f24a7497df494ae.parquet"
+    parquet_df = daft.read_parquet(PARQUET_PATH, io_config=IO_CONFIG)
     parquet_df = parquet_df.select(parquet_df["URL"], parquet_df["TEXT"], parquet_df["AESTHETIC_SCORE"])
 
     # Filter for only "pretty" images
