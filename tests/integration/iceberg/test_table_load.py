837aaf3910cbb434c70d7dd5e0caceaffa72ce31
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-02-22 14:55:39 -0800

    Fix test

--- tests/integration/iceberg/test_table_load.py
+++ tests/integration/iceberg/test_table_load.py
@@ -69,5 +69,5 @@ def test_daft_iceberg_table_filtered_collect_correct(local_iceberg_catalog):
     df = df.where(df["pos"] <= 1)
     daft_pandas = df.to_pandas()
     iceberg_pandas = tab.scan().to_arrow().to_pandas()
-    iceberg_pandas = daft_pandas.where(daft_pandas["pos"] <= 1)
+    iceberg_pandas = iceberg_pandas[iceberg_pandas["pos"] <= 1]
     assert_df_equals(daft_pandas, iceberg_pandas, sort_key=[])
