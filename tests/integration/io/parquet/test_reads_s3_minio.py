9b4ec9fe60d8f67cc38a6ac33389169e9938c6d4
Colin Ho <colin.ho99@gmail.com>
2024-03-11 10:50:43 -0700

    use eq for non ext matches

--- tests/integration/io/parquet/test_reads_s3_minio.py
+++ tests/integration/io/parquet/test_reads_s3_minio.py
@@ -43,18 +43,20 @@ def test_minio_parquet_ignore_marker_files(minio_io_config):
     bucket_name = "data-engineering-prod"
     with minio_create_bucket(minio_io_config, bucket_name=bucket_name) as fs:
         target_paths = [
-            f"s3://{bucket_name}/Y",
-            f"s3://{bucket_name}/Z",
+            f"s3://data-engineering-prod/X/no_ext_parquet_metadata",
+            f"s3://data-engineering-prod/Y/part-00000-51723f93-0ba2-42f1-a58f-154f0ed40f28.c000.snappy.parquet",
+            f"s3://data-engineering-prod/Z/part-00000-6d5c7cc6-3b4a-443e-a46a-ca9e080bda1b.c000.snappy.parquet",
         ]
         data = {"x": [1, 2, 3, 4]}
-        df = daft.from_pydict(data)
+        pa_table = pa.Table.from_pydict(data)
         for path in target_paths:
-            df.write_parquet(path, io_config=minio_io_config)
+            pq.write_table(pa_table, path, filesystem=fs)
 
         marker_files = ["_metadata", "_SUCCESS", "_common_metadata", "a.crc"]
         for marker in marker_files:
+            fs.touch(f"s3://{bucket_name}/X/{marker}")
             fs.touch(f"s3://{bucket_name}/Y/{marker}")
             fs.touch(f"s3://{bucket_name}/Z/{marker}")
 
         read = daft.read_parquet(f"s3://{bucket_name}/**", io_config=minio_io_config)
-        assert read.to_pydict() == {"x": [1, 2, 3, 4] * 2}
+        assert read.to_pydict() == {"x": [1, 2, 3, 4] * 3}
