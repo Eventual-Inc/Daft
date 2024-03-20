87c240f4ba5d04bf35fcc5ceeeabba8ef887d70d
Nupur Agrawal <nupur202000@gmail.com>
2024-03-20 22:57:11 +0530

    fix expressions test

--- tests/expressions/typing/test_str.py
+++ tests/expressions/typing/test_str.py
@@ -18,7 +18,6 @@ from tests.expressions.typing.conftest import assert_typing_resolve_vs_runtime_b
         pytest.param(lambda data, pat: data.str.endswith(pat), id="split"),
         pytest.param(lambda data, pat: data.str.endswith(pat), id="match"),
         pytest.param(lambda data, pat: data.str.concat(pat), id="concat"),
-        pytest.param(lambda data, pat: data.str.left(pat), id="left"),
     ],
 )
 def test_str_compares(binary_data_fixture, op, request):
@@ -104,4 +103,20 @@ def test_str_capitalize():
         expr=col(s.name()).str.capitalize(),
         run_kernel=s.str.capitalize,
         resolvable=True,
+    )
+
+
+@pytest.mark.parametrize(
+    "op",
+    [
+        pytest.param(lambda data, pat: data.str.left(pat), id="left"),
+    ],
+)
+def test_str_left(binary_data_fixture, op, request):
+    lhs, rhs = binary_data_fixture
+    assert_typing_resolve_vs_runtime_behavior(
+        data=binary_data_fixture,
+        expr=op(col(lhs.name()), col(rhs.name())),
+        run_kernel=lambda: op(lhs, rhs),
+        resolvable=(lhs.datatype() == DataType.string()) and (rhs.datatype() == DataType.uint32()),
     )
