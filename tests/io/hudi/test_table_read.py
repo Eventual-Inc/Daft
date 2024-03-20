5ed9ffcec81799a436e7b8c9fe87a32fca755c2e
Shiyan Xu <xu.shiyan.raymond@gmail.com>
2024-03-20 02:06:46 -0500

    fix test

--- tests/io/hudi/test_table_read.py
+++ tests/io/hudi/test_table_read.py
@@ -1,14 +1,24 @@
 from __future__ import annotations
 
 import daft
-from daft.hudi.pyhudi.table import HudiTable
-from daft.logical.schema import Schema
 
 
-def test_hudi_read_table():
-    path = "/tmp/trips_table"
+def test_hudi_read_table(unzip_table_0_x_cow_partitioned):
+    path = unzip_table_0_x_cow_partitioned
     df = daft.read_hudi(path)
-    table_schema = HudiTable(path).schema
-    expected_schema = Schema.from_pyarrow_schema(table_schema)
-    assert df.schema() == expected_schema
-    df.show()
+    assert df.schema().column_names() == [
+        "_hoodie_commit_time",
+        "_hoodie_commit_seqno",
+        "_hoodie_record_key",
+        "_hoodie_partition_path",
+        "_hoodie_file_name",
+        "ts",
+        "uuid",
+        "rider",
+        "driver",
+        "fare",
+        "city",
+    ]
+    assert df.select("rider").sort("rider").to_pydict() == {
+        "rider": ["rider-A", "rider-C", "rider-D", "rider-F", "rider-J"]
+    }
