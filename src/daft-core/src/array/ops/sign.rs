cd8ed01b39e28bb9a81e75fa0f7bd65fc008c5d2
siddharth kumar <sidkumargulia@gmail.com>
2024-03-26 00:47:05 +0530

    removed indivalual implemetation for unsigned size

--- src/daft-core/src/array/ops/sign.rs
+++ src/daft-core/src/array/ops/sign.rs
@@ -1,9 +1,7 @@
+use crate::{array::DataArray, datatypes::DaftNumericType};
 use num_traits::Signed;
-
-use crate::{
-    array::DataArray,
-    datatypes::{DaftNumericType, UInt16Array, UInt32Array, UInt64Array, UInt8Array},
-};
+use num_traits::Unsigned;
+use num_traits::{One, Zero};
 
 use common_error::DaftResult;
 
@@ -16,26 +14,17 @@ where
     }
 }
 
-impl UInt8Array {
+impl<T: DaftNumericType> DataArray<T>
+where
+    T::Native: Unsigned,
+{
     pub fn sign_unsigned(&self) -> DaftResult<Self> {
-        self.apply(|v| if v == 0 { 0 } else { 1 })
-    }
-}
-
-impl UInt16Array {
-    pub fn sign_unsigned(&self) -> DaftResult<Self> {
-        self.apply(|v| if v == 0 { 0 } else { 1 })
-    }
-}
-
-impl UInt32Array {
-    pub fn sign_unsigned(&self) -> DaftResult<Self> {
-        self.apply(|v| if v == 0 { 0 } else { 1 })
-    }
-}
-
-impl UInt64Array {
-    pub fn sign_unsigned(&self) -> DaftResult<Self> {
-        self.apply(|v| if v == 0 { 0 } else { 1 })
+        self.apply(|v| {
+            if v.is_zero() {
+                T::Native::zero()
+            } else {
+                T::Native::one()
+            }
+        })
     }
 }
