d225b464a8d4697cbde12842bf67d053be784bea
Colin Ho <colin.ho99@gmail.com>
2024-03-28 12:04:01 -0700

    fix test

--- tests/series/test_utf8_ops.py
+++ tests/series/test_utf8_ops.py
@@ -666,26 +666,27 @@ def test_series_utf8_find_bad_dtype() -> None:
     ["data", "pattern", "replacement", "expected"],
     [
         # No broadcast
-        (["foo", "barbaz", "quux"], ["o", "a", "u"], ["O", "A", "U"], ["fOo", "bArbaz", "qUux"]),
+        (["foo", "barbaz", "quux"], ["o", "a", "u"], ["O", "A", "U"], ["fOO", "bArbAz", "qUUx"]),
         # Broadcast data
-        (["foobar"], ["f", "o", "b"], ["F", "O", "B"], ["Foobar", "fOobar", "fooBar"]),
+        (["foobar"], ["f", "o", "b"], ["F", "O", "B"], ["Foobar", "fOObar", "fooBar"]),
         # Broadcast pattern
         (["123", "12", "1"], ["1"], ["2", "3", "4"], ["223", "32", "4"]),
         # Broadcast replacement
-        (["foo", "barbaz", "quux"], ["o", "a", "u"], [" "], ["f o", "b rbaz", "q ux"]),
+        (["foo", "barbaz", "quux"], ["o", "a", "u"], [" "], ["f  ", "b rb z", "q  x"]),
         # Broadcast data and pattern
-        (["foo"], ["o"], ["O", "A", "U"], ["fOo", "fAo", "fUo"]),
+        (["foo"], ["o"], ["O", "A", "U"], ["fOO", "fAA", "fUU"]),
         # Broadcast data and replacement
-        (["foo"], ["o", "f", " "], ["O"], ["fOo", "Ooo", "foo"]),
+        (["foo"], ["o", "f", " "], ["O"], ["fOO", "Ooo", "foo"]),
         # Broadcast pattern and replacement
         (["123", "12", "1"], ["1"], ["A"], ["A23", "A2", "A"]),
     ],
 )
-def test_series_utf8_replace(data, pattern, replacement, expected) -> None:
+@pytest.mark.parametrize("regex", [True, False])
+def test_series_utf8_replace(data, pattern, replacement, expected, regex) -> None:
     s = Series.from_arrow(pa.array(data, type=pa.string()))
     patterns = Series.from_arrow(pa.array(pattern, type=pa.string()))
     replacements = Series.from_arrow(pa.array(replacement, type=pa.string()))
-    result = s.str.replace(patterns, replacements)
+    result = s.str.replace(patterns, replacements, regex=regex)
     assert result.to_pylist() == expected
 
 
