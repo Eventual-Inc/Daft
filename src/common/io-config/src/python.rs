c843df195d939668661b9db39418587d425bf115
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-04-15 22:47:23 -0700

    Implement IOConfig.from_env using dynamic Python imports

--- src/common/io-config/src/python.rs
+++ src/common/io-config/src/python.rs
@@ -140,6 +140,19 @@ impl IOConfig {
     }
 
     #[staticmethod]
+    pub fn from_env(py: Python) -> PyResult<Self> {
+        let io_config_from_env_func = py
+            .import("daft")?
+            .getattr("daft")?
+            .getattr("io_config_from_env")?;
+        io_config_from_env_func.call0().map(|pyany| {
+            pyany
+                .extract()
+                .expect("io_config_from_env function must return IOConfig")
+        })
+    }
+
+    #[staticmethod]
     pub fn from_json(input: &str) -> PyResult<Self> {
         let config: config::IOConfig = serde_json::from_str(input).map_err(DaftError::from)?;
         Ok(config.into())
