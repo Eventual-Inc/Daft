f3ebad8749db90bd42e0345486113ec60eb670d3
Shiyan Xu <xu.shiyan.raymond@gmail.com>
2024-04-18 22:14:31 -0500

    check pyarrow version

--- tests/io/hudi/test_table_read.py
+++ tests/io/hudi/test_table_read.py
@@ -2,7 +2,13 @@ from __future__ import annotations
 
 import datetime
 
+import pyarrow as pa
+import pytest
+
 import daft
+
+PYARROW_LE_8_0_0 = tuple(int(s) for s in pa.__version__.split(".") if s.isnumeric()) < (8, 0, 0)
+pytestmark = pytest.mark.skipif(PYARROW_LE_8_0_0, reason="hudi only supported if pyarrow >= 8.0.0")
 
 
 def test_hudi_read_table(test_table_path):
