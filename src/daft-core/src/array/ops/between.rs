ebb255f03fad2c185cb51550fcb1b885764a674c
Guy Pozner <guy.pozner@mail.huji.ac.il>
2024-05-26 12:56:52 +0300

    add between test for arrays of same size, fix result comparison instead of value comparison

--- src/daft-core/src/array/ops/between.rs
+++ src/daft-core/src/array/ops/between.rs
@@ -31,9 +31,11 @@ where
     fn between(&self, lower: &DataArray<T>, upper: &DataArray<T>) -> Self::Output {
         match  (self.len(), lower.len(), upper.len()) {
             (v_size, l_size, u_size) if (v_size == l_size && v_size == u_size) || (l_size == 1 && u_size == 1) => {
-                let gte_res = self.gte(lower);
-                let lte_res = self.lte(upper);
-                gte_res.and(lte_res)
+                let gte_res = self.gte(lower)?;
+                let lte_res = self.lte(upper)?;
+                let res = gte_res.and(&lte_res);
+                res
+
             },
             (v_size, l_size, u_size) => Err(DaftError::ValueError(format!(
                 "trying to compare different length arrays: {}: {v_size} vs {}: {l_size} vs {}: {u_size}",
@@ -100,11 +102,24 @@ mod tests {
     use common_error::DaftResult;
 
     #[test]
-    fn test_between() -> DaftResult<()> {
-        let array = Int64Array::arange("a", 1, 5, 1)?;
-        assert_eq!(array.len(), 4);
-        let result: Vec<_> = array.between(1, 2)?.into_iter().collect();
+    fn test_between_two_scalars() -> DaftResult<()> {
+        let value = Int64Array::arange("a", 1, 5, 1)?;
+        assert_eq!(value.len(), 4);
+        let result: Vec<_> = value.between(1, 2)?.into_iter().collect();
         assert_eq!(result[..], [Some(true), Some(true), Some(false), Some(false)]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_between_two_arrays_of_same_size() -> DaftResult<()> {
+        let value = Int64Array::arange("value", 1, 4, 1)?;
+        let lower = Int64Array::arange("lower", 0, 6, 2)?;
+        let upper = Int64Array::arange("upper", -2, 8, 4)?;
+        println!("{:?}", lower);
+        println!("{:?}", value);
+        println!("{:?}", upper);
+        let result: Vec<_> = value.between(&lower, &upper)?.into_iter().collect();
+        assert_eq!(result[..], [Some(false), Some(true), Some(false)]);
         Ok(())
     }
 }
