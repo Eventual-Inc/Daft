cb767dd885a90e5fff2c000f6a634270b12b28f8
Guy Pozner <guy.pozner@mail.huji.ac.il>
2024-05-30 14:55:24 +0300

    add test for two cols and one scalar

--- tests/table/test_between.py
+++ tests/table/test_between.py
@@ -117,3 +117,38 @@ def test_between_bad_input() -> None:
     daft_table = MicroPartition.from_pydict({"a": [1, 2, 3]})
     with pytest.raises(TypeError):
         daft_table = daft_table.eval_expression_list([col("a").between([1, 2, 3], 1)])
+
+
+@pytest.mark.parametrize(
+    "value,lower,upper,expected",
+    [
+        pytest.param([1, 2, 3, 4], [1, 2, 3, 4], 2, [True, True, False, False], id="IntIntInt"),
+        pytest.param([1, 2, 3, 4], [1.0, 2.0, 3.0, 4.0], 2.0, [True, True, False, False], id="IntFloatFloat"),
+        pytest.param([1, 2, 3, 4], [1, 2, 3, 4], 2.0, [True, True, False, False], id="IntIntFloat"),
+        pytest.param([1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0], 2.0, [True, True, False, False], id="FloatFloatFloat"),
+        pytest.param([1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0], 2, [True, True, False, False], id="FloatIntInt"),
+        pytest.param([1.0, 2.0, 3.0, 4.0], [1, 2, 3, 4], 2.0, [True, True, False, False], id="FloatIntFloat"),
+        pytest.param([1.0, 2.0, 3.0, 4.0], [None, None, None, None], 1, [None, None, None, None], id="FloatNullInt"),
+        pytest.param([1.0, 2.0, 3.0, 4.0], [1, 2, 3, 4], None, [None, None, None, None], id="FloatIntNull"),
+        pytest.param(
+            [1.0, 2.0, 3.0, 4.0], [None, None, None, None], None, [None, None, None, None], id="FloatNullNull"
+        ),
+        pytest.param(
+            [None, None, None, None], [None, None, None, None], None, [None, None, None, None], id="NullNullNull"
+        ),
+        pytest.param([None, None, None, None], [1, 2, 3, 4], 1, [None, None, None, None], id="NullIntInt"),
+        pytest.param(
+            [datetime.datetime(2023, 1, 1), datetime.datetime(2022, 1, 1)],
+            [datetime.datetime(2022, 12, 30), datetime.datetime(2022, 12, 30)],
+            datetime.datetime(2023, 1, 2),
+            [True, False],
+            id="Datetime",
+        ),
+    ],
+)
+def test_table_expr_between_single_scalars(value, lower, upper, expected) -> None:
+    table = {"value": value, "lower": lower}
+    daft_table = MicroPartition.from_pydict(table)
+    daft_table = daft_table.eval_expression_list([col("value").between(col("lower"), upper)])
+    pydict = daft_table.to_pydict()
+    assert pydict["value"] == expected
