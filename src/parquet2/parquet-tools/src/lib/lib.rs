58151ab2caf89076ec058264ccc2229c58802211
universalmind303 <cory.grinstead@gmail.com>
2024-06-05 12:31:29 -0500

    remove more unused files

--- src/parquet2/parquet-tools/src/lib/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod dump;
-pub mod meta;
-pub mod rows;
-
-const SEPARATOR: &str = "--------------------------------------------------";
-pub type Result<T> = ::std::result::Result<T, Box<dyn std::error::Error>>;
