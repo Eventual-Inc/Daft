87f6706856e7da76ea3ca2ffbf6c1c2572ec3210
Colin Ho <colin.ho99@gmail.com>
2024-06-06 11:47:01 -0700

    [BUG] Allow nulls in partition column (#2344)
    Closes https://github.com/Eventual-Inc/Daft/issues/2292
    
    Currently Daft panics if there are nulls in a partition column, the
    detailed error message can be found in the linked issue.
    
    A simple reproduction:
    ```
    from deltalake import write_deltalake
    import pandas as pd
    import daft
    
    df = pd.DataFrame(
        {
            "group": [1, 2, 3, None],
            "num": list(range(4)),
        }
    )
    write_deltalake("z", df, partition_by="group", mode="overwrite")
    
    df = daft.read_deltalake("z")
    df.show()
    ```
    
    This PR modifies the partition spec equality logic and partition pruning
    semantics to allow reading nulls in partition columns.

--- src/daft-stats/src/partition_spec.rs
+++ src/daft-stats/src/partition_spec.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use daft_core::array::ops::DaftCompare;
+use daft_core::array::ops::{DaftCompare, DaftLogical};
 use daft_dsl::{ExprRef, Literal};
 use daft_table::Table;
 
@@ -42,9 +42,18 @@ impl PartialEq for PartitionSpec {
         for field_name in self.keys.schema.as_ref().fields.keys() {
             let self_column = self.keys.get_column(field_name).unwrap();
             let other_column = other.keys.get_column(field_name).unwrap();
-            let value_eq = self_column.equal(other_column).unwrap().get(0).unwrap();
-            if !value_eq {
-                return false;
+            if let Some(value_eq) = self_column.equal(other_column).unwrap().get(0) {
+                if !value_eq {
+                    return false;
+                }
+            } else {
+                // For partition spec, we treat null as equal to null, in order to allow for
+                // partitioning on columns that may have nulls.
+                let self_null = self_column.is_null().unwrap();
+                let other_null = other_column.is_null().unwrap();
+                if self_null.xor(&other_null).unwrap().get(0).unwrap() {
+                    return false;
+                }
             }
         }
 
