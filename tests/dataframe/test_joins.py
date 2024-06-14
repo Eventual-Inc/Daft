f88031bb6818d86757b97f720daf879622df6004
Sammy Sidhu <samster25@users.noreply.github.com>
2024-06-14 16:46:58 -0700

    add semi test

--- tests/dataframe/test_joins.py
+++ tests/dataframe/test_joins.py
@@ -720,3 +720,56 @@ def test_join_null_type_column(join_strategy, join_type, make_df):
 
     with pytest.raises((ExpressionTypeError, ValueError)):
         daft_df.join(daft_df2, on="id", how=join_type, strategy=join_strategy)
+
+
+@pytest.mark.parametrize("repartition_nparts", [1, 2, 4])
+@pytest.mark.parametrize(
+    "join_strategy",
+    [None, "hash", "sort_merge", "sort_merge_aligned_boundaries", "broadcast"],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    "join_type,expected",
+    [
+        (
+            "semi",
+            {
+                "id": [2, 3],
+                "values_left": ["b1", "c1"],
+            },
+        ),
+        (
+            "anti",
+            {
+                "id": [1],
+                "values_left": ["a1"],
+            },
+        ),
+    ],
+)
+def test_join_semi_anti(join_strategy, join_type, expected, make_df, repartition_nparts):
+    skip_invalid_join_strategies(join_strategy, join_type)
+
+    daft_df1 = make_df(
+        {
+            "id": [1, 2, 3],
+            "values_left": ["a1", "b1", "c1"],
+        },
+        repartition=repartition_nparts,
+    )
+    daft_df2 = make_df(
+        {
+            "id": [2, 3, 4],
+            "values_right": ["a2", "b2", "c2"],
+        },
+        repartition=repartition_nparts,
+    )
+    daft_df = (
+        daft_df1.with_column("id", daft_df1["id"].cast(DataType.int64()))
+        .join(daft_df2, on="id", how=join_type, strategy=join_strategy)
+        .sort(["id", "values_left"])
+    ).select("id", "values_left")
+
+    assert sort_arrow_table(pa.Table.from_pydict(daft_df.to_pydict()), "id") == sort_arrow_table(
+        pa.Table.from_pydict(expected), "id"
+    )
