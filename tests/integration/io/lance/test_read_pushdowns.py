e27872395922deaf488258f82c22ba41c9aa2e3a
universalmind303 <cory.grinstead@gmail.com>
2024-06-18 11:40:52 -0500

    fix test

--- tests/integration/io/lance/test_read_pushdowns.py
+++ tests/integration/io/lance/test_read_pushdowns.py
@@ -10,5 +10,5 @@ WORDS_DATASET = "s3://daft-public-data/lance/words-test-dataset"
 @pytest.mark.integration()
 def test_lance_s3_filter_pushdowns(aws_public_s3_config):
     df = daft.read_lance(WORDS_DATASET, io_config=aws_public_s3_config)
-    df = df.where(daft.col("text") == "hello world")
+    df = df.where(daft.col("text") == "hello world").collect()
     assert df.count() == 1
