db7a18492dc86a0046c2c69f2767028479a5669f
universalmind303 <cory.grinstead@gmail.com>
2024-06-18 10:57:55 -0500

    add integration test

---
+++ tests/integration/io/lance/test_read_pushdowns.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import pytest
+
+import daft
+
+WORDS_DATASET = "s3://daft-public-data/lance/words-test-dataset'"
+
+
+@pytest.mark.integration()
+def test_lance_s3_filter_pushdowns(aws_public_s3_config):
+    df = daft.read_lance(WORDS_DATASET, io_config=aws_public_s3_config)
+    df = df.where(daft.col("text") == "hello world")
+    assert df.count() == 1
