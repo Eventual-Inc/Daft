cf9968d96ee02dc53be9e4806039b20d54a128ab
Conor Kennedy <realvince7778@gmail.com>
2024-07-02 10:58:41 -0700

    Implement direct grouping of lists

--- src/daft-core/src/series/ops/groups.rs
+++ src/daft-core/src/series/ops/groups.rs
@@ -1,14 +1,14 @@
 use crate::{
     array::ops::{GroupIndicesPair, IntoGroups},
     series::Series,
-    with_match_comparable_daft_types,
+    with_match_hashable_daft_types,
 };
 use common_error::DaftResult;
 
 impl IntoGroups for Series {
     fn make_groups(&self) -> DaftResult<GroupIndicesPair> {
         let s = self.as_physical()?;
-        with_match_comparable_daft_types!(s.data_type(), |$T| {
+        with_match_hashable_daft_types!(s.data_type(), |$T| {
             let array = s.downcast::<<$T as DaftDataType>::ArrayType>()?;
             array.make_groups()
         })
