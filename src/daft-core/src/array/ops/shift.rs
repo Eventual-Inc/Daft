cf2572ec06414f3c6e38c8b7f432c2a43db5a16c
Nupur Agrawal <nupur202000@gmail.com>
2024-07-03 22:13:52 +0530

    fixes

--- src/daft-core/src/array/ops/shift.rs
+++ src/daft-core/src/array/ops/shift.rs
@@ -1,127 +1,23 @@
-use common_error::{DaftError, DaftResult};
-use num_traits::{NumCast, PrimInt};
+use std::ops::{Shl, Shr};
+
+use common_error::DaftResult;
+use num_traits::PrimInt;
 
 use crate::{
     array::DataArray,
     datatypes::{DaftIntegerType, UInt64Type},
 };
 
-use super::as_arrow::AsArrow;
-
 impl<T> DataArray<T>
 where
     T: DaftIntegerType,
     T::Native: PrimInt,
 {
-    pub fn shift_left(&self, bits: &DataArray<UInt64Type>) -> DaftResult<Self> {
-        let result = match (self.len(), bits.len()) {
-            (x, y) if x == y => self
-                .as_arrow()
-                .iter()
-                .zip(bits.as_arrow().iter())
-                .map(|(a, b)| {
-                    a.zip(b)
-                        .map(|(a, b)| {
-                            let b: usize = NumCast::from(*b).ok_or_else(|| {
-                                DaftError::ValueError("Failed to cast bits to usize".to_string())
-                            })?;
-                            Ok(*a << b)
-                        })
-                        .transpose()
-                })
-                .collect::<DaftResult<Vec<_>>>()?,
-            (1, _) => {
-                let data = self.get(0).unwrap();
-                bits.as_arrow()
-                    .iter()
-                    .map(|b| match b {
-                        Some(b) => {
-                            let b: usize = NumCast::from(*b).ok_or_else(|| {
-                                DaftError::ValueError("Failed to cast bits to usize".to_string())
-                            })?;
-                            Ok(Some(data << b))
-                        }
-                        None => Ok(None),
-                    })
-                    .collect::<DaftResult<Vec<_>>>()?
-            }
-            (_, 1) => {
-                let bits = bits.get(0).unwrap();
-                let b = NumCast::from(bits).ok_or_else(|| {
-                    DaftError::ValueError("Failed to cast bits to usize".to_string())
-                })?;
-                self.as_arrow()
-                    .iter()
-                    .map(|a| {
-                        let a = a?;
-                        Some(*a << b)
-                    })
-                    .collect::<Vec<_>>()
-            }
-            _ => {
-                return Err(DaftError::ValueError(format!(
-                    "Trying to operate on different lengths, got {} and {}",
-                    self.len(),
-                    bits.len()
-                )))
-            }
-        };
-        Ok(Self::from_iter(self.name(), result.into_iter()))
+    pub fn shift_left(&self, rhs: &DataArray<UInt64Type>) -> DaftResult<Self> {
+        self.binary_apply(rhs, |lhs, rhs| lhs.shl(rhs as usize))
     }
 
-    pub fn shift_right(&self, bits: &DataArray<UInt64Type>) -> DaftResult<Self> {
-        let result = match (self.len(), bits.len()) {
-            (x, y) if x == y => self
-                .as_arrow()
-                .iter()
-                .zip(bits.as_arrow().iter())
-                .map(|(a, b)| {
-                    a.zip(b)
-                        .map(|(a, b)| {
-                            let b: usize = NumCast::from(*b).ok_or_else(|| {
-                                DaftError::ValueError("Failed to cast bits to usize".to_string())
-                            })?;
-                            Ok(*a >> b)
-                        })
-                        .transpose()
-                })
-                .collect::<DaftResult<Vec<_>>>()?,
-            (1, _) => {
-                let data = self.get(0).unwrap();
-                bits.as_arrow()
-                    .iter()
-                    .map(|b| match b {
-                        Some(b) => {
-                            let b: usize = NumCast::from(*b).ok_or_else(|| {
-                                DaftError::ValueError("Failed to cast bits to usize".to_string())
-                            })?;
-                            Ok(Some(data >> b))
-                        }
-                        None => Ok(None),
-                    })
-                    .collect::<DaftResult<Vec<_>>>()?
-            }
-            (_, 1) => {
-                let bits = bits.get(0).unwrap();
-                let b = NumCast::from(bits).ok_or_else(|| {
-                    DaftError::ValueError("Failed to cast bits to usize".to_string())
-                })?;
-                self.as_arrow()
-                    .iter()
-                    .map(|a| {
-                        let a = a?;
-                        Some(*a >> b)
-                    })
-                    .collect::<Vec<_>>()
-            }
-            _ => {
-                return Err(DaftError::ValueError(format!(
-                    "Trying to operate on different lengths, got {} and {}",
-                    self.len(),
-                    bits.len()
-                )))
-            }
-        };
-        Ok(Self::from_iter(self.name(), result.into_iter()))
+    pub fn shift_right(&self, rhs: &DataArray<UInt64Type>) -> DaftResult<Self> {
+        self.binary_apply(rhs, |lhs, rhs| lhs.shr(rhs as usize))
     }
 }
