32a3d2d8a8672089275a4a13a8a67995555bdaee
Sammy Sidhu <samster25@users.noreply.github.com>
2024-07-03 16:10:36 -0700

    wip

---
+++ src/daft-physical-plan/src/local_plan.rs
@@ -0,0 +1,36 @@
+pub enum LocalPhysicalPlan {
+    // InMemoryScan(InMemoryScan),
+    PhysicalScan(PhysicalScan),
+    // EmptyScan(EmptyScan),
+    Project(Project),
+    Filter(Filter),
+    Limit(Limit),
+    // Explode(Explode),
+    // Unpivot(Unpivot),
+    Sort(Sort),
+    // Split(Split),
+    // Sample(Sample),
+    // MonotonicallyIncreasingId(MonotonicallyIncreasingId),
+    // Coalesce(Coalesce),
+    // Flatten(Flatten),
+    // FanoutRandom(FanoutRandom),
+    // FanoutByHash(FanoutByHash),
+    // #[allow(dead_code)]
+    // FanoutByRange(FanoutByRange),
+    // ReduceMerge(ReduceMerge),
+    Aggregate(Aggregate),
+    // Pivot(Pivot),
+    // Concat(Concat),
+    HashJoin(HashJoin),
+    // SortMergeJoin(SortMergeJoin),
+    // BroadcastJoin(BroadcastJoin),
+    PhysicalWrite(PhysicalWrite),
+    // TabularWriteJson(TabularWriteJson),
+    // TabularWriteCsv(TabularWriteCsv),
+    // #[cfg(feature = "python")]
+    // IcebergWrite(IcebergWrite),
+    // #[cfg(feature = "python")]
+    // DeltaLakeWrite(DeltaLakeWrite),
+    // #[cfg(feature = "python")]
+    // LanceWrite(LanceWrite),
+}
