7fd889da24eba6fe0d1b9b774f9e49b721213c11
universalmind303 <cory.grinstead@gmail.com>
2024-07-08 10:13:21 -0500

    pr feedback

--- src/daft-dsl/src/functions/registry.rs
+++ src/daft-dsl/src/functions/registry.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
+
+use common_error::{DaftError, DaftResult};
+use dashmap::DashMap;
 
 use super::{hash::HashFunction, ScalarUDF};
 
@@ -7,7 +10,7 @@ lazy_static::lazy_static! {
 }
 
 pub struct Registry {
-    functions: HashMap<&'static str, Arc<dyn ScalarUDF>>,
+    functions: DashMap<&'static str, Arc<dyn ScalarUDF>>,
 }
 
 impl Registry {
@@ -19,15 +22,23 @@ impl Registry {
             functions: iter.into_iter().collect(),
         }
     }
-    pub fn register(&mut self, function: Arc<dyn ScalarUDF>) {
-        self.functions.insert(function.name(), function);
+    pub fn register(&mut self, function: Arc<dyn ScalarUDF>) -> DaftResult<()> {
+        if self.functions.contains_key(function.name()) {
+            Err(DaftError::ValueError(format!(
+                "function {} already exists",
+                function.name()
+            )))
+        } else {
+            self.functions.insert(function.name(), function);
+            Ok(())
+        }
     }
 
-    pub fn get(&self, name: &str) -> Option<&Arc<dyn ScalarUDF>> {
-        self.functions.get(name)
+    pub fn get(&self, name: &str) -> Option<Arc<dyn ScalarUDF>> {
+        self.functions.get(name).map(|f| f.value().clone())
     }
 
     pub fn names(&self) -> Vec<&'static str> {
-        self.functions.keys().copied().collect()
+        self.functions.iter().map(|pair| pair.name()).collect()
     }
 }
