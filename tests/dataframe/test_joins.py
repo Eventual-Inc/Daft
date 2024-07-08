55fc032ba1ebe77c108443eac50defcd561ba30a
Conor Kennedy <32619800+Vince7778@users.noreply.github.com>
2024-07-08 11:22:35 -0700

    [BUG] Fix anti-join on different column names (#2477)
    Closes #2475.
    
    A bit of a bandaid fix. The issue was, while selecting the join
    strategy, for checking whether sort-merge join works, the joined columns
    need to be primitive types. However, in the plan we only have access to
    the output schema (as far as I know, maybe we can extract it from the
    children?). In a semi- or anti-join, the right column does not end up in
    the output schema, so the primitive type check panicked while looking
    for it. This issue does not appear if the left column has the same name
    as the right.
    
    My fix is that, since we only support SMJ for inner joins, I move the
    join type check before the primitive check. This will need to be fixed
    later if we support SMJ for anti-joins, so I added a comment.

--- tests/dataframe/test_joins.py
+++ tests/dataframe/test_joins.py
@@ -777,3 +777,62 @@ def test_join_semi_anti(join_strategy, join_type, expected, make_df, repartition
     assert sort_arrow_table(pa.Table.from_pydict(daft_df.to_pydict()), "id") == sort_arrow_table(
         pa.Table.from_pydict(expected), "id"
     )
+
+
+@pytest.mark.parametrize("repartition_nparts", [1, 2, 4])
+@pytest.mark.parametrize(
+    "join_strategy",
+    [None, "hash", "sort_merge", "sort_merge_aligned_boundaries", "broadcast"],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    "join_type,expected",
+    [
+        (
+            "semi",
+            {
+                "id_left": [2, 3],
+                "values_left": ["b1", "c1"],
+            },
+        ),
+        (
+            "anti",
+            {
+                "id_left": [1, None],
+                "values_left": ["a1", "d1"],
+            },
+        ),
+    ],
+)
+def test_join_semi_anti_different_names(join_strategy, join_type, expected, make_df, repartition_nparts):
+    skip_invalid_join_strategies(join_strategy, join_type)
+
+    daft_df1 = make_df(
+        {
+            "id_left": [1, 2, 3, None],
+            "values_left": ["a1", "b1", "c1", "d1"],
+        },
+        repartition=repartition_nparts,
+    )
+    daft_df2 = make_df(
+        {
+            "id_right": [2, 2, 3, 4],
+            "values_right": ["a2", "b2", "c2", "d2"],
+        },
+        repartition=repartition_nparts,
+    )
+    daft_df = (
+        daft_df1.with_column("id_left", daft_df1["id_left"].cast(DataType.int64()))
+        .join(
+            daft_df2,
+            left_on="id_left",
+            right_on="id_right",
+            how=join_type,
+            strategy=join_strategy,
+        )
+        .sort(["id_left", "values_left"])
+    ).select("id_left", "values_left")
+
+    assert sort_arrow_table(pa.Table.from_pydict(daft_df.to_pydict()), "id_left") == sort_arrow_table(
+        pa.Table.from_pydict(expected), "id_left"
+    )
