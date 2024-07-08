4c97fbabaaf036f3884113484b737cabc40b564d
Colin Ho <colinho@Colins-MBP.localdomain>
2024-07-08 16:36:00 -0700

    add inner pipeline and sink manager

--- src/daft-local-execution/src/pipeline.rs
+++ src/daft-local-execution/src/pipeline.rs
@@ -1,8 +1,12 @@
-use std::{pin::Pin, sync::Arc};
+use std::{borrow::BorrowMut, pin::Pin, sync::Arc};
 
 use common_error::{DaftError, DaftResult};
 use daft_micropartition::MicroPartition;
-use futures::{pin_mut, stream::FuturesUnordered, Stream, StreamExt};
+use futures::{
+    pin_mut,
+    stream::{FuturesOrdered, FuturesUnordered},
+    Stream, StreamExt,
+};
 use lazy_static::lazy_static;
 use snafu::ResultExt;
 use tokio::task::JoinHandle;
@@ -10,14 +14,159 @@ use tokio_stream::wrappers::ReceiverStream;
 
 use crate::{
     create_channel,
-    intermediate_ops::intermediate_op::{run_intermediate_operators, IntermediateOperator},
-    sinks::sink::{launch_sink, Sink},
+    intermediate_ops::intermediate_op::IntermediateOperator,
+    sinks::sink::{Sink, SinkResultType},
     sources::source::Source,
-    Sender,
+    Receiver, Sender,
 };
 
 lazy_static! {
     static ref NUM_CPUS: usize = std::thread::available_parallelism().unwrap().get();
+}
+
+pub struct InnerPipelineManager {
+    intermediate_operators: Vec<Box<dyn IntermediateOperator>>,
+    senders_and_handles: Vec<(Sender, JoinHandle<DaftResult<()>>)>,
+    sink_receivers: Vec<Receiver>,
+    curr_idx: usize,
+}
+
+impl InnerPipelineManager {
+    pub fn new(intermediate_operators: Vec<Box<dyn IntermediateOperator>>) -> Self {
+        Self {
+            intermediate_operators,
+            senders_and_handles: Vec::with_capacity(*NUM_CPUS),
+            sink_receivers: vec![],
+            curr_idx: 0,
+        }
+    }
+
+    async fn run_single_inner_pipeline(
+        mut receiver: Receiver,
+        intermediate_operators: Vec<Box<dyn IntermediateOperator>>,
+        sink_sender: Sender,
+    ) -> DaftResult<()> {
+        log::debug!(
+            "Running intermediate operators: {}",
+            intermediate_operators
+                .iter()
+                .fold(String::new(), |acc, op| { acc + &op.name() + " -> " })
+        );
+
+        while let Some(morsel) = receiver.recv().await {
+            let mut result = morsel?;
+            for op in intermediate_operators.iter() {
+                result = op.execute(&result)?;
+            }
+            let _ = sink_sender.send(Ok(result)).await;
+        }
+
+        log::debug!("Intermediate operators finished");
+        Ok(())
+    }
+
+    fn create_next_sender(&mut self) -> Sender {
+        let (intermediate_sender, intermediate_receiver) = create_channel();
+        let (sink_sender, sink_receiver) = create_channel();
+        let handle = tokio::spawn(Self::run_single_inner_pipeline(
+            intermediate_receiver,
+            self.intermediate_operators.clone(),
+            sink_sender,
+        ));
+
+        self.sink_receivers.push(sink_receiver);
+        self.senders_and_handles
+            .push((intermediate_sender.clone(), handle));
+        intermediate_sender
+    }
+
+    pub fn get_or_create_next_sender(&mut self) -> Sender {
+        let sender = if let Some((tx, _)) = self.senders_and_handles.get(self.curr_idx) {
+            tx.clone()
+        } else {
+            self.create_next_sender()
+        };
+        self.curr_idx = (self.curr_idx + 1) % self.senders_and_handles.len();
+        sender
+    }
+
+    pub fn retrieve_handles(&mut self) -> Vec<JoinHandle<DaftResult<()>>> {
+        self.senders_and_handles
+            .drain(..)
+            .map(|(_, handle)| handle)
+            .collect()
+    }
+}
+
+pub struct SinkManager {
+    sink: Option<Box<dyn Sink>>,
+    send_to_next_source: Sender,
+}
+
+impl SinkManager {
+    pub fn new(sink: Option<Box<dyn Sink>>, send_to_next_source: Sender) -> Self {
+        Self {
+            sink,
+            send_to_next_source,
+        }
+    }
+
+    async fn process_value(
+        &mut self,
+        val: DaftResult<Arc<MicroPartition>>,
+    ) -> DaftResult<SinkResultType> {
+        if let Some(sink) = self.sink.borrow_mut() {
+            sink.sink(&val?)
+        } else {
+            let _ = self.send_to_next_source.send(Ok(val?)).await;
+            Ok(SinkResultType::NeedMoreInput)
+        }
+    }
+
+    async fn finalize_values(&mut self) -> DaftResult<()> {
+        if let Some(sink) = self.sink.borrow_mut() {
+            for part in sink.finalize()? {
+                let _ = self.send_to_next_source.send(Ok(part)).await;
+            }
+        }
+        Ok(())
+    }
+
+    pub async fn run(&mut self, mut receivers: Vec<Receiver>) -> DaftResult<()> {
+        let in_order = match self.sink {
+            Some(ref sink) => sink.in_order(),
+            None => false,
+        };
+        log::debug!("Launching sink with in_order: {}", in_order);
+
+        if in_order {
+            let mut in_order_receivers = receivers
+                .iter_mut()
+                .map(|r| r.recv())
+                .collect::<FuturesOrdered<_>>();
+            while let Some(val) = in_order_receivers.next().await {
+                if let Some(val) = val {
+                    if matches!(self.process_value(val).await?, SinkResultType::Finished) {
+                        break;
+                    }
+                }
+            }
+        } else {
+            let mut unordered_receivers = receivers
+                .iter_mut()
+                .map(|r| r.recv())
+                .collect::<FuturesUnordered<_>>();
+            while let Some(val) = unordered_receivers.next().await {
+                if let Some(val) = val {
+                    if matches!(self.process_value(val).await?, SinkResultType::Finished) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        self.finalize_values().await
+    }
 }
 
 #[derive(Clone)]
@@ -54,42 +203,31 @@ impl Pipeline {
     ) -> DaftResult<()> {
         log::debug!("Running pipeline");
 
+        // Initialize the unordered handles to store the handles of the inner pipelines and the sink
+        let mut handles_unordered = FuturesUnordered::new();
+
+        // Get the data from the source
         let source_stream = source.get_data();
         pin_mut!(source_stream);
 
-        let mut intermediate_senders_and_handles: Vec<(Sender, JoinHandle<DaftResult<()>>)> =
-            Vec::with_capacity(*NUM_CPUS);
-        let mut sink_receivers = vec![];
-
-        let mut curr_idx = 0;
+        // Create the inner pipeline manager and send the data from the source to the inner pipelines
+        let mut inner_pipeline_manager = InnerPipelineManager::new(intermediate_operators);
         while let Some(morsel) = source_stream.next().await {
-            if let Some((tx, _)) = intermediate_senders_and_handles.get_mut(curr_idx) {
-                let _ = tx.send(Ok(morsel?)).await;
-            } else {
-                let (tx1, rx1) = create_channel();
-                let (tx2, rx2) = create_channel();
-                let handle = tokio::spawn(run_intermediate_operators(
-                    rx1,
-                    intermediate_operators.clone(),
-                    tx2,
-                ));
-                intermediate_senders_and_handles.push((tx1.clone(), handle));
-                sink_receivers.push(rx2);
-                let _ = tx1.send(Ok(morsel?)).await;
-            }
-
-            curr_idx = (curr_idx + 1) % intermediate_senders_and_handles.len();
+            let inner_pipeline_sender = inner_pipeline_manager.get_or_create_next_sender();
+            let _ = inner_pipeline_sender.send(morsel).await;
         }
+        handles_unordered.extend(inner_pipeline_manager.retrieve_handles());
 
-        let sink_handle = tokio::spawn(launch_sink(sink_receivers, sink, send_to_next_source));
-
-        let mut handles_unordered = FuturesUnordered::new();
-        for (sender, handle) in intermediate_senders_and_handles {
-            drop(sender);
-            handles_unordered.push(handle);
-        }
+        // Create the sink manager and run the sink
+        let sink_handle = tokio::spawn(async move {
+            let mut sink_manager = SinkManager::new(sink, send_to_next_source);
+            sink_manager
+                .run(inner_pipeline_manager.sink_receivers)
+                .await
+        });
         handles_unordered.push(sink_handle);
 
+        // Wait for the inner pipelines and the sink to finish
         tokio::select! {
             biased;
             _ = tokio::signal::ctrl_c() => {
--- src/daft-local-execution/src/sinks/sink.rs
+++ src/daft-local-execution/src/sinks/sink.rs
@@ -1,10 +1,7 @@
-use std::{borrow::BorrowMut, sync::Arc};
+use std::sync::Arc;
 
 use common_error::DaftResult;
 use daft_micropartition::MicroPartition;
-use futures::{stream::FuturesUnordered, StreamExt};
-
-use crate::{Receiver, Sender};
 
 pub enum SinkResultType {
     NeedMoreInput,
@@ -18,74 +15,3 @@ pub trait Sink: dyn_clone::DynClone + Send + Sync {
 }
 
 dyn_clone::clone_trait_object!(Sink);
-
-pub async fn launch_sink(
-    mut receivers: Vec<Receiver>,
-    mut sink: Option<Box<dyn Sink>>,
-    send_to_next_source: Sender,
-) -> DaftResult<()> {
-    let in_order = match sink {
-        Some(ref sink) => sink.in_order(),
-        None => false,
-    };
-    log::debug!("Launching sink with in_order: {}", in_order);
-
-    if in_order {
-        let mut finished_receiver_idxs = std::collections::HashSet::new();
-        let mut curr_idx = 0;
-        while finished_receiver_idxs.len() != receivers.len() {
-            if finished_receiver_idxs.contains(&curr_idx) {
-                curr_idx = (curr_idx + 1) % receivers.len();
-                continue;
-            }
-            let receiver = receivers.get_mut(curr_idx).expect("Receiver not found");
-            if let Some(val) = receiver.recv().await {
-                if let Some(sink) = sink.borrow_mut() {
-                    let sink_result = sink.sink(&val?)?;
-                    match sink_result {
-                        SinkResultType::Finished => {
-                            break;
-                        }
-                        SinkResultType::NeedMoreInput => {}
-                    }
-                } else {
-                    log::debug!("No sink, sending value to next source");
-                    let _ = send_to_next_source.send(val).await;
-                }
-            } else {
-                finished_receiver_idxs.insert(curr_idx);
-            }
-            curr_idx = (curr_idx + 1) % receivers.len();
-        }
-    } else {
-        let mut unordered_receivers = FuturesUnordered::new();
-        for receiver in receivers.iter_mut() {
-            unordered_receivers.push(receiver.recv());
-        }
-        while let Some(val) = unordered_receivers.next().await {
-            if let Some(val) = val {
-                if let Some(sink) = sink.borrow_mut() {
-                    let sink_result = sink.sink(&val?)?;
-                    match sink_result {
-                        SinkResultType::Finished => {
-                            break;
-                        }
-                        SinkResultType::NeedMoreInput => {}
-                    }
-                } else {
-                    log::debug!("No sink, sending value to next source");
-                    let _ = send_to_next_source.send(val).await;
-                }
-            }
-        }
-    }
-
-    if let Some(sink) = sink.borrow_mut() {
-        let final_values = sink.finalize()?;
-        for value in final_values {
-            log::debug!("Sending finalized value to next source");
-            let _ = send_to_next_source.send(Ok(value)).await;
-        }
-    }
-    Ok(())
-}
