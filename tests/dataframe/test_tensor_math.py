2137407cb5963d28b51923dee8bf24b2efc7dd35
Sammy Sidhu <samster25@users.noreply.github.com>
2024-07-12 17:25:50 -0700

    add basic tests

--- tests/dataframe/test_tensor_math.py
+++ tests/dataframe/test_tensor_math.py
@@ -1,18 +1,35 @@
+import itertools
+from operator import add, mul, sub
+
 import numpy as np
+import pytest
 from numpy.testing import assert_almost_equal
 
 import daft
+from daft import DataType
+
+daft_signed_int_types = [
+    DataType.int8(),
+    DataType.int16(),
+    DataType.int32(),
+    DataType.int64(),
+]
+
+daft_numeric_types = daft_signed_int_types + [DataType.float32(), DataType.float64()]
 
 
-def test_math_tensors() -> None:
+@pytest.mark.parametrize(
+    "op, ldtype, rdtype", itertools.product([add, mul, sub], daft_numeric_types, daft_numeric_types)
+)
+def test_math_tensors(op, ldtype, rdtype) -> None:
     np.random.seed(1)
-    x = np.random.random((10, 10, 1))
-    y = np.random.random((10, 10, 1))
-    expected = x * y
+    x = np.random.randint(0, 10, (12, 10, 1)).astype(ldtype.to_arrow_dtype().to_pandas_dtype())
+    y = np.random.randint(0, 10, (12, 10, 1)).astype(rdtype.to_arrow_dtype().to_pandas_dtype())
+    expected = op(x, y)
 
     df = daft.from_pydict({"x": x, "y": y})
     df = df.with_column("x", df["x"].cast(daft.DataType.tensor(daft.DataType.float32(), (10, 1))))
     df = df.with_column("y", df["y"].cast(daft.DataType.tensor(daft.DataType.float32(), (10, 1))))
 
-    result = df.with_column("z", df["x"] * df["y"]).collect().to_pydict()
+    result = df.with_column("z", op(df["x"], df["y"])).collect().to_pydict()
     assert_almost_equal(result["z"], expected)
