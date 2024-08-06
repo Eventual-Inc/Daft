aef599945a42bffc8b7fdc6be653638802964cb9
Kev Wang <kevin@eventualcomputing.com>
2024-08-06 10:38:31 -0700

    [FEAT] Enable broadcast strategy on anti and semi joins (#2621)

--- tests/dataframe/test_joins.py
+++ tests/dataframe/test_joins.py
@@ -13,10 +13,6 @@ def skip_invalid_join_strategies(join_strategy, join_type):
         pytest.skip("Sort merge currently only supports inner joins")
     elif join_strategy == "broadcast" and join_type == "outer":
         pytest.skip("Broadcast join does not support outer joins")
-    elif join_strategy == "broadcast" and join_type == "anti":
-        pytest.skip("Broadcast join does not support anti joins")
-    elif join_strategy == "broadcast" and join_type == "semi":
-        pytest.skip("Broadcast join does not support semi joins")
 
 
 def test_invalid_join_strategies(make_df):
