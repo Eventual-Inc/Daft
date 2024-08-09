9562cc6a25f1a2fded8b5c8799ebf697495287fc
Conor Kennedy <32619800+Vince7778@users.noreply.github.com>
2024-08-09 15:58:07 -0700

    [FEAT] Add wildcards in column expressions (#2629)
    Allows using wildcards in columns to match multiple columns, as well as
    subfields in structs: `col("*")`, `col("mystruct.*")`
    
    Helps address #1965, but does not support partial matches of names. I
    believe this should be done separately using a different expression like
    `regexcol()` to avoid column naming conflicts.
    
    Examples:
    ```py
    >>> df = daft.from_pydict({"a": [1, 2, 3], "b": [4, 5, 6]})
    >>> df.select("*").show()
    ╭───────┬───────╮
    │ a     ┆ b     │
    │ ---   ┆ ---   │
    │ Int64 ┆ Int64 │
    ╞═══════╪═══════╡
    │ 1     ┆ 4     │
    ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌┤
    │ 2     ┆ 5     │
    ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌┤
    │ 3     ┆ 6     │
    ╰───────┴───────╯
    >>> df.select(col("*").sqrt()).show()
    ╭────────────────────┬───────────────────╮ 
    │ a                  ┆ b                 │
    │ ---                ┆ ---               │
    │ Float64            ┆ Float64           │
    ╞════════════════════╪═══════════════════╡
    │ 1                  ┆ 2                 │
    ├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┤
    │ 1.4142135623730951 ┆ 2.23606797749979  │
    ├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┤
    │ 1.7320508075688772 ┆ 2.449489742783178 │
    ╰────────────────────┴───────────────────╯
    ```
    
    ```py
    >>> df = daft.from_pydict({"a": [{"b": 1, "c": 4}, {"b": 2, "c": 5}]})
    >>> df.select("a.*").show()
    ╭───────┬───────╮
    │ b     ┆ c     │
    │ ---   ┆ ---   │
    │ Int64 ┆ Int64 │
    ╞═══════╪═══════╡
    │ 1     ┆ 4     │
    ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌┤
    │ 2     ┆ 5     │
    ╰───────┴───────╯
    ```

--- src/daft-dsl/src/lib.rs
+++ src/daft-dsl/src/lib.rs
@@ -10,17 +10,19 @@ pub mod optimization;
 mod pyobject;
 #[cfg(feature = "python")]
 pub mod python;
+mod resolve_expr;
 mod treenode;
 pub use common_treenode;
 pub use expr::binary_op;
 pub use expr::col;
-pub use expr::{
-    is_partition_compatible, resolve_aggexpr, resolve_aggexprs, resolve_expr, resolve_exprs,
-};
+pub use expr::is_partition_compatible;
 pub use expr::{AggExpr, ApproxPercentileParams, Expr, ExprRef, Operator};
 pub use lit::{lit, null_lit, Literal, LiteralValue};
 #[cfg(feature = "python")]
 use pyo3::prelude::*;
+pub use resolve_expr::{
+    resolve_aggexprs, resolve_exprs, resolve_single_aggexpr, resolve_single_expr,
+};
 
 #[cfg(feature = "python")]
 pub fn register_modules(_py: Python, parent: &PyModule) -> PyResult<()> {
