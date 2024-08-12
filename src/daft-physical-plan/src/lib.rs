1f4083057801a66cf2ad0118be9361961fea9188
Colin Ho <colinho@Colins-MBP.localdomain>
2024-08-12 15:34:20 -0700

    more cleanup

--- src/daft-physical-plan/src/lib.rs
+++ src/daft-physical-plan/src/lib.rs
@@ -3,7 +3,7 @@ mod local_plan;
 mod translate;
 
 pub use local_plan::{
-    Concat, Distinct, Filter, HashAggregate, HashJoin, InMemoryScan, Limit, LocalPhysicalPlan,
+    Concat, Filter, HashAggregate, HashJoin, InMemoryScan, Limit, LocalPhysicalPlan,
     LocalPhysicalPlanRef, PhysicalScan, PhysicalWrite, Project, Sort, UnGroupedAggregate,
 };
 pub use translate::translate;
