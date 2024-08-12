8b38f7815f1aa7b82f6998023b0b774facf04779
universalmind303 <cory.grinstead@gmail.com>
2024-08-12 15:38:39 -0500

    impl treedisplay

--- src/common/display/src/lib.rs
+++ src/common/display/src/lib.rs
@@ -1,1 +1,31 @@
+use mermaid::MermaidDisplayOptions;
 
+pub mod ascii;
+pub mod mermaid;
+pub mod tree;
+
+pub trait DisplayAs {
+    fn display_as(&self, level: DisplayLevel) -> String;
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum DisplayLevel {
+    /// A compact display, showing only the most important details.
+    Compact,
+    /// The default display, showing common details.
+    Default,
+    /// A verbose display, showing all available details.
+    Verbose,
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "python", derive(pyo3::FromPyObject))]
+// do not change the order of the variants, pyo3 matches from top to bottom,
+// If any variants have a union, it will always match the first one,
+// So we need to order them from most specific to least specific.
+pub enum DisplayFormat {
+    // Display the tree in Mermaid format.
+    Mermaid(MermaidDisplayOptions),
+    // Display the tree in ASCII format.
+    Ascii { simple: bool },
+}
