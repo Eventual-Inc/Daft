11b7f21169f4c3042417e389a39f3229d819f8f9
Sammy Sidhu <samster25@users.noreply.github.com>
2024-08-13 14:41:26 -0700

    use secrecy

--- src/common/io-config/src/lib.rs
+++ src/common/io-config/src/lib.rs
@@ -7,19 +7,37 @@ mod gcs;
 mod http;
 mod s3;
 
-use std::fmt::{Debug, Display};
+use std::{fmt::{Debug, Display}, hash::Hash};
+
+use secrecy::{DebugSecret, ExposeSecret, Secret};
+use serde::{Deserialize, Deserializer, Serialize};
 
 pub use crate::{
     azure::AzureConfig, config::IOConfig, gcs::GCSConfig, http::HTTPConfig, s3::S3Config,
     s3::S3Credentials,
 };
 
-#[derive(Clone, serde::Deserialize, serde::Serialize, PartialEq, Eq, Hash)]
-pub struct ObfuscatedString(String);
+#[derive(Clone)]
+pub struct ObfuscatedString(Secret<String>);
 
 impl ObfuscatedString {
     pub fn as_string(&self) -> &String {
-        &self.0
+        &self.0.expose_secret()
+    }
+}
+
+
+impl PartialEq for ObfuscatedString {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.expose_secret() == other.0.expose_secret()
+    }
+}
+
+impl Eq for ObfuscatedString {}
+
+impl Hash for ObfuscatedString {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.expose_secret().hash(state)
     }
 }
 
@@ -35,8 +53,28 @@ impl Debug for ObfuscatedString {
     }
 }
 
+impl Serialize for ObfuscatedString {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer {
+        self.0.expose_secret().serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for ObfuscatedString {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(ObfuscatedString(s.into()))
+    }
+}
+
+
+
 impl From<String> for ObfuscatedString {
     fn from(value: String) -> Self {
-        ObfuscatedString(value)
+        ObfuscatedString(value.into())
     }
 }
