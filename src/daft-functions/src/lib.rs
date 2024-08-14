40db68e14e2daf25e372a3375cb293535b77801f
Raunak Bhagat <rabhagat31@gmail.com>
2024-08-13 17:34:08 -0700

    Move cbrt to a new `numeric` folder

--- src/daft-functions/src/lib.rs
+++ src/daft-functions/src/lib.rs
@@ -1,10 +1,10 @@
 #![feature(async_closure)]
-pub mod cbrt;
 pub mod count_matches;
 pub mod distance;
 pub mod hash;
 pub mod list_sort;
 pub mod minhash;
+pub mod numeric;
 pub mod tokenize;
 pub mod uri;
 
@@ -24,8 +24,7 @@ pub fn register_modules(_py: Python, parent: &PyModule) -> PyResult<()> {
     parent.add_wrapped(wrap_pyfunction!(minhash::python::minhash))?;
     parent.add_wrapped(wrap_pyfunction!(count_matches::python::utf8_count_matches))?;
     parent.add_wrapped(wrap_pyfunction!(list_sort::python::list_sort))?;
-    parent.add_wrapped(wrap_pyfunction!(cbrt::python::cbrt))?;
-
+    parent.add_wrapped(wrap_pyfunction!(numeric::cbrt::python::cbrt))?;
     Ok(())
 }
 
