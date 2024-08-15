9b8c2b286c9c0cd746bdf48838f456814fd99d95
Jay Chia <17691182+jaychia@users.noreply.github.com>
2024-08-14 18:17:32 -0700

    [FEAT] Deprecates usage of resource_request on df.with_column API (#2654)
    Fully deprecates the use of `df.with_column(..., resource_request=...)`
    in favor of using resources on UDFs instead.
    
    ---------
    
    Co-authored-by: Jay Chia <jaychia94@gmail.com@users.noreply.github.com>

--- tests/test_resource_requests.py
+++ tests/test_resource_requests.py
@@ -6,7 +6,7 @@ import pytest
 import ray
 
 import daft
-from daft import ResourceRequest, udf
+from daft import udf
 from daft.context import get_context
 from daft.daft import SystemInfo
 from daft.expressions import col
@@ -61,63 +61,41 @@ def test_partial_resource_request_overrides():
 
 
 @pytest.mark.skipif(get_context().runner_config.name not in {"py"}, reason="requires PyRunner to be in use")
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_requesting_too_many_cpus(use_legacy_resource_requesting):
+def test_requesting_too_many_cpus():
     df = daft.from_pydict(DATA)
     system_info = SystemInfo()
 
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "foo",
-            my_udf(col("id")),
-            resource_request=ResourceRequest(num_cpus=system_info.cpu_count() + 1),
-        )
-    else:
-        my_udf_parametrized = my_udf.override_options(num_cpus=system_info.cpu_count() + 1)
-        df = df.with_column(
-            "foo",
-            my_udf_parametrized(col("id")),
-        )
+    my_udf_parametrized = my_udf.override_options(num_cpus=system_info.cpu_count() + 1)
+    df = df.with_column(
+        "foo",
+        my_udf_parametrized(col("id")),
+    )
 
     with pytest.raises(RuntimeError):
         df.collect()
 
 
 @pytest.mark.skipif(get_context().runner_config.name not in {"py"}, reason="requires PyRunner to be in use")
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_requesting_too_many_gpus(use_legacy_resource_requesting):
+def test_requesting_too_many_gpus():
     df = daft.from_pydict(DATA)
 
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "foo", my_udf(col("id")), resource_request=ResourceRequest(num_gpus=cuda_device_count() + 1)
-        )
-    else:
-        my_udf_parametrized = my_udf.override_options(num_gpus=cuda_device_count() + 1)
-        df = df.with_column("foo", my_udf_parametrized(col("id")))
+    my_udf_parametrized = my_udf.override_options(num_gpus=cuda_device_count() + 1)
+    df = df.with_column("foo", my_udf_parametrized(col("id")))
 
     with pytest.raises(RuntimeError):
         df.collect()
 
 
 @pytest.mark.skipif(get_context().runner_config.name not in {"py"}, reason="requires PyRunner to be in use")
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_requesting_too_much_memory(use_legacy_resource_requesting):
+def test_requesting_too_much_memory():
     df = daft.from_pydict(DATA)
     system_info = SystemInfo()
 
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "foo",
-            my_udf(col("id")),
-            resource_request=ResourceRequest(memory_bytes=system_info.total_memory() + 1),
-        )
-    else:
-        my_udf_parametrized = my_udf.override_options(memory_bytes=system_info.total_memory() + 1)
-        df = df.with_column(
-            "foo",
-            my_udf_parametrized(col("id")),
-        )
+    my_udf_parametrized = my_udf.override_options(memory_bytes=system_info.total_memory() + 1)
+    df = df.with_column(
+        "foo",
+        my_udf_parametrized(col("id")),
+    )
 
     with pytest.raises(RuntimeError):
         df.collect()
@@ -149,24 +127,14 @@ RAY_VERSION_LT_2 = int(ray.__version__.split(".")[0]) < 2
     RAY_VERSION_LT_2, reason="The ray.get_runtime_context().get_assigned_resources() was only added in Ray >= 2.0"
 )
 @pytest.mark.skipif(get_context().runner_config.name not in {"ray"}, reason="requires RayRunner to be in use")
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_with_column_rayrunner(use_legacy_resource_requesting):
+def test_with_column_rayrunner():
     df = daft.from_pydict(DATA).repartition(2)
 
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "resources_ok",
-            assert_resources(col("id"), num_cpus=1, num_gpus=None, memory=1_000_000),
-            resource_request=ResourceRequest(num_cpus=1, memory_bytes=1_000_000, num_gpus=None),
-        )
-    else:
-        assert_resources_parametrized = assert_resources.override_options(
-            num_cpus=1, memory_bytes=1_000_000, num_gpus=None
-        )
-        df = df.with_column(
-            "resources_ok",
-            assert_resources_parametrized(col("id"), num_cpus=1, num_gpus=None, memory=1_000_000),
-        )
+    assert_resources_parametrized = assert_resources.override_options(num_cpus=1, memory_bytes=1_000_000, num_gpus=None)
+    df = df.with_column(
+        "resources_ok",
+        assert_resources_parametrized(col("id"), num_cpus=1, num_gpus=None, memory=1_000_000),
+    )
 
     df.collect()
 
@@ -175,8 +143,7 @@ def test_with_column_rayrunner(use_legacy_resource_requesting):
     RAY_VERSION_LT_2, reason="The ray.get_runtime_context().get_assigned_resources() was only added in Ray >= 2.0"
 )
 @pytest.mark.skipif(get_context().runner_config.name not in {"ray"}, reason="requires RayRunner to be in use")
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_with_column_folded_rayrunner(use_legacy_resource_requesting):
+def test_with_column_folded_rayrunner():
     df = daft.from_pydict(DATA).repartition(2)
 
     # Because of Projection Folding optimizations, the expected resource request is the max of the three .with_column requests
@@ -186,28 +153,16 @@ def test_with_column_folded_rayrunner(use_legacy_resource_requesting):
         assert_resources(col("id"), **expected),
     )
 
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "more_memory_request",
-            assert_resources(col("id"), **expected),
-            resource_request=ResourceRequest(num_cpus=1, memory_bytes=5_000_000, num_gpus=None),
-        )
-        df = df.with_column(
-            "more_cpu_request",
-            assert_resources(col("id"), **expected),
-            resource_request=ResourceRequest(num_cpus=1, memory_bytes=None, num_gpus=None),
-        )
-    else:
-        assert_resources_1 = assert_resources.override_options(num_cpus=1, memory_bytes=5_000_000, num_gpus=None)
-        assert_resources_2 = assert_resources.override_options(num_cpus=1, memory_bytes=None, num_gpus=None)
-        df = df.with_column(
-            "more_memory_request",
-            assert_resources_1(col("id"), **expected),
-        )
-        df = df.with_column(
-            "more_cpu_request",
-            assert_resources_2(col("id"), **expected),
-        )
+    assert_resources_1 = assert_resources.override_options(num_cpus=1, memory_bytes=5_000_000, num_gpus=None)
+    assert_resources_2 = assert_resources.override_options(num_cpus=1, memory_bytes=None, num_gpus=None)
+    df = df.with_column(
+        "more_memory_request",
+        assert_resources_1(col("id"), **expected),
+    )
+    df = df.with_column(
+        "more_cpu_request",
+        assert_resources_2(col("id"), **expected),
+    )
     df.collect()
 
 
@@ -233,24 +188,14 @@ def assert_num_cuda_visible_devices(c, num_gpus: int = 0):
 
 @pytest.mark.skipif(get_context().runner_config.name not in {"py"}, reason="requires PyRunner to be in use")
 @pytest.mark.skipif(no_gpu_available(), reason="requires GPUs to be available")
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_with_column_pyrunner_gpu(use_legacy_resource_requesting):
+def test_with_column_pyrunner_gpu():
     df = daft.from_pydict(DATA).repartition(5)
 
-    # We do not do any masking of devices for the local PyRunner, even if the user requests fewer GPUs
-    # than the host actually has.
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "foo",
-            assert_num_cuda_visible_devices(col("id"), num_gpus=cuda_device_count()),
-            resource_request=ResourceRequest(num_gpus=1),
-        )
-    else:
-        # We set num_gpus=1 on the UDF itself
-        df = df.with_column(
-            "foo",
-            assert_num_cuda_visible_devices(col("id"), num_gpus=cuda_device_count()),
-        )
+    # We set num_gpus=1 on the UDF itself
+    df = df.with_column(
+        "foo",
+        assert_num_cuda_visible_devices(col("id"), num_gpus=cuda_device_count()),
+    )
 
     df.collect()
 
@@ -258,56 +203,33 @@ def test_with_column_pyrunner_gpu(use_legacy_resource_requesting):
 @pytest.mark.skipif(get_context().runner_config.name not in {"ray"}, reason="requires RayRunner to be in use")
 @pytest.mark.skipif(no_gpu_available(), reason="requires GPUs to be available")
 @pytest.mark.parametrize("num_gpus", [None, 1])
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_with_column_rayrunner_gpu(num_gpus, use_legacy_resource_requesting):
+def test_with_column_rayrunner_gpu(num_gpus):
     df = daft.from_pydict(DATA).repartition(2)
 
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "num_cuda_visible_devices",
-            assert_num_cuda_visible_devices(col("id"), num_gpus=num_gpus if num_gpus is not None else 0),
-            resource_request=ResourceRequest(num_gpus=num_gpus),
-        )
-    else:
-        assert_num_cuda_visible_devices_parametrized = assert_num_cuda_visible_devices.override_options(
-            num_gpus=num_gpus
-        )
-        df = df.with_column(
-            "num_cuda_visible_devices",
-            assert_num_cuda_visible_devices_parametrized(col("id"), num_gpus=num_gpus if num_gpus is not None else 0),
-        )
+    assert_num_cuda_visible_devices_parametrized = assert_num_cuda_visible_devices.override_options(num_gpus=num_gpus)
+    df = df.with_column(
+        "num_cuda_visible_devices",
+        assert_num_cuda_visible_devices_parametrized(col("id"), num_gpus=num_gpus if num_gpus is not None else 0),
+    )
 
     df.collect()
 
 
 @pytest.mark.skipif(get_context().runner_config.name not in {"ray"}, reason="requires RayRunner to be in use")
 @pytest.mark.skipif(no_gpu_available(), reason="requires GPUs to be available")
-@pytest.mark.parametrize("use_legacy_resource_requesting", [True, False])
-def test_with_column_max_resources_rayrunner_gpu(use_legacy_resource_requesting):
+def test_with_column_max_resources_rayrunner_gpu():
     df = daft.from_pydict(DATA).repartition(2)
 
     # Because of projection folding optimizations, both UDFs should run with num_gpus=1 even though 0_gpu_col requested for 0 GPUs
-    if use_legacy_resource_requesting:
-        df = df.with_column(
-            "0_gpu_col",
-            assert_num_cuda_visible_devices(col("id"), num_gpus=1),
-            resource_request=ResourceRequest(num_gpus=0),
-        )
-        df = df.with_column(
-            "1_gpu_col",
-            assert_num_cuda_visible_devices(col("id"), num_gpus=1),
-            resource_request=ResourceRequest(num_gpus=1),
-        )
-    else:
-        assert_num_cuda_visible_devices_0 = assert_num_cuda_visible_devices.override_options(num_gpus=0)
-        assert_num_cuda_visible_devices_1 = assert_num_cuda_visible_devices.override_options(num_gpus=1)
-        df = df.with_column(
-            "0_gpu_col",
-            assert_num_cuda_visible_devices_0(col("id"), num_gpus=1),
-        )
-        df = df.with_column(
-            "1_gpu_col",
-            assert_num_cuda_visible_devices_1(col("id"), num_gpus=1),
-        )
+    assert_num_cuda_visible_devices_0 = assert_num_cuda_visible_devices.override_options(num_gpus=0)
+    assert_num_cuda_visible_devices_1 = assert_num_cuda_visible_devices.override_options(num_gpus=1)
+    df = df.with_column(
+        "0_gpu_col",
+        assert_num_cuda_visible_devices_0(col("id"), num_gpus=1),
+    )
+    df = df.with_column(
+        "1_gpu_col",
+        assert_num_cuda_visible_devices_1(col("id"), num_gpus=1),
+    )
 
     df.collect()
