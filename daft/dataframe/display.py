2127672131cfefb463f8dd0fddf0456e405bbaf9
Cory Grinstead <cory.grinstead@gmail.com>
2024-08-19 19:23:46 -0500

    [BUG]: repr mermaid fix (#2688)

--- daft/dataframe/display.py
+++ daft/dataframe/display.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Optional
 
 from daft.context import get_context
 
@@ -21,25 +21,18 @@ class SubgraphOptions:
 
 class MermaidOptions:
     simple: bool
+    bottom_up: bool
     subgraph_options: Optional[SubgraphOptions]
 
-    def __init__(self, simple: bool = False, subgraph_options: Optional[SubgraphOptions] = None):
+    def __init__(self, simple: bool = False, bottom_up=False, subgraph_options: Optional[SubgraphOptions] = None):
         self.simple = simple
+        self.bottom_up = bottom_up
         self.subgraph_options = subgraph_options
 
     def with_subgraph_options(self, name: str, subgraph_id: str):
-        opts = MermaidOptions(self.simple, SubgraphOptions(name, subgraph_id))
+        opts = MermaidOptions(self.simple, subgraph_options=SubgraphOptions(name, subgraph_id))
 
         return opts
-
-
-def make_display_options(simple: bool, format: str) -> Union[MermaidOptions, AsciiOptions]:
-    if format == "ascii":
-        return AsciiOptions(simple)
-    elif format == "mermaid":
-        return MermaidOptions(simple)
-    else:
-        raise ValueError(f"Unknown format: {format}")
 
 
 class MermaidFormatter:
@@ -53,24 +46,22 @@ class MermaidFormatter:
         builder = self.builder
         output = ""
         display_opts = MermaidOptions(simple=self.simple)
-
         # TODO handle cached plans
-
         if self.show_all:
             output = "```mermaid\n"
             output += "flowchart TD\n"
-            output += builder._builder.display_as(
+            output += builder._builder.repr_mermaid(
                 display_opts.with_subgraph_options(name="Unoptimized LogicalPlan", subgraph_id="unoptimized")
             )
             output += "\n"
 
             builder = builder.optimize()
-            output += builder._builder.display_as(
+            output += builder._builder.repr_mermaid(
                 display_opts.with_subgraph_options(name="Optimized LogicalPlan", subgraph_id="optimized")
             )
             output += "\n"
             physical_plan_scheduler = builder.to_physical_plan_scheduler(get_context().daft_execution_config)
-            output += physical_plan_scheduler._scheduler.display_as(
+            output += physical_plan_scheduler._scheduler.repr_mermaid(
                 display_opts.with_subgraph_options(name="Physical Plan", subgraph_id="physical")
             )
             output += "\n"
@@ -80,7 +71,7 @@ class MermaidFormatter:
 
         else:
             output = "```mermaid\n"
-            output += builder._builder.display_as(display_opts)
+            output += builder._builder.repr_mermaid(display_opts)
             output += "\n"
             output += "```\n"
             output += (
--- src/common/display/src/lib.rs
+++ src/common/display/src/lib.rs
@@ -1,5 +1,3 @@
-use mermaid::MermaidDisplayOptions;
-
 pub mod ascii;
 pub mod mermaid;
 pub mod tree;
@@ -17,16 +15,4 @@ pub enum DisplayLevel {
     Default,
     /// A verbose display, showing all available details.
     Verbose,
-}
-
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "python", derive(pyo3::FromPyObject))]
-// do not change the order of the variants, pyo3 matches from top to bottom,
-// If any variants have a union, it will always match the first one,
-// So we need to order them from most specific to least specific.
-pub enum DisplayFormat {
-    // Display the tree in Mermaid format.
-    Mermaid(MermaidDisplayOptions),
-    // Display the tree in ASCII format.
-    Ascii { simple: bool },
 }
