ee111d720a62594cb69e6eba63e5f9aab04d47d6
Colin Ho <colinho@Colins-MBP.localdomain>
2024-08-21 17:09:25 -0700

    use python log level

--- src/lib.rs
+++ src/lib.rs
@@ -47,11 +47,10 @@ fn should_enable_chrome_trace() -> bool {
 #[cfg(feature = "python")]
 pub mod pylib {
     use common_tracing::init_tracing;
-    use lazy_static::lazy_static;
     use pyo3::prelude::*;
-    lazy_static! {
-        static ref LOG_RESET_HANDLE: pyo3_log::ResetHandle = pyo3_log::init();
-    }
+    static LOG_RESET_HANDLE: std::sync::OnceLock<pyo3_log::ResetHandle> =
+        std::sync::OnceLock::new();
+
     #[pyfunction]
     pub fn version() -> &'static str {
         daft_core::VERSION
@@ -63,13 +62,39 @@ pub mod pylib {
     }
 
     #[pyfunction]
-    pub fn refresh_logger() {
-        LOG_RESET_HANDLE.reset();
+    pub fn refresh_logger(py: Python) -> PyResult<()> {
+        use log::LevelFilter;
+        let logging = py.import("logging")?;
+        let python_log_level = logging
+            .getattr("getLogger")?
+            .call0()?
+            .getattr("level")?
+            .extract::<i32>()?;
+
+        // https://docs.python.org/3/library/logging.html#logging-levels
+        let level_filter = if python_log_level <= 0 {
+            LevelFilter::Off
+        } else if python_log_level <= 5 {
+            LevelFilter::Trace
+        } else if python_log_level <= 10 {
+            LevelFilter::Debug
+        } else if python_log_level <= 20 {
+            LevelFilter::Info
+        } else if python_log_level <= 30 {
+            LevelFilter::Warn
+        } else {
+            LevelFilter::Error
+        };
+
+        let logger = pyo3_log::Logger::default().filter(level_filter);
+        let handle = logger.install().unwrap();
+        LOG_RESET_HANDLE.get_or_init(|| handle);
+        Ok(())
     }
 
     #[pymodule]
     fn daft(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-        refresh_logger();
+        refresh_logger(_py)?;
         init_tracing(crate::should_enable_chrome_trace());
 
         common_daft_config::register_modules(_py, m)?;
