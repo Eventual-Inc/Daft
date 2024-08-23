4f14546362fb874241d898d234f0791d8d1516f7
Kevin Wang <kevin@eventualcomputing.com>
2024-08-22 17:33:09 -0700

    Revert "use broadcast join strategy"
    This reverts commit 0fd8f3f43d7242f5c5405625637abb0c72e1b545.

--- benchmarking/tpch/answers.py
+++ benchmarking/tpch/answers.py
@@ -109,7 +109,7 @@ def q4(get_df: GetDFFunc) -> DataFrame:
     lineitems = lineitems.where(col("L_COMMITDATE") < col("L_RECEIPTDATE"))
 
     daft_df = (
-        orders.join(lineitems, left_on=col("O_ORDERKEY"), right_on=col("L_ORDERKEY"), how="semi", strategy="broadcast")
+        orders.join(lineitems, left_on=col("O_ORDERKEY"), right_on=col("L_ORDERKEY"), how="semi")
         .groupby(col("O_ORDERPRIORITY"))
         .agg(col("O_ORDERKEY").count().alias("order_count"))
         .sort(col("O_ORDERPRIORITY"))
@@ -661,7 +661,7 @@ def q22(get_df: GetDFFunc) -> DataFrame:
     )
 
     daft_df = (
-        res_1.join(orders, left_on="C_CUSTKEY", right_on="O_CUSTKEY", how="anti", strategy="broadcast")
+        res_1.join(orders, left_on="C_CUSTKEY", right_on="O_CUSTKEY", how="anti")
         .with_column("lit", lit(1))
         .join(res_2, on="lit")
         .where(col("C_ACCTBAL") > col("avg_acctbal"))
