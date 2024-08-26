b8e45f4e432b12148e5f93bc12e60930759eebf2
Colin Ho <colinho@Colins-MBP.localdomain>
2024-08-26 10:19:33 -0700

    add

--- tests/integration/io/parquet/test_reads_s3_minio.py
+++ tests/integration/io/parquet/test_reads_s3_minio.py
@@ -63,6 +63,30 @@ def test_minio_parquet_ignore_marker_files(minio_io_config):
 
 
 @pytest.mark.integration()
+def test_minio_parquet_ignore_marker_prefixes(minio_io_config):
+    from datetime import datetime
+
+    bucket_name = "data-engineering-prod"
+    with minio_create_bucket(minio_io_config, bucket_name=bucket_name) as fs:
+        target_paths = [
+            "s3://data-engineering-prod/X/part-00000-51723f93-0ba2-42f1-a58f-154f0ed40f28.c000.snappy.parquet",
+            "s3://data-engineering-prod/Y/part-00000-6d5c7cc6-3b4a-443e-a46a-ca9e080bda1b.c000.snappy.parquet",
+        ]
+        data = {"x": [1, 2, 3, 4]}
+        pa_table = pa.Table.from_pydict(data)
+        for path in target_paths:
+            pq.write_table(pa_table, path, filesystem=fs)
+
+        marker_prefixes = ["_started", "_committed"]
+        for marker_prefix in marker_prefixes:
+            fs.touch(f"s3://{bucket_name}/X/{marker_prefix}_{datetime.now().isoformat()}")
+            fs.touch(f"s3://{bucket_name}/Y/{marker_prefix}_{datetime.now().isoformat()}")
+
+        read = daft.read_parquet(f"s3://{bucket_name}/**", io_config=minio_io_config)
+        assert read.to_pydict() == {"x": [1, 2, 3, 4] * 2}
+
+
+@pytest.mark.integration()
 def test_minio_parquet_read_mismatched_schemas_no_pushdown(minio_io_config):
     # When we read files, we infer schema from the first file
     # Then when we read subsequent files, we want to be able to read the data still but add nulls for columns
