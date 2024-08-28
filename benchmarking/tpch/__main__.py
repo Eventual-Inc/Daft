2cc6e4aefd78ac6387270021d7bb6206a8e3978f
Kevin Wang <kevin@eventualcomputing.com>
2024-08-28 14:38:18 -0700

    use anti and semi joins

--- benchmarking/tpch/__main__.py
+++ benchmarking/tpch/__main__.py
@@ -198,7 +198,10 @@ def get_ray_runtime_env(requirements: str | None) -> dict:
     runtime_env = {
         "py_modules": [daft],
         "eager_install": True,
-        "env_vars": {"DAFT_PROGRESS_BAR": "0"},
+        "env_vars": {
+            "DAFT_PROGRESS_BAR": "0",
+            "DAFT_RUNNER": "ray",
+        },
     }
     if requirements:
         runtime_env.update({"pip": requirements})
