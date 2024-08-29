e33306f44594941f6d2845beb1b7affa39c31e3b
universalmind303 <cory.grinstead@gmail.com>
2024-08-29 14:24:16 -0500

    export py function

--- src/daft-functions/src/image/mod.rs
+++ src/daft-functions/src/image/mod.rs
@@ -6,6 +6,7 @@ use pyo3::prelude::*;
 #[cfg(feature = "python")]
 pub fn register_modules(_py: Python, parent: &PyModule) -> PyResult<()> {
     parent.add_wrapped(wrap_pyfunction!(decode::py_decode))?;
+    parent.add_wrapped(wrap_pyfunction!(encode::py_encode))?;
 
     Ok(())
 }
