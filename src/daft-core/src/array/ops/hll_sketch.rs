3336db4d9eba7d47b0eddd28dbd13183220c0ac1
Raunak Bhagat <rabhagat31@gmail.com>
2024-08-29 20:33:52 -0700

    Update hll_sketch to check for validity

--- src/daft-core/src/array/ops/hll_sketch.rs
+++ src/daft-core/src/array/ops/hll_sketch.rs
@@ -16,8 +16,19 @@ impl DaftHllSketchAggable for UInt64Array {
 
     fn hll_sketch(&self) -> Self::Output {
         let mut hll = HyperLogLog::default();
-        for &value in self.as_arrow().values_iter() {
-            hll.add_already_hashed(value);
+        match self.validity() {
+            Some(validity) => {
+                for (index, &value) in self.as_arrow().values_iter().enumerate() {
+                    if validity.get_bit(index) {
+                        hll.add_already_hashed(value);
+                    };
+                }
+            }
+            None => {
+                for &value in self.as_arrow().values_iter() {
+                    hll.add_already_hashed(value);
+                }
+            }
         }
         let array = (self.name(), hll.registers.as_ref() as &[u8]).into();
         Ok(array)
@@ -26,15 +37,32 @@ impl DaftHllSketchAggable for UInt64Array {
     fn grouped_hll_sketch(&self, group_indices: &GroupIndices) -> Self::Output {
         let data = self.as_arrow();
         let mut bytes = Vec::<u8>::with_capacity(group_indices.len() * NUM_REGISTERS);
-        for group in group_indices {
-            let mut hll = HyperLogLog::default();
-            for &index in group {
-                if let Some(value) = data.get(index as _) {
-                    hll.add_already_hashed(value);
-                };
+        match self.validity() {
+            Some(validity) => {
+                for group in group_indices {
+                    let mut hll = HyperLogLog::default();
+                    for &index in group {
+                        if let (Some(value), true) =
+                            (data.get(index as _), validity.get_bit(index as _))
+                        {
+                            hll.add_already_hashed(value);
+                        };
+                    }
+                    bytes.extend(hll.registers.as_ref());
+                }
             }
-            bytes.extend(hll.registers.as_ref());
-        }
+            None => {
+                for group in group_indices {
+                    let mut hll = HyperLogLog::default();
+                    for &index in group {
+                        if let Some(value) = data.get(index as _) {
+                            hll.add_already_hashed(value);
+                        };
+                    }
+                    bytes.extend(hll.registers.as_ref());
+                }
+            }
+        };
         let array = (self.name(), bytes.as_slice()).into();
         Ok(array)
     }
