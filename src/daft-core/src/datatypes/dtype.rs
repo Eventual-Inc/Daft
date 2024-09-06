55ad58649a61be1142a56432f50313db5dc1183a
Raunak Bhagat <rabhagat31@gmail.com>
2024-09-05 18:31:50 -0700

    Update how timezones are rendered

--- src/daft-core/src/datatypes/dtype.rs
+++ src/daft-core/src/datatypes/dtype.rs
@@ -70,7 +70,7 @@ pub enum DataType {
     ///
     /// When the timezone is not specified, the timestamp is considered to have no timezone
     /// and is represented _as is_
-    #[display("Time[{_0} {}]", _1.as_deref().map_or_else(|| "(n/a)".to_owned(), |zone| zone.to_owned()))]
+    #[display("Time[{_0}, {_1:?}]")]
     Timestamp(TimeUnit, Option<String>),
 
     /// An [`i32`] representing the elapsed time since UNIX epoch (1970-01-01)
