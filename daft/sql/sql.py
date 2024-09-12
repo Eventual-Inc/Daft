c19e343605cba5309255a9f5b02ab0f7957fdabd
chuanleini@tencent.com <chuanleini@tencent.com>
2024-09-12 10:07:17 +0800

    add register-table helper

--- daft/sql/sql.py
+++ daft/sql/sql.py
@@ -27,6 +27,9 @@ class SQLCatalog:
 
     def __str__(self) -> str:
         return str(self._catalog)
+    
+    def register_table(self, name: str, df: DataFrame):
+        self._catalog.register_table(name, df._get_current_builder()._builder)
 
 
 @PublicAPI
