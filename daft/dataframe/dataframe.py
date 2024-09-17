67669558b7aad66d40aac6a42623b1c3a0d002fa
Cory Grinstead <cory.grinstead@gmail.com>
2024-09-17 10:54:52 -0500

    [FEAT]: Dataframe.filter method (#2853)
    closes https://github.com/Eventual-Inc/Daft/issues/2846
    
    Note, this does not rename 'where' to 'filter' but instead just adds an
    alias. We can revisit at a later date if we want to deprecate the
    `where` method.

--- daft/dataframe/dataframe.py
+++ daft/dataframe/dataframe.py
@@ -1311,6 +1311,23 @@ class DataFrame:
         return DataFrame(builder)
 
     @DataframePublicAPI
+    def filter(self, predicate: Union[Expression, str]) -> "DataFrame":
+        """Filters rows via a predicate expression, similar to SQL ``WHERE``.
+
+        Alias for daft.DataFrame.where.
+
+        .. seealso::
+            :meth:`.where(predicate) <DataFrame.where>`
+
+        Args:
+            predicate (Expression): expression that keeps row if evaluates to True.
+
+        Returns:
+            DataFrame: Filtered DataFrame.
+        """
+        return self.where(predicate)
+
+    @DataframePublicAPI
     def where(self, predicate: Union[Expression, str]) -> "DataFrame":
         """Filters rows via a predicate expression, similar to SQL ``WHERE``.
 
