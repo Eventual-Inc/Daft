8d80919cf38ea53cc51d56c49fe113e058dd179a
Colin Ho <colinho@Colins-MacBook-Pro.local>
2024-09-20 15:17:42 -0700

    pa

--- daft/io/writer.py
+++ daft/io/writer.py
@@ -1,11 +1,8 @@
 import uuid
 from typing import Optional, Union
 
-import pyarrow as pa
-from pyarrow import csv as pacsv
-from pyarrow import parquet as papq
-
 from daft.daft import IOConfig
+from daft.dependencies import pa
 from daft.filesystem import (
     _resolve_paths_and_filesystem,
     canonicalize_protocol,
@@ -32,7 +29,7 @@ class FileWriterBase:
 
         self.file_name = f"{uuid.uuid4()}-{file_idx}.{file_format}"
         self.compression = compression if compression is not None else "none"
-        self.current_writer: Optional[Union[papq.ParquetWriter, pacsv.CSVWriter]] = None
+        self.current_writer: Optional[Union[pa.papq.ParquetWriter, pa.pacsv.CSVWriter]] = None
 
     def _create_writer(self, schema: pa.Schema):
         raise NotImplementedError("Subclasses must implement this method.")
@@ -59,9 +56,9 @@ class ParquetFileWriter(FileWriterBase):
     ):
         super().__init__(root_dir, file_idx, "parquet", compression, io_config)
 
-    def _create_writer(self, schema: pa.Schema) -> papq.ParquetWriter:
+    def _create_writer(self, schema: pa.Schema) -> pa.papq.ParquetWriter:
         file_path = f"{self.resolved_path}/{self.file_name}"
-        return papq.ParquetWriter(
+        return pa.papq.ParquetWriter(
             file_path,
             schema,
             compression=self.compression,
@@ -74,9 +71,9 @@ class CSVFileWriter(FileWriterBase):
     def __init__(self, root_dir: str, file_idx: int, io_config: Optional[IOConfig] = None):
         super().__init__(root_dir, file_idx, "csv", None, io_config)
 
-    def _create_writer(self, schema: pa.Schema) -> pacsv.CSVWriter:
+    def _create_writer(self, schema: pa.Schema) -> pa.pacsv.CSVWriter:
         file_path = f"{self.resolved_path}/{self.file_name}"
-        return pacsv.CSVWriter(
+        return pa.pacsv.CSVWriter(
             file_path,
             schema,
         )
