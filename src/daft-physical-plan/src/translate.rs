73e0ee89b55ceab80819437012b6c1602d5f95a5
Colin Ho <colinho@Colins-MBP.localdomain>
2024-09-26 16:28:11 -0700

    empty scan

--- src/daft-physical-plan/src/translate.rs
+++ src/daft-physical-plan/src/translate.rs
@@ -14,10 +14,14 @@ pub fn translate(plan: &LogicalPlanRef) -> DaftResult<LocalPhysicalPlanRef> {
                     // We should be able to pass the ScanOperator into the physical plan directly but we need to figure out the serialization story
                     let scan_tasks_iter = info.scan_op.0.to_scan_tasks(info.pushdowns.clone())?;
                     let scan_tasks = scan_tasks_iter.collect::<DaftResult<Vec<_>>>()?;
-                    Ok(LocalPhysicalPlan::physical_scan(
-                        scan_tasks,
-                        source.output_schema.clone(),
-                    ))
+                    if scan_tasks.is_empty() {
+                        Ok(LocalPhysicalPlan::empty_scan(source.output_schema.clone()))
+                    } else {
+                        Ok(LocalPhysicalPlan::physical_scan(
+                            scan_tasks,
+                            source.output_schema.clone(),
+                        ))
+                    }
                 }
                 SourceInfo::PlaceHolder(_) => {
                     panic!("We should not encounter a PlaceHolder during translation")
