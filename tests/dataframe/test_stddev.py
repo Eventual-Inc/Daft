70577abd129c5ebfdc5537a937294839f65fc116
Raunak Bhagat <rabhagat31@gmail.com>
2024-10-08 07:41:39 -0700

    Add some tests for stddev (single- and multi- partitioned)

--- tests/dataframe/test_stddev.py
+++ tests/dataframe/test_stddev.py
@@ -7,25 +7,36 @@ import daft
 
 
 def stddev(nums) -> float:
+    def sum_reducer(acc, num):
+        return acc + num if num is not None else acc
+
+    def stddev_reducer(acc, num):
+        return acc + (num - mean) ** 2 if num is not None else acc
+
     if not nums:
         return 0.0
-    sum_: float = sum(nums)
+    # sum_: float = sum(nums)
+    sum = functools.reduce(sum_reducer, nums, 0)
     count = len(nums)
-    mean = sum_ / count
-    squared_sums = functools.reduce(lambda acc, num: acc + (num - mean) ** 2, nums, 0)
+    mean = sum / count
+
+    squared_sums = functools.reduce(stddev_reducer, nums, 0)
     stddev = math.sqrt(squared_sums / count)
     return stddev
 
 
 TESTS = [
     [nums := [0], stddev(nums)],
+    [nums := [1], stddev(nums)],
     [nums := [0, 1, 2], stddev(nums)],
     [nums := [0, 0, 0], stddev(nums)],
+    [nums := [None, 0, None], stddev(nums)],
+    [nums := [None] * 10 + [0], stddev(nums)],
 ]
 
 
 @pytest.mark.parametrize("data_and_expected", TESTS)
-def test_stddev(data_and_expected):
+def test_stddev_with_single_partition(data_and_expected):
     data, expected = data_and_expected
     df = daft.from_pydict({"a": data})
     result = df.agg(daft.col("a").stddev()).collect()
