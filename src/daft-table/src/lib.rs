3e1967f39373859d5906ebf3ebbf9a51a3037ae4
Colin Ho <colinho@Colins-MacBook-Pro.local>
2024-10-13 16:29:56 -0700

    just fix replacement

--- src/daft-table/src/lib.rs
+++ src/daft-table/src/lib.rs
@@ -35,6 +35,7 @@ pub use probeable::{make_probeable_builder, Probeable, ProbeableBuilder};
 pub mod python;
 #[cfg(feature = "python")]
 pub use python::register_modules;
+use rand::seq::index::sample;
 use repr_html::html_value;
 
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
@@ -239,11 +240,14 @@ impl Table {
                 Some(seed) => StdRng::seed_from_u64(seed),
                 None => StdRng::from_rng(rand::thread_rng()).unwrap(),
             };
-            let range = Uniform::from(0..self.len() as u64);
             let values: Vec<u64> = if with_replacement {
-                (0..num).map(|_| rng.sample(range)).collect()
-            } else {
+                let range = Uniform::from(0..self.len() as u64);
                 rng.sample_iter(&range).take(num).collect()
+            } else {
+                sample(&mut rng, self.len(), num)
+                    .into_iter()
+                    .map(|i| i as u64)
+                    .collect()
             };
             let indices: daft_core::array::DataArray<daft_core::datatypes::UInt64Type> =
                 UInt64Array::from(("idx", values));
