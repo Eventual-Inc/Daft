1a3aa216613090337dbc1c32bf0045ea0b355068
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-10-13 22:52:35 -0700

    Add DataCatalogTable trait

--- src/daft-catalog/src/lib.rs
+++ src/daft-catalog/src/lib.rs
@@ -1,63 +1,99 @@
-use daft_plan::{LogicalPlanBuilder, LogicalPlanRef};
+use std::{collections::HashMap, default, sync::Arc};
+
+use daft_plan::LogicalPlanBuilder;
 use data_catalog::DataCatalog;
 
 mod data_catalog;
+mod data_catalog_table;
 mod errors;
 
 #[cfg(feature = "python")]
 pub mod python;
 
-/// The [`DaftCatalog`] is a catalog of [`DataCatalog`] implementations
+use errors::Error;
+use once_cell::sync::Lazy;
+
+/// Global singleton DaftMetaCatalog, lazily initialed from the environment
+pub(crate) static GLOBAL_DAFT_META_CATALOG: Lazy<DaftMetaCatalog> =
+    Lazy::new(DaftMetaCatalog::new_from_env);
+
+/// The [`DaftMetaCatalog`] is a catalog of [`DataCatalog`] implementations
 ///
 /// Users of Daft can register various [`DataCatalog`] with Daft, enabling
 /// discovery of tables across various [`DataCatalog`] implementations.
-pub struct DaftCatalog {}
+pub struct DaftMetaCatalog {
+    default_data_catalog: Option<Arc<dyn DataCatalog>>,
+    named_data_catalogs: HashMap<String, Arc<dyn DataCatalog>>,
+    view_catalog: HashMap<String, LogicalPlanBuilder>,
+}
 
-impl DaftCatalog {
-    /// Create a new, empty `DaftCatalog`.
-    pub fn new_empty() -> Self {
-        DaftCatalog {}
+impl DaftMetaCatalog {
+    /// Create a `DaftMetaCatalog` from the current environment
+    pub fn new_from_env() -> Self {
+        // TODO: Parse a YAML file to produce the catalog
+        DaftMetaCatalog {
+            default_data_catalog: None,
+            named_data_catalogs: default::Default::default(),
+            view_catalog: default::Default::default(),
+        }
     }
 
-    /// Create a DaftCatalog from a YAML file
-    ///
-    /// # Arguments
-    ///
-    /// * `yaml_file` - The path to the YAML file containing the catalog configuration.
-    ///
-    /// # Returns
-    ///
-    /// A new `DaftCatalog` instance configured according to the YAML file.
-    pub fn from_yaml_file(_yaml_file: &str) -> Result<Self, common_error::DaftError> {
-        todo!("Implement parsing a Daft catalog from YAML.")
-    }
-
-    /// Register a new [`DataCatalog`] with the `DaftCatalog`.
+    /// Register a new [`DataCatalog`] with the `DaftMetaCatalog`.
     ///
     /// # Arguments
     ///
     /// * `catalog` - The [`DataCatalog`] to register.
     /// * `name` - The name of the [`DataCatalog`], left as `None` if this is the default catalog
-    pub fn register_catalog(&mut self, _catalog: Box<dyn DataCatalog>, _name: Option<&str>) {
-        todo!()
+    pub fn register_catalog(&mut self, catalog: Arc<dyn DataCatalog>, name: Option<&str>) {
+        match name {
+            None => {
+                self.default_data_catalog = Some(catalog);
+            }
+            Some(name) => {
+                self.named_data_catalogs.insert(name.to_string(), catalog);
+            }
+        }
     }
 
-    pub fn register_view(&mut self, _view: LogicalPlanRef) {
-        todo!("Allow for registering views (expressed as LogicalPlans)");
+    pub fn register_view(&mut self, name: &str, view: LogicalPlanBuilder) {
+        self.view_catalog.insert(name.to_string(), view);
     }
-}
 
-/// Provides high-level functionality for reading a table of data against a [`DaftCatalog`]
-///
-/// Resolves the provided table_identifier against the catalog:
-///
-/// 1. If there is an exact match for the provided `table_identifier` in the catalog's registered views, immediately return the exact match
-/// 2. If the [`DaftCatalog`] has a default catalog, we will attempt to resolve the `table_identifier` against the default catalog
-/// 3. If the `table_identifier` is hierarchical (delimited by "."), use the first component as the Data Catalog name and resolve the rest of the components against
-///     the selected Data Catalog
-pub fn read_table(
-    _table_identifier: &str,
-    _catalog: &DaftCatalog,
-) -> errors::Result<LogicalPlanBuilder> {
-    todo!("Implement read_table");
+    /// Provides high-level functionality for reading a table of data against a [`DaftMetaCatalog`]
+    ///
+    /// Resolves the provided table_identifier against the catalog:
+    ///
+    /// 1. If there is an exact match for the provided `table_identifier` in the catalog's registered views, immediately return the exact match
+    /// 2. If the [`DaftMetaCatalog`] has a default catalog, we will attempt to resolve the `table_identifier` against the default catalog
+    /// 3. If the `table_identifier` is hierarchical (delimited by "."), use the first component as the Data Catalog name and resolve the rest of the components against
+    ///     the selected Data Catalog
+    pub fn read_table(
+        &self,
+        table_identifier: &str,
+        catalog_name: Option<&str>,
+    ) -> errors::Result<LogicalPlanBuilder> {
+        // If the name is an exact match with a registered view, return it.
+        if catalog_name.is_none() {
+            if let Some(view) = self.view_catalog.get(table_identifier) {
+                return Ok(view.clone());
+            }
+        }
+
+        // Otherwise, look for
+        let data_catalog = match catalog_name {
+            None => self.default_data_catalog.as_ref(),
+            Some(k) => self.named_data_catalogs.get(k),
+        };
+
+        if let Some(data_catalog) = data_catalog {
+            let _table = data_catalog.get_table(table_identifier);
+            todo!("Create a LogicalPlanBuilder from the DataCatalogTable");
+        } else {
+            Err(Error::CatalogNotFound {
+                name: catalog_name
+                    .map(|s| s.to_string())
+                    .unwrap_or_else(|| "default".to_string()),
+            })
+        }
+    }
 }
