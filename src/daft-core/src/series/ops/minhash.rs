ab9670f09ee9041a7d9b4df9778159701f6bf0a2
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-10-15 19:50:08 -0700

    add many hashers

--- src/daft-core/src/series/ops/minhash.rs
+++ src/daft-core/src/series/ops/minhash.rs
@@ -7,11 +7,11 @@ use crate::{
 };
 
 impl Series {
-    pub fn minhash(&self, num_hashes: usize, ngram_size: usize, seed: u32) -> DaftResult<Self> {
+    pub fn minhash(&self, num_hashes: usize, ngram_size: usize, seed: u32, hasher: &impl std::hash::BuildHasher) -> DaftResult<Self> {
         match self.data_type() {
             DataType::Utf8 => Ok(self
                 .utf8()?
-                .minhash(num_hashes, ngram_size, seed)?
+                .minhash(num_hashes, ngram_size, seed, hasher)?
                 .into_series()),
             dt => Err(DaftError::TypeError(format!(
                 "minhash not implemented for {}",
