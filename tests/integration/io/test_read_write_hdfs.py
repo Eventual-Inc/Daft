d56fc082dd2bed9d4cad0b5001eb0f325a7197bd
chuanleini@tencent.com <chuanleini@tencent.com>
2024-10-17 16:08:32 +0800

    fix

--- tests/integration/io/test_read_write_hdfs.py
+++ tests/integration/io/test_read_write_hdfs.py
@@ -2,11 +2,8 @@ import pytest
 import daft
 
 @pytest.mark.integration()
-def write_df_to_hdfs():
-    df = daft.DataFrame({"a": [1, 2, 3]})
-    df.write_parquet("hdfs://127.0.0.1:9000/data.parquet")
-
-@pytest.mark.integration()
-def read_df_from_hdfs():
-    df = daft.read_parquet("hdfs://127.0.0.1:9000/data.parquet")
-    assert df.to_pydict() == {"a": [1, 2, 3]}
+def test_write_df_to_hdfs():
+    df1 = daft.DataFrame({"a": [1, 2, 3]})
+    df1.write_parquet("hdfs://127.0.0.1:9000/data.parquet")
+    df2 = daft.read_parquet("hdfs://127.0.0.1:9000/data.parquet")
+    assert df2.to_pydict() == {"a": [1, 2, 3]}
