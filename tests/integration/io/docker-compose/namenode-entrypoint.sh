f4af121f44a0e2dac70692d2ed3dc407c44117e5
chuanleini@tencent.com <chuanleini@tencent.com>
2024-10-17 12:31:17 +0800

    fix

--- tests/integration/io/docker-compose/namenode-entrypoint.sh
+++ tests/integration/io/docker-compose/namenode-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# 检查是否需要格式化 namenode
-if [ ! -d "/hadoop/dfs/name" ] || [ -z "$(ls -A /hadoop/dfs/name)" ]; then
-  echo "Formatting namenode directory"
-  $HADOOP_HOME/bin/hdfs namenode -format
-fi
+
+echo "Formatting namenode directory"
+/opt/hadoop/bin/hdfs namenode -format
 
 # 执行原始的 entrypoint 命令
 exec "$@"
