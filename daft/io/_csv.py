bbc331d5f51aa974c5f08dc0a7913312027a35c9
Desmond Cheong <desmondcheongzx@gmail.com>
2024-10-16 17:43:40 -0700

    Update daft/io/_csv.py
    Co-authored-by: Colin Ho <chiuhong@usc.edu>

--- daft/io/_csv.py
+++ daft/io/_csv.py
@@ -57,7 +57,7 @@ def read_csv(
         allow_variable_columns (bool): Whether to allow for variable number of columns in the CSV, defaults to False. If set to True, Daft will append nulls to rows with less columns than the schema, and ignore extra columns in rows with more columns
         io_config (IOConfig): Config to be used with the native downloader
         file_path_column: Include the source path(s) as a column with this name. Defaults to None.
-        hive_partitioning: Whether to use hive-style partitioning when reading glob files. Defaults to False.
+        hive_partitioning: Whether to infer hive_style partitions from file paths and include them as columns in the Dataframe. Defaults to False.
         use_native_downloader: Whether to use the native downloader instead of PyArrow for reading Parquet. This
             is currently experimental.
 
