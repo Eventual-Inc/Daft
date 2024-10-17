fbc753340a56ff42e73d1417fd2c38068ee7cded
chuanleini@tencent.com <chuanleini@tencent.com>
2024-10-17 15:57:03 +0800

    add hdfs integration test

---
+++ tests/integration/io/test_read_write_hdfs.py
@@ -0,0 +1,10 @@
+
+@pytest.mark.integration()
+def write_df_to_hdfs():
+    df = daft.DataFrame({"a": [1, 2, 3]})
+    df.write_parquet("hdfs://127.0.0.1:9000/data.parquet")
+
+@pytest.mark.integration()
+def read_df_from_hdfs():
+    df = daft.read_parquet("hdfs://127.0.0.1:9000/data.parquet")
+    assert df.to_pydict() == {"a": [1, 2, 3]}
