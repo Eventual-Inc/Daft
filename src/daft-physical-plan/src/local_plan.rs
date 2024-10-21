16665f29c575363d43834b0603414e289521bc4f
Colin Ho <colin.ho99@gmail.com>
2024-10-21 12:49:04 -0700

    [FEAT] Enable sample for swordfish (#3079)
    Adds sample as an intermediate operator. Unskips all the sample tests
    (except one which depends on concat).
    
    Co-authored-by: Colin Ho <colinho@Colins-MacBook-Pro.local>

--- src/daft-physical-plan/src/local_plan.rs
+++ src/daft-physical-plan/src/local_plan.rs
@@ -19,7 +19,7 @@ pub enum LocalPhysicalPlan {
     // Unpivot(Unpivot),
     Sort(Sort),
     // Split(Split),
-    // Sample(Sample),
+    Sample(Sample),
     // MonotonicallyIncreasingId(MonotonicallyIncreasingId),
     // Coalesce(Coalesce),
     // Flatten(Flatten),
@@ -167,6 +167,24 @@ impl LocalPhysicalPlan {
         .arced()
     }
 
+    pub(crate) fn sample(
+        input: LocalPhysicalPlanRef,
+        fraction: f64,
+        with_replacement: bool,
+        seed: Option<u64>,
+    ) -> LocalPhysicalPlanRef {
+        let schema = input.schema().clone();
+        Self::Sample(Sample {
+            input,
+            fraction,
+            with_replacement,
+            seed,
+            schema,
+            plan_stats: PlanStats {},
+        })
+        .arced()
+    }
+
     pub(crate) fn hash_join(
         left: LocalPhysicalPlanRef,
         right: LocalPhysicalPlanRef,
@@ -211,6 +229,7 @@ impl LocalPhysicalPlan {
             | Self::UnGroupedAggregate(UnGroupedAggregate { schema, .. })
             | Self::HashAggregate(HashAggregate { schema, .. })
             | Self::Sort(Sort { schema, .. })
+            | Self::Sample(Sample { schema, .. })
             | Self::HashJoin(HashJoin { schema, .. })
             | Self::Concat(Concat { schema, .. }) => schema,
             Self::InMemoryScan(InMemoryScan { info, .. }) => &info.source_schema,
@@ -267,6 +286,16 @@ pub struct Sort {
     pub input: LocalPhysicalPlanRef,
     pub sort_by: Vec<ExprRef>,
     pub descending: Vec<bool>,
+    pub schema: SchemaRef,
+    pub plan_stats: PlanStats,
+}
+
+#[derive(Debug)]
+pub struct Sample {
+    pub input: LocalPhysicalPlanRef,
+    pub fraction: f64,
+    pub with_replacement: bool,
+    pub seed: Option<u64>,
     pub schema: SchemaRef,
     pub plan_stats: PlanStats,
 }
