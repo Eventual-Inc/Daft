4577a51f0d4cb928c2117f45c385d0dd29aa4162
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-10-22 16:27:47 -0700

    update tests

--- tests/series/test_minhash.py
+++ tests/series/test_minhash.py
@@ -1,13 +1,21 @@
+from typing import Literal
+
 import pytest
 
 from daft import DataType, Series
 
 
-def minhash_none(series, num_hashes, ngram_size, seed):
+def minhash_none(
+    series: Series,
+    num_hashes: int,
+    ngram_size: int,
+    seed: int | None,
+    hash_function: Literal["murmur3", "xxhash", "sha1"],
+) -> list[list[int] | None]:
     if seed is None:
-        return series.minhash(num_hashes, ngram_size).to_pylist()
+        return series.minhash(num_hashes, ngram_size, hash_function=hash_function).to_pylist()
     else:
-        return series.minhash(num_hashes, ngram_size, seed).to_pylist()
+        return series.minhash(num_hashes, ngram_size, seed, hash_function=hash_function).to_pylist()
 
 
 test_series = Series.from_pylist(
@@ -31,8 +39,9 @@ test_series = Series.from_pylist(
 @pytest.mark.parametrize("num_hashes", [1, 2, 16, 128])
 @pytest.mark.parametrize("ngram_size", [1, 2, 4, 5, 100])
 @pytest.mark.parametrize("seed", [1, -1, 123, None])
-def test_minhash(num_hashes, ngram_size, seed):
-    minhash = minhash_none(test_series, num_hashes, ngram_size, seed)
+@pytest.mark.parametrize("hash_function", ["murmur3", "xxhash", "sha1"])
+def test_minhash(num_hashes, ngram_size, seed, hash_function):
+    minhash = minhash_none(test_series, num_hashes, ngram_size, seed, hash_function)
     assert minhash[4] is None and minhash[-1] is None
     for lst in minhash:
         if lst is not None:
@@ -46,42 +55,47 @@ def test_minhash(num_hashes, ngram_size, seed):
 @pytest.mark.parametrize("num_hashes", [0, -1, -100])
 @pytest.mark.parametrize("ngram_size", [1, 2, 4, 5, 100])
 @pytest.mark.parametrize("seed", [1, -1, 123, None])
-def test_minhash_fails_nonpositive_num_hashes(num_hashes, ngram_size, seed):
+@pytest.mark.parametrize("hash_function", ["murmur3", "xxhash", "sha1"])
+def test_minhash_fails_nonpositive_num_hashes(num_hashes, ngram_size, seed, hash_function):
     with pytest.raises(ValueError, match="num_hashes must be positive"):
-        minhash_none(test_series, num_hashes, ngram_size, seed)
+        minhash_none(test_series, num_hashes, ngram_size, seed, hash_function)
 
 
 @pytest.mark.parametrize("num_hashes", [1, 2, 16, 128])
 @pytest.mark.parametrize("ngram_size", [0, -1, -100])
 @pytest.mark.parametrize("seed", [1, -1, 123, None])
-def test_minhash_fails_nonpositive_ngram_size(num_hashes, ngram_size, seed):
+@pytest.mark.parametrize("hash_function", ["murmur3", "xxhash", "sha1"])
+def test_minhash_fails_nonpositive_ngram_size(num_hashes, ngram_size, seed, hash_function):
     with pytest.raises(ValueError, match="ngram_size must be positive"):
-        minhash_none(test_series, num_hashes, ngram_size, seed)
+        minhash_none(test_series, num_hashes, ngram_size, seed, hash_function)
 
 
 @pytest.mark.parametrize("num_hashes", [1, 2, 16, 128])
 @pytest.mark.parametrize("ngram_size", [1, 2, 4, 5, 100])
 @pytest.mark.parametrize("seed", [1, -1, 123, None])
-def test_minhash_empty_series(num_hashes, ngram_size, seed):
+@pytest.mark.parametrize("hash_function", ["murmur3", "xxhash", "sha1"])
+def test_minhash_empty_series(num_hashes, ngram_size, seed, hash_function):
     series = Series.from_pylist([]).cast(DataType.string())
 
-    minhash = minhash_none(series, num_hashes, ngram_size, seed)
+    minhash = minhash_none(series, num_hashes, ngram_size, seed, hash_function)
     assert len(minhash) == 0
 
 
 @pytest.mark.parametrize("num_hashes", [1, 2, 16, 128])
 @pytest.mark.parametrize("ngram_size", [1, 2, 4, 5, 100])
 @pytest.mark.parametrize("seed", [1, -1, 123, None])
-def test_minhash_seed_consistency(num_hashes, ngram_size, seed):
-    minhash1 = minhash_none(test_series, num_hashes, ngram_size, seed)
-    minhash2 = minhash_none(test_series, num_hashes, ngram_size, seed)
+@pytest.mark.parametrize("hash_function", ["murmur3", "xxhash", "sha1"])
+def test_minhash_seed_consistency(num_hashes, ngram_size, seed, hash_function):
+    minhash1 = minhash_none(test_series, num_hashes, ngram_size, seed, hash_function)
+    minhash2 = minhash_none(test_series, num_hashes, ngram_size, seed, hash_function)
     assert minhash1 == minhash2
 
 
 @pytest.mark.parametrize("num_hashes", [1, 2, 16, 128])
 @pytest.mark.parametrize("ngram_size", [1, 2, 4, 5, 100])
 @pytest.mark.parametrize("seed_pair", [[1, 2], [1, 5], [None, 2], [123, 234]])
-def test_minhash_seed_differences(num_hashes, ngram_size, seed_pair):
-    minhash1 = minhash_none(test_series, num_hashes, ngram_size, seed_pair[0])
-    minhash2 = minhash_none(test_series, num_hashes, ngram_size, seed_pair[1])
+@pytest.mark.parametrize("hash_function", ["murmur3", "xxhash", "sha1"])
+def test_minhash_seed_differences(num_hashes, ngram_size, seed_pair, hash_function):
+    minhash1 = minhash_none(test_series, num_hashes, ngram_size, seed_pair[0], hash_function)
+    minhash2 = minhash_none(test_series, num_hashes, ngram_size, seed_pair[1], hash_function)
     assert minhash1 != minhash2
