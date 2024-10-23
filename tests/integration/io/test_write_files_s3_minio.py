5cb5568d681e080f3f9321bec3692576acc61a11
Colin Ho <colinho@Colins-MacBook-Pro.local>
2024-10-23 12:05:43 -0700

    init

--- tests/integration/io/test_write_files_s3_minio.py
+++ tests/integration/io/test_write_files_s3_minio.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pathlib
 import uuid
 
 import pytest
@@ -41,3 +42,91 @@ def test_writing_parquet(minio_io_config, bucket, protocol):
     )
     results.collect()
     assert len(results) == 3
+
+
+def write(
+    df: daft.DataFrame,
+    path: str | pathlib.Path,
+    format: str,
+    write_mode: str,
+    partition_cols: list[str] | None = None,
+    io_config: daft.io.IOConfig | None = None,
+):
+    if format == "parquet":
+        return df.write_parquet(
+            path,
+            write_mode=write_mode,
+            partition_cols=partition_cols,
+            io_config=io_config,
+        )
+    elif format == "csv":
+        return df.write_csv(
+            path,
+            write_mode=write_mode,
+            partition_cols=partition_cols,
+            io_config=io_config,
+        )
+    else:
+        raise ValueError(f"Unsupported format: {format}")
+
+
+def read(path: str, format: str, io_config: daft.io.IOConfig | None = None):
+    if format == "parquet":
+        return daft.read_parquet(path, io_config=io_config)
+    elif format == "csv":
+        return daft.read_csv(path, io_config=io_config)
+    else:
+        raise ValueError(f"Unsupported format: {format}")
+
+
+@pytest.mark.integration()
+@pytest.mark.parametrize("write_mode", ["append", "overwrite"])
+@pytest.mark.parametrize("format", ["csv", "parquet"])
+@pytest.mark.parametrize("num_partitions", [1, 2])
+@pytest.mark.parametrize("partition_cols", [None, ["a"]])
+@pytest.mark.parametrize("protocol", ["s3://", "s3a://"])
+def test_write_modes_s3_minio(
+    minio_io_config,
+    bucket,
+    protocol,
+    write_mode,
+    format,
+    num_partitions,
+    partition_cols,
+):
+    path = f"{protocol}{bucket}/write_modes_s3_minio-{uuid.uuid4()}"
+    existing_data = {"a": [i for i in range(10)]}
+    # Write some existing_data
+    write(
+        daft.from_pydict(existing_data).into_partitions(num_partitions),
+        path,
+        format,
+        "append",
+        partition_cols,
+        minio_io_config,
+    )
+
+    # Write some new data
+    new_data = {
+        "a": [i for i in range(10, 20)],
+    }
+    write(
+        daft.from_pydict(new_data).into_partitions(num_partitions),
+        path,
+        format,
+        write_mode,
+        partition_cols,
+        minio_io_config,
+    )
+
+    # Read back the data
+    read_path = path + "/**" if partition_cols is not None else path
+    read_back = read(read_path, format, minio_io_config).sort("a").to_pydict()
+
+    # Check the data
+    if write_mode == "append":
+        assert read_back["a"] == existing_data["a"] + new_data["a"]
+    elif write_mode == "overwrite":
+        assert read_back["a"] == new_data["a"]
+    else:
+        raise ValueError(f"Unsupported write_mode: {write_mode}")
---
+++ tests/io/test_write_modes_local.py
@@ -0,0 +1,69 @@
+import pathlib
+
+import pytest
+
+import daft
+
+
+def write(
+    df: daft.DataFrame,
+    path: str | pathlib.Path,
+    format: str,
+    write_mode: str,
+    partition_cols: list[str] | None = None,
+):
+    if format == "parquet":
+        return df.write_parquet(path, write_mode=write_mode, partition_cols=partition_cols)
+    elif format == "csv":
+        return df.write_csv(path, write_mode=write_mode, partition_cols=partition_cols)
+    else:
+        raise ValueError(f"Unsupported format: {format}")
+
+
+def read(path: str, format: str):
+    if format == "parquet":
+        return daft.read_parquet(path)
+    elif format == "csv":
+        return daft.read_csv(path)
+    else:
+        raise ValueError(f"Unsupported format: {format}")
+
+
+@pytest.mark.parametrize("write_mode", ["append", "overwrite"])
+@pytest.mark.parametrize("format", ["csv", "parquet"])
+@pytest.mark.parametrize("num_partitions", [1, 50, 100])
+@pytest.mark.parametrize("partition_cols", [None, ["a"]])
+def test_write_modes_local(tmp_path, write_mode, format, num_partitions, partition_cols):
+    existing_data = {"a": [i for i in range(100)]}
+    # Write some existing_data
+    write(
+        daft.from_pydict(existing_data).into_partitions(num_partitions),
+        tmp_path,
+        format,
+        "append",
+        partition_cols,
+    )
+
+    # Write some new data
+    new_data = {
+        "a": [i for i in range(100, 200)],
+    }
+    write(
+        daft.from_pydict(new_data).into_partitions(num_partitions),
+        tmp_path,
+        format,
+        write_mode,
+        partition_cols,
+    )
+
+    # Read back the data
+    read_path = str(tmp_path) + "/**" if partition_cols is not None else str(tmp_path)
+    read_back = read(read_path, format).sort("a").to_pydict()
+
+    # Check the data
+    if write_mode == "append":
+        assert read_back["a"] == existing_data["a"] + new_data["a"]
+    elif write_mode == "overwrite":
+        assert read_back["a"] == new_data["a"]
+    else:
+        raise ValueError(f"Unsupported write_mode: {write_mode}")
