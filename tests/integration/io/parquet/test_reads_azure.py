218d55fee7ccf7a83e8855e5c36d1b64d22eef12
Kevin Wang <kevin@eventualcomputing.com>
2024-11-01 18:13:20 -0700

    fix test url

--- tests/integration/io/parquet/test_reads_azure.py
+++ tests/integration/io/parquet/test_reads_azure.py
@@ -10,7 +10,7 @@ def test_read_azure_infer_storage_account():
     io_config1 = daft.io.IOConfig(azure=daft.io.AzureConfig(storage_account="dafttestdata", anonymous=True))
     io_config2 = daft.io.IOConfig(azure=daft.io.AzureConfig(anonymous=True))
 
-    url = "abfss://public-anonymous/mvp.parquet"
+    url = "abfss://public-anonymous@dafttestdata.dfs.core.windows.net/mvp.parquet"
 
     df1 = daft.read_parquet(url, io_config=io_config1)
     df2 = daft.read_parquet(url, io_config=io_config2)
