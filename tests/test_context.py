771e647f32e0098c671aa852e308fe6245fb1b63
Colin Ho <colinho@Colins-MacBook-Pro.local>
2024-11-13 14:56:32 +0800

    add runner switching tests

--- tests/test_context.py
+++ tests/test_context.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 import sys
 
+import pytest
+
 
 @contextlib.contextmanager
 def with_null_env():
@@ -76,3 +78,60 @@ def test_implicit_set_runner_ray():
     with with_null_env():
         result = subprocess.run([sys.executable, "-c", implicit_set_runner_script_ray], capture_output=True)
         assert result.stdout.decode().strip() == "None\nray"
+
+
+switch_local_runners_script = """
+import daft
+print(daft.context.get_context()._runner)
+daft.context.set_runner_py()
+print(daft.context.get_context()._runner.name)
+daft.context.set_runner_native()
+print(daft.context.get_context()._runner.name)
+"""
+
+
+def test_switch_local_runners():
+    """Test that a runner can be switched from Python to Native"""
+    with with_null_env():
+        result = subprocess.run([sys.executable, "-c", switch_local_runners_script], capture_output=True)
+        assert result.stdout.decode().strip() == "None\npy\nnative"
+
+
+@pytest.mark.parametrize(
+    "set_local_command",
+    [
+        pytest.param("daft.context.set_runner_native()", id="native_to_ray"),
+        pytest.param("daft.context.set_runner_py()", id="py_to_ray"),
+    ],
+)
+def test_cannot_switch_local_to_ray(set_local_command):
+    """Test that a runner cannot be switched from local to Ray"""
+    script = f"""
+import daft
+{set_local_command}
+daft.context.set_runner_ray()
+"""
+    with with_null_env():
+        result = subprocess.run([sys.executable, "-c", script], capture_output=True)
+        assert result.stderr.decode().strip().endswith("RuntimeError: Cannot set runner more than once")
+
+
+@pytest.mark.parametrize(
+    "set_new_runner_command",
+    [
+        pytest.param("daft.context.set_runner_native()", id="ray_to_native"),
+        pytest.param("daft.context.set_runner_py()", id="ray_to_py"),
+        pytest.param("daft.context.set_runner_ray()", id="ray_to_ray"),
+    ],
+)
+def test_cannot_switch_from_ray(set_new_runner_command):
+    """Test that a runner cannot be switched from Ray"""
+
+    script = f"""
+import daft
+daft.context.set_runner_ray()
+{set_new_runner_command}
+"""
+    with with_null_env():
+        result = subprocess.run([sys.executable, "-c", script], capture_output=True)
+        assert result.stderr.decode().strip().endswith("RuntimeError: Cannot set runner more than once")
