fe9ff5c53e9f8912f7d722ddfd64b6b597940133
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-11-18 01:28:22 -0800

    fix python feat stuffs

--- src/daft-connect/src/translation/logical_plan.rs
+++ src/daft-connect/src/translation/logical_plan.rs
@@ -1,7 +1,5 @@
 use daft_logical_plan::LogicalPlanBuilder;
-use daft_scan::python::pylib::ScanOperatorHandle;
 use eyre::{bail, ensure, Context};
-use pyo3::prelude::*;
 use spark_connect::{relation::RelType, Range, Relation};
 use tracing::warn;
 
@@ -21,42 +19,50 @@ pub fn to_logical_plan(relation: Relation) -> eyre::Result<LogicalPlanBuilder> {
 }
 
 fn range(range: Range) -> eyre::Result<LogicalPlanBuilder> {
-    let Range {
-        start,
-        end,
-        step,
-        num_partitions,
-    } = range;
+    #[cfg(not(feature = "python"))]
+    bail!("Range operations require Python feature to be enabled");
 
-    if let Some(partitions) = num_partitions {
-        warn!("{partitions} ignored");
+    #[cfg(feature = "python")]
+    {
+        use daft_scan::python::pylib::ScanOperatorHandle;
+        use pyo3::prelude::*;
+        let Range {
+            start,
+            end,
+            step,
+            num_partitions,
+        } = range;
+
+        if let Some(partitions) = num_partitions {
+            warn!("{partitions} ignored");
+        }
+
+        let start = start.unwrap_or(0);
+
+        let step = usize::try_from(step).wrap_err("step must be a positive integer")?;
+        ensure!(step > 0, "step must be greater than 0");
+
+        let plan = Python::with_gil(|py| {
+            let range_module = PyModule::import_bound(py, "daft.io.range")
+                .wrap_err("Failed to import daft.io.range")?;
+
+            let range = range_module
+                .getattr(pyo3::intern!(py, "RangeScanOperator"))
+                .wrap_err("Failed to get range function")?;
+
+            let range = range
+                .call1((start, end, step))
+                .wrap_err("Failed to create range scan operator")?
+                .to_object(py);
+
+            let scan_operator_handle = ScanOperatorHandle::from_python_scan_operator(range, py)?;
+
+            let plan = LogicalPlanBuilder::table_scan(scan_operator_handle.into(), None)?;
+
+            eyre::Result::<_>::Ok(plan)
+        })
+        .wrap_err("Failed to create range scan")?;
+
+        Ok(plan)
     }
-
-    let start = start.unwrap_or(0);
-
-    let step = usize::try_from(step).wrap_err("step must be a positive integer")?;
-    ensure!(step > 0, "step must be greater than 0");
-
-    let plan = Python::with_gil(|py| {
-        let range_module = PyModule::import_bound(py, "daft.io.range")
-            .wrap_err("Failed to import daft.io.range")?;
-
-        let range = range_module
-            .getattr(pyo3::intern!(py, "RangeScanOperator"))
-            .wrap_err("Failed to get range function")?;
-
-        let range = range
-            .call1((start, end, step))
-            .wrap_err("Failed to create range scan operator")?
-            .to_object(py);
-
-        let scan_operator_handle = ScanOperatorHandle::from_python_scan_operator(range, py)?;
-
-        let plan = LogicalPlanBuilder::table_scan(scan_operator_handle.into(), None)?;
-
-        eyre::Result::<_>::Ok(plan)
-    })
-    .wrap_err("Failed to create range scan")?;
-
-    Ok(plan)
 }
