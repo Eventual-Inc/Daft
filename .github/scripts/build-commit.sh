a5e3b8fefe80ae808637c367b67f3bd17293cf9f
Raunak Bhagat <rabhagat31@gmail.com>
2024-11-19 13:51:10 -0800

    Add ssh breakpoint; clean up step logic

--- .github/scripts/build-commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-gh workflow run build-commit.yaml \
-    --ref $BRANCH_NAME \
-    -f commit=$BRANCH_NAME \
-    -f machine_type="buildjet-8vcpu-ubuntu-2004"
--- .github/scripts/performance-comparisons.sh
+++
@@ -1,2 +0,0 @@
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-gh workflow run performance-comparisons.yaml --ref $BRANCH_NAME -f c1=$BRANCH_NAME
