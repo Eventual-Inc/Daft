d1c97e26ad39941da9cbac4f28d8860ebcb04794
universalmind303 <cory.grinstead@gmail.com>
2024-11-20 14:03:22 -0600

    add more test cases

--- tests/sql/test_aggs.py
+++ tests/sql/test_aggs.py
@@ -56,6 +56,8 @@ def test_aggs_sql():
         ("count(*) as cnt", "cnt > 2", {"cnt": [3, 5]}),
         ("count(*) as cnt", "count(*) > 2", {"cnt": [3, 5]}),
         ("count(*)", "count(*) > 2", {"count": [3, 5]}),
+        ("count(*) as cnt", "sum(values) > 10", {"count": [3, 5]}),
+        ("sum(values), count(*)", "id > 1", {"values": [10.0, 29.5], "count": [2, 5]}),
     ],
 )
 def test_having(agg, cond, expected):
@@ -80,3 +82,26 @@ def test_having(agg, cond, expected):
     ).to_pydict()
 
     assert actual == expected
+
+
+def test_having_non_grouped():
+    df = daft.from_pydict(
+        {
+            "id": [1, 2, 3, 3, 3, 3, 2, 1, 3, 1],
+            "values": [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5],
+            "floats": [0.01, 0.011, 0.01047, 0.02, 0.019, 0.018, 0.017, 0.016, 0.015, 0.014],
+        }
+    )
+    catalog = SQLCatalog({"df": df})
+
+    actual = daft.sql(
+        """
+    SELECT
+    count(*) ,
+    from df
+    having sum(values) > 40
+    """,
+        catalog,
+    ).to_pydict()
+
+    assert actual == {"count": [10]}
