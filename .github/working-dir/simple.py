67645b02fc60d1d8d3245d1f3b6b1383b0e772af
Raunak Bhagat <rabhagat31@gmail.com>
2024-11-23 11:21:56 -0800

    Add support for producing ray logs
    - still not able to produce daft logs (located inside of `/tmp/ray/session_*/logs/daft`)
    - only ray logs are being produced
        - this should be fine for now?

--- .github/working-dir/simple.py
+++ .github/working-dir/simple.py
@@ -1,5 +1,7 @@
 import daft
 
+daft.context.set_runner_ray("ray://localhost:10001")
+
 df = daft.from_pydict({"nums": [1, 2, 3]})
 df = df.with_column("result", daft.col("nums").cbrt()).collect()
 df.show()
