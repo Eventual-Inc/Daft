12fbe80076408ea6f5361ec49d5317065cd72115
Conrad Soon <18553610+conradsoon@users.noreply.github.com>
2024-11-24 22:34:42 +0800

    fix(clip): rework casting logic for op

--- src/daft-core/src/series/ops/clip.rs
+++ src/daft-core/src/series/ops/clip.rs
@@ -1,24 +1,10 @@
 use common_error::{DaftError, DaftResult};
-use daft_schema::prelude::*;
 
 use crate::{
     datatypes::InferDataType,
     series::{IntoSeries, Series},
+    with_match_numeric_daft_types,
 };
-
-// clip helper
-
-macro_rules! cast_and_clip {
-    ($self:expr, $min:expr, $max:expr, $output_type:expr, $method:ident) => {{
-        let self_casted = $self.cast(&$output_type)?;
-        let min_casted = $min.cast(&$output_type)?;
-        let max_casted = $max.cast(&$output_type)?;
-        Ok(self_casted
-            .$method()?
-            .clip(min_casted.$method()?, max_casted.$method()?)?
-            .into_series())
-    }};
-}
 
 impl Series {
     /// Alias for .clip()
@@ -34,7 +20,8 @@ impl Series {
             &InferDataType::from(max.data_type()),
         )?;
 
-        // It's possible that we pass in something like .clamp(None, 2) on the Python binding side, in which case we need to replace the None (which gets casted into a Series with Null type) with a dummy series of the output type.
+        // It's possible that we pass in something like .clamp(None, 2) on the Python binding side,
+        // in which case we need to cast the None to the output type.
         let create_null_series = |name: &str| Self::full_null(name, &output_type, 1);
         let min = min
             .data_type()
@@ -48,17 +35,18 @@ impl Series {
             .unwrap_or_else(|| max.clone());
 
         match &output_type {
-            // DataType::Int8 => cast_and_clip!(self, min, max, output_type, i8),
-            DataType::Int8 => cast_and_clip!(self, min, max, output_type, i8),
-            DataType::Int16 => cast_and_clip!(self, min, max, output_type, i16),
-            DataType::Int32 => cast_and_clip!(self, min, max, output_type, i32),
-            DataType::Int64 => cast_and_clip!(self, min, max, output_type, i64),
-            DataType::UInt8 => cast_and_clip!(self, min, max, output_type, u8),
-            DataType::UInt16 => cast_and_clip!(self, min, max, output_type, u16),
-            DataType::UInt32 => cast_and_clip!(self, min, max, output_type, u32),
-            DataType::UInt64 => cast_and_clip!(self, min, max, output_type, u64),
-            DataType::Float32 => cast_and_clip!(self, min, max, output_type, f32),
-            DataType::Float64 => cast_and_clip!(self, min, max, output_type, f64),
+            output_type if output_type.is_numeric() => {
+                with_match_numeric_daft_types!(output_type, |$T| {
+                    let self_casted = self.cast(output_type)?;
+                    let min_casted = min.cast(output_type)?;
+                    let max_casted = max.cast(output_type)?;
+
+                    let self_downcasted = self_casted.downcast::<<$T as DaftDataType>::ArrayType>()?;
+                    let min_downcasted = min_casted.downcast::<<$T as DaftDataType>::ArrayType>()?;
+                    let max_downcasted = max_casted.downcast::<<$T as DaftDataType>::ArrayType>()?;
+                    Ok(self_downcasted.clip(min_downcasted, max_downcasted)?.into_series())
+                })
+            }
             dt => Err(DaftError::TypeError(format!(
                 "clip not implemented for {}",
                 dt
