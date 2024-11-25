190c1b41a00cb7d1e0ab6ac9eee80adaa8b246ef
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-11-25 11:56:38 -0800

    I think its because of import vs main

--- .github/working-dir/debug-tracing.py
+++ .github/working-dir/debug-tracing.py
@@ -1,8 +1,19 @@
+import os
+
 import daft
 
 df = daft.from_pydict({"foo": [1, 2, 3]})
 df.show()
 
-print("Job is running")
-print("Context:", daft.context.get_context())
-print("Ray tracing enabled:", daft.context.get_context().daft_execution_config.enable_ray_tracing)
+print("Job is running import")
+print("import Job ID:", os.getenv("RAY_JOB_ID"))
+print("import Tracing envvar:", os.getenv("DAFT_ENABLE_RAY_TRACING"))
+print("import Context:", daft.context.get_context())
+print("import Ray tracing enabled:", daft.context.get_context().daft_execution_config.enable_ray_tracing)
+
+if __name__ == "__main__":
+    print("Job is running __main__")
+    print("__main__ Job ID:", os.getenv("RAY_JOB_ID"))
+    print("__main__ Tracing envvar:", os.getenv("DAFT_ENABLE_RAY_TRACING"))
+    print("__main__ Context:", daft.context.get_context())
+    print("__main__ Ray tracing enabled:", daft.context.get_context().daft_execution_config.enable_ray_tracing)
