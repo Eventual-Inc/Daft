0f265698c0974fd008c7bdabf3ac4201d863f4c7
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-11-25 12:00:31 -0800

    envvars not even making it through?

--- .github/working-dir/debug-tracing.py
+++ .github/working-dir/debug-tracing.py
@@ -7,6 +7,7 @@ df.show()
 
 print("Job is running import")
 print("import Job ID:", os.getenv("RAY_JOB_ID"))
+print("import Runner envvar:", os.getenv("DAFT_RUNNER"))
 print("import Tracing envvar:", os.getenv("DAFT_ENABLE_RAY_TRACING"))
 print("import Context:", daft.context.get_context())
 print("import Ray tracing enabled:", daft.context.get_context().daft_execution_config.enable_ray_tracing)
@@ -14,6 +15,7 @@ print("import Ray tracing enabled:", daft.context.get_context().daft_execution_c
 if __name__ == "__main__":
     print("Job is running __main__")
     print("__main__ Job ID:", os.getenv("RAY_JOB_ID"))
+    print("__main__ Runner envvar:", os.getenv("DAFT_RUNNER"))
     print("__main__ Tracing envvar:", os.getenv("DAFT_ENABLE_RAY_TRACING"))
     print("__main__ Context:", daft.context.get_context())
     print("__main__ Ray tracing enabled:", daft.context.get_context().daft_execution_config.enable_ray_tracing)
