490736d2049e0a3f6ad56d11e3e91a246f180261
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-11-25 20:17:14 -0800

    Add better errors and examples

--- tests/test_size_estimations.py
+++ tests/test_size_estimations.py
@@ -15,33 +15,34 @@ def get_scantask_estimated_size(pq_path: pathlib.Path, columns: list[str] | None
 
 def get_actual_size(pq_path: pathlib.Path, columns: list[str] | None = None) -> int:
     # Force materializationm of the MicroPartition using `.slice`
-    return MicroPartition.read_parquet(str(pq_path), columns=columns).slice(0, 100).size_bytes()
+    mp = MicroPartition.read_parquet(str(pq_path), columns=columns)
+    return mp.slice(0, len(mp)).size_bytes()
 
 
-def assert_close(expected: int, actual: int, pct: float = 0.05):
+def assert_close(filepath: pathlib.Path, estimated: int, actual: int, pct: float = 0.05):
     assert (
-        abs(actual - expected) / expected < pct
-    ), f"Expected estimations {expected} to be within {pct} of actual: {actual}"
+        abs(actual - estimated) / estimated < pct
+    ), f"Expected {filepath.stat().size / 1_000_000:.2f}MB file at {filepath} estimated vs actual to be within {pct * 100}%: {estimated / 1_000_000:.2f}MB vs {actual / 1000_000:.2f}MB ({((estimated - actual) / estimated) * 100:.2f}%)"
 
 
 @pytest.mark.parametrize("compression", ["snappy", None])
 @pytest.mark.parametrize("use_dictionary", [True, False])
 @pytest.mark.parametrize("unique", [True, False])
 def test_estimations_strings(tmpdir, use_dictionary, compression, unique):
-    pq_path = tmpdir / "strings.pq"
-    data = [f"{'a' * 100}{i}" for i in range(1000)] if unique else ["a" * 100 for _ in range(1000)]
+    pq_path = tmpdir / f"strings.use_dictionary={use_dictionary}.unique={unique}.compression={compression}.pq"
+    data = [f"{'a' * 100}{i}" for i in range(1000_000)] if unique else ["a" * 100 for _ in range(1000_000)]
     tbl = pa.table({"foo": data})
     papq.write_table(tbl, pq_path, use_dictionary=use_dictionary, compression=compression)
-    assert assert_close(get_scantask_estimated_size(pq_path), get_actual_size(pq_path))
+    assert assert_close(pq_path, get_scantask_estimated_size(pq_path), get_actual_size(pq_path))
 
 
 @pytest.mark.parametrize("compression", ["snappy", None])
 @pytest.mark.parametrize("use_dictionary", [True, False])
 @pytest.mark.parametrize("unique", [True, False])
 def test_estimations_ints(tmpdir, use_dictionary, compression, unique):
-    pq_path = tmpdir / "ints.pq"
+    pq_path = tmpdir / f"ints.use_dictionary={use_dictionary}.unique={unique}.compression={compression}.pq"
 
-    data = [i for i in range(1000)] if unique else [1 for _ in range(1000)]
+    data = [i for i in range(1000_000)] if unique else [1 for _ in range(1000_000)]
     tbl = pa.table({"foo": data})
     papq.write_table(tbl, pq_path, use_dictionary=use_dictionary, compression=compression)
-    assert assert_close(get_scantask_estimated_size(pq_path), get_actual_size(pq_path))
+    assert assert_close(pq_path, get_scantask_estimated_size(pq_path), get_actual_size(pq_path))
