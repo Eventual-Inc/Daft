ddaaa342c823f2e6ff652c143920552256c2ded0
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-12-02 18:21:18 -0800

    Add large file tests

--- tests/benchmarks/test_interactive_reads.py
+++ tests/benchmarks/test_interactive_reads.py
@@ -1,3 +1,4 @@
+import dataclasses
 import tempfile
 
 import boto3
@@ -19,26 +20,42 @@ def io_config():
     return IOConfig(s3=S3Config(anonymous=True))
 
 
+@dataclasses.dataclass(frozen=True)
+class FileRequest:
+    num_files: int
+    small: bool
+
+
 @pytest.fixture(scope="session")
 def files(request):
-    num_files = request.param
+    file_request = request.param
     s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
-    with tempfile.TemporaryDirectory() as tmpdir:
-        local_file = f"{tmpdir}/small-fake-data.parquet.parquet"
 
-        s3.download_file("daft-public-data", "test_fixtures/parquet/small-fake-data.parquet", local_file)
-        if request.param == 1:
-            yield local_file
+    if file_request.small:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            local_file = f"{tmpdir}/small-fake-data.parquet.parquet"
+            s3.download_file("daft-public-data", "test_fixtures/parquet/small-fake-data.parquet", local_file)
+            if file_request.num_files == 1:
+                yield local_file
+            else:
+                yield [local_file] * file_request.num_files
+
+    else:
+        remote_large_file = "s3://daft-public-data/test_fixtures/parquet/large-fake-data.parquet"
+        if file_request.num_files == 1:
+            yield remote_large_file
         else:
-            yield [local_file] * num_files
+            yield [remote_large_file] * file_request.num_files
 
 
 @pytest.mark.benchmark(group="show")
 @pytest.mark.parametrize(
     "files",
     [
-        pytest.param(1, id="1 Small File"),
-        pytest.param(100, id="100 Small Files"),
+        pytest.param(FileRequest(num_files=1, small=True), id="1 Small File"),
+        pytest.param(FileRequest(num_files=100, small=True), id="100 Small Files"),
+        pytest.param(FileRequest(num_files=1, small=False), id="1 Large File"),
+        pytest.param(FileRequest(num_files=20, small=False), id="20 Large Files"),
     ],
     indirect=True,  # This tells pytest to pass the params to the fixture
 )
@@ -54,8 +71,10 @@ def test_show(files, io_config, benchmark):
 @pytest.mark.parametrize(
     "files",
     [
-        pytest.param(1, id="1 Small File"),
-        pytest.param(100, id="100 Small Files"),
+        pytest.param(FileRequest(num_files=1, small=True), id="1 Small File"),
+        pytest.param(FileRequest(num_files=100, small=True), id="100 Small Files"),
+        pytest.param(FileRequest(num_files=1, small=False), id="1 Large File"),
+        pytest.param(FileRequest(num_files=20, small=False), id="20 Large Files"),
     ],
     indirect=True,  # This tells pytest to pass the params to the fixture
 )
@@ -71,8 +90,10 @@ def test_explain(files, io_config, benchmark):
 @pytest.mark.parametrize(
     "files, expected_count",
     [
-        pytest.param(1, 1024, id="1 Small File"),
-        pytest.param(100, 102400, id="100 Small Files"),
+        pytest.param(FileRequest(num_files=1, small=True), 1024, id="1 Small File"),
+        pytest.param(FileRequest(num_files=100, small=True), 102400, id="100 Small Files"),
+        # pytest.param(FileRequest(num_files=1, small=False), id="1 Large File"),
+        # pytest.param(FileRequest(num_files=20, small=False), id="20 Large Files"),
     ],
     indirect=True,  # This tells pytest to pass the params to the fixture
 )
@@ -89,8 +110,10 @@ def test_count(files, expected_count, io_config, benchmark):
 @pytest.mark.parametrize(
     "files",
     [
-        pytest.param(1, id="1 Small File"),
-        pytest.param(100, id="100 Small Files"),
+        pytest.param(FileRequest(num_files=1, small=True), id="1 Small File"),
+        pytest.param(FileRequest(num_files=100, small=True), id="100 Small Files"),
+        pytest.param(FileRequest(num_files=1, small=False), id="1 Large File"),
+        pytest.param(FileRequest(num_files=20, small=False), id="20 Large Files"),
     ],
     indirect=True,  # This tells pytest to pass the params to the fixture
 )
