d9c1204a90f68e76dfa7d6162250bf3d23f86959
Raunak Bhagat <rabhagat31@gmail.com>
2024-12-04 14:07:24 -0800

    Submit daft repo instead of using wheel

--- .github/ci-scripts/templatize_ray_config.py
+++ .github/ci-scripts/templatize_ray_config.py
@@ -14,7 +14,7 @@ from pydantic import BaseModel, Field
 
 CLUSTER_NAME_PLACEHOLDER = "\\{{CLUSTER_NAME}}"
 DAFT_INSTALL_PLACEHOLDER = "\\{{DAFT_INSTALL}}"
-OTHER_INSTALL_PLACEHOLDER = "\{{OTHER_INSTALLS}}"
+OTHER_INSTALL_PLACEHOLDER = "\\{{OTHER_INSTALLS}}"
 PYTHON_VERSION_PLACEHOLDER = "\\{{PYTHON_VERSION}}"
 CLUSTER_PROFILE__NODE_COUNT = "\\{{CLUSTER_PROFILE/node_count}}"
 CLUSTER_PROFILE__INSTANCE_TYPE = "\\{{CLUSTER_PROFILE/instance_type}}"
@@ -68,28 +68,14 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument("--cluster-name", required=True)
-    parser.add_argument("--daft-wheel-url")
     parser.add_argument("--daft-version")
     parser.add_argument("--python-version", required=True)
     parser.add_argument("--cluster-profile", required=True, choices=["debug_xs-x86", "medium-x86"])
-    parser.add_argument("--working-dir", required=True)
-    parser.add_argument("--entrypoint-script", required=True)
+    parser.add_argument("--script", required=True)
     args = parser.parse_args()
 
     content = content.replace(CLUSTER_NAME_PLACEHOLDER, args.cluster_name)
-
-    if args.daft_wheel_url and args.daft_version:
-        raise ValueError(
-            "Cannot specify both the `daft-wheel-name` and the `daft-version`; please choose one or the other"
-        )
-    elif args.daft_wheel_url:
-        daft_install = args.daft_wheel_url
-    elif args.daft_version:
-        daft_install = f"getdaft=={args.daft_version}"
-    else:
-        daft_install = "getdaft"
-    content = content.replace(DAFT_INSTALL_PLACEHOLDER, daft_install)
-
+    content = content.replace(DAFT_INSTALL_PLACEHOLDER, f"getdaft=={args.daft_version}" if args.daft_version else "")
     content = content.replace(PYTHON_VERSION_PLACEHOLDER, args.python_version)
 
     profile = profiles[args.cluster_profile]
@@ -101,11 +87,8 @@ if __name__ == "__main__":
         CLUSTER_PROFILE__VOLUME_MOUNT, profile.volume_mount if profile.volume_mount else NOOP_STEP
     )
 
-    working_dir = Path(args.working_dir)
-    assert working_dir.exists() and working_dir.is_dir()
-    entrypoint_script_fullpath: Path = working_dir / args.entrypoint_script
-    assert entrypoint_script_fullpath.exists() and entrypoint_script_fullpath.is_file()
-    with open(entrypoint_script_fullpath) as f:
+    assert Path(args.script).exists()
+    with open(args.script) as f:
         metadata = Metadata(**read_inline_metadata.read(f.read()))
 
     content = content.replace(OTHER_INSTALL_PLACEHOLDER, " ".join(metadata.dependencies))
