35af66514f9b8269812ecca2ba79b670997d5850
Raunak Bhagat <rabhagat31@gmail.com>
2024-12-06 15:53:51 -0800

    Add question splitting

--- benchmarking/tpcds/__main__.py
+++ benchmarking/tpcds/__main__.py
@@ -8,6 +8,33 @@ from daft.sql import SQLCatalog
 
 from . import datagen
 
+logger = logging.getLogger(__name__)
+
+
+def parse_questions_str(questions: str) -> list[int]:
+    if questions == "*":
+        return list(range(1, 100))
+
+    nums = []
+    for split in filter(lambda str: str, questions.split(",")):
+        try:
+            num = int(split)
+            nums.append(num)
+        except ValueError:
+            ints = split.split("-")
+            assert (
+                len(ints) == 2
+            ), f"A range must include two numbers split by a dash (i.e., '-'); instead got '{split}'"
+            [lower, upper] = ints
+            try:
+                lower = int(lower)
+                upper = int(upper)
+                nums.extend(range(lower, upper + 1))
+            except ValueError:
+                raise ValueError(f"Invalid range: {split}")
+
+    return nums
+
 
 def generate_tpcds_catalog(tpcds_gen_folder: Path):
     if not tpcds_gen_folder.exists():
@@ -42,12 +69,18 @@ def run_query(query_index: int, catalog: SQLCatalog) -> str:
     return duration, success, error_message if error_message else None
 
 
-def run_benchmarks(basedir: Path):
+def run_benchmarks(basedir: Path, questions: str):
     successes = {}
     failures = {}
 
+    query_indices = parse_questions_str(questions)
+    logger.info(
+        "Running the following questions: %s",
+        query_indices,
+    )
+
     catalog = generate_tpcds_catalog(basedir)
-    for query_index in range(1, 100):
+    for query_index in query_indices:
         duration, was_successful, error_msg = run_query(query_index, catalog)
         if was_successful:
             successes[query_index] = duration
@@ -68,6 +101,7 @@ def main():
         help="Path to the folder containing the TPC-DS dsdgen tool and generated data",
     )
     parser.add_argument("--scale-factor", default=0.01, type=float, help="Scale factor to run on in GB")
+    parser.add_argument("--questions", default="*", type=str, help="The questions to run")
     args = parser.parse_args()
 
     tpcds_gen_folder = Path(args.tpcds_gen_folder)
@@ -76,7 +110,7 @@ def main():
 
     basedir = tpcds_gen_folder / str(args.scale_factor)
     datagen.gen_tpcds(basedir=basedir, scale_factor=args.scale_factor)
-    run_benchmarks(basedir)
+    run_benchmarks(basedir, args.questions)
 
 
 if __name__ == "__main__":
