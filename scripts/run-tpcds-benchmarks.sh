0daa97e60e10e4bb5ef26b904e678dca8c766b63
Raunak Bhagat <r@rabh.io>
2024-12-13 14:51:10 -0800

    Remove scripts

--- scripts/run-tpcds-benchmarks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-echo "Running tpcds benchmarks on the branch: $current_branch"
-
-gh workflow run run-script.yaml \
-    --ref $current_branch \
-    -f entrypoint="ci/entrypoints/tpcds.sh"
