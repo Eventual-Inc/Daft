5efa092b1edf6b899ecf423cffba51ccd8d1b6e1
Raunak Bhagat <r@rabh.io>
2024-12-13 14:26:33 -0800

    Use debug cluster profile in script

--- scripts/run-tpcds-benchmarks.sh
+++ scripts/run-tpcds-benchmarks.sh
@@ -5,4 +5,5 @@ echo "Running tpcds benchmarks on the branch: $current_branch"
 
 gh workflow run run-script.yaml \
     --ref $current_branch \
+    -f cluster_profile="debug_xs-x86" \
     -f entrypoint="ci/entrypoints/tpcds.sh"
