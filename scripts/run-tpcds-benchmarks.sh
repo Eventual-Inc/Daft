7b223ba1fc405999099b6573edfd13bf0f3b29d8
Raunak Bhagat <r@rabh.io>
2024-12-13 14:11:04 -0800

    Edit current branch the script runs on

--- scripts/run-tpcds-benchmarks.sh
+++ scripts/run-tpcds-benchmarks.sh
@@ -4,5 +4,5 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "Running tpcds benchmarks on the branch: $current_branch"
 
 gh workflow run run-script.yaml \
-    --ref refactor/run-cluster \
+    --ref $current_branch \
     -f entrypoint="ci/entrypoints/tpcds.sh"
