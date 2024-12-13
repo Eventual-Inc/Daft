fae0ee05a0566193da730372c57bd0eeed9991dc
Kevin Wang <kevin@eventualcomputing.com>
2024-12-13 15:18:33 -0800

    remove to_dnf

--- src/daft-algebra/src/boolean.rs
+++ src/daft-algebra/src/boolean.rs
@@ -1,15 +1,5 @@
-use common_error::DaftResult;
-use common_treenode::{Transformed, TreeNode, TreeNodeRecursion};
-use daft_core::prelude::*;
+use common_treenode::{TreeNode, TreeNodeRecursion};
 use daft_dsl::{Expr, ExprRef, Operator};
-
-/// Convert a boolean expression to disjunctive normal form (https://en.wikipedia.org/wiki/Disjunctive_normal_form)
-pub fn to_dnf(expr: ExprRef, schema: &SchemaRef) -> DaftResult<Transformed<ExprRef>> {
-    // to_dnf should only be called for boolean expressions
-    debug_assert_eq!(expr.get_type(schema)?, DataType::Boolean);
-
-    todo!()
-}
 
 pub fn split_conjunction(expr: &ExprRef) -> Vec<ExprRef> {
     let mut splits = vec![];
