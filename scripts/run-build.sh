57662faa0bc5feeaedb30bbe00e417a8161f47e5
Raunak Bhagat <r@rabh.io>
2024-12-13 14:02:09 -0800

    Edit which branch the tpcds script runs on

--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-echo "Running a build on the branch $current_branch"
+echo "Running a build on the branch: $current_branch"
 
 gh workflow run build-commit.yaml --ref $current_branch -f arch='x86'
