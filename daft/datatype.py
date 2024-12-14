a0d1ada834956428c23e75ea6411eeccec256cef
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-12-14 11:53:00 +0800

    bug: ensure safe concurrent access of _ensure_registered_super_ext_type

--- daft/datatype.py
+++ daft/datatype.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import threading
 from typing import TYPE_CHECKING, Union
 
 from daft.context import get_context
@@ -578,36 +579,39 @@ DataTypeLike = Union[DataType, type]
 
 _EXT_TYPE_REGISTERED = False
 _STATIC_DAFT_EXTENSION = None
+_EXT_TYPE_REGISTERED_LOCK = threading.Lock()
 
 
 def _ensure_registered_super_ext_type():
     global _EXT_TYPE_REGISTERED
     global _STATIC_DAFT_EXTENSION
-    if not _EXT_TYPE_REGISTERED:
 
-        class DaftExtension(pa.ExtensionType):
-            def __init__(self, dtype, metadata=b""):
-                # attributes need to be set first before calling
-                # super init (as that calls serialize)
-                self._metadata = metadata
-                super().__init__(dtype, "daft.super_extension")
+    with _EXT_TYPE_REGISTERED_LOCK:
+        if not _EXT_TYPE_REGISTERED:
 
-            def __reduce__(self):
-                return type(self).__arrow_ext_deserialize__, (self.storage_type, self.__arrow_ext_serialize__())
+            class DaftExtension(pa.ExtensionType):
+                def __init__(self, dtype, metadata=b""):
+                    # attributes need to be set first before calling
+                    # super init (as that calls serialize)
+                    self._metadata = metadata
+                    super().__init__(dtype, "daft.super_extension")
 
-            def __arrow_ext_serialize__(self):
-                return self._metadata
+                def __reduce__(self):
+                    return type(self).__arrow_ext_deserialize__, (self.storage_type, self.__arrow_ext_serialize__())
 
-            @classmethod
-            def __arrow_ext_deserialize__(cls, storage_type, serialized):
-                return cls(storage_type, serialized)
+                def __arrow_ext_serialize__(self):
+                    return self._metadata
 
-        _STATIC_DAFT_EXTENSION = DaftExtension
-        pa.register_extension_type(DaftExtension(pa.null()))
-        import atexit
+                @classmethod
+                def __arrow_ext_deserialize__(cls, storage_type, serialized):
+                    return cls(storage_type, serialized)
 
-        atexit.register(lambda: pa.unregister_extension_type("daft.super_extension"))
-        _EXT_TYPE_REGISTERED = True
+            _STATIC_DAFT_EXTENSION = DaftExtension
+            pa.register_extension_type(DaftExtension(pa.null()))
+            import atexit
+
+            atexit.register(lambda: pa.unregister_extension_type("daft.super_extension"))
+            _EXT_TYPE_REGISTERED = True
 
 
 def get_super_ext_type():
