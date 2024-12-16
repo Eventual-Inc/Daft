dc20be849b18e7a116df0f78f7456b97e7cacfeb
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-12-16 15:04:56 -0800

    add disclaimer

--- tests/connect/test_analyze_plan.py
+++ tests/connect/test_analyze_plan.py
@@ -5,4 +5,7 @@ def test_analyze_plan(spark_session):
     data = [[1000, 99]]
     df1 = spark_session.createDataFrame(data, schema="Value int, Total int")
     s = df1.schema
+
+    # todo: this is INCORRECT but it is an issue with pyspark client
+    # ideally should be StructType([StructField('Value', IntegerType(), True), StructField('Total', IntegerType(), True)])
     assert str(s) == "StructType([StructField('_1', LongType(), True), StructField('_2', LongType(), True)])"
