902aec9d0b85ec1ab625463c3bb2d8a7b1a7245b
Raunak Bhagat <r@rabh.io>
2024-12-16 14:23:11 -0800

    Add subdir structure to build path

--- .github/ci-scripts/upload_wheel_to_s3.py
+++ .github/ci-scripts/upload_wheel_to_s3.py
@@ -2,43 +2,44 @@ import argparse
 from pathlib import Path
 
 import boto3
-import constants
 import wheellib
+
+BUCKET_NAME = "github-actions-artifacts-bucket"
+WHEEL_SUFFIX = ".whl"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--commit-hash", required=True)
-    parser.add_argument("--platform-substring", required=True, choices=["x86", "aarch", "arm"])
-    parser.add_argument("--path-to-wheel-dir", required=True)
+    parser.add_argument("--commit-hash", type=str, required=True)
+    parser.add_argument("--subdir", type=str, required=True)
+    parser.add_argument("--platform-substring", type=str, required=True, choices=["x86", "aarch", "arm"])
+    parser.add_argument("--path-to-wheel-dir", type=Path, required=True)
     args = parser.parse_args()
 
-    commit_hash = args.commit_hash
-    platform_substring = args.platform_substring
-    path_to_wheel_dir = Path(args.path_to_wheel_dir)
-
-    assert path_to_wheel_dir.exists(), f"Path to wheel directory does not exist: {path_to_wheel_dir}"
-    wheelpaths = iter(filepath for filepath in path_to_wheel_dir.iterdir() if filepath.suffix == constants.WHEEL_SUFFIX)
+    assert args.path_to_wheel_dir.exists(), f"Path to wheel directory does not exist: {args.path_to_wheel_dir}"
+    wheelpaths = iter(filepath for filepath in args.path_to_wheel_dir.iterdir() if filepath.suffix == WHEEL_SUFFIX)
 
     def f(wheelpath: Path) -> bool:
         platform_tag = wheellib.get_platform_tag(wheelpath.name)
-        return platform_substring in platform_tag
+        return args.platform_substring in platform_tag
 
     filtered_wheelpaths: list[Path] = list(filter(f, wheelpaths))
 
     length = len(filtered_wheelpaths)
     if length == 0:
-        raise RuntimeError(f"No wheels found that match the given platform substring: {platform_substring}; expected 1")
+        raise RuntimeError(
+            f"No wheels found that match the given platform substring: {args.platform_substring}; expected 1"
+        )
     elif length > 1:
         raise RuntimeError(
-            f"""Multiple wheels found that match the given platform substring: {platform_substring}; expected just 1
+            f"""Multiple wheels found that match the given platform substring: {args.platform_substring}; expected just 1
 Wheels available: {wheelpaths}"""
         )
     [wheelpath] = filtered_wheelpaths
     s3 = boto3.client("s3")
-    destination = Path("builds") / commit_hash / wheelpath.name
+    destination = Path("builds") / args.commit_hash / args.subdir / wheelpath.name
     s3.upload_file(
         Filename=wheelpath,
-        Bucket=constants.BUCKET_NAME,
+        Bucket=BUCKET_NAME,
         Key=str(destination),
         ExtraArgs={"ACL": "public-read"},
     )
