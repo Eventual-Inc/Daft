24dde3cf0305198635ce05feea1977557554e46b
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-12-17 11:37:32 -0800

    Update tests/connect/test_analyze_plan.py
    Co-authored-by: Cory Grinstead <cory.grinstead@gmail.com>

--- tests/connect/test_analyze_plan.py
+++ tests/connect/test_analyze_plan.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
 
+@pytest.mark.skip(
+    reason="Currently an issue in the spark connect code. It always passes the inferred schema instead of the supplied schema."
+)
 def test_analyze_plan(spark_session):
+
     data = [[1000, 99]]
     df1 = spark_session.createDataFrame(data, schema="Value int, Total int")
     s = df1.schema
