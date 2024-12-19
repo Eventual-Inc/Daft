7febe13e91f20885f9884c6fd33d36ddda426fee
Kevin Wang <kevin@eventualcomputing.com>
2024-12-19 13:44:48 -0800

    add negation tests

--- src/daft-algebra/src/boolean.rs
+++ src/daft-algebra/src/boolean.rs
@@ -156,6 +156,24 @@ mod tests {
     }
 
     #[test]
+    fn dnf_neg() {
+        // !(a & ((!b) | c)) -> (!a) | (b & (!c))
+        let expr = col("a").and(col("b").not().or(col("c"))).not();
+        let expected = col("a").not().or(col("b").and(col("c").not()));
+
+        assert_eq!(expected, to_dnf(expr));
+    }
+
+    #[test]
+    fn cnf_neg() {
+        // !(a | ((!b) & c)) -> (!a) & (b | (!c))
+        let expr = col("a").or(col("b").not().and(col("c"))).not();
+        let expected = col("a").not().and(col("b").or(col("c").not()));
+
+        assert_eq!(expected, to_cnf(expr));
+    }
+
+    #[test]
     fn dnf_nested() {
         // a & b & ((c & d) | (e & f)) -> (a & b & c & d) | (a & b & e & f)
         let expr = col("a")
