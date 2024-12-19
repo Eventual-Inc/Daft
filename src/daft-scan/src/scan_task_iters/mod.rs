e811034ea6f5e775abd58f3f46ae970897ec0a9e
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-12-19 16:35:32 +0800

    Add config flag and unit test

--- src/daft-scan/src/scan_task_iters/mod.rs
+++ src/daft-scan/src/scan_task_iters/mod.rs
@@ -316,26 +316,34 @@ fn split_and_merge_pass(
         .iter()
         .all(|st| st.as_any().downcast_ref::<ScanTask>().is_some())
     {
-        // TODO(desmond): Here we downcast Arc<dyn ScanTaskLike> to Arc<ScanTask>. ScanTask and DummyScanTask (test only) are
-        // the only non-test implementer of ScanTaskLike. It might be possible to avoid the downcast by implementing merging
-        // at the trait level, but today that requires shifting around a non-trivial amount of code to avoid circular dependencies.
-        let iter: BoxScanTaskIter = Box::new(scan_tasks.as_ref().iter().map(|st| {
-            st.clone()
-                .as_any_arc()
-                .downcast::<ScanTask>()
-                .map_err(|e| DaftError::TypeError(format!("Expected Arc<ScanTask>, found {:?}", e)))
-        }));
-        let split_tasks = split_by_row_groups(
-            iter,
-            cfg.parquet_split_row_groups_max_files,
-            cfg.scan_tasks_min_size_bytes,
-            cfg.scan_tasks_max_size_bytes,
-        );
-        let merged_tasks = merge_by_sizes(split_tasks, pushdowns, cfg);
-        let scan_tasks: Vec<Arc<dyn ScanTaskLike>> = merged_tasks
-            .map(|st| st.map(|task| task as Arc<dyn ScanTaskLike>))
-            .collect::<DaftResult<Vec<_>>>()?;
-        Ok(Arc::new(scan_tasks))
+        if cfg.scantask_splitting_level == 1 {
+            // TODO(desmond): Here we downcast Arc<dyn ScanTaskLike> to Arc<ScanTask>. ScanTask and DummyScanTask (test only) are
+            // the only non-test implementer of ScanTaskLike. It might be possible to avoid the downcast by implementing merging
+            // at the trait level, but today that requires shifting around a non-trivial amount of code to avoid circular dependencies.
+            let iter: BoxScanTaskIter = Box::new(scan_tasks.as_ref().iter().map(|st| {
+                st.clone().as_any_arc().downcast::<ScanTask>().map_err(|e| {
+                    DaftError::TypeError(format!("Expected Arc<ScanTask>, found {:?}", e))
+                })
+            }));
+            let split_tasks = split_by_row_groups(
+                iter,
+                cfg.parquet_split_row_groups_max_files,
+                cfg.scan_tasks_min_size_bytes,
+                cfg.scan_tasks_max_size_bytes,
+            );
+            let merged_tasks = merge_by_sizes(split_tasks, pushdowns, cfg);
+            let scan_tasks: Vec<Arc<dyn ScanTaskLike>> = merged_tasks
+                .map(|st| st.map(|task| task as Arc<dyn ScanTaskLike>))
+                .collect::<DaftResult<Vec<_>>>()?;
+            Ok(Arc::new(scan_tasks))
+        } else if cfg.scantask_splitting_level == 2 {
+            todo!("Implement aggressive scantask splitting");
+        } else {
+            panic!(
+                "DAFT_SCANTASK_SPLITTING_LEVEL must be either 1 or 2, received: {}",
+                cfg.scantask_splitting_level
+            );
+        }
     } else {
         Ok(scan_tasks)
     }
