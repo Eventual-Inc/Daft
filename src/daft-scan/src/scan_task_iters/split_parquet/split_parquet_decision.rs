396ca331e61abda8390f4da439d049f7f016629e
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2024-12-20 17:22:29 +0800

    Add metadata retrieval logic

--- src/daft-scan/src/scan_task_iters/split_parquet/split_parquet_decision.rs
+++ src/daft-scan/src/scan_task_iters/split_parquet/split_parquet_decision.rs
@@ -1,8 +1,13 @@
+use std::{ops::Deref, sync::Arc};
+
 use common_daft_config::DaftExecutionConfig;
 use common_error::DaftResult;
 use common_file_formats::{FileFormatConfig, ParquetSourceConfig};
 
-use crate::{scan_task_iters::BoxScanTaskIter, DataSource};
+use super::{
+    fetch_parquet_metadata::ParquetSplitScanTaskGenerator, split_parquet_file::ParquetFileSplitter,
+};
+use crate::{scan_task_iters::BoxScanTaskIter, DataSource, ScanTaskRef};
 
 /// An iterator that determines whether incoming ScanTasks should be split by Parquet rowgroups.
 ///
@@ -21,8 +26,8 @@ impl<'cfg> DecideSplitIterator<'cfg> {
 }
 
 pub(super) enum Decision {
-    Split(()),
-    NoSplit(()),
+    Split(ScanTaskRef),
+    NoSplit(ScanTaskRef),
 }
 
 impl<'cfg> Iterator for DecideSplitIterator<'cfg> {
@@ -60,11 +65,60 @@ impl<'cfg> Iterator for DecideSplitIterator<'cfg> {
                     .get_iceberg_delete_files()
                     .map_or(true, std::vec::Vec::is_empty)
             {
-                return Some(Ok(Decision::Split(())));
+                return Some(Ok(Decision::Split(scan_task)));
             } else {
-                return Some(Ok(Decision::NoSplit(())));
+                return Some(Ok(Decision::NoSplit(scan_task)));
             }
         }
         None
+    }
+}
+
+impl Deref for Decision {
+    type Target = ScanTaskRef;
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Self::Split(st) | Self::NoSplit(st) => st,
+        }
+    }
+}
+
+impl Decision {
+    pub(super) fn get_parquet_path(&self) -> &str {
+        if let [source] = &self.sources[..] {
+            source.get_path()
+        } else {
+            panic!("Calling get_parquet_path on a malformed ScanTaskSplitDecision");
+        }
+    }
+
+    pub(super) fn get_io_config(&self) -> Arc<daft_io::IOConfig> {
+        Arc::new(self.storage_config.io_config.clone().unwrap_or_default())
+    }
+
+    pub(super) fn get_parquet_source_config(&self) -> &ParquetSourceConfig {
+        if let FileFormatConfig::Parquet(cfg) = self.file_format_config.as_ref() {
+            cfg
+        } else {
+            panic!("Calling get_parquet_source_config on a malformed ScanTaskSplitDecision");
+        }
+    }
+
+    pub(super) fn apply_metadata(
+        self,
+        metadata: Option<parquet2::metadata::FileMetaData>,
+    ) -> ParquetSplitScanTaskGenerator {
+        match metadata {
+            Some(_metadata) => {
+                ParquetSplitScanTaskGenerator::Split(ParquetFileSplitter::new(self.unwrap()))
+            }
+            None => ParquetSplitScanTaskGenerator::NoSplit(std::iter::once(Ok(self.unwrap()))),
+        }
+    }
+
+    fn unwrap(self) -> ScanTaskRef {
+        match self {
+            Self::Split(st) | Self::NoSplit(st) => st,
+        }
     }
 }
