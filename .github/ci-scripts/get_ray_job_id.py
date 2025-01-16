d7486c39d77703ce0753e1cc89f91673bba612d3
Raunak Bhagat <r@rabh.io>
2025-01-16 13:46:37 -0800

    Save changes

---
+++ .github/ci-scripts/get_ray_job_id.py
@@ -0,0 +1,28 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = []
+# ///
+
+from argparse import ArgumentParser
+
+import requests
+
+
+def main(submission_id: str):
+    r = requests.get("http://localhost:8265/api/v0/jobs")
+    r.status_code
+    jobs = r.json()
+    results = jobs["data"]["result"]["result"]
+    for result in results:
+        if result["submission_id"] == submission_id:
+            print(result["job_id"])
+            break
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("submission_id", type=str)
+
+    args = parser.parse_args()
+
+    main(args.submission_id)
