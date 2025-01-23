77a28ab2f67f6294b2a1dc7de09c24fa4c435e0f
ChanChan Mao <chanchanmao1130@gmail.com>
2025-01-22 22:11:47 -0800

    make html to make docs

--- docs/hooks.py
+++ docs/hooks.py
@@ -3,5 +3,5 @@ import subprocess
 
 
 def make_api_docs(*args, **kwargs):
-    subprocess.run(["make", "html", 'SPHINXOPTS="-W --keep-going"'], check=True)
+    subprocess.run(["make", "docs", 'SPHINXOPTS="-W --keep-going"'], check=True)
     shutil.copytree("sphinx/_build/html", "mkdocs/api_docs", dirs_exist_ok=True)
