7641790cdf221096bcba1ae2f2f570cb2796b97e
ChanChan Mao <chanchanmao1130@gmail.com>
2025-01-27 18:51:02 -0800

    delete comment

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,11 +1,3 @@
-# import shutil
-# import subprocess
-
-
-# def make_api_docs(*args, **kwargs):
-#     subprocess.run(["make", "html", 'SPHINXOPTS="-W"'], check=True)
-#     shutil.copytree("docs/sphinx/_build/html", "mkdocs/api_docs", dirs_exist_ok=True)
-
 import shutil
 import subprocess
 import os
