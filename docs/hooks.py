4cd03f0e3cb657328dc303f4b3d9b3ffd277e582
ChanChan Mao <chanchanmao1130@gmail.com>
2025-01-27 19:18:49 -0800

    small commit fix

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,6 +1,7 @@
+import os
 import shutil
 import subprocess
-import os
+
 
 def make_api_docs(*args, **kwargs):
     env = os.environ.copy()
@@ -8,13 +9,18 @@ def make_api_docs(*args, **kwargs):
 
     # Run sphinx-build directly instead of using make
     venv_path = os.getenv("READTHEDOCS_VIRTUALENV_PATH", ".venv")
-    sphinx_build = os.path.join(os.path.abspath(f'{venv_path}/bin'), 'sphinx-build')
-    subprocess.run([
-        sphinx_build,
-        "-b", "html",
-        "docs/sphinx/source",  # source dir
-        "docs/sphinx/_build",  # build dir
-    ], check=True, env=env)
+    sphinx_build = os.path.join(os.path.abspath(f"{venv_path}/bin"), "sphinx-build")
+    subprocess.run(
+        [
+            sphinx_build,
+            "-b",
+            "html",
+            "docs/sphinx/source",  # source dir
+            "docs/sphinx/_build",  # build dir
+        ],
+        check=True,
+        env=env,
+    )
 
     # Copy built docs to mkdocs directory
     shutil.copytree("docs/sphinx/_build", "docs/mkdocs/api_docs", dirs_exist_ok=True)
