2485015a478b11a2db08eee4afe2bf461926dc6a
Raunak Bhagat <r@rabh.io>
2025-01-28 18:06:12 -0800

    Add basic layout-ing

--- dashboard/src/components/app-sidebar.tsx
+++ dashboard/src/components/app-sidebar.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
-
 import { SearchForm } from '@/components/search-form';
-import { VersionSwitcher } from '@/components/version-switcher';
+import { Home, TableProperties } from "lucide-react"
+
 import {
     Sidebar,
     SidebarContent,
@@ -12,171 +11,47 @@ import {
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
-    SidebarRail,
-} from '@/components/ui/sidebar';
+} from "@/components/ui/sidebar"
 
-// This is sample data.
-const data = {
-    versions: ['1.0.1', '1.1.0-alpha', '2.0.0-beta1'],
-    navMain: [
-        {
-            title: 'Getting Started',
-            url: '#',
-            items: [
-                {
-                    title: 'Installation',
-                    url: '#',
-                },
-                {
-                    title: 'Project Structure',
-                    url: '#',
-                },
-            ],
-        },
-        {
-            title: 'Building Your Application',
-            url: '#',
-            items: [
-                {
-                    title: 'Routing',
-                    url: '#',
-                },
-                {
-                    title: 'Data Fetching',
-                    url: '#',
-                    isActive: true,
-                },
-                {
-                    title: 'Rendering',
-                    url: '#',
-                },
-                {
-                    title: 'Caching',
-                    url: '#',
-                },
-                {
-                    title: 'Styling',
-                    url: '#',
-                },
-                {
-                    title: 'Optimizing',
-                    url: '#',
-                },
-                {
-                    title: 'Configuring',
-                    url: '#',
-                },
-                {
-                    title: 'Testing',
-                    url: '#',
-                },
-                {
-                    title: 'Authentication',
-                    url: '#',
-                },
-                {
-                    title: 'Deploying',
-                    url: '#',
-                },
-                {
-                    title: 'Upgrading',
-                    url: '#',
-                },
-                {
-                    title: 'Examples',
-                    url: '#',
-                },
-            ],
-        },
-        {
-            title: 'API Reference',
-            url: '#',
-            items: [
-                {
-                    title: 'Components',
-                    url: '#',
-                },
-                {
-                    title: 'File Conventions',
-                    url: '#',
-                },
-                {
-                    title: 'Functions',
-                    url: '#',
-                },
-                {
-                    title: 'next.config.js Options',
-                    url: '#',
-                },
-                {
-                    title: 'CLI',
-                    url: '#',
-                },
-                {
-                    title: 'Edge Runtime',
-                    url: '#',
-                },
-            ],
-        },
-        {
-            title: 'Architecture',
-            url: '#',
-            items: [
-                {
-                    title: 'Accessibility',
-                    url: '#',
-                },
-                {
-                    title: 'Fast Refresh',
-                    url: '#',
-                },
-                {
-                    title: 'Next.js Compiler',
-                    url: '#',
-                },
-                {
-                    title: 'Supported Browsers',
-                    url: '#',
-                },
-                {
-                    title: 'Turbopack',
-                    url: '#',
-                },
-            ],
-        },
-    ],
-};
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "home",
+        icon: Home,
+    },
+    {
+        title: "Queries",
+        url: "#",
+        icon: TableProperties,
+    },
+]
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar() {
     return (
-        <Sidebar {...props}>
+        <Sidebar>
             <SidebarHeader>
-                <VersionSwitcher
-                    versions={data.versions}
-                    defaultVersion={data.versions[0]}
-                />
                 <SearchForm />
             </SidebarHeader>
             <SidebarContent>
-                {/* We create a SidebarGroup for each parent. */}
-                {data.navMain.map((item) => (
-                    <SidebarGroup key={item.title}>
-                        <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                {item.items.map((item) => (
-                                    <SidebarMenuItem key={item.title}>
-                                        <SidebarMenuButton asChild isActive={item.isActive}>
-                                            <a href={item.url}>{item.title}</a>
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))}
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                    </SidebarGroup>
-                ))}
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
             </SidebarContent>
-            <SidebarRail />
         </Sidebar>
-    );
+    )
 }
--- dashboard/src/components/search-form.tsx
+++ dashboard/src/components/search-form.tsx
@@ -13,11 +13,11 @@ export function SearchForm({ ...props }: React.ComponentProps<'form'>) {
             <SidebarGroup className="py-0">
                 <SidebarGroupContent className="relative">
                     <Label htmlFor="search" className="sr-only">
-            Search
+                        Search
                     </Label>
                     <SidebarInput
                         id="search"
-                        placeholder="Search the docs..."
+                        placeholder="Search"
                         className="pl-8"
                     />
                     <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
