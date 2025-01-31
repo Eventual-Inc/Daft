2fc72e71d551a901ae504312dfaf9b4dae5e6e85
universalmind303 <cory.grinstead@gmail.com>
2025-01-31 15:24:58 -0600

    remove prints

--- daft/dataframe/dataframe.py
+++ daft/dataframe/dataframe.py
@@ -550,7 +550,6 @@ class DataFrame:
         if not parts:
             raise ValueError("Can't create a DataFrame from an empty list of tables.")
 
-        print("_from_tables")
         result_pset = LocalPartitionSet()
 
         for i, part in enumerate(parts):
