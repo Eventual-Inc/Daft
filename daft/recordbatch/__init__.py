03ad712f0ee32de16500206e255ad3fb63d7b147
universalmind303 <cory.grinstead@gmail.com>
2025-02-05 14:54:36 -0600

    refactor table -> recordbatch

--- daft/recordbatch/__init__.py
+++ daft/recordbatch/__init__.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 # Need to import after `.table` due to circular dep issue otherwise
 from .micropartition import MicroPartition
-from .table import RecordBatch, read_parquet_into_pyarrow, read_parquet_into_pyarrow_bulk
+from .recordbatch import RecordBatch, read_parquet_into_pyarrow, read_parquet_into_pyarrow_bulk
 
 __all__ = ["MicroPartition", "RecordBatch", "read_parquet_into_pyarrow", "read_parquet_into_pyarrow_bulk"]
