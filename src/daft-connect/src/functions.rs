47f8716d233362b036b7b2970ce5bc169c98f02e
Cory Grinstead <cory.grinstead@gmail.com>
2025-02-11 11:23:15 -0600

    feat: add functions to daft-connect (#3780)
    adds the following:
    - all of spark's [math
    functions](https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/functions.html#math-functions)
    we currently have implemented.
    - all of spark's [string
    functions](https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/functions.html#string-functions)
    we currently have implemented
    - all of spark's [normal
    functions](https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/functions.html#normal-functions)
    we currently have implemented

--- src/daft-connect/src/functions.rs
+++ src/daft-connect/src/functions.rs
@@ -1,14 +1,27 @@
 use std::{collections::HashMap, sync::Arc};
 
+use daft_dsl::{
+    functions::{ScalarFunction, ScalarUDF},
+    ExprRef,
+};
 use once_cell::sync::Lazy;
+use partition_transform::PartitionTransformFunctions;
 use spark_connect::Expression;
 
-use crate::{error::ConnectResult, spark_analyzer::SparkAnalyzer};
+use crate::{error::ConnectResult, invalid_argument_err, spark_analyzer::SparkAnalyzer};
+mod aggregate;
 mod core;
+mod math;
+mod partition_transform;
+mod string;
 
 pub(crate) static CONNECT_FUNCTIONS: Lazy<SparkFunctions> = Lazy::new(|| {
     let mut functions = SparkFunctions::new();
+    functions.register::<aggregate::AggregateFunctions>();
     functions.register::<core::CoreFunctions>();
+    functions.register::<math::MathFunctions>();
+    functions.register::<PartitionTransformFunctions>();
+    functions.register::<string::StringFunctions>();
     functions
 });
 
@@ -52,4 +65,52 @@ impl SparkFunctions {
 pub trait FunctionModule {
     /// Register this module to the given [SparkFunctions] table.
     fn register(_parent: &mut SparkFunctions);
+}
+
+struct UnaryFunction(fn(ExprRef) -> ExprRef);
+
+impl<T> SparkFunction for T
+where
+    T: ScalarUDF + 'static + Clone,
+{
+    fn to_expr(
+        &self,
+        args: &[Expression],
+        analyzer: &SparkAnalyzer,
+    ) -> ConnectResult<daft_dsl::ExprRef> {
+        let sf = ScalarFunction::new(
+            self.clone(),
+            args.iter()
+                .map(|arg| analyzer.to_daft_expr(arg))
+                .collect::<ConnectResult<Vec<_>>>()?,
+        );
+        Ok(sf.into())
+    }
+}
+
+impl SparkFunction for UnaryFunction {
+    fn to_expr(
+        &self,
+        args: &[Expression],
+        analyzer: &SparkAnalyzer,
+    ) -> ConnectResult<daft_dsl::ExprRef> {
+        match args {
+            [arg] => {
+                let arg = analyzer.to_daft_expr(arg)?;
+                Ok(self.0(arg))
+            }
+            _ => invalid_argument_err!("requires exactly one argument"),
+        }
+    }
+}
+
+struct Todo;
+impl SparkFunction for Todo {
+    fn to_expr(
+        &self,
+        _args: &[Expression],
+        _analyzer: &SparkAnalyzer,
+    ) -> ConnectResult<daft_dsl::ExprRef> {
+        invalid_argument_err!("Function not implemented")
+    }
 }
