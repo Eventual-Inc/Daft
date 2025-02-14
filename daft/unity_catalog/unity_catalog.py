55bdc06dee2f0ef645bb56e0eaf45129c78f629b
Paul Mogren <paul.mogren@rithum.com>
2025-02-14 13:43:11 -0500

    fix: revise parameter added for unity read/write mode again per additional PR comments

--- daft/unity_catalog/unity_catalog.py
+++ daft/unity_catalog/unity_catalog.py
@@ -91,14 +91,14 @@ class UnityCatalog:
         return self._paginate_to_completion(_paginated_list_tables)
 
     def load_table(
-        self, table_name: str, new_table_storage_path: str | None = None, mode: Literal["r"|"rw"] = "rw"
+        self, table_name: str, new_table_storage_path: str | None = None, operation: Literal["READ"|"READ_WRITE"] = "READ_WRITE"
     ) -> UnityCatalogTable:
         """Loads an existing Unity Catalog table. If the table is not found, and information is provided in the method to create a new table, a new table will be attempted to be registered.
 
         Args:
             table_name (str): Name of the table in Unity Catalog in the form of dot-separated, 3-level namespace
             new_table_storage_path (str, optional): Cloud storage path URI to register a new external table using this path. Unity Catalog will validate if the path is valid and authorized for the principal, else will raise an exception.
-            mode (Literal["r", "rw"], optional): The intended use of the table, which impacts authorization. Defaults to "rw".
+            mode ("READ" or "READ_WRITE", optional): The intended use of the table, which impacts authorization. Defaults to "READ_WRITE".
 
         Returns:
             UnityCatalogTable
@@ -140,7 +140,6 @@ class UnityCatalog:
         table_id = table_info.table_id
         storage_location = table_info.storage_location
         # Grab credentials from Unity catalog and place it into the Table
-        operation = "READ" if mode == "r" else "READ_WRITE"
         temp_table_credentials = self._client.temporary_table_credentials.create(operation=operation, table_id=table_id)
 
         scheme = urlparse(storage_location).scheme
