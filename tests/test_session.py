01fffdb5fb55231a4ded803e4b217550315440eb
R. Conner Howell <robert@eventualcomputing.com>
2025-02-18 17:58:32 -0800

    Implements py Table(ABC) backed by rs TableRef

--- tests/test_session.py
+++ tests/test_session.py
@@ -14,11 +14,11 @@ def test_current_session_exists():
 
 
 ###
-# ATTACH & DETACH
+# ATTACH & DETACH CATALOG
 ###
 
 
-def test_attach():
+def test_attach_catalog():
     sess = Session.empty()
     #
     # create some 'existing' catalogs
@@ -38,10 +38,10 @@ def test_attach():
     #
     # error!
     with pytest.raises(Exception, match="already exists"):
-        sess.attach(cat1, alias="cat1")
+        sess.attach_catalog(cat1, alias="cat1")
 
 
-def test_detach():
+def test_detach_catalog():
     sess = Session.empty()
     #
     # setup.
@@ -60,3 +60,52 @@ def test_detach():
     # error!
     with pytest.raises(Exception, match="not found"):
         sess.detach_catalog("cat1")
+
+
+###
+# ATTACH & DETACH TABLE
+###
+
+
+def test_attach_table():
+    sess = Session.empty()
+    #
+    # create some tables
+    df1 = daft.from_pydict({"x": [1.1, 2.2, 3.3]})
+    df2 = daft.from_pydict({"y": ["a", "b", "c"]})
+    #
+    # attach them..
+    sess.attach_table(df1, alias="tbl1")
+    sess.attach_table(df2, alias="tbl2")
+    #
+    # list_tables
+    assert 2 == len(sess.list_tables())
+    #
+    # read_table
+    assert sess.read_table("tbl1").to_pydict() == df1.to_pydict()
+    assert sess.read_table("tbl2").to_pydict() == df2.to_pydict()
+    #
+    # error!
+    with pytest.raises(Exception, match="already exists"):
+        sess.attach_table(df1, alias="tbl1")
+
+
+def test_detach_table():
+    sess = Session.empty()
+    #
+    # setup.
+    tbl1 = daft.from_pydict({})
+    tbl2 = daft.from_pydict({})
+    sess.attach_table(tbl1, alias="tbl1")
+    sess.attach_table(tbl2, alias="tbl2")
+    #
+    #
+    assert 2 == len(sess.list_tables())
+    #
+    # detach existing
+    sess.detach_table("tbl1")
+    assert 1 == len(sess.list_tables())
+    #
+    # error!
+    with pytest.raises(Exception, match="not found"):
+        sess.detach_table("tbl1")
