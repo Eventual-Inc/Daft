adf1af192091e69f0af7761fb0840ade50af8c9b
universalmind303 <cory.grinstead@gmail.com>
2025-02-24 13:40:13 -0600

    parametrize all tests to also run on ray spark

--- tests/connect/conftest.py
+++ tests/connect/conftest.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
 
+from typing import Any
+
 import pytest
 from pyspark.sql import SparkSession
 
+import daft
+
+
+@pytest.fixture(params=["local_spark", "ray_spark"])
+def spark_session(request):
+    return request.getfixturevalue(request.param)
+
 
 @pytest.fixture(scope="session")
-def spark_session():
+def local_spark():
     """Fixture to create and clean up a Spark session.
 
     This fixture is available to all test files and creates a single
@@ -16,7 +25,7 @@ def spark_session():
     # Start Daft Connect server
     server = daft.spark.connect_start()
 
-    url = f"sc://localhost:{server.port()}"
+    url = server.spark_remote_url()
 
     # Initialize Spark Connect session
     session = SparkSession.builder.appName("DaftConfigTest").remote(url).getOrCreate()
@@ -26,3 +35,54 @@ def spark_session():
     # Cleanup
     server.shutdown()
     session.stop()
+
+
+@pytest.fixture(scope="session")
+def ray_spark():
+    """Fixture to create and clean up a Spark session.
+
+    This fixture is available to all test files and creates a single
+    Spark session for the entire test suite run.
+    """
+    import daft
+
+    # Start Daft Connect server
+    server = daft.spark.connect_start_ray()
+
+    url = server.spark_remote_url()
+
+    # Initialize Spark Connect session
+    session = SparkSession.builder.appName("DaftConfigTest").remote(url).getOrCreate()
+
+    yield session
+
+    # Cleanup
+    server.shutdown()
+    session.stop()
+
+
+@pytest.fixture(scope="function")
+def make_spark_df(spark_session):
+    def _make_spark_df(data: dict[str, Any]):
+        fields = [name for name in data]
+        rows = list(zip(*[data[name] for name in fields]))
+        return spark_session.createDataFrame(rows, fields)
+
+    yield _make_spark_df
+
+
+@pytest.fixture(scope="function")
+def assert_spark_equals(spark_session):
+    def _assert_spark_dfs_eq(df1, df2):
+        if isinstance(df1, daft.DataFrame):
+            df1 = df1.to_pandas()
+        else:
+            df1 = df1.toPandas()
+        if isinstance(df2, daft.DataFrame):
+            df2 = df2.to_pandas()
+        else:
+            df2 = df2.toPandas()
+
+        assert df1.equals(df2)
+
+    yield _assert_spark_dfs_eq
