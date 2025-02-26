902f3a5277cfabae6e7cb598ee7461b57acc07e2
Colin Ho <colin.ho99@gmail.com>
2025-02-26 21:24:26 +0000

    fix

--- tests/microbenchmarks/test_file_read.py
+++ tests/microbenchmarks/test_file_read.py
@@ -13,6 +13,7 @@ from tests.conftest import get_tests_daft_runner_name
 
 @pytest.fixture(scope="module", params=[(1, 64), (8, 8), (64, 1)], ids=["1x64mib", "8x8mib", "64x1mib"])
 def gen_simple_csvs(request) -> str:
+    print(f"request: {request}")
     """Creates some CSVs in a directory. Returns the name of the directory."""
     num_files, mibs_per_file = request.param
 
@@ -34,6 +35,7 @@ def gen_simple_csvs(request) -> str:
                 dst=os.path.join(tmpdirname, f"file{i}.csv"),
             )
 
+        print(f"tmpdirname: {tmpdirname}, num_files: {num_files}, mibs_per_file: {mibs_per_file}")
         yield tmpdirname, num_files * mibs_per_file * 1024 * 128
 
 
@@ -41,13 +43,15 @@ def gen_simple_csvs(request) -> str:
 @pytest.mark.benchmark(group="file_read")
 def test_csv_read(gen_simple_csvs, benchmark):
     csv_dir, num_rows = gen_simple_csvs
+    print(f"csv_dir: {csv_dir}, num_rows: {num_rows}")
 
     def bench() -> DataFrame:
         df = daft.read_csv(csv_dir)
+        print("read ok")
         return df.collect()
 
     df = benchmark(bench)
-
+    print(f"len(df): {len(df)}")
     assert len(df) == num_rows
 
 
