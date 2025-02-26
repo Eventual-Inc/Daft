d4685890427303a27b42247d4d76be1538923447
Cory Grinstead <cory.grinstead@gmail.com>
2025-02-26 17:51:19 -0600

    feat(connect): `daft.pyspark` module (#3861)

--- tests/connect/conftest.py
+++ tests/connect/conftest.py
@@ -1,28 +1,39 @@
 from __future__ import annotations
 
 import pytest
-from pyspark.sql import SparkSession
+
+
+@pytest.fixture(params=["local_spark", "ray_spark"], scope="session")
+def spark_session(request):
+    return request.getfixturevalue(request.param)
 
 
 @pytest.fixture(scope="session")
-def spark_session():
+def local_spark():
     """Fixture to create and clean up a Spark session.
 
     This fixture is available to all test files and creates a single
     Spark session for the entire test suite run.
     """
-    from daft.daft import connect_start
-
-    # Start Daft Connect server
-    server = connect_start()
-
-    url = f"sc://localhost:{server.port()}"
+    from daft.pyspark import SparkSession
 
     # Initialize Spark Connect session
-    session = SparkSession.builder.appName("DaftConfigTest").remote(url).getOrCreate()
+    session = SparkSession.builder.appName("DaftConfigTest").local().getOrCreate()
 
     yield session
 
-    # Cleanup
-    server.shutdown()
-    session.stop()
+
+@pytest.fixture(scope="session")
+def ray_spark():
+    """Fixture to create and clean up a Spark session.
+
+    This fixture is available to all test files and creates a single
+    Spark session for the entire test suite run.
+    """
+    from daft.pyspark import SparkSession
+
+    # Initialize Spark Connect session
+
+    session = SparkSession.builder.appName("DaftConfigTest").remote("ray://localhost:10001").getOrCreate()
+
+    yield session
