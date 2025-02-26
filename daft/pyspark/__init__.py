e93efbb3c8e48ef94697ba18b10ed8a517202a48
universalmind303 <cory.grinstead@gmail.com>
2025-02-25 19:19:49 -0600

    create pyspark module

--- daft/pyspark/__init__.py
+++ daft/pyspark/__init__.py
@@ -1,3 +1,31 @@
+"""# PySpark.
+
+The `daft.pyspark` module provides a way to create a PySpark session that can be run locally or backed by a ray cluster.
+
+This serves as a way to run the daft query engine, but with a spark compatible API.
+
+
+## Example
+
+```py
+from daft.pyspark import SparkSession
+from pyspark.sql.functions import col
+
+# create a local spark session
+spark = SparkSession.builder.local().getOrCreate()
+
+# alternatively, connect to a ray cluster
+spark = SparkSession.builder.remote("ray://<ray-ip>:10001").getOrCreate()
+
+# use spark as you would with the native spark library, but with a daft backend!
+
+spark.createDataFrame([{"hello": "world"}]).select(col("hello")).show()
+
+# stop the spark session
+spark.stop()
+```
+"""
+
 from daft.daft import connect_start
 from pyspark.sql import SparkSession as PySparkSession
 
@@ -9,21 +37,41 @@ class Builder:
     def local(self):
         self._connection = connect_start()
         url = f"sc://0.0.0.0:{self._connection.port()}"
-        return PySparkSession.builder.remote(url)
+        self._builder = PySparkSession.builder.remote(url)
+        return self
 
     def remote(self, url):
         if url.startswith("ray://"):
             import daft
 
-            daft.context.set_runner_ray(address=url)
+            if url.startswith("ray://localhost"):
+                daft.context.set_runner_ray(noop_if_initialized=True)
+            else:
+                daft.context.set_runner_ray(address=url, noop_if_initialized=True)
             self._connection = connect_start()
             url = f"sc://0.0.0.0:{self._connection.port()}"
-            return PySparkSession.builder.remote(url)
+            self._builder = PySparkSession.builder.remote(url)
+            return self
         else:
-            return PySparkSession.builder.remote(url)
+            self._builder = PySparkSession.builder.remote(url)
+            return self
 
     def getOrCreate(self):
         return SparkSession(self._builder.getOrCreate(), self._connection)
+
+    def __getattr__(self, name):
+        attr = getattr(self._builder, name)
+        if callable(attr):
+
+            def wrapped(*args, **kwargs):
+                result = attr(*args, **kwargs)
+                # If result is the original builder, return self instead
+                return self if result == self._builder else result
+
+            return wrapped
+        return attr
+
+    __doc__ = property(lambda self: self._spark_session.__doc__)
 
 
 class SparkSession:
--- tests/connect/conftest.py
+++ tests/connect/conftest.py
@@ -1,28 +1,70 @@
 from __future__ import annotations
 
+from typing import Any
+
 import pytest
-from pyspark.sql import SparkSession
+
+import daft
+
+
+@pytest.fixture(params=["local_spark", "ray_spark"], scope="session")
+def spark_session(request):
+    return request.getfixturevalue(request.param)
 
 
 @pytest.fixture(scope="session")
-def spark_session():
+def local_spark():
     """Fixture to create and clean up a Spark session.
 
     This fixture is available to all test files and creates a single
     Spark session for the entire test suite run.
     """
-    from daft.daft import connect_start
-
-    # Start Daft Connect server
-    server = connect_start()
-
-    url = f"sc://localhost:{server.port()}"
+    from daft.pyspark import SparkSession
 
     # Initialize Spark Connect session
-    session = SparkSession.builder.appName("DaftConfigTest").remote(url).getOrCreate()
+    session = SparkSession.builder.appName("DaftConfigTest").local().getOrCreate()
 
     yield session
 
-    # Cleanup
-    server.shutdown()
-    session.stop()
+
+@pytest.fixture(scope="session")
+def ray_spark():
+    """Fixture to create and clean up a Spark session.
+
+    This fixture is available to all test files and creates a single
+    Spark session for the entire test suite run.
+    """
+    from daft.pyspark import SparkSession
+
+    # Initialize Spark Connect session
+
+    session = SparkSession.builder.appName("DaftConfigTest").remote("ray://localhost:10001").getOrCreate()
+
+    yield session
+
+
+@pytest.fixture(scope="function")
+def make_spark_df(spark_session):
+    def _make_spark_df(data: dict[str, Any]):
+        fields = [name for name in data]
+        rows = list(zip(*[data[name] for name in fields]))
+        return spark_session.createDataFrame(rows, fields)
+
+    yield _make_spark_df
+
+
+@pytest.fixture(scope="function")
+def assert_spark_equals(spark_session):
+    def _assert_spark_dfs_eq(df1, df2):
+        if isinstance(df1, daft.DataFrame):
+            df1 = df1.to_pandas()
+        else:
+            df1 = df1.toPandas()
+        if isinstance(df2, daft.DataFrame):
+            df2 = df2.to_pandas()
+        else:
+            df2 = df2.toPandas()
+
+        assert df1.equals(df2)
+
+    yield _assert_spark_dfs_eq
