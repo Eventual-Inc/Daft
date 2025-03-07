76b0790c71ac21e385f9b8cc633e55473d26c324
Jay Chia <17691182+jaychia@users.noreply.github.com>
2025-03-07 13:30:18 -0800

    bug: add unit test for overprovisioned class udfs

--- tests/actor_pool/test_ray_actor_pool.py
+++ tests/actor_pool/test_ray_actor_pool.py
@@ -51,3 +51,38 @@ def test_ray_actor_pool():
     assert isinstance(pm, PartialPartitionMetadata)
     assert pm.num_rows == 3
     assert result_data.to_pydict() == {"x": [4, 4, 4]}
+
+
+def test_overprovisioning():
+    ray.init()
+    num_cpus_available = ray.available_resources()["CPU"]
+    overprovisioned_cpu_count = int(num_cpus_available) + 1
+    projection = ExpressionsProjection([MyUDF(daft.col("x"))])
+    pool = RayRoundRobinActorPool(
+        "my-pool",
+        overprovisioned_cpu_count,
+        ResourceRequest(num_cpus=1),
+        projection,
+        execution_config=PyDaftExecutionConfig.from_env(),
+    )
+    initial_partition = ray.put(MicroPartition.from_pydict({"x": [1, 1, 1]}))
+    ppm = PartialPartitionMetadata(num_rows=None, size_bytes=None)
+    pool.setup()
+
+    results = [
+        pool.submit(partial_metadatas=[ppm], inputs=[initial_partition]) for i in range(overprovisioned_cpu_count)
+    ]
+    partial_meta = [pm for pm, _ in results]
+    result_data = [r for _, r in results]
+
+    # NOTE: These could time out if the system does not behave as expected (i.e. the system tries to schedule work on actors that
+    # will never actually be scheduled due to a lack of resources
+    all_results = ray.get(result_data, timeout=10)
+    all_partial_meta = ray.get(partial_meta, timeout=10)
+
+    for partial_metadata, result_data in zip(all_results, all_partial_meta):
+        assert len(partial_metadata) == 1
+        pm = partial_metadata[0]
+        assert isinstance(pm, PartialPartitionMetadata)
+        assert pm.num_rows == 3
+        assert result_data.to_pydict() == {"x": [4, 4, 4]}
