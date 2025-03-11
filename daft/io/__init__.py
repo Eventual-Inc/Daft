68d0d0ea1fdb7a3aac537bed943e9f31dc554d19
universalmind303 <cory.grinstead@gmail.com>
2025-03-10 17:08:19 -0700

    feat: daft.range function

--- daft/io/__init__.py
+++ daft/io/__init__.py
@@ -17,6 +17,7 @@ from daft.io._lance import read_lance
 from daft.io._parquet import read_parquet
 from daft.io._sql import read_sql
 from daft.io._warc import read_warc
+from daft.io._range import _range
 from daft.io.catalog import DataCatalogTable, DataCatalogType
 from daft.io.file_path import from_glob_path
 
@@ -30,6 +31,7 @@ __all__ = [
     "S3Config",
     "S3Credentials",
     "from_glob_path",
+    "_range",
     "read_csv",
     "read_deltalake",
     "read_hudi",
--- daft/io/_range.py
+++ daft/io/_range.py
@@ -9,6 +9,141 @@ from daft import DataType
 from daft.io._generator import GeneratorScanOperator
 from daft.logical.schema import Schema
 from daft.recordbatch.recordbatch import RecordBatch
+from daft.api_annotations import PublicAPI
+from daft.dataframe import DataFrame
+from typing import Union, Optional, overload
+from daft.io.scan import ScanOperator
+from daft.daft import ScanOperatorHandle
+from daft.logical.builder import LogicalPlanBuilder
+
+@PublicAPI
+@overload
+def _range(end: int) -> DataFrame:
+    """Creates a DataFrame with a range of values.
+
+    The range starts at 0 and ends at `end` (exclusive) with a step size of 1.
+
+    Example:
+        >>> import daft
+        >>> daft.range(5).show()
+        ╭───────╮
+        │ id    │
+        │ ---   │
+        │ Int64 │
+        ╞═══════╡
+        │ 0     │
+        ├╌╌╌╌╌╌╌┤
+        │ 1     │
+        ├╌╌╌╌╌╌╌┤
+        │ 2     │
+        ├╌╌╌╌╌╌╌┤
+        │ 3     │
+        ├╌╌╌╌╌╌╌┤
+        │ 4     │
+        ╰───────╯
+        <BLANKLINE>
+        (Showing first 5 of 5 rows)
+    """
+    ...
+@PublicAPI
+@overload
+def _range(start: int, end: int) -> DataFrame:
+    ...
+    """Creates a DataFrame with a range of values.
+
+    The range starts at `start` and ends at `end` (exclusive) with a step size of 1.
+
+    Example:
+        >>> import daft
+        >>> daft.range(2, 5).show()
+        ╭───────╮
+        │ id    │
+        │ ---   │
+        │ Int64 │
+        ╞═══════╡
+        │ 2     │
+        ├╌╌╌╌╌╌╌┤
+        │ 3     │
+        ├╌╌╌╌╌╌╌┤
+        │ 4     │
+        ╰───────╯
+        <BLANKLINE>
+        (Showing first 3 of 3 rows)
+    """
+
+@PublicAPI
+@overload
+def _range(start: int, end: int, step: int) -> DataFrame:
+    ...
+    """Creates a DataFrame with a range of values.
+
+    The range starts at `start` and ends at `end` (exclusive) with a step size of `step`.
+
+    Example:
+        >>> import daft
+        >>> daft.range(2, 10, 2).show()
+        ╭───────╮
+        │ id    │
+        │ ---   │
+        │ Int64 │
+        ╞═══════╡
+        │ 2     │
+        ├╌╌╌╌╌╌╌┤
+        │ 4     │
+        ├╌╌╌╌╌╌╌┤
+        │ 6     │
+        ├╌╌╌╌╌╌╌┤
+        │ 8     │
+        ╰───────╯
+        <BLANKLINE>
+        (Showing first 4 of 4 rows)
+    """
+
+@PublicAPI
+@overload
+def _range(start: int, end: int, step: int, partitions: int) -> DataFrame:
+    """Creates a DataFrame with a range of values.
+
+    The range starts at `start` and ends at `end` (exclusive) with a step size of `step`.
+    The range is partitioned into `partitions` partitions.
+
+    Example:
+        >>> import daft
+        >>> df = daft.range(2, 10, step=2, partitions=2)
+        >>> df.num_partitions()
+        2
+        >>> df.show()
+        ╭───────╮
+        │ id    │
+        │ ---   │
+        │ Int64 │
+        ╞═══════╡
+        │ 2     │
+        ├╌╌╌╌╌╌╌┤
+        │ 4     │
+        ├╌╌╌╌╌╌╌┤
+        │ 6     │
+        ├╌╌╌╌╌╌╌┤
+        │ 8     │
+        ╰───────╯
+        <BLANKLINE>
+        (Showing first 4 of 4 rows)
+    """
+    ...
+
+@PublicAPI
+def _range(start: int, end: Optional[int]=None, step: int = 1, partitions: int = 1) -> DataFrame:
+
+    if end is None:
+        end = start
+        start = 0
+    else:
+        start = start
+    scan_op = RangeScanOperator(start, end, step, partitions)
+    handle = ScanOperatorHandle.from_python_scan_operator(scan_op)
+    builder = LogicalPlanBuilder.from_tabular_scan(scan_operator=handle)
+
+    return DataFrame(builder)
 
 
 def _range_generators(
