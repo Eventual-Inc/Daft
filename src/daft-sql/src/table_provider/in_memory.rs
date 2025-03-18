2961c6f2cad17dbe7431936cace368e71f1e64dd
R. Conner Howell <robert@eventualcomputing.com>
2025-03-18 16:49:00 -0700

    feat(sql): adds show statement and documentation

--- src/daft-sql/src/table_provider/in_memory.rs
+++ src/daft-sql/src/table_provider/in_memory.rs
@@ -15,8 +15,7 @@ use daft_micropartition::{
     MicroPartition,
 };
 
-/// Create an in-memory scan from arrow arrays, like DataFrame._from_arrow
-#[allow(dead_code)]
+/// Create an in-memory scan from arrow arrays, like `DataFrame._from_arrow`
 pub fn from_arrow<S: Into<SchemaRef>>(
     schema: S,
     arrays: Vec<Box<dyn arrow2::array::Array>>,
@@ -26,15 +25,14 @@ pub fn from_arrow<S: Into<SchemaRef>>(
     from_micropartitions(vec![part])
 }
 
-/// Create an in-memory scan from load micropartitions, like DataFrame._from_micropartitions.
-#[allow(dead_code)]
+/// Create an in-memory scan from load micropartitions, like `DataFrame._from_micropartitions`
 pub fn from_micropartitions(parts: Vec<MicroPartition>) -> DaftResult<LogicalPlanBuilder> {
     if parts.is_empty() {
         return Err(DaftError::InternalError(
             "from_micropartitions requires at least one partition.".to_string(),
         ));
     }
-    //
+    // use the schema before taking all the parts
     let schema = parts[0].schema();
     // create a partition set
     let pset = MicroPartitionSet::empty();
@@ -48,7 +46,6 @@ pub fn from_micropartitions(parts: Vec<MicroPartition>) -> DaftResult<LogicalPla
     let size_bytes = pset_ref.size_bytes()?;
     let num_rows = pset_ref.len();
     let num_partitions = pset_ref.num_partitions();
-    //
     // create a scan from the entry
     let builder = LogicalPlanBuilder::in_memory_scan(
         partition_key,
@@ -70,8 +67,18 @@ fn put_partition_set_into_cache(pset: Arc<MicroPartitionSet>) -> DaftResult<Part
         // get runner as python object so we can add a partition to the cache
         let py_runner = get_context().get_or_create_runner()?.to_pyobj(py);
         let py_runner = py_runner.bind(py);
-        // create a py partition set from our rust partition set.
-        let py_pset = PyMicroPartitionSet::from(pset);
+        // TODO chore: replace LocalPartitionSet with MicroPartitionSet
+        //   We cannot use PyMicroPartition as a PartitionSet implementation
+        //   because it's not possible to make PartitionSet the base class.
+        //   This complicated using PyMicroPartition directly, and the easiest
+        //   solution would be call DataFrame._from_micropartition directly.
+        //   However, I wish to close this gap soon so for now I'm going to
+        let py_partitioning = py.import("daft.runners.partitioning")?;
+        let py_pset = py_partitioning.getattr("LocalPartitionSet")?;
+        let py_pset = py_pset.call_method1(
+            "_from_micropartition_set",
+            (PyMicroPartitionSet::from(pset),),
+        )?;
         // put in the runner's partition set cache
         let py_cache_entry = py_runner.call_method1("put_partition_set_into_cache", (py_pset,))?;
         let py_cache_entry = Arc::new(py_cache_entry.unbind());
