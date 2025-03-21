661d5bafcaf7a9f837e6617509f6529e419e482a
Kevin Wang <kevin@eventualcomputing.com>
2025-03-20 18:19:51 -0700

    use col in some tests

--- tests/expressions/test_getitem.py
+++ tests/expressions/test_getitem.py
@@ -1,15 +1,16 @@
 import daft
+from daft import col
 
 
 def test_getitem_list():
     df = daft.from_pydict({"a": [[1, 2, 3], [1], [1, 2]]})
-    df = df.select(df["a"][0].alias("first"), df["a"][1].alias("second"))
+    df = df.select(df["a"][0].alias("first"), col("a")[1].alias("second"))
     assert df.to_pydict() == {"first": [1, 1, 1], "second": [2, None, 2]}
 
 
 def test_getitem_struct():
     df = daft.from_pydict({"a": [{"x": 1, "y": "foo"}, {"x": 2, "y": "bar"}]})
-    df = df.select(df["a"]["x"], df["a"]["y"])
+    df = df.select(col("a")["x"], df["a"]["y"])
     assert df.to_pydict() == {"x": [1, 2], "y": ["foo", "bar"]}
 
 
