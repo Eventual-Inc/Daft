dc1a5d30bffce85bd10cdffd2dce19dc8186dbcd
universalmind303 <cory.grinstead@gmail.com>
2025-03-26 10:35:00 -0500

    wip

--- tests/connect/test_io.py
+++ tests/connect/test_io.py
@@ -6,6 +6,9 @@ import pytest
 
 import daft
 
+# ------
+# CSV
+# ------
 
 def test_csv_basic_roundtrip(make_spark_df, assert_spark_equals, spark_session, tmp_path):
     df = make_spark_df({"id": [1, 2, 3]})
@@ -14,6 +17,19 @@ def test_csv_basic_roundtrip(make_spark_df, assert_spark_equals, spark_session, 
 
     spark_df_read = spark_session.read.option("header", True).csv(csv_dir)
     df_read = daft.read_csv(csv_dir)
+    assert_spark_equals(df_read, spark_df_read)
+
+def test_csv_overwrite(make_spark_df, assert_spark_equals, spark_session, tmp_path):
+    df = make_spark_df({"id": [1, 2, 3]})
+    csv_dir = os.path.join(tmp_path, "csv")
+    df.write.csv(csv_dir)
+
+    df = make_spark_df({"id": [4, 5, 6]})
+    df.write.csv(csv_dir, mode="overwrite")
+
+    spark_df_read = spark_session.read.option("header", True).csv(csv_dir)
+    df_read = daft.read_csv(csv_dir)
+    assert spark_df_read.count() == 3
     assert_spark_equals(df_read, spark_df_read)
 
 
@@ -42,6 +58,9 @@ def test_write_csv_with_null_value(spark_session, tmp_path):
 def test_write_csv_with_compression(spark_session, tmp_path):
     pass
 
+# -------
+# Parquet
+# -------
 
 @pytest.mark.skip(reason="TODO: investigate why this occasionally fails in CI")
 def test_write_parquet(spark_session, tmp_path):
@@ -58,17 +77,17 @@ def test_write_parquet(spark_session, tmp_path):
 @pytest.mark.skip(reason="TODO: investigate why this occasionally fails in CI")
 def test_read_parquet(spark_session, make_spark_df, assert_spark_equals, tmp_path):
     df = daft.from_pydict({"id": [1, 2, 3]})
-    delta_dir = os.path.join(tmp_path, "test.delta")
-    df.write_parquet(delta_dir)
+    parquet_dir = os.path.join(tmp_path, "test.parquet")
+    df.write_parquet(parquet_dir)
 
-    df_read = spark_session.read.format("parquet").load(delta_dir)
+    df_read = spark_session.read.format("parquet").load(parquet_dir)
     assert_spark_equals(df, df_read)
 
 
 def test_unknown_options(spark_session, make_spark_df, assert_spark_equals, tmp_path):
-    delta_dir = os.path.join(tmp_path, "test.delta")
+    parquet_dir = os.path.join(tmp_path, "test.parquet")
 
     try:
-        spark_session.read.option("something", "idk").format("parquet").load(delta_dir).collect()
+        spark_session.read.option("something", "idk").format("parquet").load(parquet_dir).collect()
     except Exception as e:
         assert "something" in str(e)
