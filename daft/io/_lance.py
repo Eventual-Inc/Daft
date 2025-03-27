3da9a651ab7bd8defc4ab04237ef47b4de53d939
R. C. Howell <5731503+rchowell@users.noreply.github.com>
2025-03-27 09:12:48 -0700

    Update daft/io/_lance.py

--- daft/io/_lance.py
+++ daft/io/_lance.py
@@ -35,7 +35,7 @@ def read_lance(url: str, io_config: Optional["IOConfig"] = None) -> DataFrame:
 
         To ensure that this is installed with Daft, you may install: ``pip install daft[lance]``
 
-    Examples
+    Examples:
     --------
     Read a local LanceDB table:
         >>> df = daft.read_lance("s3://my-lancedb-bucket/data/")
