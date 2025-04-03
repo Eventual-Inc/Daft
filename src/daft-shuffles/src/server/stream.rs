202adb68f5d436f5c97e101bd513ab98899553c9
Colin Ho <colin.ho99@gmail.com>
2025-04-02 22:14:05 -0700

    feat: Implement flight server in rust (#4004)
    ## Summary
    
    Moves the flight server for the flight shuffle from python to rust.
    
    Previously we used PyArrow's flight implementation, but for better
    control / performance we can implement our own in rust.
    
    One notable optimization we can do here is instead of reading shuffle
    files into arrow arrays before sending them over rpc, we can read and
    send the raw bytes from the files because they are already serialized
    into arrow ipc stream format.
    
    ### Results
    
    Question | Before (s) | After (s) | Difference (s) | % Change
    -- | -- | -- | -- | --
    1 | 31.58 | 30.30 | -1.28 | -4.05%
    2 | 43.39 | 38.85 | -4.54 | -10.46%
    3 | 35.78 | 32.75 | -3.03 | -8.47%
    4 | 23.63 | 22.45 | -1.18 | -4.99%
    5 | 76.53 | 76.17 | -0.36 | -0.47%
    6 | 10.86 | 10.47 | -0.39 | -3.59%
    7 | 43.65 | 42.78 | -0.87 | -1.99%
    8 | 80.40 | 78.61 | -1.79 | -2.23%
    9 | 115.04 | 111.32 | -3.72 | -3.23%
    10 | 40.85 | 43.92 | 3.07 | 7.52%
    11 | 27.88 | 22.91 | -4.97 | -17.83%
    12 | 24.63 | 25.28 | 0.65 | 2.64%
    13 | 33.76 | 31.59 | -2.17 | -6.43%
    14 | 17.18 | 17.70 | 0.52 | 3.03%
    15 | 38.31 | 39.52 | 1.21 | 3.16%
    16 | 15.38 | 17.39 | 2.01 | 13.07%
    17 | 113.43 | 112.06 | -1.37 | -1.21%
    18 | 62.07 | 61.14 | -0.93 | -1.50%
    19 | 22.01 | 21.88 | -0.13 | -0.59%
    20 | 51.93 | 51.27 | -0.66 | -1.27%
    21 | 134.14 | 135.87 | 1.73 | 1.29%
    22 | 12.98 | 12.37 | -0.61 | -4.70%
    Total | 1055.41 | 1036.60 | -18.81 | -1.78%
    
    No noticeable changes across the board. The performance gains will
    likely come from implementing in-memory cache and optimizing the reduce
    side.
    
    ## Related Issues
    
    None
    
    ## Changes Made
    
    Implement a rust based flight server
    
    ## Checklist
    
    - [x] All tests have passed
    - [ ] Documented in API Docs
    - [ ] Documented in User Guide
    - [ ] If adding a new documentation page, doc is added to
    `docs/mkdocs.yml` navigation
    - [ ] Documentation builds and is formatted properly (tag
    [@ccmao1130](https://github.com/ccmao1130) for docs review)

---
+++ src/daft-shuffles/src/server/stream.rs
@@ -0,0 +1,147 @@
+use std::io::Read;
+
+use arrow2::io::ipc::read::OutOfSpecKind;
+use arrow_flight::FlightData;
+use common_error::{DaftError, DaftResult};
+
+/// Reading state maintenance
+struct ReadState<R> {
+    // The reader to read from
+    reader: R,
+    // The buffer to store the data
+    data_buffer: Vec<u8>,
+    // The buffer to store the message
+    message_buffer: Vec<u8>,
+}
+
+/// State machine for stream processing
+enum StreamState<R> {
+    // A tuple of the current read state and the flight data to be sent
+    Ready((ReadState<R>, FlightData)),
+    // The read state is not done, continue reading
+    Continue(ReadState<R>),
+    // The read state is done, no more data to read
+    Done,
+}
+
+const CONTINUATION_MARKER: [u8; 4] = [0xff; 4];
+
+/// A reader that reads arrow ipc files in stream format and converts them to FlightData for serving
+/// over flight. This is an optimization where we skip converting the ipc files to RecordBatches
+/// and instead read the data directly into FlightData, since we already know that the data is in
+/// arrow ipc stream format.
+pub struct FlightDataStreamReader<R: Read> {
+    state: Option<ReadState<R>>,
+}
+
+impl<R: Read> FlightDataStreamReader<R> {
+    pub fn try_new(mut reader: R) -> DaftResult<Self> {
+        // Skip stream metadata in the file since we don't need it when sending data over flight
+        skip_stream_metadata(&mut reader)?;
+        Ok(Self {
+            state: Some(ReadState {
+                reader,
+                data_buffer: Vec::new(),
+                message_buffer: Vec::new(),
+            }),
+        })
+    }
+}
+
+impl<R: Read> Iterator for FlightDataStreamReader<R> {
+    type Item = DaftResult<FlightData>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let state = self.state.take()?;
+
+        match process_next(state) {
+            Ok(StreamState::Ready((state, data))) => {
+                self.state = Some(state);
+                Some(Ok(data))
+            }
+            Ok(StreamState::Continue(state)) => {
+                self.state = Some(state);
+                self.next() // Recursive call to continue processing
+            }
+            Ok(StreamState::Done) => None,
+            Err(e) => {
+                self.state = None;
+                Some(Err(e))
+            }
+        }
+    }
+}
+
+/// Skip stream metadata on reader. We don't need it when sending data over flight.
+pub fn skip_stream_metadata<R: Read>(reader: &mut R) -> DaftResult<()> {
+    let mut meta_buf = [0u8; 4];
+    reader.read_exact(&mut meta_buf)?;
+
+    if meta_buf == CONTINUATION_MARKER {
+        reader.read_exact(&mut meta_buf)?;
+    }
+    let meta_len = i32::from_le_bytes(meta_buf);
+
+    let meta_len = meta_len
+        .try_into()
+        .map_err(|_| arrow2::error::Error::from(OutOfSpecKind::NegativeFooterLength))?;
+
+    let mut meta_buffer = vec![0u8; meta_len];
+    reader.read_exact(&mut meta_buffer)?;
+
+    Ok(())
+}
+
+/// Process next IPC message into FlightData
+fn process_next<R: Read>(mut state: ReadState<R>) -> DaftResult<StreamState<R>> {
+    let mut meta_buf = [0u8; 4];
+
+    match state.reader.read_exact(&mut meta_buf) {
+        Ok(()) => {}
+        Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
+            return Ok(StreamState::Continue(state))
+        }
+        Err(e) => return Err(DaftError::from(e)),
+    }
+
+    if meta_buf == CONTINUATION_MARKER {
+        state.reader.read_exact(&mut meta_buf)?;
+    }
+    let meta_length = i32::from_le_bytes(meta_buf);
+
+    let meta_length: usize = meta_length
+        .try_into()
+        .map_err(|_| arrow2::error::Error::from(OutOfSpecKind::NegativeFooterLength))?;
+
+    if meta_length == 0 {
+        return Ok(StreamState::Done);
+    }
+
+    // Read message header
+    state.message_buffer.resize(meta_length, 0);
+    state.reader.read_exact(&mut state.message_buffer)?;
+
+    // Read message body length
+    let message = arrow_ipc::root_as_message(&state.message_buffer)
+        .map_err(|e| DaftError::InternalError(format!("Invalid flatbuffer message: {e}")))?;
+
+    let body_length: usize = message
+        .bodyLength()
+        .try_into()
+        .map_err(|_| DaftError::InternalError("Unexpected negative integer".to_string()))?;
+
+    // Read message body
+    state.data_buffer.resize(body_length, 0);
+    state.reader.read_exact(&mut state.data_buffer)?;
+
+    let message_buffer = std::mem::take(&mut state.message_buffer);
+    let data_buffer = std::mem::take(&mut state.data_buffer);
+
+    let flight_data = FlightData {
+        data_header: message_buffer.into(),
+        data_body: data_buffer.into(),
+        ..Default::default()
+    };
+
+    Ok(StreamState::Ready((state, flight_data)))
+}
