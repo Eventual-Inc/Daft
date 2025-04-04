a9580ccce780892a5696ba621017da54cf221942
R. Conner Howell <robert@eventualcomputing.com>
2025-04-03 17:12:05 -0700

    feat: adds case-insensitive identifiers [1/3]

--- src/daft-catalog/src/bindings.rs
+++ src/daft-catalog/src/bindings.rs
@@ -1,46 +1,87 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-/// Bindings
-///
-/// Notes:
-///  - Currently using Clone because all references are arcs.
-///  - This is intentionally lightweight and everything is exact-case.
-///  - All APIs are non-fallible because callers determine what is an error.
-///  - It does not necessarily have map semantics.
-///  - Callers are responsible for case-normalization hence String, &str.
-///  - Intentionally using String and &str rather than Into and AsRef.
+/// Bindings are stored case-normalized with the case-preserved bindings.
 #[derive(Debug)]
-pub struct Bindings<T>(HashMap<String, T>);
+pub struct Bindings<T> {
+    /// case-preserved bindings (default)
+    bindings: HashMap<String, T>,
+    /// case-normalized binding aliases for insensitive lookups.
+    aliases: HashMap<String, HashSet<String>>,
+}
 
+/// Bind mode is for lvalue handling which is currently out of scope.
+#[derive(Debug, Clone, Copy)]
+pub enum BindMode {
+    Preserve,
+    Normalize,
+}
+
+/// Find mode is for rvalue handling.
+#[derive(Debug, Clone, Copy)]
+pub enum FindMode {
+    Insensitive,
+    Sensitive,
+}
+
+/// Bindings implements "bind" and "find" for handling various identifier modes.
 impl<T> Bindings<T> {
-    /// Creates an empty catalog provider.
+    /// Creates an empty bindings object.
     pub fn empty() -> Self {
-        Self(HashMap::new())
+        Self {
+            bindings: HashMap::new(),
+            aliases: HashMap::new(),
+        }
     }
 
-    /// Inserts a new binding with ownership.
-    pub fn insert(&mut self, name: String, object: T) {
-        self.0.insert(name, object);
+    /// Bind an object to the name (lvalue), overwriting any existing binding.
+    pub fn bind(&mut self, name: String, object: T) {
+        self.bindings.insert(name.clone(), object);
+        self.aliases.entry(alias(&name)).or_default().insert(name);
     }
 
-    /// Removes the binding if it exists.
+    /// Find all object by name (rvalue).
+    pub fn find(&self, name: &str, mode: FindMode) -> Vec<&T> {
+        match mode {
+            FindMode::Insensitive => self.find_aliased(name),
+            FindMode::Sensitive => self.find_binding(name),
+        }
+    }
+    /// Finds this binding by exact case.
+    #[inline]
+    fn find_binding(&self, name: &str) -> Vec<&T> {
+        self.bindings
+            .get(name)
+            .map(|binding| vec![binding])
+            .unwrap_or_default()
+    }
+
+    /// Finds all bindings mapped to this alias.
+    #[inline]
+    fn find_aliased(&self, name: &str) -> Vec<&T> {
+        self.aliases
+            .get(&alias(name))
+            .map(|names| {
+                names
+                    .iter()
+                    .filter_map(|name| self.bindings.get(name))
+                    .collect()
+            })
+            .unwrap_or_default()
+    }
+
+    /// Removes the binding if it exists (exact-case).
     pub fn remove(&mut self, name: &str) {
-        self.0.remove(name);
+        self.bindings.remove(name);
     }
 
-    /// Returns true if the binding exists.
-    pub fn exists(&self, name: &str) -> bool {
-        self.0.contains_key(name)
-    }
-
-    /// Get an object reference by name.
-    pub fn get(&self, name: &str) -> Option<&T> {
-        self.0.get(name)
+    /// Returns true if the binding exists (exact-case).
+    pub fn contains(&self, name: &str) -> bool {
+        self.bindings.contains_key(name)
     }
 
     /// List all objects matching the pattern.
     pub fn list(&self, pattern: Option<&str>) -> Vec<String> {
-        self.0
+        self.bindings
             .keys()
             .map(|k| k.as_str())
             .filter(|k| pattern.is_none() || k.contains(pattern.unwrap_or("")))
@@ -50,6 +91,11 @@ impl<T> Bindings<T> {
 
     /// Returns true iff there are no bindings.
     pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
+        self.bindings.is_empty()
     }
+}
+
+/// Using the unicode lowercase to normalize
+fn alias(name: &str) -> String {
+    name.to_lowercase()
 }
