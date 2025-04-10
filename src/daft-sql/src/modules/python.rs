c9f81e9403c8de79ac475e7f9a3376b9ccc8fdc0
universalmind303 <cory.grinstead@gmail.com>
2025-04-10 15:09:40 -0500

    make non exxpr bound args work properly

--- src/daft-sql/src/modules/python.rs
+++ src/daft-sql/src/modules/python.rs
@@ -1,11 +1,13 @@
 use common_error::DaftResult;
-use daft_dsl::functions::python::WrappedUDFClass;
+use daft_dsl::{functions::python::WrappedUDFClass, Expr};
 #[cfg(feature = "python")]
 use {
+    common_error::DaftError,
     daft_dsl::python::PyExpr,
     pyo3::{
         prelude::*,
         types::{PyDict, PyTuple},
+        IntoPyObjectExt,
     },
 };
 
@@ -23,6 +25,37 @@ impl SQLModule for SQLModulePython {
     }
 }
 
+#[cfg(feature = "python")]
+fn lit_to_py_any(py: Python, lit: &daft_dsl::LiteralValue) -> PyResult<PyObject> {
+    match lit {
+        daft_dsl::LiteralValue::Null => Ok(py.None()),
+        daft_dsl::LiteralValue::Boolean(b) => b.into_py_any(py),
+        daft_dsl::LiteralValue::Utf8(s) => s.into_py_any(py),
+        daft_dsl::LiteralValue::Binary(items) => items.into_py_any(py),
+        daft_dsl::LiteralValue::Int8(i) => i.into_py_any(py),
+        daft_dsl::LiteralValue::UInt8(u) => u.into_py_any(py),
+        daft_dsl::LiteralValue::Int16(u) => u.into_py_any(py),
+        daft_dsl::LiteralValue::UInt16(u) => u.into_py_any(py),
+        daft_dsl::LiteralValue::Int32(u) => u.into_py_any(py),
+        daft_dsl::LiteralValue::UInt32(u) => u.into_py_any(py),
+        daft_dsl::LiteralValue::Int64(u) => u.into_py_any(py),
+        daft_dsl::LiteralValue::UInt64(u) => u.into_py_any(py),
+        daft_dsl::LiteralValue::Timestamp(..) => todo!(),
+        daft_dsl::LiteralValue::Date(_) => todo!(),
+        daft_dsl::LiteralValue::Time(..) => todo!(),
+        daft_dsl::LiteralValue::Duration(..) => todo!(),
+        daft_dsl::LiteralValue::Interval(..) => todo!(),
+        daft_dsl::LiteralValue::Float64(..) => todo!(),
+        daft_dsl::LiteralValue::Decimal(..) => todo!(),
+        daft_dsl::LiteralValue::Series(series) => daft_core::python::PySeries {
+            series: series.clone(),
+        }
+        .into_py_any(py),
+        daft_dsl::LiteralValue::Python(_) => todo!(),
+        daft_dsl::LiteralValue::Struct(_) => todo!(),
+    }
+}
+
 impl SQLFunction for WrappedUDFClass {
     fn to_expr(
         &self,
@@ -37,17 +70,21 @@ impl SQLFunction for WrappedUDFClass {
 
                 for input in inputs {
                     match input {
+                        // all named (kwargs are parsed as python literals instead of exprs)
                         sqlparser::ast::FunctionArg::Named {
                             name,
                             arg,
                             operator: _,
                         } => {
                             let expr = planner.try_unwrap_function_arg_expr(arg)?;
-
-                            let py_expr = PyExpr { expr };
-                            let pyobj = py_expr.into_expr_cls(py)?;
-
-                            kwargs.set_item(name.to_string(), pyobj)?;
+                            if let Expr::Literal(lit) = expr.as_ref() {
+                                let pyany = lit_to_py_any(py, lit)?;
+                                kwargs.set_item(name.to_string(), pyany)?;
+                            } else {
+                                return Err(DaftError::InternalError(
+                                    "expected a literal, found an expression".to_string(),
+                                ));
+                            }
                         }
                         sqlparser::ast::FunctionArg::Unnamed(arg) => {
                             let expr = planner.try_unwrap_function_arg_expr(arg)?;
