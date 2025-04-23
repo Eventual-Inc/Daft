8a8410fe210e7b05afdf539e726314dde51f458e
universalmind303 <cory.grinstead@gmail.com>
2025-04-23 15:39:09 -0500

    remove bad test case

--- tests/recordbatch/utf8/test_to_datetime.py
+++ tests/recordbatch/utf8/test_to_datetime.py
@@ -14,11 +14,3 @@ def test_utf8_to_datetime():
             datetime.datetime(2021, 1, 2, 0, 0),
         ]
     }
-
-
-def test_to_datetime_with_tz_offset():
-    table = MicroPartition.from_pydict({"a": ["2020-01-01T00:00:00+01:00"]})
-    result = table.eval_expression_list([col("a").str.to_datetime("%Y-%m-%dT%H:%M:%S%z").dt.day_of_year()])
-    expected = {{"a": [365]}}
-
-    assert result.to_pydict() == expected
