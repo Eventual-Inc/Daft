dad5f3f079b9b9fb25034e885ecc2866f7bab6eb
Kevin Wang <kevin@eventualcomputing.com>
2025-05-01 11:18:25 -0700

    some fixes

--- src/daft-dashboard/build.rs
+++ src/daft-dashboard/build.rs
@@ -7,9 +7,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let frontend_dir = std::env::var("CARGO_MANIFEST_DIR")? + "/frontend/out";
 
     if !std::path::Path::new(&frontend_dir).is_dir() {
-        println!("Dashboard assets not found in {frontend_dir}, skipping dashboard build.");
-        println!("To build dashboard assets: `bun run build` in src/daft-dashboard/frontend.");
-        return Ok(());
+        if cfg!(debug_assertions) {
+            println!("Dashboard assets not found in {frontend_dir}, skipping dashboard build.");
+            println!("To build dashboard assets: `bun run build` in src/daft-dashboard/frontend.");
+            return Ok(());
+        } else {
+            panic!("Dashboard assets are required for release builds");
+        }
     }
 
     // if there's anything in the output directory, remove it
