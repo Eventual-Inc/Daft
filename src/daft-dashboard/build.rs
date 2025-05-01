1d5059bd460dda40289404548ed38e5c314dcaab
Kev Wang <kevin@eventualcomputing.com>
2025-05-01 13:36:29 -0700

    ci: compile release build with glibc 2.24 (#4272)
    ## Changes Made
    
    Downgrades our minimum supported glibc version from 2.28 to 2.24 by
    changing the manylinux Docker image. Also move bun build out of the rust
    build
    
    ## Related Issues
    
    Requested by Amazon since they use Amazon Linux 2, which has glibc 2.26
    
    ## Checklist
    
    - [x] Documented in API Docs (if applicable)
    - [x] Documented in User Guide (if applicable)
    - [x] If adding a new documentation page, doc is added to
    `docs/mkdocs.yml` navigation
    - [x] Documentation builds and is formatted properly (tag @/ccmao1130
    for docs review)

--- src/daft-dashboard/build.rs
+++ src/daft-dashboard/build.rs
@@ -1,62 +1,27 @@
-use std::process::Command;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=frontend/src/");
-    println!("cargo:rerun-if-changed=frontend/bun.lockb");
     let out_dir = std::env::var("OUT_DIR")?;
 
     // always set the env var so that the include_dir! macro doesn't panic
     println!("cargo:rustc-env=DASHBOARD_ASSETS_DIR={}", out_dir);
 
-    // Check if bun is installed
-    let bun_available = Command::new("bun")
-        .arg("--version")
-        .output()
-        .map(|_| true)
-        .unwrap_or(false);
+    let frontend_dir = std::env::var("CARGO_MANIFEST_DIR")? + "/frontend/out";
 
-    // if bun is not available, we can't build the frontend assets
-    // so we just print a warning and return
-    // but if we're in release mode, we panic
-    if !bun_available {
+    if !std::path::Path::new(&frontend_dir).is_dir() {
         if cfg!(debug_assertions) {
-            println!("cargo:warning=Bun not found, skipping dashboard frontend assets");
+            println!("Dashboard assets not found in {frontend_dir}, skipping dashboard build.");
+            println!("To build dashboard assets: `bun run build` in src/daft-dashboard/frontend.");
             return Ok(());
         } else {
-            panic!("Bun is required for release builds");
+            panic!("Dashboard assets are required for release builds");
         }
     }
 
-    // Install dependencies
-    let install_status = Command::new("bun")
-        .current_dir("./frontend")
-        .args(["install"])
-        .status()?;
-
-    assert!(install_status.success(), "Failed to install dependencies");
-
-    // Run `bun run build`
-    let mut cmd = Command::new("bun");
-    let status = cmd.current_dir("./frontend");
-
-    let status = if cfg!(debug_assertions) {
-        status.args(["run", "build", "--no-lint", "--no-mangling"])
-    } else {
-        status.args(["run", "build"])
-    };
-    let status = status.status()?;
-
-    assert!(status.success(), "Failed to build frontend assets");
-
-    let frontend_dir = std::env::var("CARGO_MANIFEST_DIR")? + "/frontend/out";
-
     // if there's anything in the output directory, remove it
-    if std::fs::metadata(&out_dir).is_ok() {
+    if std::fs::exists(&out_dir)? {
         std::fs::remove_dir_all(&out_dir)?;
     }
 
     // move the frontend assets to the output directory
     std::fs::rename(frontend_dir, out_dir)?;
-    assert!(status.success(), "Failed to build frontend assets");
     Ok(())
 }
