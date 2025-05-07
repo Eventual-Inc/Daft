3911d489e4f3b45493824612194b14bd32133096
desmondcheongzx <desmondcheongzx@gmail.com>
2025-05-07 16:46:25 -0700

    Refactor into spawners and dispatchers

--- src/daft-writers/src/native_arrow_writer.rs
+++ src/daft-writers/src/native_arrow_writer.rs
@@ -7,14 +7,14 @@ use std::{
 use async_trait::async_trait;
 use common_error::{DaftError, DaftResult};
 use common_file_formats::FileFormat;
-use common_runtime::{get_compute_runtime, Runtime};
+use common_runtime::{get_compute_runtime, Runtime, RuntimeTask};
 use daft_core::prelude::*;
 use daft_io::{parse_url, SourceType};
 use daft_micropartition::MicroPartition;
 use daft_recordbatch::RecordBatch;
 use parquet::{
     arrow::{
-        arrow_writer::{compute_leaves, get_column_writers, ArrowLeafColumn},
+        arrow_writer::{compute_leaves, get_column_writers, ArrowColumnChunk, ArrowLeafColumn},
         ArrowSchemaConverter,
     },
     basic::Compression,
@@ -24,6 +24,7 @@ use parquet::{
     },
     schema::types::SchemaDescriptor,
 };
+use tokio::sync::mpsc::Sender;
 
 use crate::AsyncFileWriter;
 
@@ -31,9 +32,12 @@ use crate::AsyncFileWriter;
 /// Default buffer size for writing to files.
 const DEFAULT_WRITE_BUFFER_SIZE: usize = 4 * 1024 * 1024;
 
+type ColumnWriterHandle = RuntimeTask<DaftResult<ArrowColumnChunk>>;
+
 pub(crate) struct NativeArrowWriter {}
 
 impl NativeArrowWriter {
+    /// Helper function that checks if we support native writes given the file format, root directory, and schema.
     pub(crate) fn native_supported(
         file_format: FileFormat,
         root_dir: &str,
@@ -43,7 +47,7 @@ impl NativeArrowWriter {
         if !matches!(file_format, FileFormat::Parquet) {
             return false;
         }
-        // TODO(desmond): Support remote writes.
+        // TODO(desmond): Currently we only support local writes.
         let (source_type, _) = match parse_url(root_dir) {
             Ok(result) => result,
             Err(_) => return false,
@@ -51,6 +55,7 @@ impl NativeArrowWriter {
         if !matches!(source_type, SourceType::File) {
             return false;
         }
+        // TODO(desmond): Currently we do not extension and timestamp types.
         let writer_properties = Arc::new(
             WriterProperties::builder()
                 .set_writer_version(WriterVersion::PARQUET_1_0)
@@ -61,7 +66,6 @@ impl NativeArrowWriter {
             Ok(schema) => {
                 for field in &schema.fields {
                     if field.data_type().has_non_arrow_rs_convertible_type() {
-                        // TODO(desmond): Support extension and timestamp types.
                         return false;
                     }
                 }
@@ -83,6 +87,7 @@ impl NativeArrowWriter {
     ) -> DaftResult<
         Box<dyn AsyncFileWriter<Input = Arc<MicroPartition>, Result = Option<RecordBatch>>>,
     > {
+        // Parse the root directory and add partition values if present.
         let (source_type, root_dir) = parse_url(root_dir)?;
         debug_assert!(
             matches!(source_type, SourceType::File),
@@ -97,7 +102,9 @@ impl NativeArrowWriter {
         };
         // Create the directories if they don't exist.
         std::fs::create_dir_all(&dir)?;
+
         let filename = dir.join(format!("{}-{}.parquet", uuid::Uuid::new_v4(), file_idx));
+
         // TODO(desmond): Explore configurations such data page size limit, writer version, etc. Parquet format v2
         // could be interesting but has much less support in the ecosystem (including ourselves).
         let writer_properties = Arc::new(
@@ -166,6 +173,66 @@ impl ArrowParquetWriter {
         self.compute_runtime = Some(get_compute_runtime());
         Ok(())
     }
+
+    /// Helper function that spawns 1 worker thread per leaf column, returning the handles to the
+    /// workers and senders to give each worker arrow leaf columns to write.
+    fn spawn_column_writer_workers(
+        &self,
+        channel_size: usize,
+    ) -> DaftResult<(Vec<ColumnWriterHandle>, Vec<Sender<ArrowLeafColumn>>)> {
+        let column_writers = get_column_writers(
+            &self.parquet_schema,
+            &self.writer_properties,
+            &self.arrow_schema,
+        )
+        .map_err(|e| DaftError::ParquetError(e.to_string()))?;
+        let compute_runtime = self
+            .compute_runtime
+            .as_ref()
+            .expect("Compute runtime should be created by now");
+        let (handles, senders): (Vec<_>, Vec<_>) = column_writers
+            .into_iter()
+            .map(|mut writer| {
+                let (send, mut recv) = tokio::sync::mpsc::channel(channel_size);
+                let handle = compute_runtime.spawn(async move {
+                    while let Some(col) = recv.recv().await {
+                        writer
+                            .write(&col)
+                            .map_err(|e| DaftError::ParquetError(e.to_string()))?;
+                    }
+                    writer
+                        .close()
+                        .map_err(|e| DaftError::ParquetError(e.to_string()))
+                });
+                (handle, send)
+            })
+            .unzip();
+        Ok((handles, senders))
+    }
+
+    /// Helper function that dispatches record batches to the worker threads.
+    async fn dispatch_record_batches_to_workers(
+        &self,
+        record_batches: &[RecordBatch],
+        mut senders: Vec<Sender<ArrowLeafColumn>>,
+    ) -> DaftResult<()> {
+        for recordbatch in record_batches {
+            let arrays = recordbatch.get_inner_arrow_arrays();
+            for ((arr, field), sender) in arrays
+                .zip(&self.arrow_schema.fields)
+                .zip(senders.iter_mut())
+            {
+                let leaves = compute_leaves(field, &arr.into())
+                    .map_err(|e| DaftError::ParquetError(e.to_string()))?;
+                for leaf in leaves {
+                    sender.send(leaf).await.map_err(|_| {
+                        DaftError::ComputeError("Failed to send column to worker".to_string())
+                    })?;
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 #[async_trait]
@@ -177,82 +244,37 @@ impl AsyncFileWriter for ArrowParquetWriter {
         if self.file_writer.is_none() {
             self.create_writer()?;
         }
-        let compute_runtime = self
-            .compute_runtime
-            .as_ref()
-            .expect("Compute runtime should be created by now");
         let starting_bytes_written = self.bytes_written();
         let record_batches = data.get_tables()?;
-        let column_writers = get_column_writers(
-            &self.parquet_schema,
-            &self.writer_properties,
-            &self.arrow_schema,
-        )
-        .map_err(|e| DaftError::ParquetError(e.to_string()))?;
-        let mut column_writer_worker_threads: Vec<_> = column_writers
-            .into_iter()
-            .map(|mut col_writer| {
-                let (send, mut recv) =
-                    tokio::sync::mpsc::channel::<ArrowLeafColumn>(record_batches.len());
-                let handle = compute_runtime.spawn(async move {
-                    while let Some(col) = recv.recv().await {
-                        col_writer.write(&col)?;
-                    }
-                    // Once we have processed all the input, close the writer to return the newly created ArrowColumnChunk.
-                    col_writer.close()
-                });
-                (handle, send)
-            })
-            .collect();
 
-        // Send the record batches to the column writer worker threads, wait for them to complete encoding,
-        // then flush their results as a new row group.
-        for record_batch in record_batches.iter() {
-            let mut worker_iter = column_writer_worker_threads.iter_mut();
-            let arrays = record_batch.get_inner_arrow_arrays();
-            for (arr, field) in arrays.zip(&self.arrow_schema.fields) {
-                for leaves in compute_leaves(field, &arr.into())
-                    .map_err(|e| DaftError::ParquetError(e.to_string()))?
-                {
-                    let _ = worker_iter
-                        .next()
-                        .expect("Worker iterator should still be valid")
-                        .1
-                        .send(leaves)
-                        .await;
-                }
-            }
-        }
+        // Initialize column writers and channels.
+        let (column_writer_handles, column_writer_senders) =
+            self.spawn_column_writer_workers(record_batches.len())?;
+
+        // Send the record batches to the column writer worker threads.
+        self.dispatch_record_batches_to_workers(&record_batches, column_writer_senders)
+            .await?;
+
         // Wait for the workers to complete encoding, and append the resulting column chunks to the row group and the file.
-        let handles: Vec<_> = column_writer_worker_threads
-            .into_iter()
-            .map(|(handle, send)| {
-                // Drop send side to signal termination.
-                drop(send);
-                handle
-            })
-            .collect();
         let mut row_group_writer = self
             .file_writer
             .as_mut()
             .expect("File writer should be created by now")
             .next_row_group()
             .map_err(|e| DaftError::ParquetError(e.to_string()))?;
-        for handle in handles {
-            let chunk = handle
-                .await?
-                .map_err(|e| DaftError::ParquetError(e.to_string()))?;
+        for handle in column_writer_handles {
+            let chunk = handle.await??;
             chunk
                 .append_to_row_group(&mut row_group_writer)
                 .map_err(|e| DaftError::ParquetError(e.to_string()))?;
         }
-        // Close the row group which writes to the underlying file.
+
+        // Close the current row group.
         row_group_writer
             .close()
             .map_err(|e| DaftError::ParquetError(e.to_string()))?;
 
-        let bytes_written = self.bytes_written() - starting_bytes_written;
-        Ok(bytes_written)
+        Ok(self.bytes_written() - starting_bytes_written)
     }
 
     async fn close(&mut self) -> DaftResult<Self::Result> {
@@ -263,7 +285,7 @@ impl AsyncFileWriter for ArrowParquetWriter {
             .expect("File writer should be created by now")
             .finish()
             .map_err(|e| DaftError::ParquetError(e.to_string()))?;
-
+        // Return a recordbatch containing the filename that we wrote to.
         let field = Field::new("path", DataType::Utf8);
         let filename_series = Series::from_arrow(
             Arc::new(field.clone()),
