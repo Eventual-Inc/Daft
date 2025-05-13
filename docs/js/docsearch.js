64fb80994f4f8d8b6e5b0176cbadfb28e39f00b8
ChanChan Mao <chanchanmao1130@gmail.com>
2025-05-12 18:39:37 -0700

    clean up comments

--- docs/js/docsearch.js
+++ docs/js/docsearch.js
@@ -1,16 +1,3 @@
-// import docsearch from '@docsearch/js';
-
-// import '@docsearch/css';
-
-// docsearch({
-//     container: '#md-search__inner',
-//     appId: 'CTX8NKR4GC',
-//     apiKey: 'eb05e80b7739efec7038d98158f39a54',
-//     indexName: 'getdaft',
-//     debug: false,
-//     insights: true,
-//   });
-
 document.addEventListener('DOMContentLoaded', function () {
     docsearch({
       container: '#docsearch',
