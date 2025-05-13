7c995a230b1b7c9f0bc6960655417937860e5212
ChanChan Mao <chanchanmao1130@gmail.com>
2025-05-12 18:07:08 -0700

    added algolia + styling

--- docs/js/docsearch.js
+++ docs/js/docsearch.js
@@ -1,11 +1,23 @@
-import docsearch from '@docsearch/js';
+// import docsearch from '@docsearch/js';
 
-import '@docsearch/css';
+// import '@docsearch/css';
 
-docsearch({
-    container: '#md-search__input',
-    appId: 'CTX8NKR4GC',
-    apiKey: 'eb05e80b7739efec7038d98158f39a54',
-    indexName: 'getdaft',
-    debug: false
+// docsearch({
+//     container: '#md-search__inner',
+//     appId: 'CTX8NKR4GC',
+//     apiKey: 'eb05e80b7739efec7038d98158f39a54',
+//     indexName: 'getdaft',
+//     debug: false,
+//     insights: true,
+//   });
+
+document.addEventListener('DOMContentLoaded', function () {
+    docsearch({
+      container: '#docsearch',
+      appId: 'CTX8NKR4GC',
+      apiKey: 'eb05e80b7739efec7038d98158f39a54',
+      indexName: 'getdaft',
+      debug: false,
+      insights: true,
+    });
   });
