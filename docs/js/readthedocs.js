2f37bac16e8ec492338577d9caacf54c9f6685cd
ChanChan Mao <chanchanmao1130@gmail.com>
2025-05-16 18:08:25 -0700

    trying adding readthedocs to nav bar 2

---
+++ docs/js/readthedocs.js
@@ -0,0 +1,24 @@
+// Use CustomEvent to generate the version selector
+document.addEventListener(
+        "readthedocs-addons-data-ready",
+        function (event) {
+          const config = event.detail.data();
+          const versioning = `
+<div class="md-version">
+  <button class="md-version__current" aria-label="Select version">
+    ${config.versions.current.slug}
+  </button>
+
+  <ul class="md-version__list">
+  ${ config.versions.active.map(
+    (version) => `
+    <li class="md-version__item">
+      <a href="${ version.urls.documentation }" class="md-version__link">
+        ${ version.slug }
+      </a>
+            </li>`).join("\n")}
+  </ul>
+</div>`;
+
+          document.querySelector(".md-header__topic").insertAdjacentHTML("beforeend", versioning);
+ });
