635aaa2bc3ff34415db825bc5fae5f8d144f35c0
ChanChan Mao <chanchanmao1130@gmail.com>
2025-05-16 18:36:49 -0700

    trying adding readthedocs to nav bar 3

--- docs/js/readthedocs.js
+++ docs/js/readthedocs.js
@@ -4,21 +4,21 @@ document.addEventListener(
         function (event) {
           const config = event.detail.data();
           const versioning = `
-<div class="md-version">
-  <button class="md-version__current" aria-label="Select version">
-    ${config.versions.current.slug}
-  </button>
+            <div class="md-version">
+              <button class="md-version__current" aria-label="Select version">
+                ${config.versions.current.slug}
+              </button>
 
-  <ul class="md-version__list">
-  ${ config.versions.active.map(
-    (version) => `
-    <li class="md-version__item">
-      <a href="${ version.urls.documentation }" class="md-version__link">
-        ${ version.slug }
-      </a>
-            </li>`).join("\n")}
-  </ul>
-</div>`;
+              <ul class="md-version__list">
+              ${ config.versions.active.map(
+                (version) => `
+                <li class="md-version__item">
+                  <a href="${ version.urls.documentation }" class="md-version__link">
+                    ${ version.slug }
+                  </a>
+                </li>`).join("\n")}
+              </ul>
+            </div>`;
 
           document.querySelector(".md-header__topic").insertAdjacentHTML("beforeend", versioning);
  });
