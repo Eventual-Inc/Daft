90a7b5eec21f2e49eb8b5008bdbb1128a2d79a1a
Colin Ho <colin.ho99@gmail.com>
2025-05-29 19:27:58 -0700

    fix make lit

--- tests/expressions/test_expressions.py
+++ tests/expressions/test_expressions.py
@@ -25,7 +25,7 @@ from daft.series import Series
         (b"a", DataType.binary()),
         (True, DataType.bool()),
         (None, DataType.null()),
-        (Series.from_pylist([1, 2, 3]), DataType.fixed_size_list(DataType.int64(), 3)),
+        (Series.from_pylist([1, 2, 3]), DataType.list(DataType.int64())),
         (date(2023, 1, 1), DataType.date()),
         (time(1, 2, 3, 4), DataType.time(timeunit=TimeUnit.from_str("us"))),
         (datetime(2023, 1, 1), DataType.timestamp(timeunit=TimeUnit.from_str("us"))),
