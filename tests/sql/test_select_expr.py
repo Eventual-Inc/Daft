43f7786e907d31b1a0aa3f99def8a003b9ba3438
flaneur <me.ssword@gmail.com>
2025-06-06 23:36:21 +0800

    chore: add test case over #4180 (#4484)
    ## Changes Made
    
    i tried to reproduce the panic in #4180 but i failed to reproduce it,
    this pr only adds a test case described in #4180
    
    ## Related Issues
    
    closes #4180
    
    ## Checklist
    
    - [ ] Documented in API Docs (if applicable)
    - [ ] Documented in User Guide (if applicable)
    - [ ] If adding a new documentation page, doc is added to
    `docs/mkdocs.yml` navigation
    - [ ] Documentation builds and is formatted properly (tag @/ccmao1130
    for docs review)

--- tests/sql/test_select_expr.py
+++ tests/sql/test_select_expr.py
@@ -40,3 +40,9 @@ def test_select_expr_functions():
     actual = daft.sql("SELECT lower('ABC') AS l, upper('xyz') AS u")
     expect = singleton().select(lit("ABC").str.lower().alias("l"), lit("xyz").str.upper().alias("u"))
     assert_eq(actual, expect)
+
+
+def test_select_struct_lit():
+    actual = daft.sql("select {'a': 'hello'}").collect()
+    expect = daft.from_pydict({"literal": [{"a": "hello"}]})
+    assert_eq(actual, expect)
