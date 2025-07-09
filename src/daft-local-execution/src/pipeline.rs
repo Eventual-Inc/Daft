f13cebb76e4fb1635d3994d3302e8be38963e260
Srinivas Lade <srinulade1@gmail.com>
2025-07-09 13:31:20 -0700

    feat: Customizable Progress Bar Statistics (#4541)

--- src/daft-local-execution/src/pipeline.rs
+++ src/daft-local-execution/src/pipeline.rs
@@ -39,21 +39,15 @@ use crate::{
     },
     sinks::{
         aggregate::AggregateSink,
-        anti_semi_hash_join_probe::AntiSemiProbeSink,
         blocking_sink::BlockingSinkNode,
         commit_write::CommitWriteSink,
-        concat::ConcatSink,
         cross_join_collect::CrossJoinCollectSink,
         dedup::DedupSink,
         grouped_aggregate::GroupedAggregateSink,
         hash_join_build::HashJoinBuildSink,
-        limit::LimitSink,
-        monotonically_increasing_id::MonotonicallyIncreasingIdSink,
-        outer_hash_join_probe::OuterHashJoinProbeSink,
         pivot::PivotSink,
         repartition::RepartitionSink,
         sort::SortSink,
-        streaming_sink::StreamingSinkNode,
         top_n::TopNSink,
         window_order_by_only::WindowOrderByOnlySink,
         window_partition_and_dynamic_frame::WindowPartitionAndDynamicFrameSink,
@@ -63,6 +57,11 @@ use crate::{
     },
     sources::{empty_scan::EmptyScanSource, in_memory::InMemorySource, source::SourceNode},
     state_bridge::BroadcastStateBridge,
+    streaming_sink::{
+        anti_semi_hash_join_probe::AntiSemiProbeSink, base::StreamingSinkNode, concat::ConcatSink,
+        limit::LimitSink, monotonically_increasing_id::MonotonicallyIncreasingIdSink,
+        outer_hash_join_probe::OuterHashJoinProbeSink,
+    },
     ExecutionRuntimeContext, PipelineCreationSnafu,
 };
 
