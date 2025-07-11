36819b5e79b18f29fd7edeb2372ae25b5d0ac942
wuzhiping <wuzhiping.007@bytedance.com>
2025-07-11 10:48:58 +0800

    I-59: fix unit test

--- tests/io/test_object_store_options.py
+++ tests/io/test_object_store_options.py
@@ -20,26 +20,25 @@ def test_convert_to_s3_config():
     config = io_config_to_storage_options(IOConfig(s3=s3_config), table_uri=table_uri)
     assert config == {
         "region": "us-east-2",
-        "endpoint_url": "https://bucket.s3.us-east-2.amazonaws.com",
+        "endpoint_url": "https://dummy_bucket.s3.us-east-2.amazonaws.com",
         "access_key_id": "dummy_ak",
         "secret_access_key": "dummy_sk",
-        "allow_http": False,
-        "allow_invalid_certificates": False,
+        "allow_http": "false",
+        "allow_invalid_certificates": "false",
         "connect_timeout": "1000ms",
-        "skip_signature": True,
-        "virtual_hosted_style_request": True,
+        "skip_signature": "true",
+        "virtual_hosted_style_request": "true",
     }
 
-    s3_config.force_virtual_addressing = False
+    s3_config = s3_config.replace(force_virtual_addressing=False)
     config = io_config_to_storage_options(IOConfig(s3=s3_config), table_uri=table_uri)
     assert config == {
         "region": "us-east-2",
         "endpoint_url": "https://s3.us-east-2.amazonaws.com",
         "access_key_id": "dummy_ak",
         "secret_access_key": "dummy_sk",
-        "allow_http": False,
-        "allow_invalid_certificates": False,
+        "allow_http": "false",
+        "allow_invalid_certificates": "false",
         "connect_timeout": "1000ms",
-        "skip_signature": True,
-        "virtual_hosted_style_request": False,
+        "skip_signature": "true",
     }
