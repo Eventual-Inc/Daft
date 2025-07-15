ed5ec1b82aec4e7645624ef44562efaf6d684089
Malcolm Greaves <malcolm@eventualcomputing.com>
2025-07-15 10:39:10 -0700

    + lance read/write failure to bug

--- bug.py
+++ bug.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
+import shutil
+import traceback
+from pathlib import Path
+
 import numpy as np
 
 import daft
 from daft import col
+
+CLEANUP = True
 
 D = 5
 
@@ -21,8 +27,54 @@ print(test_df.schema())
 # │ e           ┆ Embedding[Float32; 5] │
 # ╰─────────────┴───────────────────────╯
 
-o = "./out.pq"
-test_df.write_parquet(o)
+error: list[Exception] = []
 
-# this next line fails
-loaded_test_df = daft.read_parquet(o).collect()
+o = "out.pq"
+if Path(o).is_dir():
+    shutil.rmtree(o)
+parquet = test_df.write_parquet(o).collect()
+print(f"PARQUET:\n{parquet}")
+
+try:
+    # this next line fails
+    loaded_test_parquet_df = daft.read_parquet(o).collect()
+except Exception as e:
+    traceback.print_exc()
+    error.append(e)
+
+if CLEANUP:
+    print("Removing parquet files")
+    shutil.rmtree(o)
+
+l = "./out.lance"  # noqa: E741
+if Path(l).is_dir():
+    shutil.rmtree(l)
+lance = test_df.write_lance(l).collect()
+print(f"LANCE:\n{lance}")
+
+try:
+    # this nex line also fails
+    loaded_test_lance_df = daft.read_lance(l).collect()
+except Exception as e:
+    traceback.print_exc()
+    error.append(e)
+
+if CLEANUP:
+    print("Removing lance files")
+    shutil.rmtree(l)
+
+# print("\n"*3)
+# print("-"*80)
+# print("\n")
+
+print("-" * 80)
+
+if len(error) > 0:
+    print(f"Found {len(error)} errors!")
+    # print(f"Found {len(error)} errors:\n{error}")
+    # if len(error) == 2:
+    #    raise error[1] from error[0]
+    # else:
+    #    raise error[0]
+else:
+    print("OK! No bugs!")
