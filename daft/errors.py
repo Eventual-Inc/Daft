b10558705b4ddd25ed95f455e4dfbe11561f9f29
Srinivas Lade <srinulade1@gmail.com>
2025-07-16 16:26:05 -0700

    Suppressed

--- daft/errors.py
+++ daft/errors.py
@@ -3,3 +3,12 @@ from __future__ import annotations
 
 class ExpressionTypeError(Exception):
     pass
+
+
+class UDFException(Exception):
+    """An Daft exception raised when a UDF raises an exception.
+
+    Also provides additional context about the error.
+    """
+
+    pass
