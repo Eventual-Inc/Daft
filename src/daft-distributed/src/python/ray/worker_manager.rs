7973acd1a9f875b317723c1c88fb3d8007344790
Colin Ho <colin.ho99@gmail.com>
2025-07-17 12:42:17 -0700

    round up

--- src/daft-distributed/src/python/ray/worker_manager.rs
+++ src/daft-distributed/src/python/ray/worker_manager.rs
@@ -134,8 +134,8 @@ impl WorkerManager for RayWorkerManager {
             .into_iter()
             .map(|bundle| {
                 let mut dict = HashMap::new();
-                dict.insert("CPU", bundle.num_cpus() as i64);
-                dict.insert("GPU", bundle.num_gpus() as i64);
+                dict.insert("CPU", bundle.num_cpus().ceil() as i64);
+                dict.insert("GPU", bundle.num_gpus().ceil() as i64);
                 dict.insert("memory", bundle.memory_bytes() as i64);
                 dict
             })
