a6b1044d77754fbb7ad45965789d8ffb487a61ca
desmondcheongzx <desmondcheongzx@gmail.com>
2025-07-26 01:36:53 -0700

    Add test for url upload

--- tests/integration/io/test_files_roundtrip_s3_minio.py
+++ tests/integration/io/test_files_roundtrip_s3_minio.py
@@ -4,7 +4,7 @@ import pytest
 
 import daft
 
-from .conftest import minio_create_bucket
+from .conftest import minio_create_bucket, minio_create_public_bucket
 
 
 @pytest.mark.integration()
@@ -19,6 +19,26 @@ def test_files_roundtrip_minio_native_downloader(minio_io_config):
         df.collect()
 
         df = df.with_column("roundtrip_data", df["file_paths"].url.download(io_config=minio_io_config))
+        results = df.to_pydict()
+
+        assert results["data"] == results["roundtrip_data"] == bytes_data
+        for path, expected in zip(results["file_paths"], bytes_data):
+            assert path.startswith(folder)
+
+
+@pytest.mark.integration()
+def test_files_roundtrip_minio_anonymous_upload(anonymous_minio_io_config, minio_io_config):
+    """Test anonymous URL upload and download roundtrip."""
+    bucket_name = "my-public-bucket"
+    folder = f"s3://{bucket_name}/my-folder"
+    with minio_create_public_bucket(minio_io_config=minio_io_config, bucket_name=bucket_name):
+        bytes_data = [b"a", b"b", b"c"]
+        data = {"data": bytes_data}
+        df = daft.from_pydict(data)
+        df = df.with_column("file_paths", df["data"].url.upload(folder, io_config=anonymous_minio_io_config))
+        df.collect()
+
+        df = df.with_column("roundtrip_data", df["file_paths"].url.download(io_config=anonymous_minio_io_config))
         results = df.to_pydict()
 
         assert results["data"] == results["roundtrip_data"] == bytes_data
--- tests/integration/io/test_write_files_s3_minio.py
+++ tests/integration/io/test_write_files_s3_minio.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
-import json
 import uuid
 
-import boto3
 import pytest
 import s3fs
-from botocore.config import Config
 
 import daft
-from tests.conftest import get_tests_daft_runner_name
+from tests.conftest import get_tests_daft_runner_name, minio_create_public_bucket
 
 
 @pytest.fixture(scope="function")
@@ -26,46 +23,6 @@ def bucket(minio_io_config):
     )
     if not fs.exists(BUCKET):
         fs.mkdir(BUCKET)
-    yield BUCKET
-
-
-@pytest.fixture(scope="function")
-def anonymous_bucket(minio_io_config):
-    # For some reason s3fs is having trouble cleaning up MinIO
-    # folders created by pyarrow write_parquet. We just write to
-    # paths with random UUIDs to work around this.
-    BUCKET = "my-bucket-anonymous"
-
-    # Create authenticated S3 client to set up the bucket.
-    s3_client = boto3.client(
-        "s3",
-        endpoint_url=minio_io_config.s3.endpoint_url,
-        aws_access_key_id=minio_io_config.s3.key_id,
-        aws_secret_access_key=minio_io_config.s3.access_key,
-        config=Config(signature_version="s3v4"),
-        region_name="us-east-1",
-    )
-
-    # Create bucket if it doesn't exist.
-    try:
-        s3_client.create_bucket(Bucket=BUCKET)
-    except s3_client.exceptions.BucketAlreadyOwnedByYou:
-        pass
-
-    # Set bucket policy for anonymous access.
-    bucket_policy = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"],
-                "Resource": [f"arn:aws:s3:::{BUCKET}", f"arn:aws:s3:::{BUCKET}/*"],
-            }
-        ],
-    }
-    s3_client.put_bucket_policy(Bucket=BUCKET, Policy=json.dumps(bucket_policy))
-
     yield BUCKET
 
 
@@ -89,20 +46,22 @@ def test_writing_parquet(minio_io_config, bucket, protocol):
 
 @pytest.mark.integration()
 @pytest.mark.parametrize("protocol", ["s3://", "s3a://", "s3n://"])
-def test_writing_parquet_anonymous_mode(anonymous_minio_io_config, anonymous_bucket, protocol):
-    data = {
-        "foo": [1, 2, 3],
-        "bar": ["a", "b", "c"],
-    }
-    df = daft.from_pydict(data)
-    df = df.repartition(2)
-    results = df.write_parquet(
-        f"{protocol}{anonymous_bucket}/parquet-writes-{uuid.uuid4()}",
-        partition_cols=["bar"],
-        io_config=anonymous_minio_io_config,
-    )
-    results.collect()
-    assert len(results) == 3
+def test_writing_parquet_anonymous_mode(anonymous_minio_io_config, minio_io_config, protocol):
+    bucket_name = "my-public-bucket"
+    with minio_create_public_bucket(minio_io_config=minio_io_config, bucket_name=bucket_name):
+        data = {
+            "foo": [1, 2, 3],
+            "bar": ["a", "b", "c"],
+        }
+        df = daft.from_pydict(data)
+        df = df.repartition(2)
+        results = df.write_parquet(
+            f"{protocol}{bucket_name}/parquet-writes-{uuid.uuid4()}",
+            partition_cols=["bar"],
+            io_config=anonymous_minio_io_config,
+        )
+        results.collect()
+        assert len(results) == 3
 
 
 @pytest.mark.integration()
