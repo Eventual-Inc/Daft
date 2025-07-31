29d1dc0c2226a4c9e717adc4f33eb1960e7698d7
Colin Ho <colin.ho99@gmail.com>
2025-07-31 13:41:40 -0700

    add test

--- tests/dataframe/test_limit.py
+++ tests/dataframe/test_limit.py
@@ -22,3 +22,19 @@ def test_negative_limit():
         df.select("name").limit(-1)
 
     assert "LIMIT <n> must be greater than or equal to 0, instead got: -1" in str(excinfo.value)
+
+
+def test_multiple_limits():
+    df = daft.range(1000, partitions=100)
+    df = df.filter(daft.col("id") > 100).limit(900)
+    df = df.filter(daft.col("id") > 200).limit(800)
+    df = df.filter(daft.col("id") > 300).limit(700)
+    df = df.filter(daft.col("id") > 400).limit(600)
+    df = df.filter(daft.col("id") > 500).limit(500)
+    df = df.filter(daft.col("id") > 600).limit(400)
+    df = df.filter(daft.col("id") > 700).limit(300)
+    df = df.filter(daft.col("id") > 800).limit(200)
+    df = df.filter(daft.col("id") > 900).limit(100)
+
+    df = df.to_pydict()
+    assert df["id"] == [i for i in range(901, 1000)]
