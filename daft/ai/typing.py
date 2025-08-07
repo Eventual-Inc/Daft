c4000398687185ef6e68ad0052801dfc0097d1cc
R. C. Howell <5731503+rchowell@users.noreply.github.com>
2025-08-06 20:35:26 -0700

    feat: model resource plumbing for ml model-based functions (#4902)
    ## Changes Made
    
    * Adds embed_text daft expression
    * Adds TextEmbedder protocol 
    * Adds Provider base class
    * Adds Descriptor[T] for making deferred initialization an API invariant
    * Removed batched vs. non-batched protocols to simplify
    * Removed lifecycle methods from the protocols
    
    **SentenceTransformers**
    
    * Adds SentenceTransformersTexEmbedder implementation
    * Adds SentenceTransformersProvider implementation
    
    Will implement for additional providers upon review.
    
    * Unit tests
    * Add OpenAI provider support
    * Add VLLM provider support
    
    ## Related Issues
    
    n/a
    
    ## Checklist
    
    - [ ] Documented in API Docs (if applicable)
    - [ ] Documented in User Guide (if applicable)
    - [ ] If adding a new documentation page, doc is added to
    `docs/mkdocs.yml` navigation
    - [ ] Documentation builds and is formatted properly (tag @/ccmao1130
    for docs review)

---
+++ daft/ai/typing.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+from daft.datatype import DataType
+from daft.dependencies import np
+
+Options = dict[str, str]
+
+T = TypeVar("T")
+
+
+class Descriptor(ABC, Generic[T]):
+    """A descriptor is a serializable factory, with protocol-specific properties, used to instantiate a model."""
+
+    @abstractmethod
+    def get_provider(self) -> str:
+        """Returns the provider that created this descriptor."""
+        ...
+
+    @abstractmethod
+    def get_model(self) -> str:
+        """Returns the model identifier for this descriptor."""
+        ...
+
+    @abstractmethod
+    def get_options(self) -> Options:
+        """Returns the model instantiation options."""
+        ...
+
+    @abstractmethod
+    def instantiate(self) -> T:
+        """Instantiates and returns a concrete model instance corresponding to this descriptor."""
+
+
+# temp definition to defer complexity of a more generic embedding type to later PRs
+Embedding = np.typing.NDArray  # type: ignore[type-arg]
+
+
+@dataclass
+class EmbeddingDimensions:
+    size: int
+    dtype: DataType
+
+    def as_dtype(self) -> DataType:
+        return DataType.embedding(dtype=self.dtype, size=self.size)
