74961a3ad92b8aaca40b7f42c714d57d2a3d471e
Srinivas Lade <srinulade1@gmail.com>
2025-08-11 09:45:34 -0700

    Ready to go

--- tests/integration/io/test_url_download_s3_local_retry_server.py
+++ tests/integration/io/test_url_download_s3_local_retry_server.py
@@ -9,7 +9,7 @@ from tests.utils import sort_pydict
 @pytest.mark.integration()
 def test_url_download_local_retry_server(retry_server_s3_config):
     bucket = "80-per-second-rate-limited-gets-bucket"
-    data = {"urls": [f"s3://{bucket}/foo{i}" for i in range(100)]}
+    data = {"urls": sorted([f"s3://{bucket}/foo{i}" for i in range(100)])}
     df = daft.from_pydict(data)
     df = df.with_column("data", df["urls"].url.download(io_config=retry_server_s3_config, on_error="null"))
     assert sort_pydict(df.to_pydict(), "urls", ascending=True) == {
