e3c00433cb72538da572c30de12f94c2db320059
xloya <xiaojiebao@apache.org>
2025-08-13 12:23:20 +0800

    update

--- daft/io/lance/lance_scan.py
+++ daft/io/lance/lance_scan.py
@@ -93,7 +93,7 @@ class LanceDBScanOperator(ScanOperator, SupportsPushdownFilters):
                 if filter_required_column_names is None
                 else pushdowns.columns + filter_required_column_names
             )
-
+        required_columns = None if required_columns is None else list(set(required_columns))
         # TODO: figure out how to translate Pushdowns into LanceDB filters
         filters = None
         fragments = self._ds.get_fragments()
