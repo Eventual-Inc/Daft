9bf420ec0f2358ab8681e412dbb7e0a1f0bb3368
Kevin Wang <kevin@eventualcomputing.com>
2025-08-22 15:41:54 -0700

    feat: basic generator udf

--- daft/udf/__init__.py
+++ daft/udf/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from daft.udf.row_wise import RowWiseUdf
 from typing import overload, Callable, TypeVar, TYPE_CHECKING
 import sys
 
@@ -9,42 +8,41 @@ if sys.version_info < (3, 10):
 else:
     from typing import ParamSpec
 
-import functools
-from daft.udf.legacy import udf, UDF
+from .gen import GeneratorUdf
+from .row_wise import RowWiseUdf
+from .legacy import udf, UDF
 
 P = ParamSpec("P")
 T = TypeVar("T")
 
 if TYPE_CHECKING:
     from daft.datatype import DataTypeLike
+    from collections.abc import Iterator
 
 
 class _DaftFuncDecorator:
-    """`@daft.func` Decorator to convert a Python function into a `UDF`.
+    """Decorator to convert a Python function into a Daft user-defined function.
 
-    This decorator can be used to define a row-wise UDF that can be applied to a Daft DataFrame.
+    Daft function variants:
+    - `@daft.func` - Row-wise scalar function (1 in, 1 out)
+    - `@daft.func.gen` - Generator function (1 in, N out)
 
-    The `@daft.func` decorator transforms regular Python functions into Daft User-Defined Functions (UDFs) that operate on individual values.
+    Decorated functions accept both their original argument types and Daft Expressions, and return an Expression which can then be lazily evaluated.
+    To run the original function, call `<your_function>.eval(<args>)`.
 
-    Decorated functions accept their original argument types or Expressions. Additionally, they return lazily-evaluated Expressions. If you want to run the function directly, call `<your_function>.eval(<args>)`.
-
-    It accepts both synchronous and asynchronous functions.
-
-    It can be used with existing functions or as a decorator to define new functions.
-
-    The function either needs to be annotated with types, or you must provide a return type for the function using the `return_dtype` parameter. Daft requires type information to plan and optimize query execution.
+    Args:
+        return_dtype: The data type that this function should return. If not specified, uses the function's return type hint.
 
     Examples:
         Basic Example
 
         >>> import daft
-        >>> from daft import col
         >>> @daft.func
         ... def my_sum(a: int, b: int) -> int:
         ...     return a + b
         >>>
         >>> df = daft.from_pydict({"x": [1, 2, 3], "y": [4, 5, 6]})
-        >>> df.select(my_sum(col("x"), col("y"))).collect()
+        >>> df.select(my_sum(df["x"], df["y"])).collect()
         ╭───────╮
         │ x     │
         │ ---   │
@@ -59,15 +57,14 @@ class _DaftFuncDecorator:
         <BLANKLINE>
         (Showing first 3 of 3 rows)
 
-        Basic Example with existing function
+        Calling the decorator directly on an existing function
 
         >>> import daft
-        >>> from daft import col
         >>> def tokenize(text: str) -> list[int]:
         ...     vocab = {char: i for i, char in enumerate(text)}
         ...     return [vocab[char] for char in text]
         >>>
-        >>> daft_tokenize = daft.func(tokenize)
+        >>> daft_tokenize = daft.func(tokenize)  # creates a new function rather than modifying `tokenize`
         >>> df = daft.from_pydict({"text": ["hello", "world", "daft"]})
         >>> df.select(daft_tokenize(df["text"])).collect()
         ╭─────────────────╮
@@ -84,42 +81,40 @@ class _DaftFuncDecorator:
         <BLANKLINE>
         (Showing first 3 of 3 rows)
 
-        Basic Example with existing function and no type hints
+        Manually specifying the return type
 
         >>> import daft
-        >>> from daft import col
-        >>> def tokenize(text: str):
-        ...     vocab = {char: i for i, char in enumerate(text)}
-        ...     return [vocab[char] for char in text]
-        >>> daft_tokenize = daft.func(tokenize, return_dtype=list[int])
-        >>> df = daft.from_pydict({"text": ["hello", "world", "daft"]})
-        >>> df.select(daft_tokenize(df["text"])).collect()
-        ╭─────────────────╮
-        │ text            │
-        │ ---             │
-        │ List[Int64]     │
-        ╞═════════════════╡
-        │ [0, 1, 3, 3, 4] │
-        ├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┤
-        │ [0, 1, 2, 3, 4] │
-        ├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┤
-        │ [0, 1, 2, 3]    │
-        ╰─────────────────╯
+        >>> @daft.func(return_dtype=daft.DataType.int32())
+        ... def my_sum(a: int, b: int):
+        ...     return a + b
+        >>>
+        >>> df = daft.from_pydict({"x": [1, 2, 3], "y": [4, 5, 6]})
+        >>> df.select(my_sum(df["x"], df["y"])).collect()
+        ╭───────╮
+        │ x     │
+        │ ---   │
+        │ Int32 │
+        ╞═══════╡
+        │ 5     │
+        ├╌╌╌╌╌╌╌┤
+        │ 7     │
+        ├╌╌╌╌╌╌╌┤
+        │ 9     │
+        ╰───────╯
         <BLANKLINE>
         (Showing first 3 of 3 rows)
 
-        Async Example
+        Decorating an async function
 
         >>> import daft
         >>> import asyncio
-        >>> from daft import col
         >>> @daft.func
         ... async def my_sum(a: int, b: int) -> int:
         ...     await asyncio.sleep(1)
         ...     return a + b
         >>>
         >>> df = daft.from_pydict({"x": [1, 2, 3], "y": [4, 5, 6]})
-        >>> df.select(my_sum(col("x"), col("y"))).collect()
+        >>> df.select(my_sum(df["x"], df["y"])).collect()
         ╭───────╮
         │ x     │
         │ ---   │
@@ -145,6 +140,68 @@ class _DaftFuncDecorator:
     ) -> RowWiseUdf[P, T] | Callable[[Callable[P, T]], RowWiseUdf[P, T]]:
         def partial_udf(fn: Callable[P, T]) -> RowWiseUdf[P, T]:
             return RowWiseUdf(fn, return_dtype=return_dtype)
+
+        return partial_udf if fn is None else partial_udf(fn)
+
+    @overload
+    @staticmethod
+    def gen(
+        *, return_dtype: DataTypeLike | None = None
+    ) -> Callable[[Callable[P, Iterator[T]]], GeneratorUdf[P, T]]: ...
+
+    @overload
+    @staticmethod
+    def gen(fn: Callable[P, Iterator[T]], *, return_dtype: DataTypeLike | None = None) -> GeneratorUdf[P, T]: ...
+
+    @staticmethod
+    def gen(
+        fn: Callable[P, Iterator[T]] | None = None, *, return_dtype: DataTypeLike | None = None
+    ) -> GeneratorUdf[P, T] | Callable[[Callable[P, Iterator[T]]], GeneratorUdf[P, T]]:
+        """Decorator to convert a Python function into a Daft user-defined generator function.
+
+        This decorator can be used on Python generator functions and any Python function that returns an iterator.
+        Unlike row-wise functions created via `@daft.func` which return one value per input row, generator functions may yield multiple values per row.
+        Each value is placed in its own row, with the other output columns are broadcasted to match the number of generated values.
+
+        Args:
+            return_dtype: The data type that the iterator returned by this function should yield. If not specified, uses the function's yield type hint.
+
+        Example:
+            >>> import daft
+            >>> from typing import Iterator
+            >>> @daft.func.gen
+            ... def my_gen_func(to_repeat: str, n: int) -> Iterator[str]:
+            ...     for _ in range(n):
+            ...         yield to_repeat
+            >>>
+            >>> df = daft.from_pydict({"id": [0, 1, 2], "value": ["pip", "install", "daft"], "occurrences": [0, 2, 4]})
+            >>> df = df.select("id", my_gen_func(df["value"], df["occurrences"]))
+            >>> df.collect()
+            ╭───────┬─────────╮
+            │ id    ┆ value   │
+            │ ---   ┆ ---     │
+            │ Int64 ┆ Utf8    │
+            ╞═══════╪═════════╡
+            │ 0     ┆ None    │
+            ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+            │ 1     ┆ install │
+            ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+            │ 1     ┆ install │
+            ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+            │ 2     ┆ daft    │
+            ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+            │ 2     ┆ daft    │
+            ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+            │ 2     ┆ daft    │
+            ├╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+            │ 2     ┆ daft    │
+            ╰───────┴─────────╯
+            <BLANKLINE>
+            (Showing first 7 of 7 rows)
+        """
+
+        def partial_udf(fn: Callable[P, Iterator[T]]) -> GeneratorUdf[P, T]:
+            return GeneratorUdf(fn, return_dtype=return_dtype)
 
         return partial_udf if fn is None else partial_udf(fn)
 
