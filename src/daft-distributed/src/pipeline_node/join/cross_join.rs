26451ce59e0f997b72c8ba376c0047884b3e590a
Oliver Huang <oliverbhuang@gmail.com>
2025-09-09 15:12:44 -0700

    use streams for left and right tasks

--- src/daft-distributed/src/pipeline_node/join/cross_join.rs
+++ src/daft-distributed/src/pipeline_node/join/cross_join.rs
@@ -5,7 +5,7 @@ use common_error::DaftResult;
 use daft_local_plan::LocalPhysicalPlan;
 use daft_logical_plan::{partitioning::UnknownClusteringConfig, stats::StatsState};
 use daft_schema::schema::SchemaRef;
-use futures::StreamExt;
+use futures::{StreamExt, stream::select};
 
 use crate::{
     pipeline_node::{
@@ -75,47 +75,81 @@ impl CrossJoinNode {
 
     async fn execution_loop(
         self: Arc<Self>,
-        mut left_input: SubmittableTaskStream,
-        mut right_input: SubmittableTaskStream,
+        left_input: SubmittableTaskStream,
+        right_input: SubmittableTaskStream,
         task_id_counter: TaskIDCounter,
         result_tx: Sender<SubmittableTask<SwordfishTask>>,
     ) -> DaftResult<()> {
         let mut left_tasks = Vec::new();
-        while let Some(left_task) = left_input.next().await {
-            left_tasks.push(left_task);
-        }
+        let mut right_tasks = Vec::new();
 
-        while let Some(right_task) = right_input.next().await {
-            let right_plan = right_task.task().plan().clone();
-            let right_psets = right_task.task().psets().clone();
-            let config = right_task.task().config().clone();
+        let mut combined_stream = select(
+            left_input.map(|task| (true /* is_left */, task)),
+            right_input.map(|task| (false /* !is_left */, task)),
+        );
 
-            for left_task in &left_tasks {
-                let left_plan = left_task.task().plan();
-                let cross_join_plan = LocalPhysicalPlan::cross_join(
-                    left_plan,
-                    right_plan.clone(),
-                    self.config.schema.clone(),
-                    StatsState::NotMaterialized,
-                );
-
-                let mut psets = right_psets.clone();
-                psets.extend(left_task.task().psets().clone());
-
-                let new_task = SwordfishTask::new(
-                    TaskContext::from((self.context(), task_id_counter.next())),
-                    cross_join_plan,
-                    config.clone(),
-                    psets,
-                    SchedulingStrategy::Spread,
-                    self.context().to_hashmap(),
-                );
-
-                let task = SubmittableTask::new(new_task);
-
-                let _ = result_tx.send(task).await;
+        while let Some((is_left, task)) = combined_stream.next().await {
+            if is_left {
+                left_tasks.push(task);
+                for right_task in &right_tasks {
+                    self.create_cross_join_task(
+                        left_tasks.last().expect("left_tasks should not be empty"),
+                        right_task,
+                        &task_id_counter,
+                        &result_tx,
+                    )
+                    .await?;
+                }
+            } else {
+                right_tasks.push(task);
+                for left_task in &left_tasks {
+                    self.create_cross_join_task(
+                        left_task,
+                        right_tasks.last().expect("right_tasks should not be empty"),
+                        &task_id_counter,
+                        &result_tx,
+                    )
+                    .await?;
+                }
             }
         }
+
+        Ok(())
+    }
+
+    async fn create_cross_join_task(
+        &self,
+        left_task: &SubmittableTask<SwordfishTask>,
+        right_task: &SubmittableTask<SwordfishTask>,
+        task_id_counter: &TaskIDCounter,
+        result_tx: &Sender<SubmittableTask<SwordfishTask>>,
+    ) -> DaftResult<()> {
+        let left_plan = left_task.task().plan();
+        let right_plan = right_task.task().plan();
+        let right_psets = right_task.task().psets().clone();
+        let config = right_task.task().config().clone();
+
+        let cross_join_plan = LocalPhysicalPlan::cross_join(
+            left_plan,
+            right_plan,
+            self.config.schema.clone(),
+            StatsState::NotMaterialized,
+        );
+
+        let mut psets = right_psets;
+        psets.extend(left_task.task().psets().clone());
+
+        let new_task = SwordfishTask::new(
+            TaskContext::from((self.context(), task_id_counter.next())),
+            cross_join_plan,
+            config,
+            psets,
+            SchedulingStrategy::Spread,
+            self.context().to_hashmap(),
+        );
+
+        let _ = result_tx.send(SubmittableTask::new(new_task)).await;
+
         Ok(())
     }
 }
