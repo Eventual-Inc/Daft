e16ac5b1f57608bce4b032621787ad69ef4e66c2
Kev Wang <kevin@eventualcomputing.com>
2025-09-15 14:54:43 -0700

    feat: migrate first batch of daft functions (#5086)
    ## Changes Made
    
    First phase of our daft.functions migration + Expression namespacing
    deprecation!
    
    These changes should all be uncontroversial, and all of [the comments
    about
    them](https://www.notion.so/Expressions-Migration-Tasks-246091bd8634806a8f24eb573d8d3b3e?source=copy_link)
    have been resolved. Thus, I'm hoping that the reviews for this PR will
    be a little easier since they should only need to focus on the
    structural changes rather than the naming ones. I'm leaving the ones
    with unresolved discussions for future PRs, but this covers something
    like 95% of expressions.
    
    In addition, since the existing tests call the expression methods, which
    in turn call daft.functions, passing those tests should indicate both
    that the new functions work and that the existing methods have not been
    broken.
    
    All of the functions are straightforward Python-only changes except for
    two:
    - `daft.functions.length` has to work on binary, string, as well as list
    types. prior to that they were three separate functions, so I had to
    write a new scalar function on the Rust side
    - similarly, `daft.functions.concat` works for both string and binary,
    whereas the original functions were separate. I implemented this one by
    implementing `Expression.__add__` between two binary expressions, and
    then just used the add implementation in the new concat scalar function
    
    ## Related Issues
    
    <!-- Link to related GitHub issues, e.g., "Closes #123" -->
    
    ## Checklist
    
    - [x] Documented in API Docs (if applicable)
    - [x] Documented in User Guide (if applicable)
    - [x] If adding a new documentation page, doc is added to
    `docs/mkdocs.yml` navigation
    - [x] Documentation builds and is formatted properly (tag @/ccmao1130
    for docs review)
    
    ---------
    
    Co-authored-by: Srinivas Lade <srinulade1@gmail.com>

---
+++ daft/functions/image.py
@@ -0,0 +1,89 @@
+"""Image Functions."""
+
+from __future__ import annotations
+
+from typing import Literal
+
+from daft.daft import ImageFormat, ImageMode
+from daft.datatype import DataType
+from daft.expressions import Expression
+
+
+def resize(expr: Expression, w: int, h: int) -> Expression:
+    """Resize image into the provided width and height.
+
+    Args:
+        expr: Expression to resize.
+        w: Desired width of the resized image.
+        h: Desired height of the resized image.
+
+    Returns:
+        Expression: An Image expression representing an image column of the resized images.
+    """
+    return Expression._call_builtin_scalar_fn("image_resize", expr, w=w, h=h)
+
+
+def crop(expr: Expression, bbox: tuple[int, int, int, int] | Expression) -> Expression:
+    """Crops images with the provided bounding box.
+
+    Args:
+        expr: Expression to crop.
+        bbox (tuple[int, int, int, int] | Expression): Either a tuple of (x, y, width, height)
+            parameters for cropping, or a List Expression where each element is a length 4 List
+            which represents the bounding box for the crop
+
+    Returns:
+        Expression: An Image expression representing the cropped image
+    """
+    if not isinstance(bbox, Expression):
+        if len(bbox) != 4 or not all([isinstance(x, int) for x in bbox]):
+            raise ValueError(f"Expected `bbox` to be either a tuple of 4 ints or an Expression but received: {bbox}")
+        bbox = Expression._to_expression(bbox).cast(DataType.fixed_size_list(DataType.uint64(), 4))
+    return Expression._call_builtin_scalar_fn("image_crop", expr, bbox)
+
+
+def encode_image(expr: Expression, image_format: str | ImageFormat) -> Expression:
+    """Encode an image column as the provided image file format, returning a binary column of encoded bytes.
+
+    Args:
+        expr: The image expression to encode.
+        image_format: The image file format into which the images will be encoded.
+
+    Returns:
+        Expression: A Binary expression representing a binary column of encoded image bytes.
+    """
+    if isinstance(image_format, str):
+        image_format = ImageFormat.from_format_string(image_format.upper())
+    if not isinstance(image_format, ImageFormat):
+        raise ValueError(f"image_format must be a string or ImageFormat variant, but got: {image_format}")
+    return Expression._call_builtin_scalar_fn("image_encode", expr, image_format=image_format)
+
+
+def decode_image(
+    expr: Expression,
+    on_error: Literal["raise", "null"] = "raise",
+    mode: str | ImageMode | None = None,
+) -> Expression:
+    """Decodes the binary data in this column into images.
+
+    This can only be applied to binary columns that contain encoded images (e.g. PNG, JPEG, etc.)
+
+    Args:
+        expr: The binary expression to decode.
+        on_error: Whether to raise when encountering an error, or log a warning and return a null
+        mode: What mode to convert the images into before storing it in the column. This may prevent
+            errors relating to unsupported types.
+
+    Returns:
+        Expression: An Image expression representing an image column.
+    """
+    return Expression._call_builtin_scalar_fn("image_decode", expr, on_error=on_error, mode=mode)
+
+
+def convert_image(expr: Expression, mode: str | ImageMode) -> Expression:
+    """Convert an image expression to the specified mode."""
+    if isinstance(mode, str):
+        mode = ImageMode.from_mode_string(mode.upper())
+    if not isinstance(mode, ImageMode):
+        raise ValueError(f"mode must be a string or ImageMode variant, but got: {mode}")
+    return Expression._call_builtin_scalar_fn("to_mode", expr, mode=mode)
