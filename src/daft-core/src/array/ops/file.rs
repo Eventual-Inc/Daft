273dceb97ccdb450afb961e170aae9a5c2480551
universalmind303 <cory.grinstead@gmail.com>
2025-09-19 15:58:49 -0500

    remove dead code

--- src/daft-core/src/array/ops/file.rs
+++ src/daft-core/src/array/ops/file.rs
@@ -1,34 +1,7 @@
-use common_file::FileReference;
-
 use crate::{
     datatypes::FileArray,
     prelude::{BinaryArray, UInt8Array},
 };
-
-#[allow(clippy::len_without_is_empty)]
-pub trait AsFileObj {
-    fn name(&self) -> &str;
-    fn len(&self) -> usize;
-    fn as_file(&self, idx: usize) -> Option<FileReference>;
-}
-
-impl AsFileObj for FileArray {
-    fn name(&self) -> &str {
-        FileArray::name(self)
-    }
-
-    fn len(&self) -> usize {
-        FileArray::len(self)
-    }
-
-    fn as_file(&self, idx: usize) -> Option<FileReference> {
-        match self.get_lit(idx) {
-            crate::lit::Literal::Null => None,
-            crate::lit::Literal::File(f) => Some(f),
-            _ => unreachable!("unexpected literal type for FileArray"),
-        }
-    }
-}
 
 impl FileArray {
     pub fn discriminant_array(&self) -> UInt8Array {
