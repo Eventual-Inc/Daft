6d5189f32f388386562391186adbcaddf4a907a7
Srinivas Lade <srinulade1@gmail.com>
2025-09-21 20:55:15 -0700

    Good

--- src/daft-cli/src/python.rs
+++ src/daft-cli/src/python.rs
@@ -1,5 +1,18 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand, arg};
 use pyo3::prelude::*;
+
+#[derive(Debug, Args)]
+struct DashboardArgs {
+    #[arg(short, long, default_value_t = 80)]
+    /// The port to launch the dashboard on
+    port: u16,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Start the Daft dashboard server
+    Dashboard(DashboardArgs),
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -8,35 +21,46 @@ struct Cli {
     command: Commands,
 }
 
-#[derive(Subcommand, Debug)]
-enum Commands {
-    /// Start the Daft dashboard server
-    Dashboard,
+// ---------------- Run CLI Commands ---------------- //
+
+fn run_dashboard(py: Python, args: DashboardArgs) {
+    println!("🚀 Launching the Daft Dashboard!");
+
+    let port = args.port;
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel();
+
+    runtime.spawn(async move {
+        daft_dashboard::launch_server(port, async move { shutdown_rx.await.unwrap() })
+            .await
+            .unwrap();
+    });
+
+    println!(
+        "✨ View the Daft Dashboard at http://{}:{}",
+        daft_dashboard::DEFAULT_SERVER_ADDR,
+        port
+    );
+
+    loop {
+        if py.check_signals().is_err() {
+            println!("👋 Thanks for using Daft Dashboard! Shutting down...");
+            shutdown_tx
+                .send(())
+                .expect("Failed to shutdown Daft Dashboard");
+            break;
+        }
+    }
 }
 
 #[pyfunction]
 pub fn cli(py: Python, args: Vec<String>) {
     let cli = Cli::parse_from(args);
     match cli.command {
-        Commands::Dashboard => {
-            println!("🚀 Launching Daft Dashboard!");
-            let mut handle =
-                daft_dashboard::python::launch(false).expect("Failed to launch Daft Dashboard");
-            println!(
-                "✨ View the Daft Dashboard at http://{}:{}",
-                daft_dashboard::DEFAULT_SERVER_ADDR,
-                handle.get_port()
-            );
-            loop {
-                if py.check_signals().is_err() {
-                    println!("👋 Thanks for using Daft Dashboard! Shutting down...");
-                    handle
-                        .shutdown(true)
-                        .expect("Failed to shutdown Daft Dashboard");
-                    break;
-                }
-            }
-        }
+        Commands::Dashboard(args) => run_dashboard(py, args),
     }
 }
 
