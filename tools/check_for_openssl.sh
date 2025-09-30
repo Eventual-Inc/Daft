c1cd4164164b5bfeb2dd3a776dfedc7e009e3e2a
Desmond Cheong <desmondcheongzx@gmail.com>
2025-09-30 16:44:07 -0700

    Apply suggestion from @desmondcheongzx

--- tools/check_for_openssl.sh
+++ tools/check_for_openssl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # error if Daft depends on openssl or native-tls
-# exclude vendored azure_identity since we're vendoring it
+# exclude vendored azure_identity
 cargo tree --workspace --all-features --exclude azure_identity | grep -vzqE "(openssl-sys|native-tls)"
