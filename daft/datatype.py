de868bee4fe298bc98b97b9927dc58faa261092a
Kevin Wang <kevin@eventualcomputing.com>
2025-10-09 04:21:12 -0700

    feat: more tensor conversions

--- daft/datatype.py
+++ daft/datatype.py
@@ -12,6 +12,8 @@ from daft.runners import get_or_create_runner
 if TYPE_CHECKING:
     import builtins
 
+    import jaxtyping
+
 
 class TimeUnit:
     _timeunit: PyTimeUnit
@@ -221,33 +223,17 @@ class DataType:
             else:
                 inner_dtype = cls.python()
             return cls.tensor(inner_dtype)
-        elif check_type("numpy.bool_"):
-            return cls.bool()
-        elif check_type("numpy.int8"):
-            return cls.int8()
-        elif check_type("numpy.uint8"):
-            return cls.uint8()
-        elif check_type("numpy.int16"):
-            return cls.int16()
-        elif check_type("numpy.uint16"):
-            return cls.uint16()
-        elif check_type("numpy.int32"):
-            return cls.int32()
-        elif check_type("numpy.uint32"):
-            return cls.uint32()
-        elif check_type("numpy.int64"):
-            return cls.int64()
-        elif check_type("numpy.uint64"):
-            return cls.uint64()
-        elif check_type("numpy.float32"):
-            return cls.float32()
-        elif check_type("numpy.float64"):
-            return cls.float64()
-        elif check_type("numpy.datetime64"):
-            warnings.warn(
-                "numpy.datetime64 may be converted to either timestamp or date based on the value, defaulting to DataType.timestamp(TimeUnit.us())"
-            )
-            return cls.timestamp(TimeUnit.us(), timezone=None)
+        elif (
+            check_type("torch.Tensor")
+            or check_type("tensorflow.Tensor")
+            or check_type("jax.Array")
+            or check_type("cupy.ndarray")
+        ):
+            return cls.tensor(cls.python())
+        elif check_type("jaxtyping.AbstractArray"):
+            return cls._infer_from_jaxtyping(origin)
+        elif check_type("numpy.generic"):
+            return cls._infer_from_numpy_scalar_dtype(origin)
         elif check_type("pandas.Series"):
             warnings.warn(
                 "Cannot derive inner type from pandas.Series type, defaulting to DataType.python() for Series inner type"
@@ -264,6 +250,86 @@ class DataType:
             return cls.file()
         else:
             return cls.python()
+
+    @classmethod
+    def _infer_from_numpy_scalar_dtype(cls, t: type) -> DataType:
+        import numpy as np
+
+        if t is np.bool_:
+            return cls.bool()
+        elif t is np.int8:
+            return cls.int8()
+        elif t is np.uint8:
+            return cls.uint8()
+        elif t is np.int16:
+            return cls.int16()
+        elif t is np.uint16:
+            return cls.uint16()
+        elif t is np.int32:
+            return cls.int32()
+        elif t is np.uint32:
+            return cls.uint32()
+        elif t is np.int64:
+            return cls.int64()
+        elif t is np.uint64:
+            return cls.uint64()
+        elif t is np.float32:
+            return cls.float32()
+        elif t is np.float64:
+            return cls.float64()
+        elif t is np.datetime64:
+            warnings.warn(
+                "numpy.datetime64 may be converted to either timestamp or date based on the value, defaulting to DataType.timestamp(TimeUnit.us())"
+            )
+            return cls.timestamp(TimeUnit.us(), timezone=None)
+        else:
+            return cls.python()
+
+    @classmethod
+    def _infer_from_jaxtyping(cls, t: jaxtyping.AbstractArray) -> DataType:
+        import jaxtyping
+        from jaxtyping._array_types import _FixedDim
+
+        inferred_array_type = cls.infer_from_type(t.array_type)
+        if not inferred_array_type.is_tensor():
+            # if we do not support converting the array type to a tensor, do not infer an inner dtype or shape
+            return inferred_array_type
+
+        if t.dtype is jaxtyping.Bool:
+            inner_dtype = cls.bool()
+        elif t.dtype is jaxtyping.Int8:
+            inner_dtype = cls.int8()
+        elif t.dtype is jaxtyping.UInt8:
+            inner_dtype = cls.uint8()
+        elif t.dtype is jaxtyping.Int16:
+            inner_dtype = cls.int16()
+        elif t.dtype is jaxtyping.UInt16:
+            inner_dtype = cls.uint16()
+        elif t.dtype is jaxtyping.Int32:
+            inner_dtype = cls.int32()
+        elif t.dtype is jaxtyping.UInt32:
+            inner_dtype = cls.uint32()
+        elif t.dtype is jaxtyping.Int64 or t.dtype is jaxtyping.Int or t.dtype is jaxtyping.Integer:
+            inner_dtype = cls.int64()
+        elif t.dtype is jaxtyping.UInt64 or t.dtype is jaxtyping.UInt:
+            inner_dtype = cls.uint64()
+        elif t.dtype is jaxtyping.Float32:
+            inner_dtype = cls.float32()
+        elif t.dtype is jaxtyping.Float64 or t.dtype is jaxtyping.Float or t.dtype is jaxtyping.Real:
+            inner_dtype = cls.float64()
+        else:
+            inner_dtype = cls.python()
+
+        if len(t.dims) == 0:
+            shape = ()
+        else:
+            # only set shape if all dims are fixed
+            if all(isinstance(dim, _FixedDim) for dim in t.dims):
+                shape = tuple(dim.size for dim in t.dims)
+            else:
+                shape = None
+
+        return cls.tensor(inner_dtype, shape)
 
     @classmethod
     def infer_from_object(cls, obj: Any) -> DataType:
@@ -531,7 +597,7 @@ class DataType:
                 each tensor element to vary.
         """
         if shape is not None:
-            if not isinstance(shape, tuple) or not shape or any(not isinstance(n, int) for n in shape):
+            if not isinstance(shape, tuple) or any(not isinstance(n, int) for n in shape):
                 raise ValueError("Tensor shape must be a non-empty tuple of ints, but got: ", shape)
         return cls._from_pydatatype(PyDataType.tensor(dtype._dtype, shape))
 
