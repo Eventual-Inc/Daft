eaf0f8ec63a9bf7742e4810ef0fd848e437984af
Colin Ho <colin.ho99@gmail.com>
2025-10-10 23:13:47 -0700

    fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,7 +26,8 @@ def pytest_addoption(parser):
 
 def pytest_configure(config):
     config.addinivalue_line(
-        "markers", "integration: mark test as an integration test that runs with external dependencies"
+        "markers",
+        "integration: mark test as an integration test that runs with external dependencies",
     )
 
 
@@ -107,7 +108,10 @@ def assert_spark_equals(spark_session):
 
 class MakeDF(Protocol):
     def __call__(
-        self, data: pa.Table | dict | list, repartition: int = 1, repartition_columns: list[str] = []
+        self,
+        data: pa.Table | dict | list,
+        repartition: int = 1,
+        repartition_columns: list[str] = [],
     ) -> daft.DataFrame: ...
 
 
@@ -216,7 +220,13 @@ def check_answer(df: daft.DataFrame, expected_answer: dict[str, Any], is_sorted:
         assert_df_equals(daft_df, expected_df, assert_ordering=True, check_dtype=check_dtype)
     else:
         sort_keys = df.column_names
-        assert_df_equals(daft_df, expected_df, sort_key=sort_keys, assert_ordering=False, check_dtype=check_dtype)
+        assert_df_equals(
+            daft_df,
+            expected_df,
+            sort_key=sort_keys,
+            assert_ordering=False,
+            check_dtype=check_dtype,
+        )
 
 
 @pytest.fixture(
--- tests/dataframe/test_joins.py
+++ tests/dataframe/test_joins.py
@@ -5,7 +5,6 @@ import pytest
 
 import daft
 from daft import col
-from daft.context import get_context
 from daft.datatype import DataType
 from tests.conftest import get_tests_daft_runner_name
 from tests.utils import sort_arrow_table
@@ -16,9 +15,7 @@ def skip_invalid_join_strategies(join_strategy, join_type):
         if join_strategy not in [None, "hash"]:
             pytest.skip("Native executor fails for these tests")
     else:
-        if not get_context().daft_execution_config.use_legacy_ray_runner and join_strategy == "sort_merge":
-            pytest.skip("Sort merge joins are not supported on Flotilla")
-        elif (join_strategy == "sort_merge") and join_type != "inner":
+        if (join_strategy == "sort_merge") and join_type != "inner":
             pytest.skip("Sort merge currently only supports inner joins")
         elif join_strategy == "broadcast" and join_type == "outer":
             pytest.skip("Broadcast join does not support outer joins")
@@ -72,10 +69,9 @@ def test_rename_join_keys_in_dataframe(make_df):
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize("join_type", ["inner", "left", "right", "outer"])
-def test_joins(join_strategy, join_type, make_df, n_partitions: int, with_morsel_size):
+def test_joins(join_strategy, join_type, make_df, n_partitions, with_morsel_size):
     skip_invalid_join_strategies(join_strategy, join_type)
 
     df = make_df(
@@ -105,7 +101,6 @@ def test_joins(join_strategy, join_type, make_df, n_partitions: int, with_morsel
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize("join_type", ["inner", "left", "right", "outer"])
 def test_multicol_joins(join_strategy, join_type, make_df, n_partitions: int, with_morsel_size):
@@ -139,7 +134,6 @@ def test_multicol_joins(join_strategy, join_type, make_df, n_partitions: int, wi
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize("join_type", ["inner", "left", "right", "outer"])
 def test_dupes_join_key(join_strategy, join_type, make_df, n_partitions: int, with_morsel_size):
@@ -169,7 +163,6 @@ def test_dupes_join_key(join_strategy, join_type, make_df, n_partitions: int, wi
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize("join_type", ["inner", "left", "right", "outer"])
 def test_multicol_dupes_join_key(join_strategy, join_type, make_df, n_partitions: int, with_morsel_size):
@@ -201,7 +194,6 @@ def test_multicol_dupes_join_key(join_strategy, join_type, make_df, n_partitions
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize("join_type", ["inner", "left", "right", "outer"])
 def test_joins_all_same_key(join_strategy, join_type, make_df, n_partitions: int, with_morsel_size):
@@ -231,7 +223,6 @@ def test_joins_all_same_key(join_strategy, join_type, make_df, n_partitions: int
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,flip,expected",
@@ -314,7 +305,6 @@ def test_joins_no_overlap_disjoint(
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,flip,expected",
@@ -399,7 +389,6 @@ def test_joins_no_overlap_interleaved(
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize("join_type", ["inner", "left", "right", "outer"])
 def test_limit_after_join(join_strategy, join_type, make_df, n_partitions: int, with_morsel_size):
@@ -434,7 +423,6 @@ def test_limit_after_join(join_strategy, join_type, make_df, n_partitions: int, 
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -497,7 +485,6 @@ def test_join_with_null(join_strategy, join_type, expected, make_df, repartition
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -567,7 +554,6 @@ def test_join_with_null_multikey(join_strategy, join_type, expected, make_df, re
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -656,7 +642,6 @@ def test_join_with_null_asymmetric_multikey(
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -726,7 +711,6 @@ def test_join_all_null(join_strategy, join_type, expected, make_df, repartition_
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -770,7 +754,6 @@ def test_join_null_type_column(join_strategy, join_type, expected, make_df, with
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -823,7 +806,6 @@ def test_join_semi_anti(join_strategy, join_type, expected, make_df, repartition
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -942,7 +924,6 @@ def test_join_true_join_keys(join_type, expected_dtypes, make_df, with_morsel_si
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -1013,7 +994,6 @@ def test_join_with_alias_in_key(join_strategy, join_type, expected, make_df, wit
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
@@ -1084,7 +1064,6 @@ def test_join_same_name_alias(join_strategy, join_type, expected, make_df, with_
 @pytest.mark.parametrize(
     "join_strategy",
     [None, "hash", "sort_merge", "broadcast"],
-    indirect=True,
 )
 @pytest.mark.parametrize(
     "join_type,expected",
