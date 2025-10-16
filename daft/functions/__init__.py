467af2e36807d49e418c1dc3ed5d3cbaa76a6e7f
Kevin Wang <kevin@eventualcomputing.com>
2025-10-16 09:31:56 -0700

    chore: revert #5383

--- daft/functions/__init__.py
+++ daft/functions/__init__.py
@@ -115,7 +115,6 @@ from .misc import (
     get,
     map_get,
     slice,
-    case,
     when,
 )
 from .numeric import (
@@ -236,7 +235,6 @@ __all__ = [
     "bool_and",
     "bool_or",
     "capitalize",
-    "case",
     "cast",
     "cbrt",
     "ceil",
