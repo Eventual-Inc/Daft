fccd0aef57344d62c179955f31f240ce264d75f8
Colin Ho <colin.ho99@gmail.com>
2025-10-16 00:50:22 -0700

    use daft func

--- benchmarking/ai/document_embedding/daft_main.py
+++ benchmarking/ai/document_embedding/daft_main.py
@@ -6,7 +6,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import time
 
 import daft
-from daft import col
 from daft.functions.ai import embed_text
 
 EMBED_MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
@@ -18,62 +17,74 @@ CHUNK_OVERLAP = 200
 
 daft.context.set_runner_ray()
 
+
 # Wait for Ray cluster to be ready
 @ray.remote
 def warmup():
     pass
+
+
 ray.get([warmup.remote() for _ in range(64)])
 
 
-def extract_text_from_parsed_pdf(pdf_bytes):
+@daft.func(
+    return_dtype=daft.DataType.struct(
+        {"text": daft.DataType.string(), "page_number": daft.DataType.int64()}
+    ),
+    unnest=True,
+)
+def extract_text_from_parsed_pdf(pdf_bytes: bytes):
     try:
         doc = pymupdf.Document(stream=pdf_bytes, filetype="pdf")
         if len(doc) > MAX_PDF_PAGES:
             print(f"Skipping PDF because it has {len(doc)} pages")
-            return None
-        page_texts = [{"text": page.get_text(), "page_number": page.number} for page in doc]
-        return page_texts
+            return
+        for page in doc:
+            yield {
+                "text": page.get_text(),
+                "page_number": page.number,
+            }
     except Exception as e:
         print(f"Error extracting text from PDF {e}")
-        return None
+        return
 
 
-def chunk(text):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+@daft.func(
+    return_dtype=daft.DataType.struct(
+        {"text": daft.DataType.string(), "chunk_id": daft.DataType.int64()}
+    ),
+    unnest=True,
+)
+def chunk(text: str):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP
+    )
     chunk_iter = splitter.split_text(text)
-    chunks = []
     for chunk_index, text in enumerate(chunk_iter):
-        chunks.append(
-            {
-                "text": text,
-                "chunk_id": chunk_index,
-            }
-        )
-    return chunks
+        yield {
+            "text": text,
+            "chunk_id": chunk_index,
+        }
+
 
 start_time = time.time()
 df = daft.read_parquet(INPUT_PATH)
-df = df.where(daft.col("file_name").str.endswith(".pdf"))
+df = df.where(df["file_name"].str.endswith(".pdf"))
 df = df.with_column("pdf_bytes", df["uploaded_pdf_path"].url.download())
-df = df.with_column(
-    "pages",
-    df["pdf_bytes"].apply(
-        extract_text_from_parsed_pdf,
-        return_dtype=daft.DataType.list(daft.DataType.struct({"text": daft.DataType.string(), "page_number": daft.DataType.int64()})),
-    ),
+df = df.select(
+    "*",
+    extract_text_from_parsed_pdf(df["pdf_bytes"]),
 )
-df = df.explode("pages")
-df = df.with_columns({"page_text": col("pages")["text"], "page_number": col("pages")["page_number"]})
-df = df.where(daft.col("page_text").not_null())
-df = df.with_column(
-    "chunks",
-    df["page_text"].apply(chunk, return_dtype=daft.DataType.list(daft.DataType.struct({"text": daft.DataType.string(), "chunk_id": daft.DataType.int64()})),
-    ),
+df = df.where(df["page_text"].not_null())
+df = df.select(
+    "*",
+    chunk(df["page_text"]),
 )
-df = df.explode("chunks")
-df = df.with_columns({"chunk": col("chunks")["text"], "chunk_id": col("chunks")["chunk_id"]})
-df = df.where(daft.col("chunk").not_null())
-df = df.with_column("embedding", embed_text(df["chunk"], provider="sentence_transformers", model=EMBED_MODEL_ID))
+df = df.where(df["text"].not_null())
+df = df.with_column(
+    "embedding",
+    embed_text(df["text"], provider="sentence_transformers", model=EMBED_MODEL_ID),
+)
 df = df.select("uploaded_pdf_path", "page_number", "chunk_id", "chunk", "embedding")
 df.write_parquet(OUTPUT_PATH)
 
