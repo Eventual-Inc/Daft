7503f9933a361207c873d4a77fc7c526762008a8
universalmind303 <cory.grinstead@gmail.com>
2025-10-21 15:36:36 -0500

    use custom postprocess

--- daft/ai/transformers/protocols/image_classifier.py
+++ daft/ai/transformers/protocols/image_classifier.py
@@ -9,6 +9,7 @@ from typing_extensions import Unpack
 
 from daft.ai.protocols import ImageClassifier, ImageClassifierDescriptor
 from daft.ai.utils import get_gpu_udf_options, get_torch_device
+from daft.dependencies import tf, torch
 
 if TYPE_CHECKING:
     from PIL import Image
@@ -47,26 +48,57 @@ class TransformersImageClassifierDescriptor(ImageClassifierDescriptor):
         return TransformersImageClassifier(self.model_name, **self.model_options)
 
 
+class TransformersImageClassifierPipeline(transformers.ZeroShotImageClassificationPipeline):
+    """Modified version of the original implementation to only return the top label."""
+
+    # this postprocess was copy/pasted from the original implementation,
+    # but the output is slightly modified to only return the top label
+    def postprocess(self, model_outputs):
+        candidate_labels = model_outputs.pop("candidate_labels")
+        logits = model_outputs["logits"][0]
+        if self.framework == "pt" and "siglip" in self.model.config.model_type:
+            probs = torch.sigmoid(logits).squeeze(-1)
+            scores = probs.tolist()
+            if not isinstance(scores, list):
+                scores = [scores]
+        elif self.framework == "pt":
+            probs = logits.softmax(dim=-1).squeeze(-1)
+            scores = probs.tolist()
+            if not isinstance(scores, list):
+                scores = [scores]
+        elif self.framework == "tf":
+            probs = tf.nn.softmax(logits=logits + 1e-9, axis=-1)
+            scores = probs.numpy().tolist()
+        else:
+            raise ValueError(f"Unsupported framework: {self.framework}")
+
+        sorted_results = sorted(zip(scores, candidate_labels), key=lambda x: -x[0])
+
+        return sorted_results[0][1]
+
+
 class TransformersImageClassifier(ImageClassifier):
     """Pipeline based zero-shot image classification."""
 
     _model: str
     _options: TransformersImageClassifierOptions
-    _pipeline: transformers.ImageClassificationPipeline
+    _pipeline: transformers.ZeroShotImageClassificationPipeline
 
     def __init__(self, model_name_or_path: str, **options: Unpack[TransformersImageClassifierOptions]):
         self._model = model_name_or_path
         self._options = options
 
         self._pipeline = pipeline(
-            task="zero-shot-image-classification", model=model_name_or_path, device=get_torch_device()
+            task="zero-shot-image-classification",
+            model=model_name_or_path,
+            device=get_torch_device(),
+            pipeline_class=TransformersImageClassifierPipeline,
         )
 
     def classify_image(self, images: list[Image.Image], labels: Label | list[Label]) -> list[Label]:
         batch_size = self._options.get("batch_size", None)
-        results: list[list[TransformersImageClassifierResult]] = self._pipeline(
+        return self._pipeline(
             images,
             batch_size=batch_size,
             candidate_labels=labels,
         )
-        return [result[0]["label"] for result in results]
