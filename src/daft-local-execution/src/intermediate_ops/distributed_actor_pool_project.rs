ac1e5e89c5fb455f8f70e0b6fe6a0170ee9953fd
Colin Ho <colin.ho99@gmail.com>
2025-10-21 15:38:11 -0700

    refactor: Make helper function for calling async python functions from rust (#5432)
    ## Changes Made
    
    Make a helper function for calling async python from rust.
    
    ## Related Issues
    
    <!-- Link to related GitHub issues, e.g., "Closes #123" -->
    
    ## Checklist
    
    - [ ] Documented in API Docs (if applicable)
    - [ ] Documented in User Guide (if applicable)
    - [ ] If adding a new documentation page, doc is added to
    `docs/mkdocs.yml` navigation
    - [ ] Documentation builds and is formatted properly

--- src/daft-local-execution/src/intermediate_ops/distributed_actor_pool_project.rs
+++ src/daft-local-execution/src/intermediate_ops/distributed_actor_pool_project.rs
@@ -72,23 +72,18 @@ impl ActorHandle {
         task_locals: pyo3_async_runtimes::TaskLocals,
     ) -> DaftResult<Arc<MicroPartition>> {
         let inner = self.inner.clone();
-        let await_coroutine = async move {
-            let result = Python::attach(|py| {
+        let result = common_runtime::python::execute_python_coroutine::<_, PyMicroPartition>(
+            move |py| {
                 let coroutine = inner.call_method1(
                     py,
                     pyo3::intern!(py, "eval_input"),
                     (PyMicroPartition::from(input),),
                 )?;
-                pyo3_async_runtimes::tokio::into_future(coroutine.into_bound(py))
-            })?
-            .await?;
-            DaftResult::Ok(result)
-        };
-        let result = pyo3_async_runtimes::tokio::scope(task_locals, await_coroutine)
-            .await
-            .and_then(|result| {
-                Python::attach(|py| result.extract::<PyMicroPartition>(py)).map_err(|e| e.into())
-            })?;
+                Ok(coroutine.into_bound(py))
+            },
+            task_locals,
+        )
+        .await?;
         Ok(result.into())
     }
 }
