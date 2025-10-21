42de6a86505ad08f76f8946e0a502476aefe66bd
Cory Grinstead <cory.grinstead@gmail.com>
2025-10-21 14:00:32 -0500

    Update tests/integration/io/huggingface/test_file_read_huggingface.py
    Co-authored-by: Malcolm Greaves <malcolmgreaves@users.noreply.github.com>

--- tests/integration/io/huggingface/test_file_read_huggingface.py
+++ tests/integration/io/huggingface/test_file_read_huggingface.py
@@ -10,9 +10,6 @@ def test_read_huggingface_file_doesnt_fail():
     test_file_path = "hf://datasets/Eventual-Inc/sample-files/README.md"
 
     file = daft.File(test_file_path)
-    try:
-        with file.open() as f:
-            bytes = f.read()
-        assert len(bytes) > 0
-    except Exception as e:
-        pytest.fail(f"Failed to read file: {e}")
+    with file.open() as f:
+        bytes = f.read()
+    assert len(bytes) > 0
