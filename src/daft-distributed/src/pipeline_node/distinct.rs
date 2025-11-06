f46fd418b731e88370762d67c2198a82e5c99e2e
Srinivas Lade <srinulade1@gmail.com>
2025-11-06 12:28:21 -0800

    Add query id to OTEL stats

--- src/daft-distributed/src/pipeline_node/distinct.rs
+++ src/daft-distributed/src/pipeline_node/distinct.rs
@@ -30,11 +30,10 @@ impl DistinctNode {
         child: DistributedPipelineNode,
     ) -> Self {
         let context = PipelineNodeContext::new(
-            plan_config.plan_id,
+            plan_config.query_idx,
+            plan_config.query_id.clone(),
             node_id,
             Self::NODE_NAME,
-            vec![child.node_id()],
-            vec![child.name()],
         );
         let config = PipelineNodeConfig::new(
             schema,
--- src/daft-distributed/src/pipeline_node/into_batches.rs
+++ src/daft-distributed/src/pipeline_node/into_batches.rs
@@ -46,11 +46,10 @@ impl IntoBatchesNode {
         child: DistributedPipelineNode,
     ) -> Self {
         let context = PipelineNodeContext::new(
-            plan_config.plan_id,
+            plan_config.query_idx,
+            plan_config.query_id.clone(),
             node_id,
             Self::NODE_NAME,
-            vec![child.node_id()],
-            vec![child.name()],
         );
         let config = PipelineNodeConfig::new(
             schema,
