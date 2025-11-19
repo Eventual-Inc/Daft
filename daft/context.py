baa26c3a307f631e79e5705418c47a72a244b624
universalmind303 <cory.grinstead@gmail.com>
2025-11-19 15:03:23 -0600

    update context

--- daft/context.py
+++ daft/context.py
@@ -263,6 +263,8 @@ def set_execution_config(
     min_cpu_per_task: float | None = None,
     actor_udf_ready_timeout: int | None = None,
     maintain_order: bool | None = None,
+    enable_dynamic_batching: bool | None = None,
+    dynamic_batching_algorithm: str | None = None,
 ) -> DaftContext:
     """Globally sets various configuration parameters which control various aspects of Daft execution.
 
@@ -312,6 +314,11 @@ def set_execution_config(
         min_cpu_per_task: Minimum CPU per task in the Ray runner. Defaults to 0.5.
         actor_udf_ready_timeout: Timeout for UDF actors to be ready. Defaults to 120 seconds.
         maintain_order: Whether to maintain order during execution. Defaults to True. Some blocking sink operators (e.g. write_parquet) won't respect this flag and will always keep maintain_order as false, and propagate to child operators. It's useful to set this to False for running df.collect() when no ordering is required.
+        enable_dynamic_batching:
+            Whether to enable dynamic batching. Defaults to False.
+            Note: dynamic_batching is only possible when maintain_order is False.
+            If maintain_order=True and enable_dynamic_batching=True, dynamic batching will be disabled.
+        dynamic_batching_algorithm: The algorithm to use for dynamic batching. Defaults to 'aimd'.
     """
     # Replace values in the DaftExecutionConfig with user-specified overrides
     ctx = get_context()
@@ -350,6 +357,8 @@ def set_execution_config(
             min_cpu_per_task=min_cpu_per_task,
             actor_udf_ready_timeout=actor_udf_ready_timeout,
             maintain_order=maintain_order,
+            enable_dynamic_batching=enable_dynamic_batching,
+            dynamic_batching_algorithm=dynamic_batching_algorithm,
         )
 
         ctx._ctx._daft_execution_config = new_daft_execution_config
