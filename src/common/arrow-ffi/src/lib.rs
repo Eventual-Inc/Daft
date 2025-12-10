c29afc09502c3a8ba3b21cb6521603467c1abf13
universalmind303 <cory.grinstead@gmail.com>
2025-12-09 17:00:58 -0800

    move util into daft-arrow

--- src/common/arrow-ffi/src/lib.rs
+++ src/common/arrow-ffi/src/lib.rs
@@ -1,16 +1,14 @@
-use std::{collections::HashMap, sync::Arc};
+#![allow(deprecated, reason = "arrow2->arrow migration")]
 
 #[cfg(feature = "python")]
 use arrow::ffi::FFI_ArrowSchema;
-use daft_arrow::{
-    array::Array,
-    datatypes::{DataType as Arrow2DataType, Field},
-};
+#[cfg(feature = "python")]
+use daft_arrow::datatypes::arrow2_field_to_arrow;
+use daft_arrow::{array::Array, datatypes::DataType as Arrow2DataType};
 #[cfg(feature = "python")]
 use pyo3::ffi::Py_uintptr_t;
 #[cfg(feature = "python")]
 use pyo3::prelude::*;
-
 pub type ArrayRef = Box<dyn Array>;
 
 #[cfg(feature = "python")]
@@ -44,9 +42,9 @@ pub fn array_to_rust(py: Python, arrow_array: Bound<PyAny>) -> PyResult<ArrayRef
         // I (cory) believe this would fail if you had nested extension types.
         // I'm not sure we (or others) use nested extension types, or if this is even supported by arrow
         let dtype = Arrow2DataType::Extension(
-            ext_name.to_string(),
+            ext_name.clone(),
             Box::new(field.data_type().clone().into()),
-            metadata.map(|s| s.to_string()),
+            metadata.map(|s| s.clone()),
         );
         arr.change_type(dtype);
     }
@@ -61,7 +59,7 @@ pub fn to_py_array<'py>(
     pyarrow: &Bound<'py, PyModule>,
 ) -> PyResult<Bound<'py, PyAny>> {
     let field = daft_arrow::datatypes::Field::new("", array.data_type().clone(), true);
-    let field = convert_field(field);
+    let field = arrow2_field_to_arrow(field);
     let field = arrow::ffi::FFI_ArrowSchema::try_from(&field).map_err(|e| {
         PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
             "Failed to convert Arrow field to FFI schema: {}",
@@ -96,16 +94,16 @@ pub fn field_to_py(
     field: &daft_arrow::datatypes::Field,
     pyarrow: &Bound<PyModule>,
 ) -> PyResult<Py<PyAny>> {
-    let field = convert_field(field.clone());
+    let schema = Box::new(
+        FFI_ArrowSchema::try_from(arrow2_field_to_arrow(field.clone())).map_err(|e| {
+            use pyo3::exceptions::PyRuntimeError;
 
-    let schema = Box::new(FFI_ArrowSchema::try_from(field).map_err(|e| {
-        use pyo3::exceptions::PyRuntimeError;
-
-        PyErr::new::<PyRuntimeError, _>(format!(
-            "Failed to convert field to FFI_ArrowSchema: {}",
-            e
-        ))
-    })?);
+            PyErr::new::<PyRuntimeError, _>(format!(
+                "Failed to convert field to FFI_ArrowSchema: {}",
+                e
+            ))
+        })?,
+    );
 
     let schema_ptr: *const FFI_ArrowSchema = &raw const *schema;
 
@@ -117,80 +115,6 @@ pub fn field_to_py(
     Ok(field.into())
 }
 
-fn convert_field(field: daft_arrow::datatypes::Field) -> arrow_schema::Field {
-    use arrow_schema::{Field as ArrowField, UnionFields};
-
-    let dtype = match field.data_type {
-        Arrow2DataType::Null => arrow_schema::DataType::Null,
-        Arrow2DataType::Boolean => arrow_schema::DataType::Boolean,
-        Arrow2DataType::Int8 => arrow_schema::DataType::Int8,
-        Arrow2DataType::Int16 => arrow_schema::DataType::Int16,
-        Arrow2DataType::Int32 => arrow_schema::DataType::Int32,
-        Arrow2DataType::Int64 => arrow_schema::DataType::Int64,
-        Arrow2DataType::UInt8 => arrow_schema::DataType::UInt8,
-        Arrow2DataType::UInt16 => arrow_schema::DataType::UInt16,
-        Arrow2DataType::UInt32 => arrow_schema::DataType::UInt32,
-        Arrow2DataType::UInt64 => arrow_schema::DataType::UInt64,
-        Arrow2DataType::Float16 => arrow_schema::DataType::Float16,
-        Arrow2DataType::Float32 => arrow_schema::DataType::Float32,
-        Arrow2DataType::Float64 => arrow_schema::DataType::Float64,
-        Arrow2DataType::Timestamp(unit, tz) => {
-            arrow_schema::DataType::Timestamp(unit.into(), tz.map(Into::into))
-        }
-        Arrow2DataType::Date32 => arrow_schema::DataType::Date32,
-        Arrow2DataType::Date64 => arrow_schema::DataType::Date64,
-        Arrow2DataType::Time32(unit) => arrow_schema::DataType::Time32(unit.into()),
-        Arrow2DataType::Time64(unit) => arrow_schema::DataType::Time64(unit.into()),
-        Arrow2DataType::Duration(unit) => arrow_schema::DataType::Duration(unit.into()),
-        Arrow2DataType::Interval(unit) => arrow_schema::DataType::Interval(unit.into()),
-        Arrow2DataType::Binary => arrow_schema::DataType::Binary,
-        Arrow2DataType::FixedSizeBinary(size) => arrow_schema::DataType::FixedSizeBinary(size as _),
-        Arrow2DataType::LargeBinary => arrow_schema::DataType::LargeBinary,
-        Arrow2DataType::Utf8 => arrow_schema::DataType::Utf8,
-        Arrow2DataType::LargeUtf8 => arrow_schema::DataType::LargeUtf8,
-        Arrow2DataType::List(f) => arrow_schema::DataType::List(Arc::new((*f).into())),
-        Arrow2DataType::FixedSizeList(f, size) => {
-            arrow_schema::DataType::FixedSizeList(Arc::new((*f).into()), size as _)
-        }
-        Arrow2DataType::LargeList(f) => arrow_schema::DataType::LargeList(Arc::new((*f).into())),
-        Arrow2DataType::Struct(f) => {
-            arrow_schema::DataType::Struct(f.into_iter().map(ArrowField::from).collect())
-        }
-        Arrow2DataType::Union(fields, Some(ids), mode) => {
-            let ids = ids.into_iter().map(|x| x as _);
-            let fields = fields.into_iter().map(ArrowField::from);
-            arrow_schema::DataType::Union(UnionFields::new(ids, fields), mode.into())
-        }
-        Arrow2DataType::Union(fields, None, mode) => {
-            let ids = 0..fields.len() as i8;
-            let fields = fields.into_iter().map(ArrowField::from);
-            arrow_schema::DataType::Union(UnionFields::new(ids, fields), mode.into())
-        }
-        Arrow2DataType::Map(f, ordered) => {
-            arrow_schema::DataType::Map(Arc::new((*f).into()), ordered)
-        }
-        Arrow2DataType::Dictionary(key, value, _) => arrow_schema::DataType::Dictionary(
-            Box::new(Arrow2DataType::from(key).into()),
-            Box::new((*value).into()),
-        ),
-        Arrow2DataType::Decimal(precision, scale) => {
-            arrow_schema::DataType::Decimal128(precision as _, scale as _)
-        }
-        Arrow2DataType::Decimal256(precision, scale) => {
-            arrow_schema::DataType::Decimal256(precision as _, scale as _)
-        }
-        Arrow2DataType::Extension(name, d, metadata) => {
-            let mut metadata_map = HashMap::new();
-            metadata_map.insert("ARROW:extension:name".to_string(), name);
-            if let Some(metadata) = metadata {
-                metadata_map.insert("ARROW:extension:metadata".to_string(), metadata);
-            }
-            return convert_field(Field::new(field.name, *d, true)).with_metadata(metadata_map);
-        }
-    };
-    arrow_schema::Field::new(field.name, dtype, true)
-}
-
 #[cfg(feature = "python")]
 pub fn dtype_to_py<'py>(
     py: Python<'py>,
@@ -198,7 +122,7 @@ pub fn dtype_to_py<'py>(
     pyarrow: Bound<'py, PyModule>,
 ) -> PyResult<Bound<'py, PyAny>> {
     let field = daft_arrow::datatypes::Field::new("", dtype.clone(), true);
-    let field = convert_field(field);
+    let field = arrow2_field_to_arrow(field);
 
     let schema = Box::new(FFI_ArrowSchema::try_from(field).map_err(|e| {
         use pyo3::exceptions::PyRuntimeError;
--- src/daft-arrow/src/lib.rs
+++ src/daft-arrow/src/lib.rs
@@ -1,7 +1,6 @@
 // Re-export arrow2::* modules for centralized access
 pub use arrow2::{
-    array, chunk, compute, datatypes, error, ffi, io, offset, scalar, temporal_conversions,
-    trusted_len, types,
+    array, chunk, compute, error, ffi, io, offset, scalar, temporal_conversions, trusted_len, types,
 };
 
 pub mod buffer {
@@ -32,4 +31,86 @@ pub mod bitmap {
     // Uses come from arrow2::array::BooleanArray values buffer
     // And direct access to arrow2::array::Array objects
     pub use arrow2::bitmap::*;
+}
+
+pub mod datatypes {
+    use std::{collections::HashMap, sync::Arc};
+
+    pub use arrow2::datatypes::*;
+
+    #[deprecated(note = "use arrow instead of arrow2")]
+    pub fn arrow2_field_to_arrow(field: Field) -> arrow_schema::Field {
+        use arrow_schema::{Field as ArrowField, UnionFields};
+
+        let dtype = match field.data_type {
+            DataType::Null => arrow_schema::DataType::Null,
+            DataType::Boolean => arrow_schema::DataType::Boolean,
+            DataType::Int8 => arrow_schema::DataType::Int8,
+            DataType::Int16 => arrow_schema::DataType::Int16,
+            DataType::Int32 => arrow_schema::DataType::Int32,
+            DataType::Int64 => arrow_schema::DataType::Int64,
+            DataType::UInt8 => arrow_schema::DataType::UInt8,
+            DataType::UInt16 => arrow_schema::DataType::UInt16,
+            DataType::UInt32 => arrow_schema::DataType::UInt32,
+            DataType::UInt64 => arrow_schema::DataType::UInt64,
+            DataType::Float16 => arrow_schema::DataType::Float16,
+            DataType::Float32 => arrow_schema::DataType::Float32,
+            DataType::Float64 => arrow_schema::DataType::Float64,
+            DataType::Timestamp(unit, tz) => {
+                arrow_schema::DataType::Timestamp(unit.into(), tz.map(Into::into))
+            }
+            DataType::Date32 => arrow_schema::DataType::Date32,
+            DataType::Date64 => arrow_schema::DataType::Date64,
+            DataType::Time32(unit) => arrow_schema::DataType::Time32(unit.into()),
+            DataType::Time64(unit) => arrow_schema::DataType::Time64(unit.into()),
+            DataType::Duration(unit) => arrow_schema::DataType::Duration(unit.into()),
+            DataType::Interval(unit) => arrow_schema::DataType::Interval(unit.into()),
+            DataType::Binary => arrow_schema::DataType::Binary,
+            DataType::FixedSizeBinary(size) => arrow_schema::DataType::FixedSizeBinary(size as _),
+            DataType::LargeBinary => arrow_schema::DataType::LargeBinary,
+            DataType::Utf8 => arrow_schema::DataType::Utf8,
+            DataType::LargeUtf8 => arrow_schema::DataType::LargeUtf8,
+            DataType::List(f) => arrow_schema::DataType::List(Arc::new((*f).into())),
+            DataType::FixedSizeList(f, size) => {
+                arrow_schema::DataType::FixedSizeList(Arc::new((*f).into()), size as _)
+            }
+            DataType::LargeList(f) => arrow_schema::DataType::LargeList(Arc::new((*f).into())),
+            DataType::Struct(f) => {
+                arrow_schema::DataType::Struct(f.into_iter().map(ArrowField::from).collect())
+            }
+            DataType::Union(fields, Some(ids), mode) => {
+                let ids = ids.into_iter().map(|x| x as _);
+                let fields = fields.into_iter().map(ArrowField::from);
+                arrow_schema::DataType::Union(UnionFields::new(ids, fields), mode.into())
+            }
+            DataType::Union(fields, None, mode) => {
+                let ids = 0..fields.len() as i8;
+                let fields = fields.into_iter().map(ArrowField::from);
+                arrow_schema::DataType::Union(UnionFields::new(ids, fields), mode.into())
+            }
+            DataType::Map(f, ordered) => {
+                arrow_schema::DataType::Map(Arc::new((*f).into()), ordered)
+            }
+            DataType::Dictionary(key, value, _) => arrow_schema::DataType::Dictionary(
+                Box::new(DataType::from(key).into()),
+                Box::new((*value).into()),
+            ),
+            DataType::Decimal(precision, scale) => {
+                arrow_schema::DataType::Decimal128(precision as _, scale as _)
+            }
+            DataType::Decimal256(precision, scale) => {
+                arrow_schema::DataType::Decimal256(precision as _, scale as _)
+            }
+            DataType::Extension(name, d, metadata) => {
+                let mut metadata_map = HashMap::new();
+                metadata_map.insert("ARROW:extension:name".to_string(), name);
+                if let Some(metadata) = metadata {
+                    metadata_map.insert("ARROW:extension:metadata".to_string(), metadata);
+                }
+                return arrow2_field_to_arrow(Field::new(field.name, *d, true))
+                    .with_metadata(metadata_map);
+            }
+        };
+        arrow_schema::Field::new(field.name, dtype, true)
+    }
 }
