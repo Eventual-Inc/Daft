71601d31c6b8f44999d0eb3e6008d5687e8ea273
universalmind303 <cory.grinstead@gmail.com>
2025-12-11 11:14:20 -0800

    remove arrow usage from 'src/daft-functions-tokenize/src/decode.rs'

--- src/daft-functions-tokenize/src/decode.rs
+++ src/daft-functions-tokenize/src/decode.rs
@@ -1,4 +1,3 @@
-#![allow(deprecated, reason = "arrow2 migration")]
 use std::sync::Arc;
 
 use common_error::{DaftError, DaftResult};
@@ -74,8 +73,7 @@ fn decode_list(series: &Series, bpe: &DaftBPE) -> DaftResult<String> {
         )));
     }
     let series = series.cast(&DataType::UInt32)?;
-    let data = series.u32()?.as_arrow2();
-    let tokens: &[u32] = data.values().as_slice();
+    let tokens: &[u32] = series.u32()?.as_slice();
     bpe.decode(tokens)
 }
 
