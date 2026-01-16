90be557607222e9c55d9bf5913f7f7f3db707cf9
Cory Grinstead <cory.grinstead@gmail.com>
2026-01-16 15:45:42 -0600

    refactor(arrow2): remove some arrow2 based from impls (#6054)
    ## Changes Made
    
    <!-- Describe what changes were made and why. Include implementation
    details if necessary. -->
    
    ## Related Issues
    
    <!-- Link to related GitHub issues, e.g., "Closes #123" -->

--- src/daft-core/src/array/ops/null.rs
+++ src/daft-core/src/array/ops/null.rs
@@ -114,18 +114,17 @@ macro_rules! check_nullity_nested_array {
                     .collect::<Vec<_>>()
                     .as_slice(),
             ))),
-            Some(nulls) => Ok(BooleanArray::from((
-                $arr.name(),
-                daft_arrow::array::BooleanArray::new(
-                    daft_arrow::datatypes::DataType::Boolean,
-                    daft_arrow::buffer::from_null_buffer(if $is_null {
-                        nulls.inner().not().into()
+            Some(nulls) => BooleanArray::from_arrow(
+                Field::new($arr.name(), DataType::Boolean),
+                Arc::new(arrow::array::BooleanArray::new(
+                    if $is_null {
+                        nulls.inner().not()
                     } else {
-                        nulls.clone()
-                    }),
+                        nulls.clone().into_inner()
+                    },
                     None,
-                ),
-            ))),
+                )),
+            ),
         }
     }};
 }
