978fc55775af01f0336a64ed4766227f3216330e
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2026-01-29 11:20:54 -0500

    rm embedding.py

--- daft/functions/distance.py
+++ daft/functions/distance.py
@@ -13,7 +13,28 @@ def cosine_distance(left: Expression, right: Expression) -> Expression:
         right (FixedSizeList or Embedding Expression): The right vector
 
     Returns:
-        Expression (Float64 Expression): the cosine distance between the two vectors.
+        Expression (Float64 Expression): an expression with the cosine distance between the two vectors.
+
+    Examples:
+        >>> import daft
+        >>> from daft.functions import cosine_distance
+        >>>
+        >>> df = daft.from_pydict({"e1": [[1, 2, 3], [1, 2, 3]], "e2": [[1, 2, 3], [-1, -2, -3]]})
+        >>> dtype = daft.DataType.fixed_size_list(daft.DataType.float32(), 3)
+        >>> df = df.with_column("dist", cosine_distance(df["e1"].cast(dtype), df["e2"].cast(dtype)))
+        >>> df.show()
+        ╭─────────────┬──────────────┬─────────╮
+        │ e1          ┆ e2           ┆ dist    │
+        │ ---         ┆ ---          ┆ ---     │
+        │ List[Int64] ┆ List[Int64]  ┆ Float64 │
+        ╞═════════════╪══════════════╪═════════╡
+        │ [1, 2, 3]   ┆ [1, 2, 3]    ┆ 0       │
+        ├╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+        │ [1, 2, 3]   ┆ [-1, -2, -3] ┆ 2       │
+        ╰─────────────┴──────────────┴─────────╯
+        <BLANKLINE>
+        (Showing first 2 of 2 rows)
+
     """
     return Expression._call_builtin_scalar_fn("cosine_distance", left, right)
 
