e943e627f187a9e0dcc3b610c15ec38e360222bd
universalmind303 <cory.grinstead@gmail.com>
2026-02-05 16:29:15 -0600

    recordbatch

--- src/common/arrow-ffi/src/lib.rs
+++ src/common/arrow-ffi/src/lib.rs
@@ -260,47 +260,6 @@ pub fn array_to_rust_v2(value: &Bound<PyAny>) -> PyResult<(ArrayData, Field)> {
     Ok((data, field))
 }
 
-pub fn array_to_rust(py: Python, arrow_array: Bound<PyAny>) -> PyResult<ArrayRef> {
-    // prepare a pointer to receive the Array struct
-    let array = Box::new(arrow::ffi::FFI_ArrowArray::empty());
-    let schema = Box::new(arrow::ffi::FFI_ArrowSchema::empty());
-
-    let array_ptr = &raw const *array;
-    let schema_ptr = &raw const *schema;
-
-    // make the conversion through PyArrow's private API
-    // this changes the pointer's memory and is thus unsafe. In particular, `_export_to_c` can go out of bounds
-    arrow_array.call_method1(
-        pyo3::intern!(py, "_export_to_c"),
-        (array_ptr as Py_uintptr_t, schema_ptr as Py_uintptr_t),
-    )?;
-
-    let mut data = unsafe { arrow::ffi::from_ffi(*array, schema.as_ref()) }.map_err(|e| {
-        PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
-            "Failed to convert Arrow array to Rust: {}",
-            e
-        ))
-    })?;
-    data.align_buffers();
-    // this DOES not properly preserve metadata, so we need to update the fields with any extension types.
-    let mut arr = daft_arrow::array::from_data(&data);
-
-    let field = arrow_schema::Field::try_from(schema.as_ref()).unwrap();
-    if let Some(ext_name) = field.metadata().get("ARROW:extension:name") {
-        let metadata = field.metadata().get("ARROW:extension:metadata");
-        // I (cory) believe this would fail if you had nested extension types.
-        // I'm not sure we (or others) use nested extension types, or if this is even supported by arrow
-        let dtype = Arrow2DataType::Extension(
-            ext_name.clone(),
-            Box::new(field.data_type().clone().into()),
-            metadata.cloned(),
-        );
-        arr.change_type(dtype);
-    }
-
-    Ok(arr)
-}
-
 pub fn to_py_array<'py>(
     py: Python<'py>,
     array: ArrayRef,
