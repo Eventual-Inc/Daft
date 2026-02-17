6ded63f244c7cb34eab979287658a25902108b60
cckellogg <cckellogg@gmail.com>
2026-02-17 12:22:27 -0800

    refactor(arrow2): migrate groups to arrow-rs (#6185)
    ## Changes Made
    
    Migrate groups to arrow-rs and add a few test cases.
    
    ---------
    
    Co-authored-by: universalmind303 <cory.grinstead@gmail.com>

--- src/daft-core/src/array/values.rs
+++ src/daft-core/src/array/values.rs
@@ -1,6 +1,6 @@
 use common_error::{DaftError, DaftResult};
 
-use crate::prelude::{AsArrow, Utf8Array};
+use crate::prelude::{AsArrow, BinaryArray, BooleanArray, Utf8Array};
 
 impl Utf8Array {
     /// Returns an iterator of `&str` over the non-null values in this array.
@@ -12,6 +12,38 @@ impl Utf8Array {
         if arrow2_arr.validity().is_some() {
             return Err(DaftError::ComputeError(
                 "Utf8Array::values with nulls".to_string(),
+            ));
+        }
+        Ok(arrow2_arr.values_iter())
+    }
+}
+
+impl BinaryArray {
+    /// Returns an iterator of `&[u8]` over the non-null values in this array.
+    ///
+    /// NOTE: this will error if there are any null values.
+    /// If you need to handle nulls, use the `.iter()` method instead.
+    pub fn values(&self) -> DaftResult<impl Iterator<Item = &[u8]>> {
+        let arrow2_arr = self.as_arrow2();
+        if arrow2_arr.validity().is_some() {
+            return Err(DaftError::ComputeError(
+                "BinaryArray::values with nulls".to_string(),
+            ));
+        }
+        Ok(arrow2_arr.values_iter())
+    }
+}
+
+impl BooleanArray {
+    /// Returns an iterator of `bool` over the non-null values in this array.
+    ///
+    /// NOTE: this will error if there are any null values.
+    /// If you need to handle nulls, use the `.iter()` method instead.
+    pub fn values(&self) -> DaftResult<impl Iterator<Item = bool>> {
+        let arrow2_arr = self.as_arrow2();
+        if arrow2_arr.validity().is_some() {
+            return Err(DaftError::ComputeError(
+                "BooleanArray::values with nulls".to_string(),
             ));
         }
         Ok(arrow2_arr.values_iter())
