c41c2420c564641a24df4ffd9226a9d38655e0b0
Cory Grinstead <cory.grinstead@gmail.com>
2026-02-19 17:21:34 -0600

    refactor(arrow2): remove arrow2 from daft-recordbatch (#6231)
    ## Changes Made
    
    removed all usages of arrow2 and daft-arrow from daft-recordbatch except
    for probeable, as probable requires dyn_compare to be refactored to
    arrow_rs first.
    
    ## Related Issues
    
    https://github.com/Eventual-Inc/Daft/pull/6227

--- src/daft-core/src/array/boolean.rs
+++ src/daft-core/src/array/boolean.rs
@@ -5,4 +5,10 @@ impl BooleanArray {
     pub fn as_bitmap(&self) -> &daft_arrow::bitmap::Bitmap {
         self.as_arrow2().values()
     }
+    pub fn false_count(&self) -> usize {
+        self.as_arrow().expect("BooleanArray").false_count()
+    }
+    pub fn true_count(&self) -> usize {
+        self.as_arrow().expect("BooleanArray").true_count()
+    }
 }
