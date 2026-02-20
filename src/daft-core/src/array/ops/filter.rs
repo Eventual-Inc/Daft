0d9b785cd536d595883f754f83b42101ab0ebece
universalmind303 <cory.grinstead@gmail.com>
2026-02-20 14:02:26 -0600

    minor cleanup

--- src/daft-core/src/array/ops/filter.rs
+++ src/daft-core/src/array/ops/filter.rs
@@ -40,43 +40,34 @@ impl StructArray {
     }
 }
 
-// TODO(desmond): Migrate this to arrow-rs after migrating growable internals. We can't use
-// arrow::compute::filter here because PythonArray::to_arrow() pickles Python objects into bytes,
-// and from_arrow() unpickles them back into *new* objects, breaking Python object identity.
 #[cfg(feature = "python")]
 impl PythonArray {
-    #[allow(deprecated, reason = "arrow2->arrow migration")]
     pub fn filter(&self, mask: &BooleanArray) -> DaftResult<Self> {
-        use std::borrow::Cow;
-
-        use daft_arrow::bitmap::utils::SlicesIterator;
-
         use super::full::FullNull;
         use crate::array::growable::{Growable, GrowableArray};
 
-        let keep_bitmap = match mask.as_arrow2().validity() {
-            None => Cow::Borrowed(mask.as_arrow2().values()),
-            Some(nulls) => Cow::Owned(mask.as_arrow2().values() & nulls),
+        let keep_bitmap = match mask.nulls() {
+            None => mask.to_bitmap(),
+            Some(nulls) => &mask.to_bitmap() & nulls.inner(),
         };
 
-        let num_invalid = keep_bitmap.as_ref().unset_bits();
+        let num_invalid = keep_bitmap.len() - keep_bitmap.count_set_bits();
         if num_invalid == 0 {
             return Ok(self.clone());
         } else if num_invalid == mask.len() {
             return Ok(Self::empty(self.name(), self.data_type()));
         }
 
-        let slice_iter = SlicesIterator::new(keep_bitmap.as_ref());
         let mut growable = Self::make_growable(
             self.name(),
             self.data_type(),
             vec![self],
             false,
-            slice_iter.slots(),
+            keep_bitmap.count_set_bits(),
         );
 
-        for (start_keep, len_keep) in slice_iter {
-            growable.extend(0, start_keep, len_keep);
+        for (start, end) in keep_bitmap.set_slices() {
+            growable.extend(0, start, end - start);
         }
 
         Ok(growable.build()?.downcast::<Self>()?.clone())
