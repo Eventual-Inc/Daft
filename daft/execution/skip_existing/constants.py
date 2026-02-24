8644608a13ef9151cad3264f614e30246b446675
wangzheyan <wangzheyan@bytedance.com>
2026-02-24 21:31:43 +0800

    refactor: make super parameter configurable

--- daft/execution/skip_existing/constants.py
+++ daft/execution/skip_existing/constants.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+import os
+
 # For skip_existing in ray mode, timeout for the placement group to be ready
-PLACEMENT_GROUP_READY_TIMEOUT_SECONDS = 50
+PLACEMENT_GROUP_READY_TIMEOUT_SECONDS = int(
+    os.environ.get("DAFT_SKIP_EXISTING_PLACEMENT_GROUP_READY_TIMEOUT_SECONDS", "50")
+)
 # For skip_existing in ray mode, timeout for the async await to complete
-ASYNC_AWAIT_TIMEOUT_SECONDS = 1000
+ASYNC_AWAIT_TIMEOUT_SECONDS = int(os.environ.get("DAFT_SKIP_EXISTING_ASYNC_AWAIT_TIMEOUT_SECONDS", "1000"))
