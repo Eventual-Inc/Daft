5de45f2300ed6cb7121ffcd90a9c5ef24b53b0e8
Srinivas Lade <srinulade1@gmail.com>
2026-02-26 20:54:43 -0800

    oh forgot to add this

--- benchmarking/common_crawl/extract.py
+++ benchmarking/common_crawl/extract.py
@@ -45,6 +45,7 @@ def process_html(html_bytes: bytes | None):
 @daft.udf(
     return_dtype=daft.DataType.struct({"extracted_text": daft.DataType.string(), "text_length": daft.DataType.int32()}),
     batch_size=128,
+    use_process=True,
 )
 def daft_extract_text(content: daft.Series):
     """Daft UDF to extract text from HTML content using OWM.
