786b82bffec750e9ea43ddda9cf5818dc0e4ea27
Lucas <54979663+Lucas61000@users.noreply.github.com>
2026-04-01 23:48:46 +0800

    feat: add support for writing _SUCCESS files in parquet operations (#6090)
    ## Changes Made
    
    - Add support for writing a _SUCCESS file upon successful completion of
    parquet write operations.
    - Add parameter to existing write_parquet method.
    `df.write_parquet("/path/to/output", write_success_file=True)`
    <!-- Describe what changes were made and why. Include implementation
    details if necessary. -->
    
    ## Related Issues
     Closes #4085
    <!-- Link to related GitHub issues, e.g., "Closes #123" -->

--- src/daft-logical-plan/src/builder/mod.rs
+++ src/daft-logical-plan/src/builder/mod.rs
@@ -805,6 +805,7 @@ impl LogicalPlanBuilder {
         &self,
         root_dir: &str,
         write_mode: WriteMode,
+        write_success_file: bool,
         file_format: FileFormat,
         format_option: Option<FormatSinkOption>,
         partition_cols: Option<Vec<ExprRef>>,
@@ -825,6 +826,7 @@ impl LogicalPlanBuilder {
             partition_cols,
             compression,
             io_config,
+            write_success_file,
         ));
 
         let logical_plan: LogicalPlan =
@@ -1490,6 +1492,7 @@ impl PyLogicalPlanBuilder {
     #[pyo3(signature = (
         root_dir,
         write_mode,
+        write_success_file,
         file_format,
         format_option=None,
         partition_cols=None,
@@ -1500,6 +1503,7 @@ impl PyLogicalPlanBuilder {
         &self,
         root_dir: &str,
         write_mode: WriteMode,
+        write_success_file: bool,
         file_format: FileFormat,
         format_option: Option<PyFormatSinkOption>,
         partition_cols: Option<Vec<PyExpr>>,
@@ -1511,6 +1515,7 @@ impl PyLogicalPlanBuilder {
             .table_write(
                 root_dir,
                 write_mode,
+                write_success_file,
                 file_format,
                 format_option.map(|p| p.inner),
                 partition_cols.map(pyexprs_to_exprs),
